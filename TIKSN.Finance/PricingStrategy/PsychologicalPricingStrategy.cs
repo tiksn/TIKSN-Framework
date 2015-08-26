8f8a0950dd2bcb6f8e1a073ab9542352d28881fa
Tigran TIKSN Torosyan <personal@tiksn.com>
2015-08-26 12:26:01 -0500

    Pricing strategy

---
+++ TIKSN.Finance/PricingStrategy/PsychologicalPricingStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIKSN.Finance.PricingStrategy
+{
+	public class PsychologicalPricingStrategy : IPricingStrategy
+	{
+		public Money EstimateMarketPrice(Money basePrice)
+		{
+			var estimatedPrice = EstimateMarketPrice(basePrice.Amount);
+
+			return new Money(basePrice.Currency, estimatedPrice);
+		}
+
+		public decimal EstimateMarketPrice(decimal basePrice)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
