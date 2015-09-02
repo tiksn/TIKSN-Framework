5b8e1fe80cc3e4730d5f60b0bdc9bfd907c323f8
tiksn <personal@tiksn.com>
2015-09-02 11:50:47 -0500

    graph converter

---
+++ TIKSN.Finance/CompositeChainCurrencyConverter.cs
@@ -0,0 +1,58 @@
+﻿using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIKSN.Finance
+{
+	public class CompositeChainCurrencyConverter : CompositeCurrencyConverter
+	{
+		public CompositeChainCurrencyConverter(ICurrencyConversionCompositionStrategy compositionStrategy)
+			: base(compositionStrategy)
+		{
+
+		}
+
+		public override Task<Money> ConvertCurrencyAsync(Money baseMoney, CurrencyInfo counterCurrency, DateTimeOffset asOn)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override Task<IEnumerable<CurrencyPair>> GetCurrencyPairsAsync(DateTimeOffset asOn)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override Task<decimal> GetExchangeRateAsync(CurrencyPair pair, DateTimeOffset asOn)
+		{
+			throw new NotImplementedException();
+		}
+
+		private async void GetConverterChain(DateTimeOffset asOn)
+		{
+
+		}
+
+		private async Task<BidirectionalGraph<CurrencyInfo, TaggedEdge<CurrencyInfo, ICurrencyConverter>>> CreateGraph(DateTimeOffset asOn)
+		{
+			var graph = new BidirectionalGraph<CurrencyInfo, TaggedEdge<CurrencyInfo, ICurrencyConverter>>();
+
+			foreach (var converter in this.converters)
+			{
+				var currencyPairs = await converter.GetCurrencyPairsAsync(asOn);
+
+				foreach (var currencyPair in currencyPairs)
+				{
+					graph.AddVertex(currencyPair.BaseCurrency);
+					graph.AddVertex(currencyPair.CounterCurrency);
+
+					graph.AddVerticesAndEdge(new TaggedEdge<CurrencyInfo, ICurrencyConverter>(currencyPair.BaseCurrency, currencyPair.CounterCurrency, converter));
+				}
+			}
+
+			return graph;
+		}
+	}
+}
---
+++ TIKSN.Finance/ICurrencyConversionCompositionStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIKSN.Finance
+{
+	public interface ICurrencyConversionCompositionStrategy
+	{
+		Task<decimal> GetExchangeRateAsync(IEnumerable<ICurrencyConverter> converters, CurrencyPair pair, DateTimeOffset asOn);
+
+		Task<Money> ConvertCurrencyAsync(Money baseMoney, IEnumerable<ICurrencyConverter> converters, CurrencyInfo counterCurrency, DateTimeOffset asOn);
+	}
+}
