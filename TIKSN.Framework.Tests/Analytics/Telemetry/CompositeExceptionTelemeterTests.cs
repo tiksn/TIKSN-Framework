7955983c8c48413e11de5d5945afada2a152608f
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-05-07 20:31:30 +0300

    New tests are added

---
+++ TIKSN.Framework.Tests/Analytics/Telemetry/CompositeExceptionTelemeterTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.QualityTools.Testing.Fakes;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TIKSN.Framework.Tests.Unity;
+using TIKSN.Analytics.Telemetry;
+
+namespace TIKSN.Framework.Tests.Analytics.Telemetry
+{
+	[TestClass]
+	public class CompositeExceptionTelemeterTests
+	{
+		
+	}
+}
