995124435b1c569913565a7046dd4ce475fbb551
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-05-08 01:29:52 +0300

    NBU converter is created

---
+++ TIKSN.Finance/ForeignExchange/NationalBankOfUkraine.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TIKSN.Finance.ForeignExchange
+{
+	/// <summary>
+	/// Exchange rate converter of National Bank of Ukraine
+	/// </summary>
+	/// <seealso cref="TIKSN.Finance.ICurrencyConverter"/>
+	public class NationalBankOfUkraine : ICurrencyConverter
+	{
+		private const string WebServiceUrlFormat = "http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date={0:yyyyMMdd}&json";
+
+		public async Task<Money> ConvertCurrencyAsync(Money baseMoney, CurrencyInfo counterCurrency, DateTimeOffset asOn)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<CurrencyPair>> GetCurrencyPairsAsync(DateTimeOffset asOn)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<decimal> GetExchangeRateAsync(CurrencyPair pair, DateTimeOffset asOn)
+		{
+			throw new NotImplementedException();
+		}
+
+		private async Task<IEnumerable<ExchangeRateRecord>> FetchExchangeRatesAsync(DateTimeOffset asOn)
+		{
+			var httpClient = new HttpClient();
+			var response = await httpClient.GetStringAsync(string.Format(WebServiceUrlFormat, asOn));
+			return JsonConvert.DeserializeObject<List<ExchangeRateRecord>>(response);
+		}
+
+		private class ExchangeRateRecord
+		{
+		}
+	}
+}
--- TIKSN.Framework.Tests/Analytics/Telemetry/CompositeExceptionTelemeterTests.cs
+++ TIKSN.Framework.Tests/Analytics/Telemetry/CompositeExceptionTelemeterTests.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.QualityTools.Testing.Fakes;
-using System.Net.Http;
-using System.Threading.Tasks;
-using TIKSN.Framework.Tests.Unity;
-using TIKSN.Analytics.Telemetry;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TIKSN.Framework.Tests.Analytics.Telemetry
 {
 	[TestClass]
 	public class CompositeExceptionTelemeterTests
 	{
-		
+
 	}
 }
