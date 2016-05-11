59a551fd1afbfce319d650801ea2da22a977216d
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-05-12 01:57:15 +0300

    telemeters

---
+++ TIKSN.Framework.Full/Analytics/Telemetry/PowerShellExceptionTelemeter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace TIKSN.Analytics.Telemetry
+{
+	public class PowerShellExceptionTelemeter : IExceptionTelemeter
+	{
+		private readonly Cmdlet cmdlet;
+
+		public PowerShellExceptionTelemeter(Cmdlet cmdlet)
+		{
+			this.cmdlet = cmdlet;
+		}
+
+		public Task TrackException(Exception exception, TelemetrySeverityLevel severityLevel)
+		{
+			cmdlet.WriteError(new ErrorRecord(exception, null, ErrorCategory.InvalidOperation, null));
+
+			return Task.FromResult<object>(null);
+		}
+
+		public Task TrackException(Exception exception)
+		{
+			cmdlet.WriteError(new ErrorRecord(exception, null, ErrorCategory.InvalidOperation, null));
+
+			return Task.FromResult<object>(null);
+		}
+	}
+}
---
+++ TIKSN.Framework.Full/Analytics/Telemetry/PowerShellMetricTelemeter.cs
@@ -0,0 +1,22 @@
+﻿using System.Management.Automation;
+using System.Threading.Tasks;
+
+namespace TIKSN.Analytics.Telemetry
+{
+	public class PowerShellMetricTelemeter : IMetricTelemeter
+	{
+		private readonly Cmdlet cmdlet;
+
+		public PowerShellMetricTelemeter(Cmdlet cmdlet)
+		{
+			this.cmdlet = cmdlet;
+		}
+
+		public Task TrackMetric(string metricName, decimal metricValue)
+		{
+			cmdlet.WriteVerbose($"METRIC: {metricName} - {metricValue}");
+
+			return Task.FromResult<object>(null);
+		}
+	}
+}
