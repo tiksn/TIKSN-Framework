3f145c1de935742e73c406538b5cf30f53dc6cd8
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-05-11 23:37:44 +0300

    should process

---
+++ TIKSN.Core/Progress/IUserConfirmation.cs
@@ -0,0 +1,9 @@
+﻿namespace TIKSN.Progress
+{
+	public interface IUserConfirmation
+	{
+		bool ShouldProcess(string target);
+		bool ShouldProcess(string target, string action);
+		bool ShouldProcess(string verboseDescription, string verboseWarning, string caption);
+	}
+}
