8626f3ba7cb53d3310b4387a5d1e7d758823d6a1
tiksn <personal@tiksn.com>
2016-05-12 01:36:25 +0300

    silent user confirmation

---
+++ TIKSN.Core/Progress/SilentUserConfirmation.cs
@@ -0,0 +1,20 @@
+﻿namespace TIKSN.Progress
+{
+	public class SilentUserConfirmation : IUserConfirmation
+	{
+		public bool ShouldProcess(string target)
+		{
+			return true;
+		}
+
+		public bool ShouldProcess(string target, string action)
+		{
+			return true;
+		}
+
+		public bool ShouldProcess(string verboseDescription, string verboseWarning, string caption)
+		{
+			return true;
+		}
+	}
+}
