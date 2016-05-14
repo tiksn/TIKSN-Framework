83a316e2abfbdf0ace956922f48f2af5c49dd34e
tiksn <personal@tiksn.com>
2016-05-15 02:29:28 +0300

    repository

---
+++ TIKSN.Core/Data/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Data
+{
+	public interface IRepository<T> where T : class
+	{
+		Task AddAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task RemoveAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task RemoveRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task UpdateAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task UpdateRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default(CancellationToken));
+	}
+}
