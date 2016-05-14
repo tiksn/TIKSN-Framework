563bb39bc78e95e60623d89f0a8a317712ecfadb
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-05-14 22:03:04 +0300

    Final version of 1.4 is published

--- TIKSN.Core/Properties/AssemblyInfo.cs
+++ TIKSN.Core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.22.0")]
+[assembly: AssemblyFileVersion("1.4.22.0")]
 [assembly: InternalsVisibleTo("TIKSN.Framework.Tests")]
 [assembly: InternalsVisibleTo("TIKSN.Core.Explorables")]
