66d48ca59d9758f5afa0c0468865514be25babc3
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-06-11 16:31:55 +0300

    TIKSN.Versioning is moved to TIKSN.Core, unit tests are converted to xUnit

---
+++ UnitTests/Versioning/MilestoneTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace TIKSN.Versioning.Tests
+{
+	public class MilestoneTests
+	{
+		[Fact]
+		public void Test1()
+		{
+			Assert.True(Milestone.PreAlpha < Milestone.Alpha);
+		}
+
+		[Fact]
+		public void Test2()
+		{
+			Assert.True(Milestone.Alpha < Milestone.Beta);
+		}
+
+		[Fact]
+		public void Test3()
+		{
+			Assert.True(Milestone.Beta < Milestone.ReleaseCandidate);
+		}
+
+		[Fact]
+		public void Test4()
+		{
+			Assert.True(Milestone.ReleaseCandidate < Milestone.RTM);
+		}
+
+		[Fact]
+		public void Test5()
+		{
+			Assert.True(Milestone.RTM < Milestone.GA);
+		}
+	}
+}
