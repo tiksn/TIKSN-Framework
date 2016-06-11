944e6f45401c79cbf5dbb1e7646e1e88e5c99aa3
Tigran Torosyan <ttorosyan@deloitte.com>
2016-06-11 18:00:49 +0300

    unit tests coverted to xUnit partially

---
+++ UnitTests/Finance/CurrencyInfoTests.cs
@@ -0,0 +1,268 @@
+﻿using Xunit;
+
+namespace TIKSN.Finance.Tests
+{
+	public class CurrencyInfoTests
+	{
+		[Fact]
+		public void CreateArmenianDram001()
+		{
+			System.Globalization.RegionInfo AM = new System.Globalization.RegionInfo("AM");
+			CurrencyInfo AMD = new CurrencyInfo(AM);
+
+			Assert.Equal("AMD", AMD.ISOCurrencySymbol);
+			Assert.Equal("\u058F", AMD.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateArmenianDram002()
+		{
+			System.Globalization.RegionInfo AM = new System.Globalization.RegionInfo("hy-AM");
+			CurrencyInfo AMD = new CurrencyInfo(AM);
+
+			Assert.Equal("AMD", AMD.ISOCurrencySymbol);
+			Assert.Equal("֏", AMD.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateEuro001()
+		{
+			System.Globalization.RegionInfo DE = new System.Globalization.RegionInfo("DE");
+			CurrencyInfo Euro = new CurrencyInfo(DE);
+
+			Assert.Equal("EUR", Euro.ISOCurrencySymbol);
+			Assert.Equal("€", Euro.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateEuro002()
+		{
+			System.Globalization.RegionInfo FR = new System.Globalization.RegionInfo("FR");
+			CurrencyInfo Euro = new CurrencyInfo(FR);
+
+			Assert.Equal("EUR", Euro.ISOCurrencySymbol);
+			Assert.Equal("€", Euro.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateEuro003()
+		{
+			System.Globalization.RegionInfo DE = new System.Globalization.RegionInfo("de-DE");
+			CurrencyInfo Euro = new CurrencyInfo(DE);
+
+			Assert.Equal("EUR", Euro.ISOCurrencySymbol);
+			Assert.Equal("€", Euro.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateUnitedStatesDollar001()
+		{
+			System.Globalization.RegionInfo US = new System.Globalization.RegionInfo("US");
+			CurrencyInfo USD = new CurrencyInfo(US);
+
+			Assert.Equal("USD", USD.ISOCurrencySymbol);
+			Assert.Equal("$", USD.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateUnitedStatesDollar002()
+		{
+			System.Globalization.RegionInfo US = new System.Globalization.RegionInfo("en-US");
+			CurrencyInfo USD = new CurrencyInfo(US);
+
+			Assert.Equal("USD", USD.ISOCurrencySymbol);
+			Assert.Equal("$", USD.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateUnitedStatesDollar003()
+		{
+			System.Globalization.RegionInfo US = new System.Globalization.RegionInfo("es-US");
+			CurrencyInfo USD = new CurrencyInfo(US);
+
+			Assert.Equal("USD", USD.ISOCurrencySymbol);
+			Assert.Equal("$", USD.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateUnitedStatesDollar004()
+		{
+			System.Globalization.RegionInfo SV = new System.Globalization.RegionInfo("SV");
+			CurrencyInfo USD = new CurrencyInfo(SV);
+
+			Assert.Equal("USD", USD.ISOCurrencySymbol);
+			Assert.Equal("$", USD.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateUnitedStatesDollar005()
+		{
+			System.Globalization.RegionInfo SV = new System.Globalization.RegionInfo("es-SV");
+			CurrencyInfo USD = new CurrencyInfo(SV);
+
+			Assert.Equal("USD", USD.ISOCurrencySymbol);
+			Assert.Equal("$", USD.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateUnitedStatesDollar006()
+		{
+			System.Globalization.RegionInfo EC = new System.Globalization.RegionInfo("EC");
+			CurrencyInfo USD = new CurrencyInfo(EC);
+
+			Assert.Equal("USD", USD.ISOCurrencySymbol);
+			Assert.Equal("$", USD.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CreateUnitedStatesDollar007()
+		{
+			System.Globalization.RegionInfo EC = new System.Globalization.RegionInfo("es-EC");
+			CurrencyInfo USD = new CurrencyInfo(EC);
+
+			Assert.Equal("USD", USD.ISOCurrencySymbol);
+			Assert.Equal("$", USD.CurrencySymbol);
+		}
+
+		[Fact]
+		public void CurrencyComparison001()
+		{
+			System.Globalization.RegionInfo R1 = new System.Globalization.RegionInfo("en-US");
+			System.Globalization.RegionInfo R2 = new System.Globalization.RegionInfo("es-US");
+			System.Globalization.RegionInfo R3 = new System.Globalization.RegionInfo("US");
+
+			Assert.True(R1 != R2);
+			Assert.True(R1 != R3);
+			Assert.True(R3 != R2);
+
+			CurrencyInfo C1 = new CurrencyInfo(R1);
+			CurrencyInfo C2 = new CurrencyInfo(R2);
+			CurrencyInfo C3 = new CurrencyInfo(R3);
+
+			Assert.True(C1.Equals(C1));
+			Assert.True(C2.Equals(C2));
+			Assert.True(C3.Equals(C3));
+
+			Assert.True(C1.Equals(C2));
+			Assert.True(C2.Equals(C1));
+			Assert.True(C1.Equals(C3));
+			Assert.True(C3.Equals(C1));
+			Assert.True(C2.Equals(C3));
+			Assert.True(C3.Equals(C2));
+		}
+
+		[Fact]
+		public void CurrencyComparison002()
+		{
+			System.Globalization.RegionInfo US = new System.Globalization.RegionInfo("US");
+			System.Globalization.RegionInfo AM = new System.Globalization.RegionInfo("AM");
+
+			CurrencyInfo USD = new CurrencyInfo(US);
+			CurrencyInfo AMD = new CurrencyInfo(AM);
+
+			Assert.False(AMD.Equals(USD));
+			Assert.False(USD.Equals(AMD));
+		}
+
+		[Fact]
+		public void CurrencyComparison003()
+		{
+			System.Globalization.RegionInfo US = new System.Globalization.RegionInfo("US");
+			System.Globalization.RegionInfo SV = new System.Globalization.RegionInfo("SV");
+
+			CurrencyInfo USD1 = new CurrencyInfo(US);
+			CurrencyInfo USD2 = new CurrencyInfo(SV);
+
+			Assert.True(USD1 == USD2);
+			Assert.False(USD1 != USD2);
+		}
+
+		[Fact]
+		public void CurrencyComparison004()
+		{
+			System.Globalization.RegionInfo US = new System.Globalization.RegionInfo("US");
+			System.Globalization.RegionInfo AM = new System.Globalization.RegionInfo("AM");
+
+			CurrencyInfo USD = new CurrencyInfo(US);
+			CurrencyInfo AMD = new CurrencyInfo(AM);
+
+			Assert.True(AMD != USD);
+			Assert.False(AMD == USD);
+		}
+
+		[Fact]
+		public void CurrencyIsoSymbolAndNumber001()
+		{
+			var currency = new CurrencyInfo("eur");
+
+			Assert.Equal("EUR", currency.ISOCurrencySymbol);
+			Assert.Equal("EUR", currency.CurrencySymbol);
+			Assert.Equal("EUR", currency.ToString());
+			Assert.Equal(false, currency.IsFund);
+			Assert.Equal(true, currency.IsCurrent);
+		}
+
+		public void CurrencyIsoSymbolAndNumber002()
+		{
+			var currency = new CurrencyInfo("BOV");
+
+			Assert.Equal("BOV", currency.ISOCurrencySymbol);
+			Assert.Equal("BOV", currency.CurrencySymbol);
+			Assert.Equal("BOV", currency.ToString());
+			Assert.Equal(true, currency.IsFund);
+			Assert.Equal(true, currency.IsCurrent);
+		}
+
+		public void CurrencyIsoSymbolAndNumber003()
+		{
+			var currency = new CurrencyInfo("XRE");
+
+			Assert.Equal("XRE", currency.ISOCurrencySymbol);
+			Assert.Equal("XRE", currency.CurrencySymbol);
+			Assert.Equal("XRE", currency.ToString());
+			Assert.Equal(true, currency.IsFund);
+			Assert.Equal(false, currency.IsCurrent);
+		}
+
+		public void CurrencyIsoSymbolAndNumber004()
+		{
+			var currency = new CurrencyInfo(new System.Globalization.RegionInfo("en-US"));
+
+			Assert.Equal("USD", currency.ISOCurrencySymbol);
+			Assert.Equal("$", currency.CurrencySymbol);
+			Assert.Equal("USD", currency.ToString());
+			Assert.Equal(false, currency.IsFund);
+			Assert.Equal(true, currency.IsCurrent);
+		}
+
+		[Fact]
+		public void Equals001()
+		{
+			System.Globalization.RegionInfo US = new System.Globalization.RegionInfo("US");
+			CurrencyInfo USD1 = new CurrencyInfo(US);
+			CurrencyInfo USD2 = USD1;
+			CurrencyInfo SomeCurrency1 = null;
+			object Something1 = new object();
+			object Something2 = null;
+			object USD3 = USD1;
+			object USD4 = new CurrencyInfo(US);
+
+			Assert.True(USD1.Equals(USD1));
+			Assert.True(USD1.Equals(USD2));
+			Assert.False(USD1.Equals(SomeCurrency1));
+			Assert.False(USD1.Equals(Something1));
+			Assert.False(USD1.Equals(Something2));
+			Assert.True(USD1.Equals(USD3));
+			Assert.True(USD1.Equals(USD4));
+		}
+
+		[Fact]
+		public void ToString001()
+		{
+			System.Globalization.RegionInfo US = new System.Globalization.RegionInfo("US");
+			CurrencyInfo USD = new CurrencyInfo(US);
+
+			Assert.Equal("USD", USD.ToString());
+		}
+	}
+}
---
+++ UnitTests/Finance/ForeignExchange/BankOfCanadaTests.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using TIKSN.Finance.ForeignExchange;
+using Xunit;
+
+namespace TIKSN.Finance.Tests.ForeignExchange
+{
+	public class BankOfCanadaTests
+	{
+		[Fact]
+		public async Task Calculate001()
+		{
+			var bank = new BankOfCanada();
+
+			foreach (var pair in await bank.GetCurrencyPairsAsync(DateTimeOffset.Now))
+			{
+				Money Before = new Money(pair.BaseCurrency, 10m);
+				decimal rate = await bank.GetExchangeRateAsync(pair, DateTimeOffset.Now);
+				Money After = await bank.ConvertCurrencyAsync(Before, pair.CounterCurrency, DateTimeOffset.Now);
+
+				Assert.True(After.Amount == rate * Before.Amount);
+				Assert.True(After.Currency == pair.CounterCurrency);
+			}
+		}
+
+		[Fact]
+		public async Task ConversionDirection001()
+		{
+			var Bank = new BankOfCanada();
+
+			var CanadianDollar = new CurrencyInfo(new RegionInfo("CA"));
+			var PoundSterling = new CurrencyInfo(new RegionInfo("GB"));
+
+			var BeforeInPound = new Money(PoundSterling, 100m);
+
+			var AfterInDollar = await Bank.ConvertCurrencyAsync(BeforeInPound, CanadianDollar, DateTimeOffset.Now);
+
+			Assert.True(BeforeInPound.Amount < AfterInDollar.Amount);
+		}
+
+		[Fact]
+		public async Task ConvertCurrency001()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			var CurrencyPairs = await Bank.GetCurrencyPairsAsync(DateTimeOffset.Now);
+
+			foreach (CurrencyPair pair in CurrencyPairs)
+			{
+				Money Before = new Money(pair.BaseCurrency, decimal.One);
+
+				Money After = await Bank.ConvertCurrencyAsync(Before, pair.CounterCurrency, DateTimeOffset.Now);
+
+				Assert.True(After.Amount > decimal.Zero);
+			}
+		}
+
+		[Fact]
+		public async Task ConvertCurrency002()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			var CurrencyPairs = await Bank.GetCurrencyPairsAsync(DateTimeOffset.Now);
+
+			foreach (CurrencyPair pair in CurrencyPairs)
+			{
+				Money Before = new Money(pair.BaseCurrency, decimal.One);
+
+				Money After = await Bank.ConvertCurrencyAsync(Before, pair.CounterCurrency, DateTimeOffset.Now);
+
+				Assert.True(After.Currency == pair.CounterCurrency);
+			}
+		}
+
+		[Fact]
+		public async Task ConvertCurrency003()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			var CurrencyPairs = await Bank.GetCurrencyPairsAsync(DateTimeOffset.Now);
+
+			foreach (CurrencyPair pair in CurrencyPairs)
+			{
+				Money Before = new Money(pair.BaseCurrency, 10m);
+
+				Money After = await Bank.ConvertCurrencyAsync(Before, pair.CounterCurrency, DateTimeOffset.Now);
+
+				decimal rate = await Bank.GetExchangeRateAsync(pair, DateTimeOffset.Now);
+
+				Assert.True(After.Currency == pair.CounterCurrency);
+				Assert.True(After.Amount == rate * Before.Amount);
+			}
+		}
+
+		[Fact]
+		public async Task ConvertCurrency004()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			RegionInfo US = new RegionInfo("US");
+			RegionInfo CA = new RegionInfo("CA");
+
+			CurrencyInfo USD = new CurrencyInfo(US);
+			CurrencyInfo CAD = new CurrencyInfo(CA);
+
+			Money Before = new Money(USD, 100m);
+
+			await Assert.ThrowsAsync<ArgumentException>(
+				async () => await Bank.ConvertCurrencyAsync(Before, CAD, DateTimeOffset.Now.AddMinutes(1d)));
+		}
+
+		[Fact]
+		public async Task ConvertCurrency005()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			RegionInfo US = new RegionInfo("US");
+			RegionInfo CA = new RegionInfo("CA");
+
+			CurrencyInfo USD = new CurrencyInfo(US);
+			CurrencyInfo CAD = new CurrencyInfo(CA);
+
+			Money Before = new Money(USD, 100m);
+			
+			await Assert.ThrowsAsync<ArgumentException>(
+				async () => await Bank.ConvertCurrencyAsync(Before, CAD, DateTimeOffset.Now.AddDays(-20d)));
+		}
+
+		[Fact]
+		public async Task ConvertCurrency006()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			RegionInfo AO = new RegionInfo("AO");
+			RegionInfo BW = new RegionInfo("BW");
+
+			CurrencyInfo AOA = new CurrencyInfo(AO);
+			CurrencyInfo BWP = new CurrencyInfo(BW);
+
+			Money Before = new Money(AOA, 100m);
+			
+			await Assert.ThrowsAsync<ArgumentException>(
+				async () => await Bank.ConvertCurrencyAsync(Before, BWP, DateTimeOffset.Now));
+		}
+
+		[Fact]
+		public async Task CurrencyPairs001()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			var CurrencyPairs = await Bank.GetCurrencyPairsAsync(DateTimeOffset.Now);
+
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/USD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/ARS"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/AUD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/BRL"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/CLP"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/CNY"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/COP"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/HRK"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/CZK"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/DKK"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/EUR"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/GTQ"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/HNL"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/HKD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/HUF"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/ISK"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/INR"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/IDR"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/ILS"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/JMD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/JPY"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/MYR"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/MXN"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/MAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/MMK"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/NZD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/NOK"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/PKR"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/PAB"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/PEN"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/PHP"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/PLN"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/RON"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/RUB"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/RSD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/SGD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/ZAR"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/KRW"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/LKR"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/SEK"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/CHF"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/TWD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/THB"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/TTD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/TND"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/TRY"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/AED"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/GBP"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/VEF"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/VND"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/XAF"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CAD/XCD"));
+
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "USD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "ARS/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "AUD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "BRL/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CLP/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CNY/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "COP/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "HRK/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CZK/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "DKK/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "EUR/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "GTQ/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "HNL/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "HKD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "HUF/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "ISK/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "INR/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "IDR/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "ILS/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "JMD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "JPY/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "MYR/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "MXN/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "MAD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "MMK/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "NZD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "NOK/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "PKR/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "PAB/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "PEN/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "PHP/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "PLN/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "RON/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "RUB/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "RSD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "SGD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "ZAR/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "KRW/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "LKR/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "SEK/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "CHF/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "TWD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "THB/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "TTD/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "TND/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "TRY/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "AED/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "GBP/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "VEF/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "VND/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "XAF/CAD"));
+			Assert.True(CurrencyPairs.Any(C => C.ToString() == "XCD/CAD"));
+
+			//Assert.Equal(104, CurrencyPairs.Count());
+		}
+
+		[Fact]
+		public async Task CurrencyPairs002()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			var CurrencyPairs = await Bank.GetCurrencyPairsAsync(DateTimeOffset.Now);
+
+			foreach (CurrencyPair pair in CurrencyPairs)
+			{
+				CurrencyPair reversePair = new CurrencyPair(pair.CounterCurrency, pair.BaseCurrency);
+
+				Assert.True(CurrencyPairs.Any(C => C == reversePair));
+			}
+		}
+
+		[Fact]
+		public async Task CurrencyPairs003()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			var pairSet = new HashSet<CurrencyPair>();
+
+			var CurrencyPairs = await Bank.GetCurrencyPairsAsync(DateTimeOffset.Now);
+
+			foreach (CurrencyPair pair in CurrencyPairs)
+			{
+				pairSet.Add(pair);
+			}
+
+			Assert.True(pairSet.Count == CurrencyPairs.Count());
+		}
+
+		[Fact]
+		public async Task CurrencyPairs004()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+			
+			await Assert.ThrowsAsync<ArgumentException>(
+				async () => await Bank.GetCurrencyPairsAsync(DateTimeOffset.Now.AddDays(-10)));
+		}
+
+		[Fact]
+		public async Task CurrencyPairs005()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+			
+			await Assert.ThrowsAsync<ArgumentException>(
+				async () => await Bank.GetCurrencyPairsAsync(DateTimeOffset.Now.AddDays(10)));
+		}
+
+		[Fact]
+		public async Task Fetch001()
+		{
+			var Bank = new BankOfCanada();
+
+			await Bank.FetchAsync();
+		}
+
+		[Fact]
+		public async Task GetExchangeRate001()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			var CurrencyPairs = await Bank.GetCurrencyPairsAsync(DateTimeOffset.Now);
+
+			foreach (CurrencyPair pair in CurrencyPairs)
+			{
+				decimal rate = await Bank.GetExchangeRateAsync(pair, DateTimeOffset.Now);
+
+				Assert.True(rate > decimal.Zero);
+			}
+		}
+
+		[Fact]
+		public async Task GetExchangeRate002()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			RegionInfo US = new RegionInfo("US");
+			RegionInfo CA = new RegionInfo("CA");
+
+			CurrencyInfo USD = new CurrencyInfo(US);
+			CurrencyInfo CAD = new CurrencyInfo(CA);
+
+			CurrencyPair pair = new CurrencyPair(CAD, USD);
+			
+			await Assert.ThrowsAsync<ArgumentException>(
+				async () => await Bank.GetExchangeRateAsync(pair, DateTimeOffset.Now.AddMinutes(1d)));
+		}
+
+		[Fact]
+		public async Task GetExchangeRate003()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			RegionInfo US = new RegionInfo("US");
+			RegionInfo CA = new RegionInfo("CA");
+
+			CurrencyInfo USD = new CurrencyInfo(US);
+			CurrencyInfo CAD = new CurrencyInfo(CA);
+
+			CurrencyPair pair = new CurrencyPair(CAD, USD);
+			
+			await Assert.ThrowsAsync<ArgumentException>(
+				async () => await Bank.GetExchangeRateAsync(pair, DateTimeOffset.Now.AddDays(-20d)));
+		}
+
+		[Fact]
+		public async Task GetExchangeRate004()
+		{
+			BankOfCanada Bank = new BankOfCanada();
+
+			RegionInfo AO = new RegionInfo("AO");
+			RegionInfo BW = new RegionInfo("BW");
+
+			CurrencyInfo AOA = new CurrencyInfo(AO);
+			CurrencyInfo BWP = new CurrencyInfo(BW);
+
+			CurrencyPair pair = new CurrencyPair(BWP, AOA);
+			
+			await Assert.ThrowsAsync<ArgumentException>(
+				async () => await Bank.GetExchangeRateAsync(pair, DateTimeOffset.Now));
+		}
+	}
+}
