46964b7b41945d4214ac7f63bd60b8404a851163
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-08-27 22:39:11 +0300

    closes #56 windows registry as .net core configuration provider

---
+++ TIKSN.Core/Configuration/WindowsRegistryConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
+
+namespace TIKSN.Configuration
+{
+    public static class WindowsRegistryConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddWindowsRegistry(this IConfigurationBuilder configurationBuilder, string rootKey, RegistryView registryView = RegistryView.Default)
+        {
+            return configurationBuilder.Add(new WindowsRegistryConfigurationSource
+            {
+                RootKey = rootKey,
+                RegistryView = registryView
+            });
+        }
+    }
+}
