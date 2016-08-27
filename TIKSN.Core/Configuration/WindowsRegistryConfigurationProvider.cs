2e8be757c3050a2fe4e6a2610b6c3abd4e473769
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-08-27 22:39:11 +0300

    closes #56 windows registry as .net core configuration provider

---
+++ TIKSN.Core/Configuration/WindowsRegistryConfigurationProvider.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
+
+namespace TIKSN.Configuration
+{
+    public class WindowsRegistryConfigurationProvider : ConfigurationProvider
+    {
+        private readonly RegistryView _registryView;
+        private readonly string _rootKey;
+
+        public WindowsRegistryConfigurationProvider(string rootKey, RegistryView registryView)
+        {
+            _registryView = registryView;
+            _rootKey = rootKey;
+        }
+
+        public override void Load()
+        {
+            Data.Clear();
+
+            using (var machineKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, _registryView))
+            using (var userKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, _registryView))
+            {
+                PopulateKeys(machineKey, null);
+            }
+        }
+
+        private void PopulateKeys(RegistryKey currentRegistryKey, string parentConfigurationKey)
+        {
+            PopulateValues(currentRegistryKey, parentConfigurationKey);
+
+            var subKeyNames = currentRegistryKey.GetSubKeyNames();
+
+            foreach (var subKeyName in subKeyNames)
+            {
+                using (var registrySubKey = currentRegistryKey.OpenSubKey(subKeyName))
+                {
+                    PopulateKeys(currentRegistryKey, ConfigurationPath.Combine(parentConfigurationKey, subKeyName));
+                }
+            }
+        }
+
+        private void PopulateValues(RegistryKey currentRegistryKey, string parentConfigurationKey)
+        {
+            var valueNames = currentRegistryKey.GetValueNames();
+
+            foreach (var valueName in valueNames)
+            {
+                var valueData = currentRegistryKey.GetValue(valueName);
+
+                Set(ConfigurationPath.Combine(parentConfigurationKey, valueName), valueData.ToString());
+            }
+        }
+    }
+}
---
+++ TIKSN.Core/Configuration/WindowsRegistryConfigurationSource.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
+
+namespace TIKSN.Configuration
+{
+    public class WindowsRegistryConfigurationSource : IConfigurationSource
+    {
+        public RegistryView RegistryView { get; set; }
+        public string RootKey { get; set; }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new WindowsRegistryConfigurationProvider(RootKey, RegistryView);
+        }
+    }
+}
