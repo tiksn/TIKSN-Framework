03a7349bcd97eb03fcc08c9f98d7c3cc657a792c
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-09-01 14:10:22 +0300

    Hockey App telemeters are created

---
+++ TIKSN.Core/Analytics/Telemetry/HockeyAppTraceTelemeter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TIKSN.Analytics.Telemetry
+{
+    public class HockeyAppTraceTelemeter : HockeyAppTelemeterBase, ITraceTelemeter
+    {
+        public Task TrackTrace(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TrackTrace(string message, TelemetrySeverityLevel severityLevel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
