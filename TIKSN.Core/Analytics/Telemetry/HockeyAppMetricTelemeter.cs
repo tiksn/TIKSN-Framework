9009408bc4298321ef0806ca306a1aeb120cc8b3
Tigran Torosyan <ttorosyan@deloitte.com>
2016-09-01 14:10:22 +0300

    Hockey App telemeters are created

---
+++ TIKSN.Core/Analytics/Telemetry/HockeyAppMetricTelemeter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TIKSN.Analytics.Telemetry
+{
+    public class HockeyAppMetricTelemeter : HockeyAppTelemeterBase, IMetricTelemeter
+    {
+        public Task TrackMetric(string metricName, decimal metricValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
