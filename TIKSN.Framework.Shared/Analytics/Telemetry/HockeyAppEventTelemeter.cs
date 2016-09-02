2c498d6b0044f859ccea9e6715e5184d699af578
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-09-02 19:27:42 +0300

    HockeyApp telemetry is implemented for UWP and Full

--- TIKSN.Framework.Shared/Analytics/Telemetry/HockeyAppEventTelemeter.cs
+++ TIKSN.Framework.Shared/Analytics/Telemetry/HockeyAppEventTelemeter.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Microsoft.HockeyApp;
 using System.Threading.Tasks;
 
 namespace TIKSN.Analytics.Telemetry
 {
     public class HockeyAppEventTelemeter : HockeyAppTelemeterBase, IEventTelemeter
     {
+        public HockeyAppEventTelemeter(IHockeyClient hockeyClient) : base(hockeyClient)
+        {
+        }
+
         public Task TrackEvent(string name)
         {
-            throw new NotImplementedException();
+            hockeyClient.TrackEvent(name);
+
+            return Task.FromResult<object>(null);
         }
     }
 }
--- TIKSN.Framework.Shared/Analytics/Telemetry/HockeyAppMetricTelemeter.cs
+++ TIKSN.Framework.Shared/Analytics/Telemetry/HockeyAppMetricTelemeter.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Microsoft.HockeyApp;
 using System.Threading.Tasks;
 
 namespace TIKSN.Analytics.Telemetry
 {
     public class HockeyAppMetricTelemeter : HockeyAppTelemeterBase, IMetricTelemeter
     {
+        public HockeyAppMetricTelemeter(IHockeyClient hockeyClient) : base(hockeyClient)
+        {
+        }
+
         public Task TrackMetric(string metricName, decimal metricValue)
         {
-            throw new NotImplementedException();
+            hockeyClient.TrackMetric(metricName, (double)metricValue);
+
+            return Task.FromResult<object>(null);
         }
     }
 }
