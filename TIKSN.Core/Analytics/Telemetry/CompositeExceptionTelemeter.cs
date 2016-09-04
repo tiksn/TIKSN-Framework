c1160f21a4345bf91f543639b5643e842e64abe8
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-09-04 18:44:10 +0300

    composite telemeter switched to ienumerable over array

--- TIKSN.Core/Analytics/Telemetry/CompositeExceptionTelemeter.cs
+++ TIKSN.Core/Analytics/Telemetry/CompositeExceptionTelemeter.cs
@@ -1,55 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using TIKSN.Configuration;
 
 namespace TIKSN.Analytics.Telemetry
 {
-	public class CompositeExceptionTelemeter : IExceptionTelemeter
-	{
-		private readonly IConfiguration<CommonConfiguration> commonConfiguration;
-		private readonly IExceptionTelemeter[] exceptionTelemeters;
+    public class CompositeExceptionTelemeter : IExceptionTelemeter
+    {
+        private readonly IConfiguration<CommonConfiguration> commonConfiguration;
+        private readonly IEnumerable<IExceptionTelemeter> exceptionTelemeters;
 
-		public CompositeExceptionTelemeter(IConfiguration<CommonConfiguration> commonConfiguration, IExceptionTelemeter[] exceptionTelemeters)
-		{
-			this.commonConfiguration = commonConfiguration;
-			this.exceptionTelemeters = exceptionTelemeters;
-		}
+        public CompositeExceptionTelemeter(IConfiguration<CommonConfiguration> commonConfiguration, IEnumerable<IExceptionTelemeter> exceptionTelemeters)
+        {
+            this.commonConfiguration = commonConfiguration;
+            this.exceptionTelemeters = exceptionTelemeters;
+        }
 
-		public async Task TrackException(Exception exception)
-		{
-			if (commonConfiguration.GetConfiguration().IsExceptionTrackingEnabled)
-			{
-				foreach (var exceptionTelemeter in exceptionTelemeters)
-				{
-					try
-					{
-						await exceptionTelemeter.TrackException(exception);
-					}
-					catch (Exception ex)
-					{
-						Debug.WriteLine(ex);
-					}
-				}
-			}
-		}
+        public async Task TrackException(Exception exception)
+        {
+            if (commonConfiguration.GetConfiguration().IsExceptionTrackingEnabled)
+            {
+                foreach (var exceptionTelemeter in exceptionTelemeters)
+                {
+                    try
+                    {
+                        await exceptionTelemeter.TrackException(exception);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                    }
+                }
+            }
+        }
 
-		public async Task TrackException(Exception exception, TelemetrySeverityLevel severityLevel)
-		{
-			if (commonConfiguration.GetConfiguration().IsExceptionTrackingEnabled)
-			{
-				foreach (var exceptionTelemeter in exceptionTelemeters)
-				{
-					try
-					{
-						await exceptionTelemeter.TrackException(exception, severityLevel);
-					}
-					catch (Exception ex)
-					{
-						Debug.WriteLine(ex);
-					}
-				}
-			}
-		}
-	}
+        public async Task TrackException(Exception exception, TelemetrySeverityLevel severityLevel)
+        {
+            if (commonConfiguration.GetConfiguration().IsExceptionTrackingEnabled)
+            {
+                foreach (var exceptionTelemeter in exceptionTelemeters)
+                {
+                    try
+                    {
+                        await exceptionTelemeter.TrackException(exception, severityLevel);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                    }
+                }
+            }
+        }
+    }
 }
--- TIKSN.Core/Analytics/Telemetry/CompositeTraceTelemeter.cs
+++ TIKSN.Core/Analytics/Telemetry/CompositeTraceTelemeter.cs
@@ -1,55 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using TIKSN.Configuration;
 
 namespace TIKSN.Analytics.Telemetry
 {
-	public class CompositeTraceTelemeter : ITraceTelemeter
-	{
-		private readonly IConfiguration<CommonConfiguration> commonConfiguration;
-		private readonly ITraceTelemeter[] traceTelemeters;
+    public class CompositeTraceTelemeter : ITraceTelemeter
+    {
+        private readonly IConfiguration<CommonConfiguration> commonConfiguration;
+        private readonly IEnumerable<ITraceTelemeter> traceTelemeters;
 
-		public CompositeTraceTelemeter(IConfiguration<CommonConfiguration> commonConfiguration, ITraceTelemeter[] traceTelemeters)
-		{
-			this.commonConfiguration = commonConfiguration;
-			this.traceTelemeters = traceTelemeters;
-		}
+        public CompositeTraceTelemeter(IConfiguration<CommonConfiguration> commonConfiguration, IEnumerable<ITraceTelemeter> traceTelemeters)
+        {
+            this.commonConfiguration = commonConfiguration;
+            this.traceTelemeters = traceTelemeters;
+        }
 
-		public async Task TrackTrace(string message)
-		{
-			if (commonConfiguration.GetConfiguration().IsTraceTrackingEnabled)
-			{
-				foreach (var traceTelemeter in traceTelemeters)
-				{
-					try
-					{
-						await traceTelemeter.TrackTrace(message);
-					}
-					catch (Exception ex)
-					{
-						Debug.WriteLine(ex);
-					}
-				}
-			}
-		}
+        public async Task TrackTrace(string message)
+        {
+            if (commonConfiguration.GetConfiguration().IsTraceTrackingEnabled)
+            {
+                foreach (var traceTelemeter in traceTelemeters)
+                {
+                    try
+                    {
+                        await traceTelemeter.TrackTrace(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                    }
+                }
+            }
+        }
 
-		public async Task TrackTrace(string message, TelemetrySeverityLevel severityLevel)
-		{
-			if (commonConfiguration.GetConfiguration().IsTraceTrackingEnabled)
-			{
-				foreach (var traceTelemeter in traceTelemeters)
-				{
-					try
-					{
-						await traceTelemeter.TrackTrace(message, severityLevel);
-					}
-					catch (Exception ex)
-					{
-						Debug.WriteLine(ex);
-					}
-				}
-			}
-		}
-	}
+        public async Task TrackTrace(string message, TelemetrySeverityLevel severityLevel)
+        {
+            if (commonConfiguration.GetConfiguration().IsTraceTrackingEnabled)
+            {
+                foreach (var traceTelemeter in traceTelemeters)
+                {
+                    try
+                    {
+                        await traceTelemeter.TrackTrace(message, severityLevel);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                    }
+                }
+            }
+        }
+    }
 }
