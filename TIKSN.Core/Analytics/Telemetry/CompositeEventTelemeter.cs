acd4d091c137f32efce970b4a17c8fe94e647ac9
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-09-04 18:44:10 +0300

    composite telemeter switched to ienumerable over array

--- TIKSN.Core/Analytics/Telemetry/CompositeEventTelemeter.cs
+++ TIKSN.Core/Analytics/Telemetry/CompositeEventTelemeter.cs
@@ -1,37 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using TIKSN.Configuration;
 
 namespace TIKSN.Analytics.Telemetry
 {
-	public class CompositeEventTelemeter : IEventTelemeter
-	{
-		private readonly IConfiguration<CommonConfiguration> commonConfiguration;
-		private readonly IEventTelemeter[] eventTelemeters;
+    public class CompositeEventTelemeter : IEventTelemeter
+    {
+        private readonly IConfiguration<CommonConfiguration> commonConfiguration;
+        private readonly IEnumerable<IEventTelemeter> eventTelemeters;
 
-		public CompositeEventTelemeter(IConfiguration<CommonConfiguration> commonConfiguration, IEventTelemeter[] eventTelemeters)
-		{
-			this.commonConfiguration = commonConfiguration;
-			this.eventTelemeters = eventTelemeters;
-		}
+        public CompositeEventTelemeter(IConfiguration<CommonConfiguration> commonConfiguration, IEnumerable<IEventTelemeter> eventTelemeters)
+        {
+            this.commonConfiguration = commonConfiguration;
+            this.eventTelemeters = eventTelemeters;
+        }
 
-		public async Task TrackEvent(string name)
-		{
-			if (commonConfiguration.GetConfiguration().IsEventTrackingEnabled)
-			{
-				foreach (var eventTelemeter in eventTelemeters)
-				{
-					try
-					{
-						await eventTelemeter.TrackEvent(name);
-					}
-					catch (Exception ex)
-					{
-						Debug.WriteLine(ex);
-					}
-				}
-			}
-		}
-	}
+        public async Task TrackEvent(string name)
+        {
+            if (commonConfiguration.GetConfiguration().IsEventTrackingEnabled)
+            {
+                foreach (var eventTelemeter in eventTelemeters)
+                {
+                    try
+                    {
+                        await eventTelemeter.TrackEvent(name);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex);
+                    }
+                }
+            }
+        }
+    }
 }
