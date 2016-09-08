b82777da1ac109380fb4f9e5324cfe3bde86240f
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-09-08 22:33:05 +0300

    Unit of Work is added

---
+++ TIKSN.Core/Data/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace TIKSN.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task CompleteAsync()
+        {
+            return dbContext.SaveChangesAsync();
+        }
+    }
+}
