3878883acff198a1a4650cc134b6fbd0be7f1dbf
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-16 16:01:54 +0300

    xamarin metrics telemetry

--- TIKSN.Framework.Android/Analytics/Telemetry/HockeyAppMetricTelemeter.cs
+++ TIKSN.Framework.Android/Analytics/Telemetry/HockeyAppMetricTelemeter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HockeyApp;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace TIKSN.Analytics.Telemetry
@@ -7,7 +8,12 @@ namespace TIKSN.Analytics.Telemetry
     {
         public Task TrackMetric(string metricName, decimal metricValue)
         {
-            throw new NotImplementedException();
+            var measurements = new Dictionary<string, double>();
+            measurements.Add(metricName, (double)metricValue);
+
+            MetricsManager.TrackEvent(metricName, null, measurements);
+
+            return Task.FromResult<object>(null);
         }
     }
 }
