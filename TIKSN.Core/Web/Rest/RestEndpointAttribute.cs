7fb48fa0f7dda96f249c0c99c77561ebbe948330
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-23 19:33:57 +0300

    rest endpoint and content attribute

---
+++ TIKSN.Core/Web/Rest/RestEndpointAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TIKSN.Web.Rest
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class RestEndpointAttribute : Attribute
+    {
+        public RestEndpointAttribute(string apiName, RestVerb verb, string resourceTemplate)
+        {
+            ApiName = apiName;
+            Verb = verb;
+            ResourceTemplate = resourceTemplate;
+        }
+
+        public string ApiName { get; private set; }
+
+        public RestVerb Verb { get; private set; }
+
+        public string ResourceTemplate { get; private set; }
+    }
+}
