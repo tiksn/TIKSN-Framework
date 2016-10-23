87fcdd9c1455a3c381aa51b73134b5688205e018
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-23 20:03:48 +0300

    content type and accept type is added

--- TIKSN.Core/Web/Rest/RestContentAttribute.cs
+++ TIKSN.Core/Web/Rest/RestContentAttribute.cs
@@ -5,5 +5,11 @@ namespace TIKSN.Web.Rest
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class RestContentAttribute : Attribute
     {
+        public RestContentAttribute(string mediaType = "application/json")
+        {
+            MediaType = mediaType;
+        }
+
+        public string MediaType { get; private set; }
     }
 }
--- TIKSN.Core/Web/Rest/RestEndpointAttribute.cs
+++ TIKSN.Core/Web/Rest/RestEndpointAttribute.cs
@@ -5,17 +5,20 @@ namespace TIKSN.Web.Rest
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class RestEndpointAttribute : Attribute
     {
-        public RestEndpointAttribute(string apiName, RestVerb verb, string resourceTemplate)
+        public RestEndpointAttribute(string apiName, RestVerb verb, string resourceTemplate, string mediaType = "application/json")
         {
             ApiName = apiName;
             Verb = verb;
             ResourceTemplate = resourceTemplate;
+            MediaType = mediaType;
         }
 
         public string ApiName { get; private set; }
 
-        public RestVerb Verb { get; private set; }
+        public string MediaType { get; private set; }
 
         public string ResourceTemplate { get; private set; }
+
+        public RestVerb Verb { get; private set; }
     }
 }
