9831178ae5143df4b2c434933c4c58fb91a136dc
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-23 18:44:27 +0300

    serializer and decerializer base

---
+++ TIKSN.Core/Serialization/DeserializerBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TIKSN.Analytics.Telemetry;
+
+namespace TIKSN.Serialization
+{
+    public abstract class DeserializerBase : SerializationBase, IDeserializer
+    {
+        public DeserializerBase(IExceptionTelemeter exceptionTelemeter) : base(exceptionTelemeter)
+        {
+        }
+
+        public T Deserialize<T>(string text)
+        {
+            try
+            {
+                return DeserializeInternal<T>(text);
+            }
+            catch (Exception ex)
+            {
+                _exceptionTelemeter.TrackException(ex);
+
+                return default(T);
+            }
+        }
+
+        protected abstract T DeserializeInternal<T>(string text);
+    }
+}
---
+++ TIKSN.Core/Serialization/SerializationBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using TIKSN.Analytics.Telemetry;
+
+namespace TIKSN.Serialization
+{
+    public abstract class SerializationBase
+    {
+        protected readonly IExceptionTelemeter _exceptionTelemeter;
+
+        public SerializationBase(IExceptionTelemeter exceptionTelemeter)
+        {
+            Contract.Requires<ArgumentNullException>(exceptionTelemeter != null);
+            _exceptionTelemeter = exceptionTelemeter;
+        }
+    }
+}
