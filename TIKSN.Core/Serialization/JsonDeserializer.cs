bab666df66c42c5173ec2dd94ee057c3394977cb
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-23 19:05:26 +0300

    json decerializer and deserializer use base classes

--- TIKSN.Core/Serialization/JsonDeserializer.cs
+++ TIKSN.Core/Serialization/JsonDeserializer.cs
@@ -1,33 +1,17 @@
 using Newtonsoft.Json;
-using System;
-using System.Diagnostics.Contracts;
 using TIKSN.Analytics.Telemetry;
 
 namespace TIKSN.Serialization
 {
-    public class JsonDeserializer : IDeserializer
+    public class JsonDeserializer : DeserializerBase
     {
-        private readonly IExceptionTelemeter _exceptionTelemeter;
-
-        public JsonDeserializer(IExceptionTelemeter exceptionTelemeter)
+        public JsonDeserializer(IExceptionTelemeter exceptionTelemeter) : base(exceptionTelemeter)
         {
-            Contract.Requires<ArgumentNullException>(exceptionTelemeter != null);
-
-            _exceptionTelemeter = exceptionTelemeter;
         }
 
-        public T Deserialize<T>(string text)
+        protected override T DeserializeInternal<T>(string text)
         {
-            try
-            {
-                return JsonConvert.DeserializeObject<T>(text);
-            }
-            catch (Exception ex)
-            {
-                _exceptionTelemeter.TrackException(ex);
-
-                return default(T);
-            }
+            return JsonConvert.DeserializeObject<T>(text);
         }
     }
 }
