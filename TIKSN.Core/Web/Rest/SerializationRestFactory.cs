37fa03fbfaccdbcde6cc3dedd1b2134bfed4caa7
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-23 20:31:48 +0300

    serialization rest factory

---
+++ TIKSN.Core/Web/Rest/SerializationRestFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using TIKSN.Serialization;
+
+namespace TIKSN.Web.Rest
+{
+    public class SerializationRestFactory : ISerializerRestFactory, IDeserializerRestFactory
+    {
+        private readonly IDictionary<string, Tuple<ISerializer, IDeserializer>> map;
+
+        public SerializationRestFactory(JsonSerializer jsonSerializer, JsonDeserializer jsonDeserializer, DotNetXmlSerializer dotNetXmlSerializer, DotNetXmlDeserializer dotNetXmlDeserializer)
+        {
+            map = new Dictionary<string, Tuple<ISerializer, IDeserializer>>();
+            map.Add("application/json", new Tuple<ISerializer, IDeserializer>(jsonSerializer, jsonDeserializer));
+            map.Add("application/xml", new Tuple<ISerializer, IDeserializer>(dotNetXmlSerializer, dotNetXmlDeserializer));
+        }
+
+        IDeserializer IDeserializerRestFactory.Create(string mediaType)
+        {
+            return map[mediaType].Item2;
+        }
+
+        ISerializer ISerializerRestFactory.Create(string mediaType)
+        {
+            return map[mediaType].Item1;
+        }
+    }
+}
