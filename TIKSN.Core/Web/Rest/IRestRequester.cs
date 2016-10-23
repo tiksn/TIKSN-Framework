08b034942c50c77285459b5a4aa049ed01b2922f
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-23 19:18:49 +0300

    rest requester interface

---
+++ TIKSN.Core/Web/Rest/IRestRequester.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Web.Rest
+{
+    public interface IRestRequester
+    {
+        Task<TResult> Request<TResult, TRequest>(TRequest request, CancellationToken cancellationToken);
+    }
+}
