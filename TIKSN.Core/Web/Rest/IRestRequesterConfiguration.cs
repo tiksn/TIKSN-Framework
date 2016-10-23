47259366f31ceec1196f936add083a8bd43b020b
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-23 19:40:56 +0300

    rest requester configuration

---
+++ TIKSN.Core/Web/Rest/IRestRequesterConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TIKSN.Web.Rest
+{
+    public interface IRestRequesterConfiguration
+    {
+        Task<Uri> GetBaseUrl(string apiName);
+    }
+}
