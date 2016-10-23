21f161041b5a6748091f718fbd2f029e582c8e35
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-23 19:33:57 +0300

    rest endpoint and content attribute

---
+++ TIKSN.Core/Web/Rest/RestContentAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TIKSN.Web.Rest
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class RestContentAttribute : Attribute
+    {
+    }
+}
