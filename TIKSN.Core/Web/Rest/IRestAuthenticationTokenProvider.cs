577b9742c9a977fd159c203ac8d3071217ed7073
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-24 13:23:56 +0300

    Authentication token types

---
+++ TIKSN.Core/Web/Rest/IRestAuthenticationTokenProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace TIKSN.Web.Rest
+{
+    public interface IRestAuthenticationTokenProvider
+    {
+        Task<string> GetAuthenticationToken(string apiName);
+    }
+}
--- TIKSN.Core/Web/Rest/RestEndpointAttribute.cs
+++ TIKSN.Core/Web/Rest/RestEndpointAttribute.cs
@@ -5,15 +5,18 @@ namespace TIKSN.Web.Rest
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class RestEndpointAttribute : Attribute
     {
-        public RestEndpointAttribute(string apiName, RestVerb verb, string resourceTemplate, string mediaType = "application/json")
+        public RestEndpointAttribute(string apiName, RestVerb verb, string resourceTemplate, RestAuthenticationType authentication = RestAuthenticationType.None, string mediaType = "application/json")
         {
             ApiName = apiName;
             Verb = verb;
             ResourceTemplate = resourceTemplate;
             MediaType = mediaType;
+            Authentication = authentication;
         }
 
         public string ApiName { get; private set; }
+
+        public RestAuthenticationType Authentication { get; private set; }
 
         public string MediaType { get; private set; }
 
