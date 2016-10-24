fa1b8b79a8cdd64b30d864cc14e46e7e3177617d
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-24 13:24:44 +0300

    Authentication token support

--- TIKSN.Core/Web/Rest/RestRequester.cs
+++ TIKSN.Core/Web/Rest/RestRequester.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
 using System.Text;
 using System.Threading;
@@ -13,16 +14,22 @@ namespace TIKSN.Web.Rest
         private readonly IRestRequesterConfiguration _configuration;
         private readonly IDeserializerRestFactory _deserializerRestFactory;
         private readonly ISerializerRestFactory _serializerRestFactory;
+        private readonly IRestAuthenticationTokenProvider _restAuthenticationTokenProvider;
 
-        public RestRequester(IRestRequesterConfiguration configuration, ISerializerRestFactory serializerRestFactory, IDeserializerRestFactory deserializerRestFactory)
+        public RestRequester(IRestRequesterConfiguration configuration,
+            ISerializerRestFactory serializerRestFactory,
+            IDeserializerRestFactory deserializerRestFactory,
+            IRestAuthenticationTokenProvider restAuthenticationTokenProvider)
         {
             Contract.Requires<ArgumentNullException>(configuration != null);
             Contract.Requires<ArgumentNullException>(serializerRestFactory != null);
             Contract.Requires<ArgumentNullException>(deserializerRestFactory != null);
+            Contract.Requires<ArgumentNullException>(restAuthenticationTokenProvider != null);
 
             _configuration = configuration;
             _serializerRestFactory = serializerRestFactory;
             _deserializerRestFactory = deserializerRestFactory;
+            _restAuthenticationTokenProvider = restAuthenticationTokenProvider;
         }
 
         public async Task<TResult> Request<TResult, TRequest>(TRequest request, CancellationToken cancellationToken)
@@ -78,30 +85,42 @@ namespace TIKSN.Web.Rest
                 }
             }
 
-            var resourceParameters = await _configuration.GetResourceParameters(restEndpointAttribute.ApiName);
-            if (resourceParameters != null)
-            {
-                foreach (var resourceParameter in resourceParameters)
-                {
-                    resourceLocation = ReplaceParameterValueInLocation(resourceLocation, resourceParameter.Key, resourceParameter.Value);
-                }
-            }
+            resourceLocation = await SetResourceParameters(restEndpointAttribute, resourceLocation);
 
             var baseUrl = await _configuration.GetBaseUrl(restEndpointAttribute.ApiName);
             var requestUrl = new Uri(baseUrl, resourceLocation);
 
             var httpClient = new HttpClient();
-
-            var defaultHeaders = await _configuration.GetDefaultHeaders(restEndpointAttribute.ApiName);
-            if (defaultHeaders != null)
-            {
-                foreach (var defaultHeader in defaultHeaders)
-                {
-                    httpClient.DefaultRequestHeaders.Add(defaultHeader.Key, defaultHeader.Value);
-                }
-            }
+            await SetDefaultHeaders(restEndpointAttribute, httpClient);
+            await SetAuthenticationHeader(restEndpointAttribute, httpClient);
 
             return await MakeRequest<TResult>(httpClient, restEndpointAttribute.Verb, requestUrl, requestContent, requestContentMediaType, restEndpointAttribute.MediaType, cancellationToken);
+        }
+
+        private async Task SetAuthenticationHeader(RestEndpointAttribute restEndpointAttribute, HttpClient httpClient)
+        {
+            var authenticationSchema = string.Empty;
+
+            switch (restEndpointAttribute.Authentication)
+            {
+                case RestAuthenticationType.None:
+                    return;
+
+                case RestAuthenticationType.Basic:
+                    authenticationSchema = "Basic";
+                    break;
+
+                case RestAuthenticationType.Bearer:
+                    authenticationSchema = "Bearer";
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Authentication type '{restEndpointAttribute.Authentication.ToString()}' is not supported.");
+            }
+
+            var authenticationToken = await _restAuthenticationTokenProvider.GetAuthenticationToken(restEndpointAttribute.ApiName);
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authenticationSchema, authenticationToken);
         }
 
         private static string ReplaceParameterValueInLocation(string resourceLocation, string parameterName, string parameterValue)
@@ -158,6 +177,32 @@ namespace TIKSN.Web.Rest
             }
 
             return result;
+        }
+
+        private async Task SetDefaultHeaders(RestEndpointAttribute restEndpointAttribute, HttpClient httpClient)
+        {
+            var defaultHeaders = await _configuration.GetDefaultHeaders(restEndpointAttribute.ApiName);
+            if (defaultHeaders != null)
+            {
+                foreach (var defaultHeader in defaultHeaders)
+                {
+                    httpClient.DefaultRequestHeaders.Add(defaultHeader.Key, defaultHeader.Value);
+                }
+            }
+        }
+
+        private async Task<string> SetResourceParameters(RestEndpointAttribute restEndpointAttribute, string resourceLocation)
+        {
+            var resourceParameters = await _configuration.GetResourceParameters(restEndpointAttribute.ApiName);
+            if (resourceParameters != null)
+            {
+                foreach (var resourceParameter in resourceParameters)
+                {
+                    resourceLocation = ReplaceParameterValueInLocation(resourceLocation, resourceParameter.Key, resourceParameter.Value);
+                }
+            }
+
+            return resourceLocation;
         }
     }
 }
