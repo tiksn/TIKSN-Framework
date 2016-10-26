c07ffb9695615c798d3d1c89941c3162cbd16c04
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-10-26 17:14:20 +0300

    Audible telemeters and options model

---
+++ TIKSN.Core/Analytics/Telemetry/AudibleTelemeter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using TIKSN.Speech;
+
+namespace TIKSN.Analytics.Telemetry
+{
+    public class AudibleTelemeter : IEventTelemeter, IExceptionTelemeter, IMetricTelemeter, ITraceTelemeter
+    {
+        private readonly ITextToSpeechService _textToSpeechService;
+
+        public AudibleTelemeter(ITextToSpeechService textToSpeechService)
+        {
+            Contract.Requires<ArgumentNullException>(textToSpeechService != null);
+
+            _textToSpeechService = textToSpeechService;
+        }
+
+        public Task TrackEvent(string name)
+        {
+            return _textToSpeechService.SpeakAsync($"Event {name} occurred.");
+        }
+
+        public Task TrackException(Exception exception)
+        {
+            return _textToSpeechService.SpeakAsync(exception.Message);
+        }
+
+        public Task TrackException(Exception exception, TelemetrySeverityLevel severityLevel)
+        {
+            return _textToSpeechService.SpeakAsync($"{severityLevel}. {exception.Message}");
+        }
+
+        public Task TrackMetric(string metricName, decimal metricValue)
+        {
+            return _textToSpeechService.SpeakAsync($"Metric {metricName} is {metricValue}.");
+        }
+
+        public Task TrackTrace(string message)
+        {
+            return _textToSpeechService.SpeakAsync(message);
+        }
+
+        public Task TrackTrace(string message, TelemetrySeverityLevel severityLevel)
+        {
+            return _textToSpeechService.SpeakAsync($"{severityLevel} {message}");
+        }
+    }
+}
---
+++ TIKSN.Core/Speech/TextToSpeechOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace TIKSN.Speech
+{
+    public class TextToSpeechOptions
+    {
+    }
+}
