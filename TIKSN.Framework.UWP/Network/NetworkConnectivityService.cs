31b5d4bb2fca17ffe64a34219f3e4246b03ccae9
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-11-09 20:50:45 +0200

    closes #81

--- TIKSN.Framework.UWP/Network/NetworkConnectivityService.cs
+++ TIKSN.Framework.UWP/Network/NetworkConnectivityService.cs
@@ -4,25 +4,28 @@ using Windows.Networking.Connectivity;
 
 namespace TIKSN.Network
 {
-	public class NetworkConnectivityService : NetworkConnectivityServiceBase
-	{
-		public NetworkConnectivityService(IObserver<InternetConnectivityState> internetConnectivityStateObserver) : base(internetConnectivityStateObserver)
-		{
-			Observable.FromEvent<NetworkStatusChangedEventHandler, InternetConnectivityState>(
-				h => (e) => GetInternetConnectivityStateInternal(),
-				h => NetworkInformation.NetworkStatusChanged += h,
-				h => NetworkInformation.NetworkStatusChanged -= h)
-				.Subscribe(internetConnectivityStateObserver);
-		}
+    public class NetworkConnectivityService : NetworkConnectivityServiceBase
+    {
+        private readonly IObservable<InternetConnectivityState> internetConnectivityStateInternal;
 
-		protected override InternetConnectivityState GetInternetConnectivityStateInternal()
-		{
-			var connectionProfile = NetworkInformation.GetInternetConnectionProfile();
+        public NetworkConnectivityService() : base()
+        {
+            internetConnectivityStateInternal = Observable.FromEvent<NetworkStatusChangedEventHandler, InternetConnectivityState>(
+                h => (e) => GetInternetConnectivityStateInternal(),
+                h => NetworkInformation.NetworkStatusChanged += h,
+                h => NetworkInformation.NetworkStatusChanged -= h);
+        }
 
-			if (connectionProfile == null || connectionProfile.GetNetworkConnectivityLevel() != NetworkConnectivityLevel.InternetAccess)
-				return new InternetConnectivityState(false, false, false);
+        protected override IObservable<InternetConnectivityState> InternetConnectivityStateInternal { get { return internetConnectivityStateInternal; } }
 
-			return new InternetConnectivityState(true, connectionProfile.IsWlanConnectionProfile, connectionProfile.IsWwanConnectionProfile);
-		}
-	}
+        protected override InternetConnectivityState GetInternetConnectivityStateInternal()
+        {
+            var connectionProfile = NetworkInformation.GetInternetConnectionProfile();
+
+            if (connectionProfile == null || connectionProfile.GetNetworkConnectivityLevel() != NetworkConnectivityLevel.InternetAccess)
+                return new InternetConnectivityState(false, false, false);
+
+            return new InternetConnectivityState(true, connectionProfile.IsWlanConnectionProfile, connectionProfile.IsWwanConnectionProfile);
+        }
+    }
 }
