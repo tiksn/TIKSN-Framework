32d875111129e88b1cec609cc6e8f635263e3dad
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-11-16 00:10:15 +0200

    closes #83

---
+++ TIKSN.Framework.Full/PowerShell/PowerShellLoggerOptions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TIKSN.PowerShell
+{
+    public class PowerShellLoggerOptions
+    {
+        public LogLevel MinLevel { get; set; }
+
+        public bool IncludeScopes { get; set; }
+    }
+}
