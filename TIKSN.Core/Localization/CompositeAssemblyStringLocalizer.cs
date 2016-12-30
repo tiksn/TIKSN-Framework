12c2090fdb3bf166db6c8c2f26fcfe5739e7e70d
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-12-30 14:46:42 +0200

    CompositeStringLocalizer added

---
+++ TIKSN.Core/Localization/CompositeAssemblyStringLocalizer.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Resources;
+
+namespace TIKSN.Localization
+{
+    public abstract class CompositeAssemblyStringLocalizer : CompositeStringLocalizer
+    {
+        private readonly IResourceNamesCache resourceNamesCache;
+        private Lazy<IEnumerable<IStringLocalizer>> localizers;
+
+        protected CompositeAssemblyStringLocalizer(IResourceNamesCache resourceNamesCache)
+        {
+            this.resourceNamesCache = resourceNamesCache;
+            localizers = new Lazy<IEnumerable<IStringLocalizer>>(CreateLocalizers, false);
+        }
+
+        public override IEnumerable<IStringLocalizer> Localizers
+        {
+            get
+            {
+                return localizers.Value;
+            }
+        }
+
+        protected abstract IEnumerable<Assembly> GetAssemblies();
+
+        private IEnumerable<IStringLocalizer> CreateLocalizers()
+        {
+            var result = new List<IStringLocalizer>();
+
+            var assemblies = GetAssemblies();
+
+            foreach (var assembly in assemblies)
+            {
+                foreach (var manifestResourceName in assembly.GetManifestResourceNames())
+                {
+                    if (manifestResourceName.EndsWith(".resources", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var resourceName = manifestResourceName.Substring(0, manifestResourceName.Length - 10);
+                        var resourceManager = new ResourceManager(resourceName, assembly);
+                        result.Add(new ResourceManagerStringLocalizer(resourceManager, assembly, resourceName, resourceNamesCache));
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
