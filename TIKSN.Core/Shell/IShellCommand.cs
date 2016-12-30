5732cd06a51f67e462c61fd90bdf8e386b686cb6
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-12-30 22:42:05 +0200

    basic shell command types

---
+++ TIKSN.Core/Shell/IShellCommand.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace TIKSN.Shell
+{
+    public interface IShellCommand
+    {
+        Task ExecuteAsync();
+    }
+}
---
+++ TIKSN.Core/Shell/ShellCommandParameterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TIKSN.Shell
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class ShellCommandParameterAttribute : Attribute
+    {
+        public ShellCommandParameterAttribute(string parameterNameKey)
+        {
+            ParameterNameKey = new Guid(parameterNameKey);
+        }
+
+        public Guid ParameterNameKey { get; private set; }
+
+        public bool Mandatory { get; set; }
+
+        public int Position { get; set; }
+    }
+}
