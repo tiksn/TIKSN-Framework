b3daf8ffeb6773b833954a28b47a5c0b15712a87
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-12-30 21:48:42 +0200

    shared assembly info

--- TIKSN.Core/Properties/AssemblyInfo.cs
+++ TIKSN.Core/Properties/AssemblyInfo.cs
@@ -2,25 +2,9 @@
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following set of attributes.
-// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("TIKSN.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TIKSN Framework")]
-[assembly: AssemblyCopyright("Copyright © 2016, TIKSN Lab")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-// Major Version Minor Version Build Number Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
-// as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.32.0")]
-[assembly: AssemblyFileVersion("1.5.32.0")]
 [assembly: InternalsVisibleTo("TIKSN.Framework.Tests")]
 [assembly: InternalsVisibleTo("TIKSN.Core.Explorables")]
