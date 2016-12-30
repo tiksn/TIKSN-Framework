0893740c5cec145a2d78b39ca508e3ec4f090ff2
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-12-30 21:48:42 +0200

    shared assembly info

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("TIKSN Development")]
+[assembly: AssemblyProduct("TIKSN Framework")]
+[assembly: AssemblyCopyright("Copyright © 2016, TIKSN Lab")]
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyDescription("Flavor=Debug")] // a.k.a. “Comments”
+
+#else
+
+[assembly: AssemblyConfiguration("Retail")]
+
+[assembly: AssemblyDescription("Flavor=Retail")] // a.k.a. “Comments”
+
+#endif
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyInformationalVersion("1.6.0.0")] // a.k.a. “Product version”
+
