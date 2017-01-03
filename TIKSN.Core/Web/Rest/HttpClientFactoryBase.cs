a43dccead3f38996879089b51ad51c4d65349c73
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-01-03 23:49:23 +0200

    HttpClientFactoryBase

---
+++ TIKSN.Core/Web/Rest/HttpClientFactoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TIKSN.Web.Rest
+{
+	public abstract class HttpClientFactoryBase : IHttpClientFactory
+	{
+		public Task<HttpClient> Create(Guid apiKey)
+		{
+			throw new KeyNotFoundException($"API key {apiKey} was not found.");
+		}
+	}
+}
