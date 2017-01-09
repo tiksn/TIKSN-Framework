4d798278e98c335f19625918b373c331cbac049c
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-01-09 18:44:06 +0200

    Dependency Registration

---
+++ TIKSN.Core/DependencyRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TIKSN
+{
+	public static class DependencyRegistration
+	{
+		public static void Register(IServiceCollection services)
+		{
+
+		}
+	}
+}
