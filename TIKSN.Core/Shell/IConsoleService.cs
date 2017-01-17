2b92b508e263b3f5aa6ece8124584a34a7523abb
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-01-17 12:28:59 +0200

    nugets are updated console service is added

---
+++ TIKSN.Core/Shell/IConsoleService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TIKSN.Shell
+{
+    public interface IConsoleService
+    {
+        string ReadLine(string promptMessage, ConsoleColor promptForegroundColor);
+
+        int UserPrompt(string message, params string[] options);
+
+        void WriteError(string errorMessage);
+
+        void WriteObject<T>(T value);
+
+        void WriteObjects<T>(IEnumerable<T> values);
+    }
+}
