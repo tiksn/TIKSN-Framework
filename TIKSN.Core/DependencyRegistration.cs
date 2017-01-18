a514fb7c4d38e1417a85ec3cdd7c51f59d60ac55
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-01-18 10:44:54 +0200

    resource added

--- TIKSN.Core/DependencyRegistration.cs
+++ TIKSN.Core/DependencyRegistration.cs
@@ -1,23 +1,26 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Localization;
 using TIKSN.Serialization;
 using TIKSN.Web.Rest;
 
 namespace TIKSN
 {
-	public static class DependencyRegistration
-	{
-		public static void Register(IServiceCollection services)
-		{
+    public static class DependencyRegistration
+    {
+        public static void Register(IServiceCollection services)
+        {
             services.AddLocalization();
             services.AddLogging();
             services.AddOptions();
 
+            services.TryAddSingleton<IResourceNamesCache, ResourceNamesCache>();
+
             services.TryAddSingleton<IRestRequester, RestRequester>();
-			services.TryAddSingleton<DotNetXmlDeserializer, DotNetXmlDeserializer>();
-			services.TryAddSingleton<DotNetXmlSerializer, DotNetXmlSerializer>();
-			services.TryAddSingleton<JsonDeserializer, JsonDeserializer>();
-			services.TryAddSingleton<JsonSerializer, JsonSerializer>();
-		}
-	}
+            services.TryAddSingleton<DotNetXmlDeserializer, DotNetXmlDeserializer>();
+            services.TryAddSingleton<DotNetXmlSerializer, DotNetXmlSerializer>();
+            services.TryAddSingleton<JsonDeserializer, JsonDeserializer>();
+            services.TryAddSingleton<JsonSerializer, JsonSerializer>();
+        }
+    }
 }
