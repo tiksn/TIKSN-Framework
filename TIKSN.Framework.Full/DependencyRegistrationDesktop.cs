34cef815be156827b40fb8f7bb7fdc4099d6af08
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-01-22 02:16:49 +0200

    dependency registration is added

--- TIKSN.Framework.Full/DependencyRegistrationDesktop.cs
+++ TIKSN.Framework.Full/DependencyRegistrationDesktop.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using TIKSN.Network;
 using TIKSN.Shell;
 using TIKSN.Speech;
 
 namespace TIKSN
 {
-    public static class DependencyRegistrationDesktop
-    {
-        public static void Register(IServiceCollection services)
-        {
-            services.TryAddSingleton<IConsoleService, ConsoleService>();
-            services.TryAddSingleton<IShellCommandEngine, ShellCommandEngine>();
-            services.TryAddSingleton<ITextToSpeechService, TextToSpeechService>();
-        }
-    }
+	public static class DependencyRegistrationDesktop
+	{
+		public static void Register(IServiceCollection services)
+		{
+			services.TryAddSingleton<IConsoleService, ConsoleService>();
+			services.TryAddSingleton<INetworkConnectivityService, NetworkConnectivityService>();
+			services.TryAddSingleton<IShellCommandEngine, ShellCommandEngine>();
+			services.TryAddSingleton<ITextToSpeechService, TextToSpeechService>();
+		}
+	}
 }
