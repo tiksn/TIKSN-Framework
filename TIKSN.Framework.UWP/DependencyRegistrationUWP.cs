f555b6a7053478f4f1261725ece85f3767b596aa
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-01-22 02:12:32 +0200

    UWP dependency registration

---
+++ TIKSN.Framework.UWP/DependencyRegistrationUWP.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TIKSN.Network;
+using TIKSN.Settings;
+
+namespace TIKSN
+{
+	public static class DependencyRegistrationUWP
+	{
+		public static void Register(IServiceCollection services)
+		{
+			services.TryAddSingleton<ISettingsService, SettingsService>();
+			services.TryAddSingleton<INetworkConnectivityService, NetworkConnectivityService>();
+		}
+	}
+}
