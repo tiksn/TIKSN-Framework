792e1d4c067d3041876a7bc4c771ee0c7ddf197a
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-01-29 16:01:56 +0200

    PlatformCompositionRootSetupBase

---
+++ TIKSN.Framework.Platform/DependencyInjection/PlatformCompositionRootSetupBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TIKSN.DependencyInjection
+{
+	public abstract class PlatformCompositionRootSetupBase : CompositionRootSetupBase
+	{
+		protected override void ConfigureServices(IServiceCollection services)
+		{
+			PlatformDependencyRegistration.Register(services);
+		}
+	}
+}
