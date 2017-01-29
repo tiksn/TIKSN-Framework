4863a41569d4a8de04026e30af1116178828d0a5
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-01-29 15:48:19 +0200

    autofac composition methods

--- TIKSN.Core/DependencyInjection/AutofacCompositionRootSetupBase.cs
+++ TIKSN.Core/DependencyInjection/AutofacCompositionRootSetupBase.cs
@@ -1,6 +1,35 @@
-﻿namespace TIKSN.DependencyInjection
+﻿using Autofac;
+using Autofac.Core;
+using Autofac.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace TIKSN.DependencyInjection
 {
 	public abstract class AutofacCompositionRootSetupBase : CompositionRootSetupBase
 	{
+		protected override IServiceProvider CreateServiceProviderInternal()
+		{
+			var builder = new ContainerBuilder();
+			builder.Populate(services);
+
+			foreach (var module in GetAutofacModules())
+			{
+				builder.RegisterModule(module);
+			}
+
+			ConfigureContainerBuilder(builder);
+
+			var applicationContainer = builder.Build();
+
+			return new AutofacServiceProvider(applicationContainer);
+		}
+
+		protected abstract void ConfigureContainerBuilder(ContainerBuilder builder);
+
+		protected virtual IEnumerable<IModule> GetAutofacModules()
+		{
+			yield return new CoreModule();
+		}
 	}
 }
