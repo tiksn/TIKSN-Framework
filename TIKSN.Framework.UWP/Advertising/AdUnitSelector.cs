d56cc2f5255ea334770431a378811a9b6952e243
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-02-20 23:44:37 +0200

    selector base

---
+++ TIKSN.Framework.UWP/Advertising/AdUnitSelector.cs
@@ -0,0 +1,14 @@
+﻿using Template10.Utils;
+
+namespace TIKSN.Advertising
+{
+	public class AdUnitSelector : IAdUnitSelector
+	{
+		public AdUnit Select(AdUnitBundle adUnitBundle)
+		{
+			var deviceDispositions = DeviceUtils.Current().DeviceDisposition();
+
+			return null;
+		}
+	}
+}
