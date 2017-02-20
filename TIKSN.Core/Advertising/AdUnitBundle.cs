9f3b1c4cea61ab7e534833dd3fb4a1058a4a4dc6
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-02-20 23:05:21 +0200

    ad unit models added

---
+++ TIKSN.Core/Advertising/AdUnitBundle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TIKSN.Advertising
+{
+	public class AdUnitBundle
+	{
+		public AdUnitBundle(AdUnit designTime, AdUnit tablet, AdUnit mobile)
+		{
+			Tablet = tablet ?? mobile ?? designTime;
+			DesignTime = designTime;
+			Mobile = mobile ?? tablet ?? designTime;
+
+			if (designTime == null)
+				throw new ArgumentNullException(nameof(designTime));
+
+			if (!designTime.IsTest)
+				throw new ArgumentException($"Value of designTime.IsTest must be true.", nameof(designTime));
+
+			if (tablet == null && mobile == null)
+				throw new ArgumentException($"Arguments {nameof(tablet)} and {nameof(mobile)} cannot be null simultaneously.");
+		}
+
+		public AdUnit DesignTime { get; }
+
+		public AdUnit Mobile { get; }
+		public AdUnit Tablet { get; }
+	}
+}
