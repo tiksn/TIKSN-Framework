f7f24a080fee79896cd3269c379129fd6cf2547e
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-02-20 23:21:48 +0200

    factory base and selector base added

---
+++ TIKSN.Core/Advertising/AdUnitFactoryBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace TIKSN.Advertising
+{
+	public abstract class AdUnitFactoryBase : IAdUnitFactory
+	{
+		protected readonly Dictionary<string, AdUnitBundle> _adUnitBundles;
+		private readonly IAdUnitSelector _adUnitSelector;
+
+		protected AdUnitFactoryBase(IAdUnitSelector adUnitSelector)
+		{
+			_adUnitSelector = adUnitSelector;
+			_adUnitBundles = new Dictionary<string, AdUnitBundle>();
+
+			Register();
+		}
+
+		public AdUnit Create(string key)
+		{
+			var adUnitBundle = _adUnitBundles[key];
+
+			return _adUnitSelector.Select(adUnitBundle);
+		}
+
+		protected abstract void Register();
+	}
+}
