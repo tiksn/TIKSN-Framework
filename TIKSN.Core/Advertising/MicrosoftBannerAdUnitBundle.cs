505faa65376131e149d96ae2837ddebcb7dfa9e5
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-02-21 01:43:59 +0200

    design time ad unit fix

--- TIKSN.Core/Advertising/MicrosoftBannerAdUnitBundle.cs
+++ TIKSN.Core/Advertising/MicrosoftBannerAdUnitBundle.cs
@@ -3,7 +3,7 @@
 	public class MicrosoftBannerAdUnitBundle : AdUnitBundle
 	{
 		public static readonly AdUnit MicrosoftTestModeBannerAdUnit = new AdUnit("3f83fe91-d6be-434d-a0ae-7351c5a997f1",
-			"10865270");
+			"10865270", true);
 
 		public MicrosoftBannerAdUnitBundle(AdUnit tablet, AdUnit mobile) : base(MicrosoftTestModeBannerAdUnit, tablet, mobile)
 		{
--- TIKSN.Core/Advertising/MicrosoftInterstitialAdUnitBundle.cs
+++ TIKSN.Core/Advertising/MicrosoftInterstitialAdUnitBundle.cs
@@ -3,7 +3,7 @@
 	public class MicrosoftInterstitialAdUnitBundle : AdUnitBundle
 	{
 		public static readonly AdUnit MicrosoftTestModeInterstitialAdUnit = new AdUnit(
-			"d25517cb-12d4-4699-8bdc-52040c712cab", "11389925");
+			"d25517cb-12d4-4699-8bdc-52040c712cab", "11389925", true);
 
 		public MicrosoftInterstitialAdUnitBundle(AdUnit tablet, AdUnit mobile)
 			: base(MicrosoftTestModeInterstitialAdUnit, tablet, mobile)
