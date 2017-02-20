d70823d33f0db3f58c7db53bcf2482e7292ff694
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-02-20 23:05:21 +0200

    ad unit models added

---
+++ TIKSN.Core/Advertising/AdUnit.cs
@@ -0,0 +1,17 @@
+﻿namespace TIKSN.Advertising
+{
+	public class AdUnit
+	{
+		public AdUnit(string applicationId, string adUnitId, bool isTest = false)
+		{
+			ApplicationId = applicationId;
+			AdUnitId = adUnitId;
+			IsTest = isTest;
+		}
+
+		public string AdUnitId { get; }
+		public string ApplicationId { get; }
+
+		public bool IsTest { get; }
+	}
+}
