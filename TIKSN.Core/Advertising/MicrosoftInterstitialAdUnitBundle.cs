c9420bf06e3b2a1f7365de868f01af604b4df8bf
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-03-04 01:27:21 +0200

    ad provider added to ad unit.

--- TIKSN.Core/Advertising/MicrosoftInterstitialAdUnitBundle.cs
+++ TIKSN.Core/Advertising/MicrosoftInterstitialAdUnitBundle.cs
@@ -2,7 +2,7 @@
 {
 	public class MicrosoftInterstitialAdUnitBundle : AdUnitBundle
 	{
-		public static readonly AdUnit MicrosoftTestModeInterstitialAdUnit = new AdUnit(
+		public static readonly AdUnit MicrosoftTestModeInterstitialAdUnit = new AdUnit(AdProviders.Microsoft,
 			"d25517cb-12d4-4699-8bdc-52040c712cab", "11389925", true);
 
 		public MicrosoftInterstitialAdUnitBundle(AdUnit tablet, AdUnit mobile)
