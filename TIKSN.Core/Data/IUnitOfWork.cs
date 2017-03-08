276ed5e83556e3c720e829eaf682b45bf5a6c1a2
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-03-08 17:08:03 +0200

    Closes #104

--- TIKSN.Core/Data/IUnitOfWork.cs
+++ TIKSN.Core/Data/IUnitOfWork.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TIKSN.Data
 {
-	public interface IUnitOfWork : IDisposable
-	{
-		Task CompleteAsync();
-	}
+    public interface IUnitOfWork : IDisposable
+    {
+        Task CompleteAsync(CancellationToken cancellationToken = default(CancellationToken));
+    }
 }
