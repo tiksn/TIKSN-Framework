9f73baf57b08516d92409d323af503f643772f37
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-03-18 19:37:02 +0200

    updated microsoft ad bundles constractors

--- TIKSN.Core/Advertising/MicrosoftInterstitialAdUnitBundle.cs
+++ TIKSN.Core/Advertising/MicrosoftInterstitialAdUnitBundle.cs
@@ -1,12 +1,12 @@
 ﻿namespace TIKSN.Advertising
 {
-	public class MicrosoftInterstitialAdUnitBundle : AdUnitBundle
+	public class MicrosoftInterstitialAdUnitBundle : MicrosoftAdUnitBundle
 	{
 		public static readonly AdUnit MicrosoftTestModeInterstitialAdUnit = new AdUnit(AdProviders.Microsoft,
 			"d25517cb-12d4-4699-8bdc-52040c712cab", "11389925", true);
 
-		public MicrosoftInterstitialAdUnitBundle(AdUnit tablet, AdUnit mobile)
-			: base(MicrosoftTestModeInterstitialAdUnit, tablet, mobile)
+		public MicrosoftInterstitialAdUnitBundle(string tabletApplicationId, string tabletAdUnitId, string mobileApplicationId, string mobileAdUnitId)
+			: base(MicrosoftTestModeInterstitialAdUnit, tabletApplicationId, tabletAdUnitId, mobileApplicationId, mobileAdUnitId)
 		{
 		}
 	}
