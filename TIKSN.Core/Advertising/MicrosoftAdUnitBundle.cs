5acb43f591fb30f9ed81875ed7e947b77c4223e0
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-03-18 19:37:02 +0200

    updated microsoft ad bundles constractors

---
+++ TIKSN.Core/Advertising/MicrosoftAdUnitBundle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TIKSN.Advertising
+{
+	public abstract class MicrosoftAdUnitBundle : AdUnitBundle
+	{
+		public MicrosoftAdUnitBundle(AdUnit designTime, string tabletApplicationId, string tabletAdUnitId, string mobileApplicationId, string mobileAdUnitId)
+			: base(designTime, CreateAdUnit(tabletApplicationId, tabletAdUnitId), CreateAdUnit(mobileApplicationId, mobileAdUnitId))
+		{
+		}
+
+		private static AdUnit CreateAdUnit(string applicationId, string adUnitId)
+		{
+			if (applicationId == null && adUnitId == null)
+				return null;
+
+			if (applicationId != null && adUnitId != null)
+				return new AdUnit(AdProviders.Microsoft, applicationId, adUnitId);
+
+			throw new ArgumentException($"Values of {nameof(applicationId)} and {nameof(adUnitId)} must either be null or have value simultaneously.");
+		}
+	}
+}
--- TIKSN.Core/Advertising/MicrosoftBannerAdUnitBundle.cs
+++ TIKSN.Core/Advertising/MicrosoftBannerAdUnitBundle.cs
@@ -1,12 +1,13 @@
 ﻿namespace TIKSN.Advertising
 {
-	public class MicrosoftBannerAdUnitBundle : AdUnitBundle
+	public class MicrosoftBannerAdUnitBundle : MicrosoftAdUnitBundle
 	{
 		public static readonly AdUnit MicrosoftTestModeBannerAdUnit = new AdUnit(AdProviders.Microsoft,
 			"3f83fe91-d6be-434d-a0ae-7351c5a997f1",
 			"10865270", true);
 
-		public MicrosoftBannerAdUnitBundle(AdUnit tablet, AdUnit mobile) : base(MicrosoftTestModeBannerAdUnit, tablet, mobile)
+		public MicrosoftBannerAdUnitBundle(string tabletApplicationId, string tabletAdUnitId, string mobileApplicationId, string mobileAdUnitId)
+			: base(MicrosoftTestModeBannerAdUnit, tabletApplicationId, tabletAdUnitId, mobileApplicationId, mobileAdUnitId)
 		{
 		}
 	}
