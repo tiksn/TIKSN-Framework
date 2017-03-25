72e394368606591d18252c7e42a1b096f54d65a3
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-03-26 01:19:12 +0200

    added shell command sample

---
+++ Samples/Shell Commander/Shell Commander/TextsStringLocalizer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Collections.Generic;
+using System.Reflection;
+using TIKSN.DependencyInjection;
+using TIKSN.Localization;
+
+namespace Shell_Commander
+{
+	public class TextsStringLocalizer : CompositeAssemblyStringLocalizer
+	{
+		public TextsStringLocalizer(IResourceNamesCache resourceNamesCache) : base(resourceNamesCache)
+		{
+		}
+
+		protected override IEnumerable<Assembly> GetAssemblies()
+		{
+			yield return this.GetType().GetTypeInfo().Assembly;
+
+			yield return typeof(DependencyRegistration).GetTypeInfo().Assembly;
+		}
+	}
+}
