78c43071ba3da553dea3bd1dafed705ff3692c03
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-02 22:07:18 +0300

    azure storage repository implementation

---
+++ TIKSN.Core/Data/AzureStorage/IAzureTableStorageRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Data.AzureStorage
+{
+	public interface IAzureTableStorageRepository<T> where T : ITableEntity
+	{
+		Task AddAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task AddOrMergeAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task AddOrReplaceAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task DeleteAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task MergeAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task ReplaceAsync(T entity, CancellationToken cancellationToken = default(CancellationToken));
+	}
+}
