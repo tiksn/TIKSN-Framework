1703493bf5b79692cf842af0af51ba1a1be863e5
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-05 00:10:03 +0300

    azure table storage repository adapter is added

---
+++ TIKSN.Core/Data/BatchOperationHelper.cs
@@ -0,0 +1,22 @@
+﻿using LiteGuard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Data
+{
+	internal static class BatchOperationHelper
+	{
+		internal static Task BatchOperationAsync<T>(IEnumerable<T> entities, CancellationToken cancellationToken, Func<T, CancellationToken, Task> singleOperation)
+		{
+			Guard.AgainstNullArgument(nameof(entities), entities);
+			Guard.AgainstNullArgument(nameof(singleOperation), singleOperation);
+
+			var tasks = entities.Select(entity => singleOperation?.Invoke(entity, cancellationToken));
+
+			return Task.WhenAll(tasks);
+		}
+	}
+}
---
+++ TIKSN.Core/Data/IMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace TIKSN.Data
+{
+	public interface IMapper<TSource, TDestination>
+	{
+		TDestination Map(TSource source);
+	}
+}
