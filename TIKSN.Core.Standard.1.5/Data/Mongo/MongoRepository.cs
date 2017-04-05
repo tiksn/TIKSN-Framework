0aeb05f5eb86211fe9f75fdd10dc542fcf23fabe
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-06 00:56:21 +0300

    mongo repository constructor added

---
+++ TIKSN.Core.Standard.1.5/Data/Mongo/MongoRepository.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace TIKSN.Data.Mongo
+{
+	public class MongoRepository<T> : IMongoRepository<T>
+	{
+		private readonly IMongoCollection<T> collection;
+
+		public MongoRepository(IMongoDatabaseProvider mongoDatabaseProvider, string collectionName)
+		{
+			var database = mongoDatabaseProvider.GetDatabase();
+			collection = database.GetCollection<T>(collectionName);
+		}
+	}
+}
