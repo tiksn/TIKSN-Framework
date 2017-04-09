0fd51101e18b266190e37b96c4d2157bf21eaf1e
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-09 08:30:41 +0300

    finished mongo repository implementation

--- TIKSN.Core.Standard.1.5/Data/Mongo/IMongoRepository.cs
+++ TIKSN.Core.Standard.1.5/Data/Mongo/IMongoRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace TIKSN.Data.Mongo
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Data.Mongo
 {
-	public interface IMongoRepository<T> : IRepository<T>
+	public interface IMongoRepository<TDocument, TField> : IRepository<TDocument> where TDocument : IEntity<TField> where TField : IEquatable<TField>
 	{
+		Task<TDocument> GetAsync(TField id, CancellationToken cancellationToken = default(CancellationToken));
 	}
 }
