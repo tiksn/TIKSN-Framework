cbcb6603447c829de9632421504837b1f62293be
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-21 00:16:20 +0300

    sample app created

---
+++ Samples/Rest Service and Repository/Console Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
