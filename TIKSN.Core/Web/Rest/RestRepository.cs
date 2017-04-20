ffcde65b23453caa4c2524cafe5adef46af97544
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-21 00:11:59 +0300

    Rest repository created

---
+++ TIKSN.Core/Web/Rest/RestRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Data;
+
+namespace TIKSN.Web.Rest
+{
+	public class RestRepository<T> : IRepository<T>
+    {
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public RestRepository(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+        public Task AddAsync(T entity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
