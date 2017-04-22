b00b324f089a1f5925f2ca159a0f7635508b6005
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-22 03:24:58 +0300

    Models and context is added

---
+++ Samples/Rest Service and Repository/Web Service/Data/Entities/CultureEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Web_Service.Data.Entities
+{
+	public partial class CultureEntity
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int? ParentId { get; set; }
+        public int Lcid { get; set; }
+        public string EnglishName { get; set; }
+        public string NativeName { get; set; }
+        public int? RegionId { get; set; }
+
+        public virtual CultureEntity Parent { get; set; }
+        public virtual ICollection<CultureEntity> InverseParent { get; set; }
+        public virtual RegionEntity Region { get; set; }
+    }
+}
---
+++ Samples/Rest Service and Repository/Web Service/Data/Entities/CurrencyEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Web_Service.Data.Entities
+{
+	public partial class CurrencyEntity
+    {
+        public CurrencyEntity()
+        {
+            Regions = new HashSet<RegionEntity>();
+        }
+
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int Number { get; set; }
+        public string CurrencySymbol { get; set; }
+        public bool IsCurrent { get; set; }
+        public string IsFund { get; set; }
+
+        public virtual ICollection<RegionEntity> Regions { get; set; }
+    }
+}
