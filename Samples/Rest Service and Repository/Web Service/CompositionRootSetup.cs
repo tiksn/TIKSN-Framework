810de4498fdbbe77888424ff0bcf3fe5e0f3527b
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-22 05:02:36 +0300

    framework and configuration is updated

--- Samples/Rest Service and Repository/Web Service/CompositionRootSetup.cs
+++ Samples/Rest Service and Repository/Web Service/CompositionRootSetup.cs
@@ -6,6 +6,7 @@ using TIKSN.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Web_Service.Data.Entities;
 using LiteGuard;
+using Microsoft.Extensions.Logging;
 
 namespace Web_Service
 {
@@ -23,12 +24,23 @@ namespace Web_Service
 		{
 		}
 
+		protected override void ConfigureLogging(ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(true);
+			loggerFactory.AddDebug(LogLevel.Trace);
+
+			base.ConfigureLogging(loggerFactory);
+		}
+
 		protected override void ConfigureOptions(IServiceCollection services, IConfigurationRoot configuration)
 		{
 		}
 
 		protected override void ConfigureServices(IServiceCollection services)
 		{
+			services.AddSingleton(_env);
+			services.AddMvcCore();
+			services.AddEntityFrameworkSqlServer();
 			services.AddDbContext<InternationalizationContext>(options =>
 				options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Internationalization;Trusted_Connection=True;"));
 		}
