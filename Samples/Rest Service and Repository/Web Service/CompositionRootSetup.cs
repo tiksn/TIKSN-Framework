097b3d37a4e8f0289e76860db8a1bed9ee540ebd
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-22 03:25:28 +0300

    project configuration

---
+++ Samples/Rest Service and Repository/Web Service/CompositionRootSetup.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TIKSN.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Web_Service.Data.Entities;
+using LiteGuard;
+
+namespace Web_Service
+{
+	public class CompositionRootSetup : AutofacCompositionRootSetupBase
+	{
+		private readonly IHostingEnvironment _env;
+
+		public CompositionRootSetup(IHostingEnvironment env)
+		{
+			Guard.AgainstNullArgument(nameof(env), env);
+			_env = env;
+		}
+
+		protected override void ConfigureContainerBuilder(ContainerBuilder builder)
+		{
+		}
+
+		protected override void ConfigureOptions(IServiceCollection services, IConfigurationRoot configuration)
+		{
+		}
+
+		protected override void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<InternationalizationContext>(options =>
+				options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Internationalization;Trusted_Connection=True;"));
+		}
+
+		protected override void SetupConfiguration(IConfigurationBuilder builder)
+		{
+			builder
+				.SetBasePath(_env.ContentRootPath)
+				.AddYamlFile("appsettings.yaml")
+				.AddYamlFile($"appsettings.{_env.EnvironmentName}.yaml", optional: true, reloadOnChange: true)
+				.AddEnvironmentVariables();
+			base.SetupConfiguration(builder);
+		}
+	}
+}
--- Samples/Rest Service and Repository/Web Service/Startup.cs
+++ Samples/Rest Service and Repository/Web Service/Startup.cs
@@ -1,43 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Web_Service
 {
-    public class Startup
-    {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
+	public class Startup
+	{
+		private IHostingEnvironment _env;
+		public Startup(IHostingEnvironment env)
+		{
+			_env = env;
+		}
 
-        public IConfigurationRoot Configuration { get; }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public IServiceProvider ConfigureServices(IServiceCollection services)
+		{
+			var compositionRootSetup = new CompositionRootSetup(_env);
+			var serviceProvider = compositionRootSetup.CreateServiceProvider();
+			return serviceProvider;
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            app.UseMvc();
-        }
-    }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			//app.UseMvc();
+		}
+	}
 }
