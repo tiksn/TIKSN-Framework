12ee3fec41cfd666af55bf0d8b34b89efc6330ef
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-23 03:11:52 +0300

    submission models are added

---
+++ Samples/Rest Service and Repository/Common Models/CultureSubmissionModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Common_Models
+{
+	public class CultureSubmissionModel
+    {
+		public string Code { get; set; }
+		public int? ParentId { get; set; }
+		public int Lcid { get; set; }
+		public string EnglishName { get; set; }
+		public string NativeName { get; set; }
+		public int? RegionId { get; set; }
+	}
+}
