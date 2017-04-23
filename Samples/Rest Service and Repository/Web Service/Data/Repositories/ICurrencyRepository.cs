2f25af5fb78acae31dd1499ca208edae7b46c6fd
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-23 05:12:48 +0300

    migration added

--- Samples/Rest Service and Repository/Web Service/Data/Repositories/ICurrencyRepository.cs
+++ Samples/Rest Service and Repository/Web Service/Data/Repositories/ICurrencyRepository.cs
@@ -1,9 +1,15 @@
-﻿using TIKSN.Data;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Data;
 using Web_Service.Data.Entities;
 
 namespace Web_Service.Data.Repositories
 {
 	public interface ICurrencyRepository : IRepository<CurrencyEntity>
-    {
-    }
+	{
+		Task<CurrencyEntity> GetAsync(int ID, CancellationToken cancellationToken = default(CancellationToken));
+
+		Task<IEnumerable<CurrencyEntity>> GetAllAsync(CancellationToken cancellationToken = default(CancellationToken));
+	}
 }
