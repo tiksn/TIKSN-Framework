0ae8bca22c943a452c2cc7fccabfcb9d41669724
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-23 04:36:31 +0300

    unit of work facrory is added

---
+++ TIKSN.Core/Data/UnitOfWorkFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace TIKSN.Data
+{
+	public class UnitOfWorkFactory<TContext> : IUnitOfWorkFactory where TContext : DbContext
+	{
+		private readonly IServiceProvider serviceProvider;
+
+		public UnitOfWorkFactory(IServiceProvider serviceProvider)
+		{
+			this.serviceProvider = serviceProvider;
+		}
+
+		public IUnitOfWork Create()
+		{
+			var dbContext = serviceProvider.GetRequiredService<TContext>();
+
+			return new EntityUnitOfWork(dbContext);
+		}
+	}
+}
