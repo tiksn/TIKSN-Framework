6ee762c0995f5ff0311df692e3898edb0b9ada28
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-23 03:03:10 +0300

    rest models added

---
+++ Samples/Rest Service and Repository/Common Models/CultureModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Common_Models
+{
+	public class CultureModel
+    {
+		public int Id { get; set; }
+		public string Code { get; set; }
+		public int? ParentId { get; set; }
+		public int Lcid { get; set; }
+		public string EnglishName { get; set; }
+		public string NativeName { get; set; }
+		public int? RegionId { get; set; }
+	}
+}
