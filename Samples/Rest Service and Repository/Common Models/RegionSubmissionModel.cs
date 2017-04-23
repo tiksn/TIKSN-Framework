28e614a5b0b2d94f8697418af32722680176d188
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-04-23 03:11:52 +0300

    submission models are added

---
+++ Samples/Rest Service and Repository/Common Models/RegionSubmissionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Common_Models
+{
+	public class RegionSubmissionModel
+    {
+		public string Code { get; set; }
+		public bool GeoId { get; set; }
+		public int CurrencyId { get; set; }
+		public string EnglishName { get; set; }
+		public string NativeName { get; set; }
+	}
+}
