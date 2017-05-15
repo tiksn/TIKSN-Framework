2505684d98457b87997d67d924f47ba15ab62f6c
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-15 19:33:43 +0300

    moved tests to UnitTests project

--- UnitTests/Advertising/MicrosoftInterstitialAdUnitBundleTests.cs
+++ UnitTests/Advertising/MicrosoftInterstitialAdUnitBundleTests.cs
@@ -1,20 +1,19 @@
 ﻿using FluentAssertions;
-using TIKSN.Advertising;
 using Xunit;
 
-namespace TIKSN.Framework.Tests.Advertising
+namespace TIKSN.Advertising.Tests
 {
-	public class MicrosoftBannerAdUnitBundleTests
+	public class MicrosoftInterstitialAdUnitBundleTests
 	{
 		[Fact]
-		public void CreateMicrosoftBannerAdUnitBundle()
+		public void CreateMicrosoftInterstitialAdUnitBundle()
 		{
 			string tabletApplicationId = "b008491d-9b9b-4b78-9d5c-28e08b573268";
 			string tabletAdUnitId = "d34e3fdd-1840-455d-abab-f8698b5f3318";
 			string mobileApplicationId = "6d0d7c55-0f1a-4208-a7b9-a6943c84488a";
 			string mobileAdUnitId = "b7c073c3-5e40-47ae-b02b-eb2fa3f90036";
 
-			var bundle = new MicrosoftBannerAdUnitBundle(tabletApplicationId, tabletAdUnitId, mobileApplicationId, mobileAdUnitId);
+			var bundle = new MicrosoftInterstitialAdUnitBundle(tabletApplicationId, tabletAdUnitId, mobileApplicationId, mobileAdUnitId);
 
 			bundle.Tablet.ApplicationId.Should().Be(tabletApplicationId);
 			bundle.Tablet.AdUnitId.Should().Be(tabletAdUnitId);
