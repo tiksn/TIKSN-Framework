9a98bb01c31bf3fefd4969710d7fed1f7a5beaed
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-19 14:07:02 +0300

    controllers completed

--- Samples/Rest Service and Repository/Web Service/Controllers/CurrenciesController.cs
+++ Samples/Rest Service and Repository/Web Service/Controllers/CurrenciesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Web_Service.Data.Repositories;
@@ -9,58 +10,89 @@ using TIKSN.Data;
 
 namespace Web_Service.Controllers
 {
-	[Route("api/[controller]")]
-	public class CurrenciesController : Controller
-	{
-		private readonly ICurrencyRepository currencyRepository;
-		private readonly IUnitOfWorkFactory unitOfWorkFactory;
-		private readonly IMapper mapper;
-		public CurrenciesController(ICurrencyRepository currencyRepository, IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
-		{
-			this.currencyRepository = currencyRepository;
-			this.unitOfWorkFactory = unitOfWorkFactory;
-			this.mapper = mapper;
-		}
+    [Route("api/[controller]")]
+    public class CurrenciesController : Controller
+    {
+        private readonly ICurrencyRepository currencyRepository;
+        private readonly IUnitOfWorkFactory unitOfWorkFactory;
+        private readonly IMapper mapper;
 
-		[HttpGet]
-		public async Task<IEnumerable<CurrencyModel>> Get()
-		{
-			var entities = await currencyRepository.GetAllAsync();
+        public CurrenciesController(ICurrencyRepository currencyRepository, IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+        {
+            this.currencyRepository = currencyRepository;
+            this.unitOfWorkFactory = unitOfWorkFactory;
+            this.mapper = mapper;
+        }
 
-			return mapper.Map<IEnumerable<CurrencyModel>>(entities);
-		}
+        [HttpGet]
+        public async Task<IEnumerable<CurrencyModel>> Get()
+        {
+            var entities = await currencyRepository.GetAllAsync();
 
-		// GET api/values/5
-		[HttpGet("{id}")]
-		public string Get(int id)
-		{
-			return "value";
-		}
+            return mapper.Map<IEnumerable<CurrencyModel>>(entities);
+        }
 
-		// POST api/values
-		[HttpPost]
-		public async Task Post([FromBody]CurrencyModel model)
-		{
-			using (var unitOfWork = unitOfWorkFactory.Create())
-			{
-				var entity = mapper.Map<CurrencyEntity>(model);
+        [HttpGet("{id}")]
+        public async Task<CurrencyModel> Get(int id)
+        {
+            if (id != 0)
+                throw new ArgumentException();
 
-				await currencyRepository.AddAsync(entity);
+            var entity = await currencyRepository.GetAsync(id);
 
-				await unitOfWork.CompleteAsync();
-			}
-		}
+            return mapper.Map<CurrencyModel>(entity);
+        }
 
-		// PUT api/values/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody]string value)
-		{
-		}
+        [HttpPost]
+        public async Task Post([FromBody]CurrencyModel model)
+        {
+            if (model.Id != 0)
+                throw new ArgumentException();
 
-		// DELETE api/values/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
-		}
-	}
+            using (var unitOfWork = unitOfWorkFactory.Create())
+            {
+                var entity = mapper.Map<CurrencyEntity>(model);
+
+                await currencyRepository.AddAsync(entity);
+
+                await unitOfWork.CompleteAsync();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody]CurrencyModel model)
+        {
+            if (id != 0)
+                throw new ArgumentException();
+
+            if (model.Id != 0 && model.Id != id)
+                throw new ArgumentException();
+
+            using (var unitOfWork = unitOfWorkFactory.Create())
+            {
+                var entity = mapper.Map<CurrencyEntity>(model);
+                entity.Id = id;
+
+                await currencyRepository.UpdateAsync(entity);
+
+                await unitOfWork.CompleteAsync();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            if (id != 0)
+                throw new ArgumentException();
+
+            using (var unitOfWork = unitOfWorkFactory.Create())
+            {
+                var entity = await currencyRepository.GetAsync(id);
+
+                await currencyRepository.RemoveAsync(entity);
+
+                await unitOfWork.CompleteAsync();
+            }
+        }
+    }
 }
--- Samples/Rest Service and Repository/Web Service/Controllers/RegionsController.cs
+++ Samples/Rest Service and Repository/Web Service/Controllers/RegionsController.cs
@@ -1,53 +1,97 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
+using Common_Models;
 using Microsoft.AspNetCore.Mvc;
 using Web_Service.Data.Repositories;
 using TIKSN.Data;
+using Web_Service.Data.Entities;
 
 namespace Web_Service.Controllers
 {
-	[Route("api/[controller]")]
-	public class RegionsController : Controller
-	{
-		private readonly IRegionRepository regionRepository;
-		private readonly IUnitOfWorkFactory unitOfWorkFactory;
+    [Route("api/[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IUnitOfWorkFactory unitOfWorkFactory;
+        private readonly IMapper _mapper;
 
-		public RegionsController(IRegionRepository regionRepository, IUnitOfWorkFactory unitOfWorkFactory)
-		{
-			this.regionRepository = regionRepository;
-			this.unitOfWorkFactory = unitOfWorkFactory;
-		}
+        public RegionsController(IRegionRepository regionRepository, IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.unitOfWorkFactory = unitOfWorkFactory;
+            _mapper = mapper;
+        }
 
-		// GET api/values
-		[HttpGet]
-		public IEnumerable<string> Get()
-		{
-			return new string[] { "value1", "value2" };
-		}
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
 
-		// GET api/values/5
-		[HttpGet("{id}")]
-		public string Get(int id)
-		{
-			return "value";
-		}
+        [HttpGet("{id}")]
+        public async Task<RegionModel> Get(int id)
+        {
+            if (id != 0)
+                throw new ArgumentException();
 
-		// POST api/values
-		[HttpPost]
-		public async Task Post([FromBody]string value)
-		{
-		}
+            var entity = await regionRepository.GetAsync(id);
 
-		// PUT api/values/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody]string value)
-		{
-		}
+            return _mapper.Map<RegionModel>(entity);
+        }
 
-		// DELETE api/values/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
-		}
-	}
+        [HttpPost]
+        public async Task Post([FromBody]RegionModel model)
+        {
+            if (model.Id != 0)
+                throw new ArgumentException();
+
+            using (var unitOfWork = unitOfWorkFactory.Create())
+            {
+                var entity = _mapper.Map<RegionEntity>(model);
+
+                await regionRepository.AddAsync(entity);
+
+                await unitOfWork.CompleteAsync();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody]RegionModel model)
+        {
+            if (id != 0)
+                throw new ArgumentException();
+
+            if (model.Id != 0 && model.Id != id)
+                throw new ArgumentException();
+
+            using (var unitOfWork = unitOfWorkFactory.Create())
+            {
+                var entity = _mapper.Map<RegionEntity>(model);
+                entity.Id = id;
+
+                await regionRepository.UpdateAsync(entity);
+
+                await unitOfWork.CompleteAsync();
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            if (id != 0)
+                throw new ArgumentException();
+
+            using (var unitOfWork = unitOfWorkFactory.Create())
+            {
+                var entity = await regionRepository.GetAsync(id);
+
+                await regionRepository.RemoveAsync(entity);
+
+                await unitOfWork.CompleteAsync();
+            }
+        }
+    }
 }
