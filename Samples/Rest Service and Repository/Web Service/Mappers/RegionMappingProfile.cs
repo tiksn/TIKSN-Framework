44ca1517b8d1b7a1c34ef630d1992bc4d8992356
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-19 13:55:28 +0300

    region profile added

---
+++ Samples/Rest Service and Repository/Web Service/Mappers/RegionMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Common_Models;
+using Web_Service.Data.Entities;
+
+namespace Web_Service.Mappers
+{
+    public class RegionMappingProfile : Profile
+    {
+        public RegionMappingProfile()
+        {
+            CreateMap<RegionEntity, RegionModel>();
+            CreateMap<RegionModel, RegionEntity>();
+        }
+    }
+}
