cc91b35f8b52ea112cd50517866b8474285664c3
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-19 12:16:00 +0300

    repository interfaces updated

--- Samples/Rest Service and Repository/Web Service/Data/Repositories/ICultureRepository.cs
+++ Samples/Rest Service and Repository/Web Service/Data/Repositories/ICultureRepository.cs
@@ -1,9 +1,15 @@
-﻿using TIKSN.Data;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Data;
 using Web_Service.Data.Entities;
 
 namespace Web_Service.Data.Repositories
 {
-	public interface ICultureRepository : IRepository<CultureEntity>
+    public interface ICultureRepository : IRepository<CultureEntity>
     {
+        Task<CultureEntity> GetAsync(int ID, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<IEnumerable<CultureEntity>> GetAllAsync(CancellationToken cancellationToken = default(CancellationToken));
     }
 }
