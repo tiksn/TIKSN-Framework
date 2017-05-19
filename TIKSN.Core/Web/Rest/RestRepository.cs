7c615eacb66561b403efd03bdcde8b60147fb8c1
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-20 02:14:58 +0300

    rest repository partial implementation

--- TIKSN.Core/Web/Rest/RestRepository.cs
+++ TIKSN.Core/Web/Rest/RestRepository.cs
@@ -1,48 +1,108 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using TIKSN.Data;
 
 namespace TIKSN.Web.Rest
 {
-	public class RestRepository<T> : IRepository<T>
-    {
+	public class RestRepository<T> : IRepository<T>, IRestRepository<T>
+	{
+		private readonly IRestAuthenticationTokenProvider _restAuthenticationTokenProvider;
+		private readonly IOptions<RestRepositoryOptions<T>> _options;
+		private readonly ISerializerRestFactory _serializerRestFactory;
 		private readonly IHttpClientFactory _httpClientFactory;
 
-		public RestRepository(IHttpClientFactory httpClientFactory)
+		public RestRepository(
+			IHttpClientFactory httpClientFactory,
+			ISerializerRestFactory serializerRestFactory,
+			IRestAuthenticationTokenProvider restAuthenticationTokenProvider,
+			IOptions<RestRepositoryOptions<T>> options)
 		{
 			_httpClientFactory = httpClientFactory;
+			_serializerRestFactory = serializerRestFactory;
+			_options = options;
+			_restAuthenticationTokenProvider = restAuthenticationTokenProvider;
 		}
 
-        public Task AddAsync(T entity, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+		public Task AddAsync(T entity, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+		public Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task RemoveAsync(T entity, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+		public async Task<T> GetAsync(string id, CancellationToken cancellationToken)
+		{
+			var httpClient = await GetHttpClientAsync();
+			throw new NotImplementedException();
+		}
 
-        public Task RemoveRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+		public Task RemoveAsync(T entity, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task UpdateAsync(T entity, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+		public Task RemoveRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
 
-        public Task UpdateRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public Task UpdateAsync(T entity, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		private async Task<HttpClient> GetHttpClientAsync()
+		{
+			var httpClient = await _httpClientFactory.Create(_options.Value.ApiKey);
+
+			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_options.Value.MediaType));
+
+			if (_options.Value.AcceptLanguages != null)
+				foreach (var acceptLanguage in _options.Value.AcceptLanguages)
+					httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(acceptLanguage.Value, acceptLanguage.Key));
+
+			await SetAuthenticationHeader(httpClient);
+
+			return httpClient;
+		}
+
+		private async Task SetAuthenticationHeader(HttpClient httpClient)
+		{
+			var authenticationSchema = string.Empty;
+
+			switch (_options.Value.Authentication)
+			{
+				case RestAuthenticationType.None:
+					return;
+
+				case RestAuthenticationType.Basic:
+					authenticationSchema = "Basic";
+					break;
+
+				case RestAuthenticationType.Bearer:
+					authenticationSchema = "Bearer";
+					break;
+
+				default:
+					throw new NotSupportedException($"Authentication type '{_options.Value.Authentication.ToString()}' is not supported.");
+			}
+
+			var authenticationToken = await _restAuthenticationTokenProvider.GetAuthenticationToken(_options.Value.ApiKey);
+
+			httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authenticationSchema, authenticationToken);
+		}
+	}
 }
---
+++ TIKSN.Core/Web/Rest/RestRepositoryOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TIKSN.Web.Rest
+{
+	public class RestRepositoryOptions<T>
+	{
+		public RestRepositoryOptions()
+		{
+			MediaType = "application/json";
+		}
+
+		public Guid ApiKey { get; set; }
+
+		public string MediaType { get; set; }
+
+		public RestAuthenticationType Authentication { get; set; }
+
+		public Dictionary<double, string> AcceptLanguages { get; set; }
+	}
+}
