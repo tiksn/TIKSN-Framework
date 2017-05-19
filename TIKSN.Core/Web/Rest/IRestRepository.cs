88e293e48eea678216454fca140582e289ca8157
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-20 02:14:58 +0300

    rest repository partial implementation

---
+++ TIKSN.Core/Web/Rest/IRestRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Web.Rest
+{
+	public interface IRestRepository<T>
+	{
+		Task<T> GetAsync(string id, CancellationToken cancellationToken = default(CancellationToken));
+	}
+}
