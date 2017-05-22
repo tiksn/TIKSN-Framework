cc660d65fe3aa4f258edfc7fa86314b9ca6a32cf
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-23 01:46:16 +0300

    rest repository implementation

--- TIKSN.Core/Web/Rest/RestRepository.cs
+++ TIKSN.Core/Web/Rest/RestRepository.cs
@@ -9,59 +9,104 @@ using TIKSN.Data;
 
 namespace TIKSN.Web.Rest
 {
-	public class RestRepository<T> : IRepository<T>, IRestRepository<T>
+	public class RestRepository<TEntity, TIdentity> : IRestRepository<TEntity, TIdentity> where TEntity : IEntity<TIdentity> where TIdentity : IEquatable<TIdentity>
 	{
-		private readonly IRestAuthenticationTokenProvider _restAuthenticationTokenProvider;
-		private readonly IOptions<RestRepositoryOptions<T>> _options;
-		private readonly ISerializerRestFactory _serializerRestFactory;
+		private readonly IDeserializerRestFactory _deserializerRestFactory;
 		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly IOptions<RestRepositoryOptions<TEntity>> _options;
+		private readonly IRestAuthenticationTokenProvider _restAuthenticationTokenProvider;
+		private readonly ISerializerRestFactory _serializerRestFactory;
 
 		public RestRepository(
 			IHttpClientFactory httpClientFactory,
 			ISerializerRestFactory serializerRestFactory,
+			IDeserializerRestFactory deserializerRestFactory,
 			IRestAuthenticationTokenProvider restAuthenticationTokenProvider,
-			IOptions<RestRepositoryOptions<T>> options)
+			IOptions<RestRepositoryOptions<TEntity>> options)
 		{
 			_httpClientFactory = httpClientFactory;
 			_serializerRestFactory = serializerRestFactory;
 			_options = options;
 			_restAuthenticationTokenProvider = restAuthenticationTokenProvider;
+			_deserializerRestFactory = deserializerRestFactory;
 		}
 
-		public Task AddAsync(T entity, CancellationToken cancellationToken)
+		public Task AddAsync(TEntity entity, CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			return AddObjectAsync(entity, cancellationToken);
 		}
 
-		public Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+		public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			return AddObjectAsync(entities, cancellationToken);
 		}
 
-		public async Task<T> GetAsync(string id, CancellationToken cancellationToken)
+		public async Task<TEntity> GetAsync(string id, CancellationToken cancellationToken)
 		{
 			var httpClient = await GetHttpClientAsync();
-			throw new NotImplementedException();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
+
+			uriTemplate.Fill("ID", id);
+
+			var requestUrl = uriTemplate.Compose();
+
+			var response = await httpClient.GetAsync(requestUrl, cancellationToken);
+
+			return await ObjectifyResponse<TEntity>(response);
 		}
 
-		public Task RemoveAsync(T entity, CancellationToken cancellationToken)
+		public async Task RemoveAsync(TEntity entity, CancellationToken cancellationToken)
+		{
+			var httpClient = await GetHttpClientAsync();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
+
+			uriTemplate.Fill("ID", entity.ID.ToString());
+
+			var requestUrl = uriTemplate.Compose();
+
+			var response = await httpClient.GetAsync(requestUrl, cancellationToken);
+
+			response.EnsureSuccessStatusCode();
+		}
+
+		public Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task RemoveRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+		public async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+		{
+			var httpClient = await GetHttpClientAsync();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
+
+			uriTemplate.Fill("ID", entity.ID.ToString());
+
+			var requestUrl = uriTemplate.Compose();
+
+			var response = await httpClient.PutAsync(requestUrl, GetContent(entity), cancellationToken);
+
+			response.EnsureSuccessStatusCode();
+		}
+
+		public Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task UpdateAsync(T entity, CancellationToken cancellationToken)
+		private async Task AddObjectAsync(object requestContent, CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			var httpClient = await GetHttpClientAsync();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
+			var requestUrl = uriTemplate.Compose();
+
+			var response = await httpClient.PostAsync(requestUrl, GetContent(requestContent), cancellationToken);
+
+			response.EnsureSuccessStatusCode();
 		}
 
-		public Task UpdateRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+		private HttpContent GetContent(object requestContent)
 		{
-			throw new NotImplementedException();
+			return new StringContent(_serializerRestFactory.Create(_options.Value.MediaType).Serialize(requestContent), _options.Value.Encoding, _options.Value.MediaType);
 		}
 
 		private async Task<HttpClient> GetHttpClientAsync()
@@ -77,6 +122,15 @@ namespace TIKSN.Web.Rest
 			await SetAuthenticationHeader(httpClient);
 
 			return httpClient;
+		}
+
+		private async Task<TResult> ObjectifyResponse<TResult>(HttpResponseMessage response)
+		{
+			response.EnsureSuccessStatusCode();
+
+			var content = await response.Content.ReadAsStringAsync();
+
+			return _deserializerRestFactory.Create(_options.Value.MediaType).Deserialize<TResult>(content);
 		}
 
 		private async Task SetAuthenticationHeader(HttpClient httpClient)
