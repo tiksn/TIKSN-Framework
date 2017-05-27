450302f260f1c60177f6e039443164df5d69036b
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-27 14:11:36 +0300

    request to create 2 cultures

---
+++ Samples/Rest Service and Repository/Console Client/Rest/RestAuthenticationTokenProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using TIKSN.Web.Rest;
+
+namespace Console_Client.Rest
+{
+	public class RestAuthenticationTokenProvider : IRestAuthenticationTokenProvider
+	{
+		public Task<string> GetAuthenticationToken(Guid apiKey)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
