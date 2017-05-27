e83ae50de704e7214a1cca9622242d494f6044c6
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-27 14:11:36 +0300

    request to create 2 cultures

--- Samples/Rest Service and Repository/Console Client/Rest/HttpClientFactory.cs
+++ Samples/Rest Service and Repository/Console Client/Rest/HttpClientFactory.cs
@@ -1,16 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Common_Models;
+using Microsoft.Extensions.Options;
+using System;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using TIKSN.Web;
 using TIKSN.Web.Rest;
 
 namespace Console_Client.Rest
 {
-    public class HttpClientFactory : HttpClientFactoryBase
+	public class HttpClientFactory : HttpClientFactoryBase
 	{
+		private readonly IOptions<WebApiOptions<CultureModel>> cultureOptions;
+
+		public HttpClientFactory(IOptions<WebApiOptions<CultureModel>> cultureOptions)
+		{
+			this.cultureOptions = cultureOptions;
+		}
+
 		public override Task<HttpClient> Create(Guid apiKey)
 		{
+			var client = new HttpClient();
+
+			if (apiKey == cultureOptions.Value.ApiKey)
+			{
+				client.BaseAddress = cultureOptions.Value.BaseAddress;
+				return Task.FromResult(client);
+			}
+
 			return base.Create(apiKey);
 		}
 	}
