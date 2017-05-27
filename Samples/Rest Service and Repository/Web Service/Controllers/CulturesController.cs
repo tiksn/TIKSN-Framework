d37e2a694ee5864346adc94b38dc2aae2df322e8
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-27 11:26:52 +0300

    Controllers fixed

--- Samples/Rest Service and Repository/Web Service/Controllers/CulturesController.cs
+++ Samples/Rest Service and Repository/Web Service/Controllers/CulturesController.cs
@@ -10,86 +10,86 @@ using Web_Service.Data.Entities;
 
 namespace Web_Service.Controllers
 {
-    [Route("api/[controller]")]
-    public class CulturesController : Controller
-    {
-        private readonly ICultureRepository cultureRepository;
-        private readonly IUnitOfWorkFactory _unitOfWorkFactory;
-        private readonly IMapper _mapper;
+	[Route("api/[controller]")]
+	public class CulturesController : Controller
+	{
+		private readonly ICultureRepository cultureRepository;
+		private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+		private readonly IMapper _mapper;
 
-        public CulturesController(ICultureRepository cultureRepository, IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
-        {
-            this.cultureRepository = cultureRepository;
-            _unitOfWorkFactory = unitOfWorkFactory;
-            _mapper = mapper;
-        }
+		public CulturesController(ICultureRepository cultureRepository, IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+		{
+			this.cultureRepository = cultureRepository;
+			_unitOfWorkFactory = unitOfWorkFactory;
+			_mapper = mapper;
+		}
 
-        [HttpGet]
-        public async Task<IEnumerable<CultureModel>> Get()
-        {
-            var entities = await cultureRepository.GetAllAsync();
+		[HttpGet]
+		public async Task<IEnumerable<CultureModel>> Get()
+		{
+			var entities = await cultureRepository.GetAllAsync();
 
-            return _mapper.Map<IEnumerable<CultureModel>>(entities);
-        }
+			return _mapper.Map<IEnumerable<CultureModel>>(entities);
+		}
 
-        [HttpGet("{id}")]
-        public async Task<CultureModel> Get(int id)
-        {
-            if (id != 0)
-                throw new ArgumentException();
+		[HttpGet("{id}")]
+		public async Task<CultureModel> Get(int id)
+		{
+			if (id != 0)
+				throw new ArgumentException();
 
-            var entity = await cultureRepository.GetAsync(id);
+			var entity = await cultureRepository.GetAsync(id);
 
-            return _mapper.Map<CultureModel>(entity);
-        }
+			return _mapper.Map<CultureModel>(entity);
+		}
 
-        [HttpPost]
-        public async Task Post([FromBody]CultureModel model)
-        {
-            if (model.Id != 0)
-                throw new ArgumentException();
+		[HttpPost]
+		public async Task Post([FromBody]CultureModel model)
+		{
+			if (model.ID != 0)
+				throw new ArgumentException();
 
-            using (var unitOfWork = _unitOfWorkFactory.Create())
-            {
-                var entity = _mapper.Map<CultureEntity>(model);
+			using (var unitOfWork = _unitOfWorkFactory.Create())
+			{
+				var entity = _mapper.Map<CultureEntity>(model);
 
-                await cultureRepository.AddAsync(entity);
+				await cultureRepository.AddAsync(entity);
 
-                await unitOfWork.CompleteAsync();
-            }
-        }
+				await unitOfWork.CompleteAsync();
+			}
+		}
 
-        [HttpPut("{id}")]
-        public async Task Put(int id, [FromBody]CultureModel model)
-        {
-            if (id != 0)
-                throw new ArgumentException();
+		[HttpPut("{id}")]
+		public async Task Put(int id, [FromBody]CultureModel model)
+		{
+			if (id != 0)
+				throw new ArgumentException();
 
-            if (model.Id != 0 && model.Id != id)
-                throw new ArgumentException();
+			if (model.ID != 0 && model.ID != id)
+				throw new ArgumentException();
 
-            using (var unitOfWork = _unitOfWorkFactory.Create())
-            {
-                var entity = _mapper.Map<CultureEntity>(model);
-                entity.Id = id;
+			using (var unitOfWork = _unitOfWorkFactory.Create())
+			{
+				var entity = _mapper.Map<CultureEntity>(model);
+				entity.Id = id;
 
-                await cultureRepository.UpdateAsync(entity);
+				await cultureRepository.UpdateAsync(entity);
 
-                await unitOfWork.CompleteAsync();
-            }
-        }
+				await unitOfWork.CompleteAsync();
+			}
+		}
 
-        [HttpDelete("{id}")]
-        public async Task Delete(int id)
-        {
-            using (var unitOfWork = _unitOfWorkFactory.Create())
-            {
-                var entity = await cultureRepository.GetAsync(id);
+		[HttpDelete("{id}")]
+		public async Task Delete(int id)
+		{
+			using (var unitOfWork = _unitOfWorkFactory.Create())
+			{
+				var entity = await cultureRepository.GetAsync(id);
 
-                await cultureRepository.RemoveAsync(entity);
+				await cultureRepository.RemoveAsync(entity);
 
-                await unitOfWork.CompleteAsync();
-            }
-        }
-    }
+				await unitOfWork.CompleteAsync();
+			}
+		}
+	}
 }
--- Samples/Rest Service and Repository/Web Service/Controllers/CurrenciesController.cs
+++ Samples/Rest Service and Repository/Web Service/Controllers/CurrenciesController.cs
@@ -10,89 +10,89 @@ using TIKSN.Data;
 
 namespace Web_Service.Controllers
 {
-    [Route("api/[controller]")]
-    public class CurrenciesController : Controller
-    {
-        private readonly ICurrencyRepository currencyRepository;
-        private readonly IUnitOfWorkFactory unitOfWorkFactory;
-        private readonly IMapper mapper;
+	[Route("api/[controller]")]
+	public class CurrenciesController : Controller
+	{
+		private readonly ICurrencyRepository currencyRepository;
+		private readonly IUnitOfWorkFactory unitOfWorkFactory;
+		private readonly IMapper mapper;
 
-        public CurrenciesController(ICurrencyRepository currencyRepository, IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
-        {
-            this.currencyRepository = currencyRepository;
-            this.unitOfWorkFactory = unitOfWorkFactory;
-            this.mapper = mapper;
-        }
+		public CurrenciesController(ICurrencyRepository currencyRepository, IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper)
+		{
+			this.currencyRepository = currencyRepository;
+			this.unitOfWorkFactory = unitOfWorkFactory;
+			this.mapper = mapper;
+		}
 
-        [HttpGet]
-        public async Task<IEnumerable<CurrencyModel>> Get()
-        {
-            var entities = await currencyRepository.GetAllAsync();
+		[HttpGet]
+		public async Task<IEnumerable<CurrencyModel>> Get()
+		{
+			var entities = await currencyRepository.GetAllAsync();
 
-            return mapper.Map<IEnumerable<CurrencyModel>>(entities);
-        }
+			return mapper.Map<IEnumerable<CurrencyModel>>(entities);
+		}
 
-        [HttpGet("{id}")]
-        public async Task<CurrencyModel> Get(int id)
-        {
-            if (id != 0)
-                throw new ArgumentException();
+		[HttpGet("{id}")]
+		public async Task<CurrencyModel> Get(int id)
+		{
+			if (id != 0)
+				throw new ArgumentException();
 
-            var entity = await currencyRepository.GetAsync(id);
+			var entity = await currencyRepository.GetAsync(id);
 
-            return mapper.Map<CurrencyModel>(entity);
-        }
+			return mapper.Map<CurrencyModel>(entity);
+		}
 
-        [HttpPost]
-        public async Task Post([FromBody]CurrencyModel model)
-        {
-            if (model.Id != 0)
-                throw new ArgumentException();
+		[HttpPost]
+		public async Task Post([FromBody]CurrencyModel model)
+		{
+			if (model.ID != 0)
+				throw new ArgumentException();
 
-            using (var unitOfWork = unitOfWorkFactory.Create())
-            {
-                var entity = mapper.Map<CurrencyEntity>(model);
+			using (var unitOfWork = unitOfWorkFactory.Create())
+			{
+				var entity = mapper.Map<CurrencyEntity>(model);
 
-                await currencyRepository.AddAsync(entity);
+				await currencyRepository.AddAsync(entity);
 
-                await unitOfWork.CompleteAsync();
-            }
-        }
+				await unitOfWork.CompleteAsync();
+			}
+		}
 
-        [HttpPut("{id}")]
-        public async Task Put(int id, [FromBody]CurrencyModel model)
-        {
-            if (id != 0)
-                throw new ArgumentException();
+		[HttpPut("{id}")]
+		public async Task Put(int id, [FromBody]CurrencyModel model)
+		{
+			if (id != 0)
+				throw new ArgumentException();
 
-            if (model.Id != 0 && model.Id != id)
-                throw new ArgumentException();
+			if (model.ID != 0 && model.ID != id)
+				throw new ArgumentException();
 
-            using (var unitOfWork = unitOfWorkFactory.Create())
-            {
-                var entity = mapper.Map<CurrencyEntity>(model);
-                entity.Id = id;
+			using (var unitOfWork = unitOfWorkFactory.Create())
+			{
+				var entity = mapper.Map<CurrencyEntity>(model);
+				entity.Id = id;
 
-                await currencyRepository.UpdateAsync(entity);
+				await currencyRepository.UpdateAsync(entity);
 
-                await unitOfWork.CompleteAsync();
-            }
-        }
+				await unitOfWork.CompleteAsync();
+			}
+		}
 
-        [HttpDelete("{id}")]
-        public async Task Delete(int id)
-        {
-            if (id != 0)
-                throw new ArgumentException();
+		[HttpDelete("{id}")]
+		public async Task Delete(int id)
+		{
+			if (id != 0)
+				throw new ArgumentException();
 
-            using (var unitOfWork = unitOfWorkFactory.Create())
-            {
-                var entity = await currencyRepository.GetAsync(id);
+			using (var unitOfWork = unitOfWorkFactory.Create())
+			{
+				var entity = await currencyRepository.GetAsync(id);
 
-                await currencyRepository.RemoveAsync(entity);
+				await currencyRepository.RemoveAsync(entity);
 
-                await unitOfWork.CompleteAsync();
-            }
-        }
-    }
+				await unitOfWork.CompleteAsync();
+			}
+		}
+	}
 }
