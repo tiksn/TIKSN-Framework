7eba25dabb6a966e6ca6809d368201325f161eb0
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-27 14:11:36 +0300

    request to create 2 cultures

--- Samples/Rest Service and Repository/Console Client/CompositionRootSetup.cs
+++ Samples/Rest Service and Repository/Console Client/CompositionRootSetup.cs
@@ -4,7 +4,9 @@ using Console_Client.Rest;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TIKSN.Analytics.Telemetry;
 using TIKSN.DependencyInjection;
+using TIKSN.Web;
 using TIKSN.Web.Rest;
 
 namespace Console_Client
@@ -17,19 +19,30 @@ namespace Console_Client
 			builder.RegisterType<RestRepository<CultureModel, int>>().As<IRestRepository<CultureModel, int>>();
 			builder.RegisterType<CulturesRestRepository>().As<ICulturesRestRepository>();
 			builder.RegisterType<HttpClientFactory>().As<IHttpClientFactory>();
+			builder.RegisterType<RestAuthenticationTokenProvider>().As<IRestAuthenticationTokenProvider>();
+			builder.RegisterType<TelemetryLogger>().As<IExceptionTelemeter>().SingleInstance();
+		}
+
+		protected override void ConfigureLogging(ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole();
 		}
 
 		protected override void ConfigureOptions(IServiceCollection services, IConfigurationRoot configuration)
 		{
+			services.Configure<RestRepositoryOptions<CultureModel>>(configuration.GetSection("RestRepository").GetSection("Culture"));
+			services.Configure<WebApiOptions<CultureModel>>(configuration.GetSection("RestRepository").GetSection("Culture"));
 		}
 
 		protected override void ConfigureServices(IServiceCollection services)
 		{
 		}
 
-		protected override void ConfigureLogging(ILoggerFactory loggerFactory)
+		protected override void SetupConfiguration(IConfigurationBuilder builder)
 		{
-			loggerFactory.AddConsole();
+			builder
+				.AddYamlFile("appsettings.yaml");
+			base.SetupConfiguration(builder);
 		}
 	}
 }
