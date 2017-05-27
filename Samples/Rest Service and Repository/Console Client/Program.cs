680c127231ba73693c4aec69a862f4bda847692a
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-27 11:52:21 +0300

    culture repository added and registered

--- Samples/Rest Service and Repository/Console Client/Program.cs
+++ Samples/Rest Service and Repository/Console Client/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Common_Models;
+using Console_Client.Rest;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Globalization;
 using System.Threading.Tasks;
 using TIKSN.Shell;
 
@@ -7,13 +10,34 @@ namespace Console_Client
 {
 	class Program : IShellCommand
 	{
-		public Program()
-		{
+		private readonly ICulturesRestRepository _culturesRestRepository;
 
+		public Program(ICulturesRestRepository culturesRestRepository)
+		{
+			_culturesRestRepository = culturesRestRepository;
 		}
 
 		public async Task ExecuteAsync()
 		{
+			await _culturesRestRepository.AddAsync(new CultureModel
+			{
+				Code = "en",
+				EnglishName = "English",
+				Lcid = 123,
+				NativeName = "English",
+				ParentId = null,
+				RegionId = null
+			});
+
+			await _culturesRestRepository.AddAsync(new CultureModel
+			{
+				Code = "en-US",
+				EnglishName = "English (US)",
+				Lcid = 123,
+				NativeName = "English (United States)",
+				ParentId = null,
+				RegionId = null
+			});
 		}
 
 		static void Main(string[] args)
---
+++ Samples/Rest Service and Repository/Console Client/Rest/CulturesRestRepository.cs
@@ -0,0 +1,22 @@
+﻿using Common_Models;
+using System;
+using System.Collections.Generic;
+using TIKSN.Web.Rest;
+using Microsoft.Extensions.Options;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Console_Client.Rest
+{
+	public class CulturesRestRepository : RestRepository<CultureModel, int>, ICulturesRestRepository
+	{
+		public CulturesRestRepository(IHttpClientFactory httpClientFactory, ISerializerRestFactory serializerRestFactory, IDeserializerRestFactory deserializerRestFactory, IRestAuthenticationTokenProvider restAuthenticationTokenProvider, IOptions<RestRepositoryOptions<CultureModel>> options) : base(httpClientFactory, serializerRestFactory, deserializerRestFactory, restAuthenticationTokenProvider, options)
+		{
+		}
+
+		public Task<IEnumerable<CultureModel>> GetAllAsync(CancellationToken cancellationToken = default(CancellationToken))
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
