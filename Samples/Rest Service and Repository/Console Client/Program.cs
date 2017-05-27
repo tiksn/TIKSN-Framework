d23563f4dd4eb775c0bf453fba12862ff6515856
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-05-27 11:27:18 +0300

    Client made executable

--- Samples/Rest Service and Repository/Console Client/Program.cs
+++ Samples/Rest Service and Repository/Console Client/Program.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using TIKSN.Shell;
 
 namespace Console_Client
 {
-	class Program
+	class Program : IShellCommand
 	{
+		public Program()
+		{
+
+		}
+
+		public async Task ExecuteAsync()
+		{
+		}
+
 		static void Main(string[] args)
 		{
 			var compositionRootSetup = new CompositionRootSetup();
 			var serviceProvider = compositionRootSetup.CreateServiceProvider();
 
+			serviceProvider.GetRequiredService<Program>().ExecuteAsync().GetAwaiter().GetResult();
+
+			Console.WriteLine("Execution completed!");
 			Console.ReadLine();
 		}
 	}
