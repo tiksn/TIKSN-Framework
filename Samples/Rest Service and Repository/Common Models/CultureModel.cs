c18f6cd5cc214b7db26a445d4aa6a681da7e2309
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-02 14:08:54 +0300

    fixed issue with ID and POST

--- Samples/Rest Service and Repository/Common Models/CultureModel.cs
+++ Samples/Rest Service and Repository/Common Models/CultureModel.cs
@@ -2,9 +2,10 @@
 
 namespace Common_Models
 {
-	public class CultureModel : Entity<int>
+	public class CultureModel : IEntity<int>
     {
-		public string Code { get; set; }
+	    public int ID { get; set; }
+	    public string Code { get; set; }
 		public int? ParentId { get; set; }
 		public int Lcid { get; set; }
 		public string EnglishName { get; set; }
--- Samples/Rest Service and Repository/Common Models/RegionModel.cs
+++ Samples/Rest Service and Repository/Common Models/RegionModel.cs
@@ -2,8 +2,9 @@
 
 namespace Common_Models
 {
-	public class RegionModel : Entity<int>
+	public class RegionModel : IEntity<int>
 	{
+		public int ID { get; set; }
 		public string Code { get; set; }
 		public bool GeoId { get; set; }
 		public int CurrencyId { get; set; }
