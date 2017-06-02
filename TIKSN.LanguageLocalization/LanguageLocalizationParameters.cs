544449301f4cd7116fe0e6478b1aae083fdaadc3
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-02 23:24:02 +0300

    language and region projects are added

---
+++ TIKSN.LanguageLocalization/LanguageLocalizationParameters.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace TIKSN.LanguageLocalization
+{
+	public static class LanguageLocalizationParameters
+	{
+		public static string GetDefaultCultureName()
+		{
+			return typeof(LanguageLocalizationParameters).GetTypeInfo().Assembly.GetName().CultureName;
+		}
+	}
+}
