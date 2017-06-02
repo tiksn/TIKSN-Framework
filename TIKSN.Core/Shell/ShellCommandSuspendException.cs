fed03b2329b3fe41d77e873de12aa9dd568ecd85
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-02 22:57:33 +0300

    suspention is acceptable

---
+++ TIKSN.Core/Shell/ShellCommandSuspendException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TIKSN.Shell
+{
+	public class ShellCommandSuspendException : Exception
+	{
+		public ShellCommandSuspendException() { }
+		public ShellCommandSuspendException(string message) : base(message) { }
+		public ShellCommandSuspendException(string message, Exception inner) : base(message, inner) { }
+	}
+}
