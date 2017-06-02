0140cd49a60ae86e114915099932207dab6ea6ca
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-02 14:08:54 +0300

    fixed issue with ID and POST

--- Samples/Rest Service and Repository/Common Models/CurrencyModel.cs
+++ Samples/Rest Service and Repository/Common Models/CurrencyModel.cs
@@ -2,8 +2,9 @@
 
 namespace Common_Models
 {
-	public class CurrencyModel : Entity<int>
+	public class CurrencyModel : IEntity<int>
 	{
+		public int ID { get; set; }
 		public string Code { get; set; }
 		public int Number { get; set; }
 		public string CurrencySymbol { get; set; }
