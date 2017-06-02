2758fbbfac0bb40658af11be06c9fa6df91d72b5
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-02 13:46:06 +0300

    there is no ID in URL at POST

--- TIKSN.Core/Web/Rest/RestRepository.cs
+++ TIKSN.Core/Web/Rest/RestRepository.cs
@@ -9,187 +9,190 @@ using TIKSN.Data;
 
 namespace TIKSN.Web.Rest
 {
-    public class RestRepository<TEntity, TIdentity> : IRestRepository<TEntity, TIdentity> where TEntity : IEntity<TIdentity> where TIdentity : IEquatable<TIdentity>
-    {
-        private readonly IDeserializerRestFactory _deserializerRestFactory;
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IOptions<RestRepositoryOptions<TEntity>> _options;
-        private readonly IRestAuthenticationTokenProvider _restAuthenticationTokenProvider;
-        private readonly ISerializerRestFactory _serializerRestFactory;
+	public class RestRepository<TEntity, TIdentity> : IRestRepository<TEntity, TIdentity> where TEntity : IEntity<TIdentity> where TIdentity : IEquatable<TIdentity>
+	{
+		private readonly IDeserializerRestFactory _deserializerRestFactory;
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly IOptions<RestRepositoryOptions<TEntity>> _options;
+		private readonly IRestAuthenticationTokenProvider _restAuthenticationTokenProvider;
+		private readonly ISerializerRestFactory _serializerRestFactory;
 
-        public RestRepository(
-            IHttpClientFactory httpClientFactory,
-            ISerializerRestFactory serializerRestFactory,
-            IDeserializerRestFactory deserializerRestFactory,
-            IRestAuthenticationTokenProvider restAuthenticationTokenProvider,
-            IOptions<RestRepositoryOptions<TEntity>> options)
-        {
-            _httpClientFactory = httpClientFactory;
-            _serializerRestFactory = serializerRestFactory;
-            _options = options;
-            _restAuthenticationTokenProvider = restAuthenticationTokenProvider;
-            _deserializerRestFactory = deserializerRestFactory;
-        }
+		public RestRepository(
+			IHttpClientFactory httpClientFactory,
+			ISerializerRestFactory serializerRestFactory,
+			IDeserializerRestFactory deserializerRestFactory,
+			IRestAuthenticationTokenProvider restAuthenticationTokenProvider,
+			IOptions<RestRepositoryOptions<TEntity>> options)
+		{
+			_httpClientFactory = httpClientFactory;
+			_serializerRestFactory = serializerRestFactory;
+			_options = options;
+			_restAuthenticationTokenProvider = restAuthenticationTokenProvider;
+			_deserializerRestFactory = deserializerRestFactory;
+		}
 
-        public Task AddAsync(TEntity entity, CancellationToken cancellationToken)
-        {
-            return AddObjectAsync(entity, cancellationToken);
-        }
+		public Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+		{
+			return AddObjectAsync(entity, cancellationToken);
+		}
 
-        public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
-        {
-            return AddObjectAsync(entities, cancellationToken);
-        }
+		public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+		{
+			return AddObjectAsync(entities, cancellationToken);
+		}
 
-        public async Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken)
-        {
-            var httpClient = await GetHttpClientAsync();
-            var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
+		public async Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken)
+		{
+			var httpClient = await GetHttpClientAsync();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
 
-            uriTemplate.Fill("ID", id.ToString());
+			uriTemplate.Fill("ID", id.ToString());
 
-            var requestUrl = uriTemplate.Compose();
+			var requestUrl = uriTemplate.Compose();
 
-            var response = await httpClient.GetAsync(requestUrl, cancellationToken);
+			var response = await httpClient.GetAsync(requestUrl, cancellationToken);
 
-            return await ObjectifyResponse<TEntity>(response, true);
-        }
+			return await ObjectifyResponse<TEntity>(response, true);
+		}
 
-        public async Task RemoveAsync(TEntity entity, CancellationToken cancellationToken)
-        {
-            var httpClient = await GetHttpClientAsync();
-            var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
+		public async Task RemoveAsync(TEntity entity, CancellationToken cancellationToken)
+		{
+			var httpClient = await GetHttpClientAsync();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
 
-            uriTemplate.Fill("ID", entity.ID.ToString());
+			uriTemplate.Fill("ID", entity.ID.ToString());
 
-            var requestUrl = uriTemplate.Compose();
+			var requestUrl = uriTemplate.Compose();
 
-            var response = await httpClient.DeleteAsync(requestUrl, cancellationToken);
+			var response = await httpClient.DeleteAsync(requestUrl, cancellationToken);
 
-            response.EnsureSuccessStatusCode();
-        }
+			response.EnsureSuccessStatusCode();
+		}
 
-        public Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+		public Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
 
-        public async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
-        {
-            var httpClient = await GetHttpClientAsync();
-            var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
+		public async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
+		{
+			var httpClient = await GetHttpClientAsync();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
 
-            uriTemplate.Fill("ID", entity.ID.ToString());
+			uriTemplate.Fill("ID", entity.ID.ToString());
 
-            var requestUrl = uriTemplate.Compose();
+			var requestUrl = uriTemplate.Compose();
 
-            var response = await httpClient.PutAsync(requestUrl, GetContent(entity), cancellationToken);
+			var response = await httpClient.PutAsync(requestUrl, GetContent(entity), cancellationToken);
 
-            response.EnsureSuccessStatusCode();
-        }
+			response.EnsureSuccessStatusCode();
+		}
 
-        public Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+		public Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
 
-        protected async Task<IEnumerable<TEntity>> SearchAsync(IEnumerable<KeyValuePair<string, string>> parameters, CancellationToken cancellationToken)
-        {
-            var httpClient = await GetHttpClientAsync();
-            var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
+		protected async Task<IEnumerable<TEntity>> SearchAsync(IEnumerable<KeyValuePair<string, string>> parameters, CancellationToken cancellationToken)
+		{
+			var httpClient = await GetHttpClientAsync();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
 
-            foreach (var parameter in parameters)
-                uriTemplate.Fill(parameter.Key, parameter.Value);
+			foreach (var parameter in parameters)
+				uriTemplate.Fill(parameter.Key, parameter.Value);
 
-            var requestUrl = uriTemplate.Compose();
+			var requestUrl = uriTemplate.Compose();
 
-            var response = await httpClient.GetAsync(requestUrl, cancellationToken);
+			var response = await httpClient.GetAsync(requestUrl, cancellationToken);
 
-            return await ObjectifyResponse<IEnumerable<TEntity>>(response, false);
-        }
+			return await ObjectifyResponse<IEnumerable<TEntity>>(response, false);
+		}
 
-        protected async Task<TEntity> SingleOrDefaultAsync(IEnumerable<KeyValuePair<string, string>> parameters, CancellationToken cancellationToken)
-        {
-            var httpClient = await GetHttpClientAsync();
-            var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
+		protected async Task<TEntity> SingleOrDefaultAsync(IEnumerable<KeyValuePair<string, string>> parameters, CancellationToken cancellationToken)
+		{
+			var httpClient = await GetHttpClientAsync();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
 
-            foreach (var parameter in parameters)
-                uriTemplate.Fill(parameter.Key, parameter.Value);
+			foreach (var parameter in parameters)
+				uriTemplate.Fill(parameter.Key, parameter.Value);
 
-            var requestUrl = uriTemplate.Compose();
+			var requestUrl = uriTemplate.Compose();
 
-            var response = await httpClient.GetAsync(requestUrl, cancellationToken);
+			var response = await httpClient.GetAsync(requestUrl, cancellationToken);
 
-            return await ObjectifyResponse<TEntity>(response, true);
-        }
+			return await ObjectifyResponse<TEntity>(response, true);
+		}
 
-        private async Task AddObjectAsync(object requestContent, CancellationToken cancellationToken)
-        {
-            var httpClient = await GetHttpClientAsync();
-            var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
-            var requestUrl = uriTemplate.Compose();
+		private async Task AddObjectAsync(object requestContent, CancellationToken cancellationToken)
+		{
+			var httpClient = await GetHttpClientAsync();
+			var uriTemplate = new UriTemplate(_options.Value.ResourceTemplate);
 
-            var response = await httpClient.PostAsync(requestUrl, GetContent(requestContent), cancellationToken);
+			uriTemplate.Fill("ID", string.Empty);
 
-            response.EnsureSuccessStatusCode();
-        }
+			var requestUrl = uriTemplate.Compose();
 
-        private HttpContent GetContent(object requestContent)
-        {
-            return new StringContent(_serializerRestFactory.Create(_options.Value.MediaType).Serialize(requestContent), _options.Value.Encoding, _options.Value.MediaType);
-        }
+			var response = await httpClient.PostAsync(requestUrl, GetContent(requestContent), cancellationToken);
 
-        private async Task<HttpClient> GetHttpClientAsync()
-        {
-            var httpClient = await _httpClientFactory.Create(_options.Value.ApiKey);
+			response.EnsureSuccessStatusCode();
+		}
 
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_options.Value.MediaType));
+		private HttpContent GetContent(object requestContent)
+		{
+			return new StringContent(_serializerRestFactory.Create(_options.Value.MediaType).Serialize(requestContent), _options.Value.Encoding, _options.Value.MediaType);
+		}
 
-            if (_options.Value.AcceptLanguages != null)
-                foreach (var acceptLanguage in _options.Value.AcceptLanguages)
-                    httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(acceptLanguage.Value, acceptLanguage.Key));
+		private async Task<HttpClient> GetHttpClientAsync()
+		{
+			var httpClient = await _httpClientFactory.Create(_options.Value.ApiKey);
 
-            await SetAuthenticationHeader(httpClient);
+			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_options.Value.MediaType));
 
-            return httpClient;
-        }
+			if (_options.Value.AcceptLanguages != null)
+				foreach (var acceptLanguage in _options.Value.AcceptLanguages)
+					httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(acceptLanguage.Value, acceptLanguage.Key));
 
-        private async Task<TResult> ObjectifyResponse<TResult>(HttpResponseMessage response, bool defaultIfNotFound)
-        {
-            if (defaultIfNotFound && response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                return default(TResult);
+			await SetAuthenticationHeader(httpClient);
 
-            response.EnsureSuccessStatusCode();
+			return httpClient;
+		}
 
-            var content = await response.Content.ReadAsStringAsync();
+		private async Task<TResult> ObjectifyResponse<TResult>(HttpResponseMessage response, bool defaultIfNotFound)
+		{
+			if (defaultIfNotFound && response.StatusCode == System.Net.HttpStatusCode.NotFound)
+				return default(TResult);
 
-            return _deserializerRestFactory.Create(_options.Value.MediaType).Deserialize<TResult>(content);
-        }
+			response.EnsureSuccessStatusCode();
 
-        private async Task SetAuthenticationHeader(HttpClient httpClient)
-        {
-            var authenticationSchema = string.Empty;
+			var content = await response.Content.ReadAsStringAsync();
 
-            switch (_options.Value.Authentication)
-            {
-                case RestAuthenticationType.None:
-                    return;
+			return _deserializerRestFactory.Create(_options.Value.MediaType).Deserialize<TResult>(content);
+		}
 
-                case RestAuthenticationType.Basic:
-                    authenticationSchema = "Basic";
-                    break;
+		private async Task SetAuthenticationHeader(HttpClient httpClient)
+		{
+			var authenticationSchema = string.Empty;
 
-                case RestAuthenticationType.Bearer:
-                    authenticationSchema = "Bearer";
-                    break;
+			switch (_options.Value.Authentication)
+			{
+				case RestAuthenticationType.None:
+					return;
 
-                default:
-                    throw new NotSupportedException($"Authentication type '{_options.Value.Authentication}' is not supported.");
-            }
+				case RestAuthenticationType.Basic:
+					authenticationSchema = "Basic";
+					break;
 
-            var authenticationToken = await _restAuthenticationTokenProvider.GetAuthenticationToken(_options.Value.ApiKey);
+				case RestAuthenticationType.Bearer:
+					authenticationSchema = "Bearer";
+					break;
 
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authenticationSchema, authenticationToken);
-        }
-    }
+				default:
+					throw new NotSupportedException($"Authentication type '{_options.Value.Authentication}' is not supported.");
+			}
+
+			var authenticationToken = await _restAuthenticationTokenProvider.GetAuthenticationToken(_options.Value.ApiKey);
+
+			httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authenticationSchema, authenticationToken);
+		}
+	}
 }
