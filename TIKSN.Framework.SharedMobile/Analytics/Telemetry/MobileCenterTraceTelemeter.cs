b4c39a8d54e398b218a8e1392cbfccbf20a8d040
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-10 22:37:45 +0300

    trace and exception telemeters are added

---
+++ TIKSN.Framework.SharedMobile/Analytics/Telemetry/MobileCenterTraceTelemeter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Mobile;
+using TIKSN.Analytics.Telemetry;
+
+namespace TIKSN.Framework.SharedMobile.Analytics.Telemetry
+{
+	public class MobileCenterTraceTelemeter : ITraceTelemeter
+	{
+		public Task TrackTrace(string message)
+		{
+			return TrackTrace(message, TelemetrySeverityLevel.Information);
+		}
+
+		public Task TrackTrace(string message, TelemetrySeverityLevel severityLevel)
+		{
+			switch (severityLevel)
+			{
+				case TelemetrySeverityLevel.Verbose:
+					MobileCenterLog.Verbose(MobileCenterLog.LogTag, message);
+					break;
+				case TelemetrySeverityLevel.Information:
+					MobileCenterLog.Info(MobileCenterLog.LogTag, message);
+					break;
+				case TelemetrySeverityLevel.Warning:
+					MobileCenterLog.Warn(MobileCenterLog.LogTag, message);
+					break;
+				case TelemetrySeverityLevel.Error:
+					MobileCenterLog.Error(MobileCenterLog.LogTag, message);
+					break;
+				case TelemetrySeverityLevel.Critical:
+					MobileCenterLog.Error(MobileCenterLog.LogTag, message);
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(severityLevel));
+			}
+
+			return Task.CompletedTask;
+		}
+	}
+}
