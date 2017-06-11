7b60c1a5d74053ea1be8dd27bc3e581602845335
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-11 03:15:51 +0300

    abstract options types cannot be validated

--- TIKSN.Core/DependencyInjection/CompositionRootSetupBase.cs
+++ TIKSN.Core/DependencyInjection/CompositionRootSetupBase.cs
@@ -134,6 +134,8 @@ namespace TIKSN.DependencyInjection
 
 						if (pInfo.PropertyType == typeof(string))
 							isNotSpecified = string.IsNullOrEmpty(pInfo.GetValue(options.Value)?.ToString());
+						else if (pInfo.PropertyType.GetTypeInfo().IsAbstract)
+							logger.LogDebug(1714558096, $"{pInfo.Name} property of {optionType.FullName} object is abstract type of {pInfo.PropertyType}. Abstract option types cannot be validated.");
 						else if (pInfo.PropertyType.GetMatchingConstructor(Type.EmptyTypes) != null && pInfo.GetValue(options.Value) == Activator.CreateInstance(pInfo.PropertyType))
 							isNotSpecified = true;
 
