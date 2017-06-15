f93df061d7e8045f08d6d122a96930c3886c6311
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-15 21:50:19 +0300

    memory cache, options and repository cahce

---
+++ TIKSN.Core/Data/Cache/MemoryCacheDecoratorOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TIKSN.Data.Cache
+{
+	public class MemoryCacheDecoratorOptions
+	{
+		public DateTimeOffset? AbsoluteExpiration { get; set; }
+		public TimeSpan? AbsoluteExpirationRelativeToNow { get; set; }
+		public TimeSpan? SlidingExpiration { get; set; }
+	}
+
+	public class MemoryCacheDecoratorOptions<T> : MemoryCacheDecoratorOptions
+	{
+	}
+}
