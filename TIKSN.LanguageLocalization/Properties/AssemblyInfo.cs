981f68259754f63d937a5116037b86f79439ae52
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-17 20:17:28 +0300

    Localization enabled for languages and regions

---
+++ TIKSN.LanguageLocalization/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("TIKSN.LanguageLocalization")]
+[assembly: AssemblyCulture("en")]
+[assembly: NeutralResourcesLanguage("en")]
---
+++ TIKSN.RegionLocalization/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("TIKSN.RegionLocalization")]
+[assembly: AssemblyCulture("en-US")]
+[assembly: NeutralResourcesLanguage("en-US")]
