2830f087081ded47e512b2e07266c90fa65aca8d
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-26 02:27:55 +0300

    localization sample added

---
+++ Samples/SelectingLocalization/AllCulturesLocalization/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Globalization;
+using System.Threading;
+using TIKSN.Localization;
+
+namespace SelectingLocalization
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var compositionRoot = new CompositionRoot();
+			var serviceProvider = compositionRoot.CreateServiceProvider();
+
+			var localizer = serviceProvider.GetRequiredService<IStringLocalizer>();
+			var selector = serviceProvider.GetRequiredService<ILocalizationSelector>();
+
+			foreach (var cultiure in CultureInfo.GetCultures(CultureTypes.AllCultures))
+			{
+				var l = localizer.WithCulture(cultiure);
+
+				Console.WriteLine($"Culture '{cultiure.Name}' is selected.");
+				Console.WriteLine(l.GetRequiredString("066c2ac3-cacc-4271-9ed8-8a5cf9fb8369"));
+			}
+
+			Print(localizer);
+			Select(selector, "ru-RU");
+			Print(localizer);
+			Select(selector, "ru-UA");
+			Print(localizer);
+			Select(selector, "ru");
+			Print(localizer);
+			Select(selector, "hy-AM");
+			Print(localizer);
+			Select(selector, "hy");
+			Print(localizer);
+
+			Console.ReadLine();
+		}
+
+		private static void Select(ILocalizationSelector selector, string name)
+		{
+			selector.Select(new CultureInfo(name));
+			Console.WriteLine($"Culture '{name}' is selected.");
+		}
+
+		private static void Print(IStringLocalizer localizer)
+		{
+			Console.WriteLine(localizer.GetRequiredString("066c2ac3-cacc-4271-9ed8-8a5cf9fb8369"));
+		}
+	}
+}
