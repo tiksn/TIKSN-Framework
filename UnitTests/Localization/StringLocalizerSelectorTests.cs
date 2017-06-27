fb42c2ed82d8550e8def9fba87cb4b829d9d426e
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-27 18:42:14 +0300

    New tests added for localization

---
+++ UnitTests/Localization/StringLocalizerSelectorTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+using Xunit;
+
+namespace TIKSN.Localization.Tests
+{
+	public class StringLocalizerSelectorTests
+	{
+		[Theory]
+		[InlineData("066c2ac3-cacc-4271-9ed8-8a5cf9fb8369", "ru", "Реклама")]
+		[InlineData("066c2ac3-cacc-4271-9ed8-8a5cf9fb8369", "ru-RU", "Реклама")]
+		[InlineData("066c2ac3-cacc-4271-9ed8-8a5cf9fb8369", "hy", "Գովազդ")]
+		[InlineData("066c2ac3-cacc-4271-9ed8-8a5cf9fb8369", "hy-AM", "Գովազդ")]
+		public void SelectLocalization(string resourceKey, string cultureName, string expectedLocalization)
+		{
+			var resourceNamesCache = new ResourceNamesCache();
+			var testStringLocalizer = new TestStringLocalizer(resourceNamesCache);
+			var stringLocalizerSelector = new StringLocalizerSelector(testStringLocalizer);
+
+			stringLocalizerSelector.Select(new CultureInfo(cultureName));
+
+			var actualLocalization = stringLocalizerSelector.GetRequiredString(resourceKey);
+
+			actualLocalization.Should().Be(expectedLocalization);
+		}
+	}
+}
