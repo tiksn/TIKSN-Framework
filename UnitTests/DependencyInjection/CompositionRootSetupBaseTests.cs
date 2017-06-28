7a7db9aa625082b58340665f888f03ab3b5da874
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-29 00:32:27 +0300

    parameterless option property type test added

---
+++ UnitTests/DependencyInjection/CompositionRootSetupBaseTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace TIKSN.DependencyInjection.Tests
+{
+	public class CompositionRootSetupBaseTests
+    {
+		[Fact]
+		public void OptionsValidationForNonParameterLessPropertyType()
+		{
+			var compositionRoot = new TestCompositionRootSetup();
+			compositionRoot.CreateServiceProvider();
+		}
+    }
+}
