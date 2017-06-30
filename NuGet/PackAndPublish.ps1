8464608cd6a5205b8a107525c492ea0b9776f2d3
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-30 23:59:23 +0300

    publish script added

---
+++ NuGet/PackAndPublish.ps1
@@ -0,0 +1,7 @@
+﻿Remove-Item TIKSN-Framework.*.nupkg
+
+nuget pack .\TIKSN-Framework.nuspec
+
+$packageName = Get-ChildItem -Name TIKSN-Framework.*.nupkg
+
+nuget push "$packageName" -source nuget.org
