f81d1b44ca37fb4afd289b0994a97e8a1993c76f
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-07-01 20:59:17 +0300

    DI for tests

--- UnitTests/DependencyInjection/CompositionRootSetupBaseTests.cs
+++ UnitTests/DependencyInjection/CompositionRootSetupBaseTests.cs
@@ -1,14 +1,22 @@
-﻿using Xunit;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace TIKSN.DependencyInjection.Tests
 {
 	public class CompositionRootSetupBaseTests
-    {
+	{
+		private readonly TestCompositionRootSetup _compositionRoot;
+
+		public CompositionRootSetupBaseTests(ITestOutputHelper testOutputHelper)
+		{
+			_compositionRoot = new TestCompositionRootSetup(testOutputHelper, configureOptions: (s, c) => s.Configure<TestOptions>(c));
+		}
+
 		[Fact]
 		public void OptionsValidationForNonParameterLessPropertyType()
 		{
-			var compositionRoot = new TestCompositionRootSetup();
-			compositionRoot.CreateServiceProvider();
+			_compositionRoot.CreateServiceProvider();
 		}
-    }
+	}
 }
