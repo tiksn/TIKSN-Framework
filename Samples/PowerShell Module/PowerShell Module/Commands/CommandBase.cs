f0694613a0afd480312a31643a65c1c7cb6a4f61
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-07-02 07:06:34 +0300

    PowerShell Module sample added

---
+++ Samples/PowerShell Module/PowerShell Module/Commands/CommandBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PowerShell_Module.Commands
+{
+	public abstract class CommandBase : TIKSN.PowerShell.CommandBase
+	{
+		protected override IServiceProvider CreateServiceProvider()
+		{
+			var compositionRootSetup = new CompositionRootSetup(this);
+
+			return compositionRootSetup.CreateServiceProvider();
+		}
+	}
+}
