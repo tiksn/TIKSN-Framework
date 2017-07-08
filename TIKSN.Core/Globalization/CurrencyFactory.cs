68f0078fe4ad0c3b64af9e482f2c18286bb8ac5f
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-07-08 21:14:35 +0300

    CurrencyFactory implemented

---
+++ TIKSN.Core/Globalization/CurrencyFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using TIKSN.Data.Cache;
+using TIKSN.Finance;
+
+namespace TIKSN.Globalization
+{
+	public class CurrencyFactory : MemoryCacheDecoratorBase<CurrencyInfo>, ICurrencyFactory
+	{
+		public CurrencyFactory(IMemoryCache memoryCache, IOptions<MemoryCacheDecoratorOptions> genericOptions, IOptions<MemoryCacheDecoratorOptions<CurrencyInfo>> specificOptions) : base(memoryCache, genericOptions, specificOptions)
+		{
+		}
+
+		public CurrencyInfo Create(string isoCurrencySymbol)
+		{
+			var cacheKey = Tuple.Create(entityType, isoCurrencySymbol.ToUpperInvariant());
+
+			return GetFromMemoryCache(cacheKey, () => new CurrencyInfo(isoCurrencySymbol));
+		}
+
+		public CurrencyInfo Create(RegionInfo region)
+		{
+			var cacheKey = Tuple.Create(entityType, region.ISOCurrencySymbol);
+
+			return GetFromMemoryCache(cacheKey, () => new CurrencyInfo(region));
+		}
+	}
+}
