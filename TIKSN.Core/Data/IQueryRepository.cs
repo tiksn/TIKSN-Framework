8e8366358a8d4eb63418951ae51742ac259b95ed
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-08-02 23:48:47 +0300

    IQueryRepository added

---
+++ TIKSN.Core/Data/IQueryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TIKSN.Data
+{
+	public interface IQueryRepository<TEntity, TIdentity>
+		where TEntity : IEntity<TIdentity>
+		where TIdentity : IEquatable<TIdentity>
+	{
+		Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken = default(CancellationToken));
+	}
+}
