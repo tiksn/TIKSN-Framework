4be333a3224efba63e59c40594ac6b7fcd7edf90
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-08-14 23:33:54 +0300

    IDistributedTypedCache added

---
+++ TIKSN.Core/Data/Cache/DistributedTypedCache.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TIKSN.Data.Cache
+{
+    public class DistributedTypedCache : IDistributedTypedCache
+    {
+    }
+}
