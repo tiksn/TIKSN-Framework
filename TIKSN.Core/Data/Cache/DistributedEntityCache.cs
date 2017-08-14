3196e29e9e0306a0f75106f031da03ed6de645ce
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-08-15 02:01:26 +0300

    Distributed Entity Cache abstraction added

---
+++ TIKSN.Core/Data/Cache/DistributedEntityCache.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TIKSN.Data.Cache
+{
+	public class DistributedEntityCache : IDistributedEntityCache
+	{
+		public TEntity Get<TEntity, TIdentity>(TIdentity identity)
+			where TEntity : IEntity<TIdentity>
+			where TIdentity : IEquatable<TIdentity>
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<TEntity> GetAsync<TEntity, TIdentity>(TIdentity identity)
+			where TEntity : IEntity<TIdentity>
+			where TIdentity : IEquatable<TIdentity>
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Set<TEntity, TIdentity>(TIdentity identity, TEntity entity)
+			where TEntity : IEntity<TIdentity>
+			where TIdentity : IEquatable<TIdentity>
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SetAsync<TEntity, TIdentity>(TIdentity identity, TEntity entity)
+			where TEntity : IEntity<TIdentity>
+			where TIdentity : IEquatable<TIdentity>
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- TIKSN.Core/Data/Cache/DistributedTypedCache.cs
+++ TIKSN.Core/Data/Cache/DistributedTypedCache.cs
@@ -1,10 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace TIKSN.Data.Cache
 {
-    public class DistributedTypedCache : IDistributedTypedCache
-    {
-    }
+	public class DistributedTypedCache : IDistributedTypedCache
+	{
+		public T Get<T>(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> GetAsync<T>(string key)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Set<T>(string key, T value, DistributedCacheEntryOptions options)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SetAsync<T>(string key, T value, DistributedCacheEntryOptions options)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
