0d716979ac5dd6415db97d4cd5ee4310a7c38f31
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-08-15 17:35:37 +0300

    version bump

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.1.11.0")]
-[assembly: AssemblyFileVersion("2.1.11.0")]
-[assembly: AssemblyInformationalVersion("2.1.11.0")] // a.k.a. “Product version”
+[assembly: AssemblyVersion("2.1.12.0")]
+[assembly: AssemblyFileVersion("2.1.12.0")]
+[assembly: AssemblyInformationalVersion("2.1.12.0")] // a.k.a. “Product version”
