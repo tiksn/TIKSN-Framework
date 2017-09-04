1109fe3e497823ecae5aa63f75b514e1b9fc6d84
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-09-04 17:56:47 +0300

    distributed cache repository added

---
+++ TIKSN.Core/Data/Cache/Distributed/DistributedCacheQueryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
+using TIKSN.Serialization;
+
+namespace TIKSN.Data.Cache.Distributed
+{
+	public class DistributedCacheQueryRepository<TEntity, TIdentity> : DistributedCacheRepository<TEntity, TIdentity>, IQueryRepository<TEntity, TIdentity>
+		where TEntity : IEntity<TIdentity>
+		where TIdentity : IEquatable<TIdentity>
+	{
+		protected DistributedCacheQueryRepository(
+			IDistributedCache distributedCache,
+			ISerializer<byte[]> serializer,
+			IDeserializer<byte[]> deserializer,
+			IOptions<DistributedCacheDecoratorOptions> genericOptions,
+			IOptions<DistributedCacheDecoratorOptions<TEntity>> specificOptions) : base(distributedCache, serializer, deserializer, genericOptions, specificOptions)
+		{
+		}
+
+		public Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken)
+		{
+			return GetFromDistributedCacheAsync<TEntity>(CreateEntryCacheKey(id), cancellationToken);
+		}
+	}
+}
