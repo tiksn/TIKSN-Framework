e71c8ba58033b2bc6f90ae8f5e4ac12e00a3dee3
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-09-05 17:44:20 +0300

    LiteDB Database provider

---
+++ TIKSN.Core/Data/LiteDB/LiteDbDatabaseProvider.cs
@@ -0,0 +1,24 @@
+﻿using LiteDB;
+using Microsoft.Extensions.Configuration;
+
+namespace TIKSN.Data.LiteDB
+{
+	public class LiteDbDatabaseProvider : ILiteDbDatabaseProvider
+	{
+		private readonly IConfigurationRoot _configuration;
+		private readonly string _connectionStringKey;
+
+		public LiteDbDatabaseProvider(IConfigurationRoot configuration, string connectionStringKey)
+		{
+			_configuration = configuration;
+			_connectionStringKey = connectionStringKey;
+		}
+
+		public LiteDatabase GetDatabase()
+		{
+			var connectionString = _configuration.GetConnectionString(_connectionStringKey);
+
+			return new LiteDatabase(connectionString);
+		}
+	}
+}
