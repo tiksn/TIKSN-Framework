5f296ec64730ac9165e6e52cd35d2a93c3e7904b
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-09-06 23:10:33 +0300

    Simple Xml Bond Deserializer and Serializer

---
+++ TIKSN.Core/Serialization/Bond/SimpleXmlBondSerializer.cs
@@ -0,0 +1,24 @@
+﻿using Bond.Protocols;
+using System.IO;
+using TIKSN.Analytics.Telemetry;
+
+namespace TIKSN.Serialization.Bond
+{
+	public class SimpleXmlBondSerializer : SerializerBase<byte[]>
+	{
+		public SimpleXmlBondSerializer(IExceptionTelemeter exceptionTelemeter) : base(exceptionTelemeter)
+		{
+		}
+
+		protected override byte[] SerializeInternal(object obj)
+		{
+			using (var output = new MemoryStream())
+			{
+				var writer = new SimpleXmlWriter(output);
+				global::Bond.Serialize.To(writer, obj);
+
+				return output.ToArray();
+			}
+		}
+	}
+}
