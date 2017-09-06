f0d01df65d5cbfcfa47d17a025a256a804d89ef2
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-09-06 21:50:47 +0300

    Compact Binary Bond Serializer and Deserializer

---
+++ TIKSN.Core/Serialization/Bond/CompactBinaryBondDeserializer.cs
@@ -0,0 +1,24 @@
+﻿using Bond.IO.Safe;
+using Bond.Protocols;
+using Bond;
+using System.Collections.Generic;
+using System.Text;
+using TIKSN.Analytics.Telemetry;
+
+namespace TIKSN.Serialization.Bond
+{
+	public class CompactBinaryBondDeserializer : DeserializerBase<byte[]>
+	{
+		public CompactBinaryBondDeserializer(IExceptionTelemeter exceptionTelemeter) : base(exceptionTelemeter)
+		{
+		}
+
+		protected override T DeserializeInternal<T>(byte[] serial)
+		{
+			var input = new InputBuffer(serial);
+			var reader = new CompactBinaryReader<InputBuffer>(input);
+
+			return Deserialize<T>.From(reader);
+		}
+	}
+}
---
+++ TIKSN.Core/Serialization/Bond/CompactBinaryBondSerializer.cs
@@ -0,0 +1,26 @@
+﻿using Bond.IO.Safe;
+using Bond.Protocols;
+using Bond;
+using System.Collections.Generic;
+using System.Text;
+using TIKSN.Analytics.Telemetry;
+
+namespace TIKSN.Serialization.Bond
+{
+	public class CompactBinaryBondSerializer : SerializerBase<byte[]>
+	{
+		public CompactBinaryBondSerializer(IExceptionTelemeter exceptionTelemeter) : base(exceptionTelemeter)
+		{
+		}
+
+		protected override byte[] SerializeInternal(object obj)
+		{
+			var output = new OutputBuffer();
+			var writer = new CompactBinaryWriter<OutputBuffer>(output);
+
+			Serialize<byte[]>.To(writer, obj);
+
+			return output.Data.Array;
+		}
+	}
+}
