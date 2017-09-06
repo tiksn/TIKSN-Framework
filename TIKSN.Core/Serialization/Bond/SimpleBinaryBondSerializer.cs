af34271fedb709399abaf109fc3e8ec1299b9bc4
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-09-06 22:52:55 +0300

    Simple Binary Bond Serializer and Deserializer

---
+++ TIKSN.Core/Serialization/Bond/SimpleBinaryBondSerializer.cs
@@ -0,0 +1,23 @@
+﻿using Bond.IO.Safe;
+using Bond.Protocols;
+using TIKSN.Analytics.Telemetry;
+
+namespace TIKSN.Serialization.Bond
+{
+	public class SimpleBinaryBondSerializer : SerializerBase<byte[]>
+	{
+		public SimpleBinaryBondSerializer(IExceptionTelemeter exceptionTelemeter) : base(exceptionTelemeter)
+		{
+		}
+
+		protected override byte[] SerializeInternal(object obj)
+		{
+			var output = new OutputBuffer();
+			var writer = new SimpleBinaryWriter<OutputBuffer>(output);
+
+			global::Bond.Serialize.To(writer, obj);
+
+			return output.Data.Array;
+		}
+	}
+}
