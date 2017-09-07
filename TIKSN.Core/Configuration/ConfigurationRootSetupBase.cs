aedbbdc51e58ac07cc3f69a5d6437396ecc45761
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-09-07 12:21:51 +0300

    configuration root setup base class added

---
+++ TIKSN.Core/Configuration/ConfigurationRootSetupBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TIKSN.Configuration
+{
+	public abstract class ConfigurationRootSetupBase
+	{
+		public virtual IConfigurationRoot CreateConfigurationRoot()
+		{
+			var builder = new ConfigurationBuilder();
+
+			SetupConfiguration(builder);
+
+			return builder.Build();
+		}
+
+		protected virtual void SetupConfiguration(IConfigurationBuilder builder)
+		{
+			builder.AddInMemoryCollection();
+		}
+	}
+}
