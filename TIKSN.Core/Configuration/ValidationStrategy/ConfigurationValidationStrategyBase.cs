5a75699715484d01e527ff4ada507beb28414083
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-09-12 15:23:59 +0300

    validation strategies

---
+++ TIKSN.Core/Configuration/ValidationStrategy/ConfigurationValidationStrategyBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TIKSN.Configuration.ValidationStrategy
+{
+	public abstract class ConfigurationValidationStrategyBase<T> : IConfigurationValidationStrategy<T>
+	{
+		protected readonly IServiceProvider _serviceProvider;
+
+		protected ConfigurationValidationStrategyBase(IServiceProvider serviceProvider)
+		{
+			_serviceProvider = serviceProvider;
+		}
+
+		public void Validate(T instance)
+		{
+			var validator = GetConfigurationValidator();
+
+			if (validator != null)
+				validator.ValidateConfiguration(instance);
+		}
+
+		protected abstract IPartialConfigurationValidator<T> GetConfigurationValidator();
+	}
+}
---
+++ TIKSN.Core/Configuration/ValidationStrategy/OptionalConfigurationValidationStrategy.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace TIKSN.Configuration.ValidationStrategy
+{
+	public class OptionalConfigurationValidationStrategy<T> : ConfigurationValidationStrategyBase<T>
+	{
+		public OptionalConfigurationValidationStrategy(IServiceProvider serviceProvider) : base(serviceProvider)
+		{
+		}
+
+		protected override IPartialConfigurationValidator<T> GetConfigurationValidator()
+		{
+			return _serviceProvider.GetService<IPartialConfigurationValidator<T>>();
+		}
+	}
+}
