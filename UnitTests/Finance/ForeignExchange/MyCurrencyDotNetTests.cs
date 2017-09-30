8cb20f4c8efbe1f73f9ac515702706b2e10f0f8e
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-09-30 23:35:42 +0300

    test created

---
+++ UnitTests/Finance/ForeignExchange/MyCurrencyDotNetTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TIKSN.DependencyInjection.Tests;
+using TIKSN.Globalization;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TIKSN.Finance.ForeignExchange.Tests
+{
+	public class MyCurrencyDotNetTests
+	{
+		private readonly IServiceProvider _serviceProvider;
+
+		public MyCurrencyDotNetTests(ITestOutputHelper testOutputHelper)
+		{
+			_serviceProvider = new TestCompositionRootSetup(testOutputHelper, services =>
+			{
+				services.AddSingleton<ICurrencyFactory, CurrencyFactory>();
+				services.AddSingleton<IRegionFactory, RegionFactory>();
+			}).CreateServiceProvider();
+		}
+
+		[Fact]
+		public async Task GetCurrencyPairsAsync()
+		{
+			var myCurrencyDotNet = new MyCurrencyDotNet(_serviceProvider.GetRequiredService<ICurrencyFactory>(), _serviceProvider.GetRequiredService<IRegionFactory>());
+
+			var pairs = await myCurrencyDotNet.GetCurrencyPairsAsync(DateTimeOffset.Now);
+
+			pairs.Count().Should().BeGreaterThan(0);
+		}
+	}
+}
