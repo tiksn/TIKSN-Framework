3a44eb2a6871f4ea336ba8fc169c42f56d21aa01
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-17 18:16:02 +0300

    MyCurrencyDotNet supports forex

--- TIKSN.Core/Finance/ForeignExchange/ExchangeRateService.cs
+++ TIKSN.Core/Finance/ForeignExchange/ExchangeRateService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TIKSN.Finance.ForeignExchange.Bank;
+using TIKSN.Finance.ForeignExchange.Cumulative;
 using TIKSN.Globalization;
 
 namespace TIKSN.Finance.ForeignExchange
@@ -10,11 +11,12 @@ namespace TIKSN.Finance.ForeignExchange
 	{
 		private readonly Dictionary<int, IExchangeRateProvider> _providers;
 
-		public ExchangeRateService(ICurrencyFactory currencyFactory)
+		public ExchangeRateService(ICurrencyFactory currencyFactory, IRegionFactory regionFactory)
 		{
 			_providers = new Dictionary<int, IExchangeRateProvider>();
 
 			_providers.Add(9596, new CentralBankOfArmenia(currencyFactory));
+			_providers.Add(2893, new MyCurrencyDotNet(currencyFactory, regionFactory));
 		}
 
 		public Task InitializeAsync()
