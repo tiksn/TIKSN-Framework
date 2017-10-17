d762bea0c2940feb516cdffa0fe9c2e9a2f14767
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-18 01:39:22 +0300

    bank of Russia added to providers list

--- TIKSN.Core/Finance/ForeignExchange/ExchangeRateService.cs
+++ TIKSN.Core/Finance/ForeignExchange/ExchangeRateService.cs
@@ -7,23 +7,23 @@ using TIKSN.Globalization;
 
 namespace TIKSN.Finance.ForeignExchange
 {
-	public class ExchangeRateService : IExchangeRateService
-	{
-		private readonly Dictionary<int, IExchangeRateProvider> _providers;
+    public class ExchangeRateService : IExchangeRateService
+    {
+        private readonly Dictionary<int, IExchangeRateProvider> _providers;
 
-		public ExchangeRateService(ICurrencyFactory currencyFactory, IRegionFactory regionFactory)
-		{
-			_providers = new Dictionary<int, IExchangeRateProvider>();
+        public ExchangeRateService(ICurrencyFactory currencyFactory, IRegionFactory regionFactory)
+        {
+            _providers = new Dictionary<int, IExchangeRateProvider>();
 
-			_providers.Add(9596, new CentralBankOfArmenia(currencyFactory));
-			_providers.Add(2893, new MyCurrencyDotNet(currencyFactory, regionFactory));
+            _providers.Add(9596, new CentralBankOfArmenia(currencyFactory));
+            _providers.Add(2893, new MyCurrencyDotNet(currencyFactory, regionFactory));
             _providers.Add(2328, new BankOfRussia(currencyFactory));
 
         }
 
-		public Task InitializeAsync()
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
