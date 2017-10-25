c373ca4c90cf83774922183ed0a8b4e2007631f7
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-25 17:26:06 +0300

    Full .net implementation of IKnownFolders

---
+++ TIKSN.Framework.Full/FileSystem/KnownFolders.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace TIKSN.FileSystem
+{
+    public class KnownFolders : IKnownFolders
+    {
+        private readonly Assembly _mainAssembly;
+        private readonly KnownFolderVersionConsideration _versionConsideration;
+
+        public KnownFolders(Assembly mainAssembly, KnownFolderVersionConsideration versionConsideration)
+        {
+            _mainAssembly = mainAssembly;
+            _versionConsideration = versionConsideration;
+
+            LocalAppData = GetFromSpecialFolder(Environment.SpecialFolder.LocalApplicationData);
+            RoamingAppData = GetFromSpecialFolder(Environment.SpecialFolder.ApplicationData);
+        }
+
+        public IFileProvider LocalAppData { get; }
+
+        public IFileProvider RoamingAppData { get; }
+
+        private IFileProvider GetFromFolderPath(string folderPath)
+        {
+            return new PhysicalFileProvider(folderPath);
+        }
+
+        private IFileProvider GetFromSpecialFolder(Environment.SpecialFolder specialFolder)
+        {
+            var folderPath = Environment.GetFolderPath(specialFolder);
+
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(_mainAssembly.Location);
+
+            folderPath = Path.Combine(folderPath, fileVersionInfo.CompanyName);
+            folderPath = Path.Combine(folderPath, fileVersionInfo.ProductVersion);
+
+            switch (_versionConsideration)
+            {
+                case KnownFolderVersionConsideration.None:
+                    break;
+
+                case KnownFolderVersionConsideration.Major:
+                    folderPath = Path.Combine(folderPath, _mainAssembly.GetName().Version.ToString(1));
+                    break;
+
+                case KnownFolderVersionConsideration.MajorMinor:
+                    folderPath = Path.Combine(folderPath, _mainAssembly.GetName().Version.ToString(2));
+                    break;
+
+                case KnownFolderVersionConsideration.MajorMinorBuild:
+                    folderPath = Path.Combine(folderPath, _mainAssembly.GetName().Version.ToString(3));
+                    break;
+
+                case KnownFolderVersionConsideration.MajorMinorBuildRevision:
+                    folderPath = Path.Combine(folderPath, _mainAssembly.GetName().Version.ToString(4));
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(_versionConsideration));
+            }
+
+            return GetFromFolderPath(folderPath);
+        }
+    }
+}
