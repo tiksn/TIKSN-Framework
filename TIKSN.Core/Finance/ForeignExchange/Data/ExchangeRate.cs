431271452fffda0eb28c82c37a7960e6c7cb1246
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-25 18:04:47 +0300

    entities added

---
+++ TIKSN.Core/Finance/ForeignExchange/Data/ExchangeRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TIKSN.Finance.ForeignExchange.Data
+{
+    public class ExchangeRate
+    {
+        public long Id { get; set; }
+        public string BaseCurrencyCode { get; set; }
+        public string CounterCurrencyCode { get; set; }
+        public string AsOn { get; set; }
+        public string Rate { get; set; }
+        public long? ForeignExchangeId { get; set; }
+
+        public virtual ForeignExchange ForeignExchange { get; set; }
+    }
+}
