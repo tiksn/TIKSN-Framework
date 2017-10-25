6a16664e5bf3d41a512cc166a700932869687e8d
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-25 18:30:17 +0300

    repositories added

---
+++ TIKSN.Core/Finance/ForeignExchange/Data/EntityFrameworkCore/ExchangeRateRepository.cs
@@ -0,0 +1,11 @@
+﻿using TIKSN.Data.EntityFrameworkCore;
+
+namespace TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore
+{
+    public class ExchangeRateRepository : EntityQueryRepository<ExchangeRatesContext, ExchangeRateEntity, int>
+    {
+        public ExchangeRateRepository(ExchangeRatesContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ TIKSN.Core/Finance/ForeignExchange/Data/EntityFrameworkCore/ForeignExchangeQueryRepository.cs
@@ -0,0 +1,11 @@
+﻿using TIKSN.Data.EntityFrameworkCore;
+
+namespace TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore
+{
+    public class ForeignExchangeQueryRepository : EntityQueryRepository<ExchangeRatesContext, ForeignExchangeEntity, int>
+    {
+        public ForeignExchangeQueryRepository(ExchangeRatesContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
