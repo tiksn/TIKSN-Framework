9d565e95899038bcf2fefbb2979b11cbc72a1722
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-27 16:18:10 +0300

    interfaces enhanced

--- TIKSN.Core/Finance/ForeignExchange/Data/IExchangeRateRepository.cs
+++ TIKSN.Core/Finance/ForeignExchange/Data/IExchangeRateRepository.cs
@@ -1,8 +1,12 @@
-﻿using TIKSN.Data;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Data;
 
 namespace TIKSN.Finance.ForeignExchange.Data
 {
     public interface IExchangeRateRepository : IQueryRepository<ExchangeRateEntity, int>, IRepository<ExchangeRateEntity>
     {
+        Task<ExchangeRateEntity> GetAsync(string baseCurrencyCode, string counterCurrencyCode, DateTimeOffset dateFrom, DateTimeOffset dateTo, CancellationToken cancellationToken);
     }
 }
--- TIKSN.Core/Finance/ForeignExchange/IExchangeRateService.cs
+++ TIKSN.Core/Finance/ForeignExchange/IExchangeRateService.cs
@@ -1,10 +1,15 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TIKSN.Finance.ForeignExchange
 {
     public interface IExchangeRateService
     {
+        Task<Money> ConvertCurrencyAsync(Money baseMoney, CurrencyInfo counterCurrency, DateTimeOffset asOn, CancellationToken cancellationToken = default);
+
+        Task<decimal> GetExchangeRateAsync(CurrencyPair pair, DateTimeOffset asOn, CancellationToken cancellationToken = default);
+
         Task InitializeAsync(CancellationToken cancellationToken = default);
     }
 }
