b4d11b120d6cf5bc418a91a5f5c39d45c06020c4
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-27 21:04:36 +0300

    ExchangeRateRepository implementation

--- TIKSN.Core/Finance/ForeignExchange/Data/EntityFrameworkCore/ExchangeRateRepository.cs
+++ TIKSN.Core/Finance/ForeignExchange/Data/EntityFrameworkCore/ExchangeRateRepository.cs
@@ -1,11 +1,46 @@
-﻿using TIKSN.Data.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TIKSN.Data.EntityFrameworkCore;
 
 namespace TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore
 {
-    public class ExchangeRateRepository : EntityQueryRepository<ExchangeRatesContext, ExchangeRateEntity, int>
+    public class ExchangeRateRepository : EntityQueryRepository<ExchangeRatesContext, ExchangeRateEntity, int>, IExchangeRateRepository
     {
         public ExchangeRateRepository(ExchangeRatesContext dbContext) : base(dbContext)
         {
+        }
+
+        public async Task<ExchangeRateEntity> GetAsync(
+            int foreignExchangeID,
+            string baseCurrencyCode,
+            string counterCurrencyCode,
+            DateTimeOffset asOn,
+            CancellationToken cancellationToken)
+        {
+            return Entities
+                .OrderBy(entity => Math.Abs((entity.AsOn - asOn).Ticks))
+                .First();
+        }
+
+        public async Task<IReadOnlyCollection<ExchangeRateEntity>> SearchAsync(
+            int foreignExchangeID,
+            string baseCurrencyCode,
+            string counterCurrencyCode,
+            DateTimeOffset dateFrom,
+            DateTimeOffset dateTo,
+            CancellationToken cancellationToken)
+        {
+            return await Entities
+                .Where(item =>
+                    item.BaseCurrencyCode == baseCurrencyCode &&
+                    item.CounterCurrencyCode == counterCurrencyCode &&
+                    item.ForeignExchangeID == foreignExchangeID &&
+                    item.AsOn >= dateFrom && item.AsOn <= dateTo)
+                .ToArrayAsync(cancellationToken);
         }
     }
 }
