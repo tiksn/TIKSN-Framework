43fbbc94e4a8569f7167ae6b19b05d7433e295ac
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-11-02 14:17:40 +0200

    nlog remote log viewer options

--- TIKSN.Core/Analytics/Logging/NLog/NLogLoggingSetupBase.cs
+++ TIKSN.Core/Analytics/Logging/NLog/NLogLoggingSetupBase.cs
@@ -1,41 +1,63 @@
 ﻿using Microsoft.Extensions.Logging;
 using NLog.Config;
 using NLog.Extensions.Logging;
+using NLog.Layouts;
 using NLog.Targets;
+using TIKSN.Analytics.Logging.NLog;
+using TIKSN.Configuration;
 
 namespace TIKSN.Analytics.Logging
 {
-	public abstract class NLogLoggingSetupBase : ILoggingSetup
+    public abstract class NLogLoggingSetupBase : ILoggingSetup
     {
-		protected readonly LoggingConfiguration _loggingConfiguration;
+        protected readonly LoggingConfiguration _loggingConfiguration;
         private readonly ILoggerFactory _loggerFactory;
+        private readonly IPartialConfiguration<RemoteNLogViewerOptions> _remoteNLogViewerOptions;
 
-        protected NLogLoggingSetupBase(ILoggerFactory loggerFactory)
-		{
-			_loggingConfiguration = new LoggingConfiguration();
+        protected NLogLoggingSetupBase(ILoggerFactory loggerFactory, IPartialConfiguration<RemoteNLogViewerOptions> remoteNLogViewerOptions)
+        {
+            _loggingConfiguration = new LoggingConfiguration();
             _loggerFactory = loggerFactory;
+            _remoteNLogViewerOptions = remoteNLogViewerOptions;
         }
 
-		public void Setup()
-		{
-			SetupNLog();
+        public void Setup()
+        {
+            SetupNLog();
 
-			_loggerFactory.AddNLog();
-			_loggerFactory.ConfigureNLog(_loggingConfiguration);
-		}
+            var options = _remoteNLogViewerOptions.GetConfiguration();
+            var nLogViewerTarget = new NLogViewerTarget()
+            {
+                IncludeNLogData = options.IncludeNLogData,
+                AppInfo = options.AppInfo,
+                IncludeCallSite = options.IncludeCallSite,
+                IncludeSourceInfo = options.IncludeSourceInfo,
+                IncludeMdc = options.IncludeMdc,
+                IncludeMdlc = options.IncludeMdlc,
+                IncludeNdc = options.IncludeNdc
+            };
 
-		protected abstract void SetupNLog();
+            if (options.Url != null)
+                nLogViewerTarget.Layout = Layout.FromString(options.Url.AbsoluteUri);
 
-		protected void AddForAllLevels(Target target)
-		{
-			_loggingConfiguration.AddTarget(target);
-			_loggingConfiguration.AddRuleForAllLevels(target);
-		}
+            AddForAllLevels(nLogViewerTarget);
 
-		protected void AddForOneLevel(NLog.LogLevel level, Target target)
-		{
-			_loggingConfiguration.AddTarget(target);
-			_loggingConfiguration.AddRuleForOneLevel(level, target);
-		}
-	}
+            _loggerFactory.AddNLog();
+            _loggerFactory.ConfigureNLog(_loggingConfiguration);
+        }
+
+        protected abstract void SetupNLog();
+
+        protected void AddForAllLevels(Target target)
+        {
+            _loggingConfiguration.AddTarget(target);
+            _loggingConfiguration.AddRuleForAllLevels(target);
+        }
+
+        protected void AddForOneLevel(global::NLog.LogLevel level, Target target)
+        {
+            _loggingConfiguration.AddTarget(target);
+            _loggingConfiguration.AddRuleForOneLevel(level, target);
+        }
+    }
 }
