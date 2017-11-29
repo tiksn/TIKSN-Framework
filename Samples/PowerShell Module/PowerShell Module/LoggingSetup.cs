dce606328b0f28d642fdfd555b9602e0f56e5958
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-11-29 15:27:35 +0100

    modernized and updated sample

---
+++ Samples/PowerShell Module/PowerShell Module/LoggingSetup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using TIKSN.Analytics.Logging;
+using TIKSN.PowerShell;
+
+namespace PowerShell_Module
+{
+    public class LoggingSetup : LoggingSetupBase
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public LoggingSetup(ILoggerFactory loggerFactory, IServiceProvider serviceProvider) : base(loggerFactory)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
+        public override void Setup()
+        {
+            base.Setup();
+
+            _loggerFactory.AddPowerShell(_serviceProvider);
+        }
+    }
+}
