1de55e088b4f9c1ada3a4b5782904a3cd6efb5cf
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-12-16 23:08:11 +0200

    logger added

--- TIKSN.Core/Data/EntityFrameworkCore/EntityUnitOfWork.cs
+++ TIKSN.Core/Data/EntityFrameworkCore/EntityUnitOfWork.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,22 +10,41 @@ namespace TIKSN.Data.EntityFrameworkCore
     public class EntityUnitOfWork : UnitOfWorkBase
     {
         private readonly DbContext[] _dbContexts;
+        private readonly ILogger<EntityUnitOfWork> _logger;
 
-        public EntityUnitOfWork(DbContext[] dbContexts)
+        public EntityUnitOfWork(ILogger<EntityUnitOfWork> logger, DbContext[] dbContexts)
         {
             _dbContexts = dbContexts;
+            _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
         }
 
         public override async Task CompleteAsync(CancellationToken cancellationToken)
         {
+            _logger.LogDebug("EF unit of work completion started.");
+
             var tasks = _dbContexts.Select(dbContext => dbContext.SaveChangesAsync(cancellationToken)).ToArray();
 
             await Task.WhenAll(tasks);
+
+            _logger.LogDebug("EF unit of work completion ended.");
         }
 
         protected override bool IsDirty()
         {
-            return _dbContexts.Any(dbContext => dbContext.ChangeTracker.HasChanges());
+            var changes = new List<bool>();
+
+            foreach (var dbContext in _dbContexts)
+            {
+                _logger.LogDebug($"AutoDetectChangesEnabled = {dbContext.ChangeTracker.AutoDetectChangesEnabled}");
+                dbContext.ChangeTracker.DetectChanges();
+                changes.Add(dbContext.ChangeTracker.HasChanges());
+            }
+
+            var hasChanges = changes.Any(change => change);
+
+            _logger.LogDebug($"Has changes: {hasChanges}");
+
+            return hasChanges;
         }
     }
 }
--- TIKSN.Core/Data/EntityFrameworkCore/EntityUnitOfWorkFactoryBase.cs
+++ TIKSN.Core/Data/EntityFrameworkCore/EntityUnitOfWorkFactoryBase.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace TIKSN.Data.EntityFrameworkCore
@@ -7,17 +8,25 @@ namespace TIKSN.Data.EntityFrameworkCore
     public abstract class EntityUnitOfWorkFactoryBase : IUnitOfWorkFactory
     {
         private readonly IServiceProvider serviceProvider;
+        protected readonly ILogger<EntityUnitOfWorkFactoryBase> _logger;
+        private readonly ILogger<EntityUnitOfWork> _entityUnitOfWorkLogger;
 
-        protected EntityUnitOfWorkFactoryBase(IServiceProvider serviceProvider)
+        protected EntityUnitOfWorkFactoryBase(ILogger<EntityUnitOfWorkFactoryBase> logger, ILogger<EntityUnitOfWork> entityUnitOfWorkLogger, IServiceProvider serviceProvider)
         {
             this.serviceProvider = serviceProvider;
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _entityUnitOfWorkLogger = entityUnitOfWorkLogger ?? throw new ArgumentNullException(nameof(entityUnitOfWorkLogger));
         }
 
         public IUnitOfWork Create()
         {
+            _logger.LogDebug("Creating unit of work.");
+
             var dbContexts = GetContexts();
 
-            return new EntityUnitOfWork(dbContexts);
+            _logger.LogDebug($"Created {dbContexts.Length} context(s).");
+
+            return new EntityUnitOfWork(_entityUnitOfWorkLogger, dbContexts);
         }
 
         protected abstract DbContext[] GetContexts();
