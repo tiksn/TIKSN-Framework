b0a9b026017b2665ea12298aa85c2b919ee7c0f0
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-12-25 22:48:01 +0200

    updated packages

---
+++ Samples/Shell Commander/Shell Commander/LoggingSetup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using TIKSN.Analytics.Logging;
+
+namespace Shell_Commander
+{
+    public class LoggingSetup : LoggingSetupBase
+    {
+        public LoggingSetup(ILoggerFactory loggerFactory) : base(loggerFactory)
+        {
+        }
+
+        public override void Setup()
+        {
+            base.Setup();
+
+            _loggerFactory.AddDebug(LogLevel.Debug);
+            _loggerFactory.AddConsole(LogLevel.Debug);
+        }
+    }
+}
