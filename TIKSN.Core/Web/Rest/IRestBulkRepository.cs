ef0c67ceaf574c7c79ff4a378b59453e0436f332
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-01-04 15:38:27 +0200

    removed default cancellation tokens

--- TIKSN.Core/Web/Rest/IRestBulkRepository.cs
+++ TIKSN.Core/Web/Rest/IRestBulkRepository.cs
@@ -8,12 +8,12 @@ namespace TIKSN.Web.Rest
 {
     public interface IRestBulkRepository<TEntity, TIdentity> where TEntity : IEntity<TIdentity> where TIdentity : IEquatable<TIdentity>
     {
-        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default(CancellationToken));
+        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
 
-        Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken = default(CancellationToken));
+        Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken);
 
-        Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default(CancellationToken));
+        Task RemoveAsync(TEntity entity, CancellationToken cancellationToken);
 
-        Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default(CancellationToken));
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
     }
 }
