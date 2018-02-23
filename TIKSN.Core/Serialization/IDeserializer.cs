61c9a830e2b25773bffe5c0da15cd7f7014affce
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-02-24 00:34:47 +0200

    comments added

--- TIKSN.Core/Serialization/IDeserializer.cs
+++ TIKSN.Core/Serialization/IDeserializer.cs
@@ -1,7 +1,16 @@
 ﻿namespace TIKSN.Serialization
 {
+    /// <summary>
+    /// Deserializer interface
+    /// </summary>
+    /// <typeparam name="TSerial">Type to deserialize from, usually string or byte array</typeparam>
     public interface IDeserializer<TSerial> where TSerial : class
     {
+        /// <summary>
+        /// Deserialize to <typeparamref name="TSerial"/> type
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         T Deserialize<T>(TSerial serial);
     }
 }
