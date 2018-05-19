ae51eab6a48186b9c3ffd9a5acec0cc6e840656a
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-05-19 21:20:20 +0300

    known folder configuration added

---
+++ TIKSN.Framework.SharedDesktop/FileSystem/KnownFoldersConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace TIKSN.FileSystem
+{
+    public class KnownFoldersConfiguration
+    {
+        public KnownFoldersConfiguration(Assembly mainAssembly, KnownFolderVersionConsideration versionConsideration)
+        {
+            MainAssembly = mainAssembly ?? throw new ArgumentNullException(nameof(mainAssembly));
+            VersionConsideration = versionConsideration;
+        }
+
+        public Assembly MainAssembly { get; }
+        public KnownFolderVersionConsideration VersionConsideration { get; }
+    }
+}
