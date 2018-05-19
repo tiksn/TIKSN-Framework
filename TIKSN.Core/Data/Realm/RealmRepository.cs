5e66523e1578caaf34fa955952eaff8da8333cbe
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-05-19 18:55:15 +0300

    repository implemented

--- TIKSN.Core/Data/Realm/RealmRepository.cs
+++ TIKSN.Core/Data/Realm/RealmRepository.cs
@@ -19,42 +19,59 @@ namespace TIKSN.Data.Realm
 
         public Task AddAsync(TEntity entity, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _unitOfWork.Realm.Add(entity);
+
+            return Task.CompletedTask;
         }
 
         public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return BatchOperationHelper.BatchOperationAsync(entities, cancellationToken, AddAsync);
         }
 
-        public Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken)
+        public async Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var entity = await GetOrDefaultAsync(id, cancellationToken);
+
+            if (entity == null)
+                throw new NullReferenceException($"No element were found with id '{id}'");
+
+            return entity;
         }
 
         public Task<TEntity> GetOrDefaultAsync(TIdentity id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if (id is long iid)
+                return Task.FromResult(_unitOfWork.Realm.Find<TEntity>(iid));
+
+            if (id is string sid)
+                return Task.FromResult(_unitOfWork.Realm.Find<TEntity>(sid));
+
+            return Task.FromException<TEntity>(new InvalidCastException("ID must be either long or string."));
         }
 
         public Task RemoveAsync(TEntity entity, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _unitOfWork.Realm.Remove(entity);
+
+            return Task.CompletedTask;
         }
 
         public Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return BatchOperationHelper.BatchOperationAsync(entities, cancellationToken, RemoveAsync);
         }
 
         public Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _unitOfWork.Realm.Add(entity, true);
+
+            return Task.CompletedTask;
         }
 
         public Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return BatchOperationHelper.BatchOperationAsync(entities, cancellationToken, UpdateAsync);
         }
     }
 }
