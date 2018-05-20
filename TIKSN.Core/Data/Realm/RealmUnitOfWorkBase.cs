2990c6e2e9f7dc7c43bccef7bbb67386d293f772
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-05-20 19:47:56 +0300

    RealmUnitOfWorkBase modifications

--- TIKSN.Core/Data/Realm/RealmUnitOfWorkBase.cs
+++ TIKSN.Core/Data/Realm/RealmUnitOfWorkBase.cs
@@ -1,32 +1,37 @@
-﻿using System;
+﻿using Realms;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Realms;
 
 namespace TIKSN.Data.Realm
 {
-    public abstract class RealmUnitOfWorkBase : UnitOfWorkBase, IRealmUnitOfWork
+    public abstract class RealmUnitOfWorkBase : UnitOfWorkBase
     {
+        private readonly Realms.Realm _realm;
         private readonly Transaction _transaction;
+        private bool _completed;
 
         protected RealmUnitOfWorkBase(Realms.Realm realm)
         {
-            Realm = realm ?? throw new ArgumentNullException(nameof(realm));
+            _realm = realm ?? throw new ArgumentNullException(nameof(realm));
+
             _transaction = realm.BeginWrite();
-        }
 
-        public Realms.Realm Realm { get; }
-
-        protected override bool IsDirty()
-        {
-            return Realm.IsInTransaction;
+            _completed = false;
         }
 
         public override Task CompleteAsync(CancellationToken cancellationToken)
         {
             _transaction.Commit();
 
+            _completed = true;
+
             return Task.CompletedTask;
+        }
+
+        protected override bool IsDirty()
+        {
+            return !_completed;
         }
     }
 }
