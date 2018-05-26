acaaadb2e7448089ef38fa8e2d4ce474bcb13bda
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-05-26 17:48:01 +0300

    register a settings service

--- TIKSN.Framework.Android/DependencyInjection/PlatformModule.cs
+++ TIKSN.Framework.Android/DependencyInjection/PlatformModule.cs
@@ -1,8 +1,13 @@
 using Autofac;
+using TIKSN.Settings;
 
 namespace TIKSN.DependencyInjection
 {
     public class PlatformModule : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SettingsService>().As<ISettingsService>().SingleInstance();
+        }
     }
 }
