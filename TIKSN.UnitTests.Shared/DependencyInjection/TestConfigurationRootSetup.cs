012de4f2a48fd22fadcbe16fd230b77aa87052e3
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-03-27 17:27:48 +0200

    UserSecrets added

--- TIKSN.UnitTests.Shared/DependencyInjection/TestConfigurationRootSetup.cs
+++ TIKSN.UnitTests.Shared/DependencyInjection/TestConfigurationRootSetup.cs
@@ -1,8 +1,15 @@
-﻿using TIKSN.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using TIKSN.Configuration;
 
 namespace TIKSN.DependencyInjection.Tests
 {
     public class TestConfigurationRootSetup : ConfigurationRootSetupBase
     {
+        protected override void SetupConfiguration(IConfigurationBuilder builder)
+        {
+            base.SetupConfiguration(builder);
+
+            builder.AddUserSecrets("TIKSN-Framework-tests-bee60aa7-e44d-4baf-9587-a0a3d5800585");
+        }
     }
 }
