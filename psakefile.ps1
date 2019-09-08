b5dec81440a9af216074dc8186c34c11519e5f53
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-08 19:29:37 +0300

    Update psakefile.ps1

--- psakefile.ps1
+++ psakefile.ps1
@@ -18,6 +18,21 @@ Task Init {
    $ticks = $date.Ticks
    $trashFolder = Join-Path -Path . -ChildPath ".trash"
    $script:trashFolder = Join-Path -Path $trashFolder -ChildPath $ticks.ToString("D19")
-   New-Item -Path $script:trashFolder -ItemType Directory
+   New-Item -Path $script:trashFolder -ItemType Directory | Out-Null
    $script:trashFolder = Resolve-Path -Path $script:trashFolder
+
+   $script:buildArtifactsFolder = Join-Path -Path $script:trashFolder -ChildPath "artifacts"
+   New-Item -Path $script:buildArtifactsFolder -ItemType Directory | Out-Null
+
+   $script:armBuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "any"
+   New-Item -Path $script:armBuildArtifactsFolder -ItemType Directory | Out-Null
+
+   $script:anyBuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "arm"
+   New-Item -Path $script:anyBuildArtifactsFolder -ItemType Directory | Out-Null
+
+   $script:x64BuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "x64"
+   New-Item -Path $script:x64BuildArtifactsFolder -ItemType Directory | Out-Null
+
+   $script:x86BuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "x86"
+   New-Item -Path $script:x86BuildArtifactsFolder -ItemType Directory | Out-Null
 }
