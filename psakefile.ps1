3c208ee007fdffcb1e190c807368b1c4c6c1b6be
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-08 21:25:04 +0300

    Update psakefile.ps1

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,3 +1,7 @@
+Properties {
+    $PackageId = 'TIKSN-Framework'
+}
+
 Task Publish -depends Pack {
 }
 
@@ -17,31 +21,49 @@ Task BuildUWP -depends EstimateVersions
 Task CreateReferenceAssembliesForUWP -depends EstimateVersions
 
 Task EstimateVersions -depends Clean {
+    if ($Version) {
+        $Script:NextVersion = $Version
+    }
+    else {
+        $foundPackages = Find-Package -Name $PackageId -AllVersions -AllowPrereleaseVersions -ProviderName NuGet -Source nuget.org
+
+        $foundPackages = $foundPackages | Where-Object { $_.Name -eq $PackageId }
+
+        $foundPackageVersions = $foundPackages | Select-Object -ExpandProperty Version
+        $foundPackageVersions = $foundPackageVersions | Where-Object { [System.Management.Automation.SemanticVersion]::TryParse($_, [ref][System.Management.Automation.SemanticVersion]$value) }
+        $foundPackageVersions = $foundPackageVersions | ForEach-Object { [System.Management.Automation.SemanticVersion]$_ }
+        $foundPackageVersions = $foundPackageVersions | Sort-Object -Descending
+        $latestPackageVersion = $foundPackageVersions | Select-Object -First 1
+
+        $nextBuildLabel = ([int]$latestPackageVersion.BuildLabel) + 1
+
+        $Script:NextVersion = [System.Management.Automation.SemanticVersion]::New($latestPackageVersion.Major, $latestPackageVersion.Minor, $latestPackageVersion.Patch, $latestPackageVersion.PreReleaseLabel, $nextBuildLabel)
+    }
 }
 
 Task Clean -depends Init {
 }
 
 Task Init {
-   $date = Get-Date
-   $ticks = $date.Ticks
-   $trashFolder = Join-Path -Path . -ChildPath ".trash"
-   $script:trashFolder = Join-Path -Path $trashFolder -ChildPath $ticks.ToString("D19")
-   New-Item -Path $script:trashFolder -ItemType Directory | Out-Null
-   $script:trashFolder = Resolve-Path -Path $script:trashFolder
+    $date = Get-Date
+    $ticks = $date.Ticks
+    $trashFolder = Join-Path -Path . -ChildPath ".trash"
+    $script:trashFolder = Join-Path -Path $trashFolder -ChildPath $ticks.ToString("D19")
+    New-Item -Path $script:trashFolder -ItemType Directory | Out-Null
+    $script:trashFolder = Resolve-Path -Path $script:trashFolder
 
-   $script:buildArtifactsFolder = Join-Path -Path $script:trashFolder -ChildPath "artifacts"
-   New-Item -Path $script:buildArtifactsFolder -ItemType Directory | Out-Null
+    $script:buildArtifactsFolder = Join-Path -Path $script:trashFolder -ChildPath "artifacts"
+    New-Item -Path $script:buildArtifactsFolder -ItemType Directory | Out-Null
 
-   $script:armBuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "any"
-   New-Item -Path $script:armBuildArtifactsFolder -ItemType Directory | Out-Null
+    $script:armBuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "any"
+    New-Item -Path $script:armBuildArtifactsFolder -ItemType Directory | Out-Null
 
-   $script:anyBuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "arm"
-   New-Item -Path $script:anyBuildArtifactsFolder -ItemType Directory | Out-Null
+    $script:anyBuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "arm"
+    New-Item -Path $script:anyBuildArtifactsFolder -ItemType Directory | Out-Null
 
-   $script:x64BuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "x64"
-   New-Item -Path $script:x64BuildArtifactsFolder -ItemType Directory | Out-Null
+    $script:x64BuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "x64"
+    New-Item -Path $script:x64BuildArtifactsFolder -ItemType Directory | Out-Null
 
-   $script:x86BuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "x86"
-   New-Item -Path $script:x86BuildArtifactsFolder -ItemType Directory | Out-Null
+    $script:x86BuildArtifactsFolder = Join-Path -Path $script:buildArtifactsFolder -ChildPath "x86"
+    New-Item -Path $script:x86BuildArtifactsFolder -ItemType Directory | Out-Null
 }
