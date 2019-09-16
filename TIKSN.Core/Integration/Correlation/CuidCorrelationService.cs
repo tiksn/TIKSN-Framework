ab18be217397c171d2cbb9c3a721e66b5ce88087
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-16 14:00:42 +0300

    Create CuidCorrelationService.cs

---
+++ TIKSN.Core/Integration/Correlation/CuidCorrelationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TIKSN.Integration.Correlation
+{
+    public class CuidCorrelationService : ICorrelationService
+    {
+        public CorrelationID Create(string stringRepresentation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CorrelationID Create(byte[] byteArrayRepresentation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CorrelationID Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
