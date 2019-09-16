b14a9b65572500861e9082d6310836592b796cce
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-16 13:23:15 +0300

    Create CorrelationID.cs

---
+++ TIKSN.Core/Integration/Correlation/CorrelationID.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace TIKSN.Integration.Correlation
+{
+    /// <summary>
+    /// Correlation ID
+    /// </summary>
+    public struct CorrelationID : IEquatable<CorrelationID>
+    {
+        /// <summary>
+        /// Empty Correlation ID.
+        /// </summary>
+        public static readonly CorrelationID Empty = new CorrelationID();
+
+        private readonly byte[] _byteArrayRepresentation;
+        private readonly string _stringRepresentation;
+
+        internal CorrelationID(string stringRepresentation, byte[] byteArrayRepresentation)
+        {
+            _stringRepresentation = stringRepresentation ?? throw new ArgumentNullException(nameof(stringRepresentation));
+            _byteArrayRepresentation = byteArrayRepresentation ?? throw new ArgumentNullException(nameof(byteArrayRepresentation));
+        }
+
+        public static implicit operator byte[](CorrelationID correlationId)
+        {
+            return correlationId._byteArrayRepresentation;
+        }
+
+        public static implicit operator string(CorrelationID correlationId)
+        {
+            return correlationId._stringRepresentation;
+        }
+
+        /// <summary>
+        /// Indicates whether this instance and a <paramref name="other"/> are equal by their binary representation.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(CorrelationID other)
+        {
+            return _byteArrayRepresentation.SequenceEqual(other._byteArrayRepresentation);
+        }
+
+        /// <summary>
+        /// Indicates whether this instance and a specified object are equal.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is CorrelationID))
+                return false;
+
+            var other = (CorrelationID)obj;
+
+            return Equals(other);
+        }
+
+        /// <summary>
+        /// Returns binary representation.
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ToByteArray()
+        {
+            return _byteArrayRepresentation;
+        }
+
+        /// <summary>
+        /// Returns string representation.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return _stringRepresentation;
+        }
+    }
+}
