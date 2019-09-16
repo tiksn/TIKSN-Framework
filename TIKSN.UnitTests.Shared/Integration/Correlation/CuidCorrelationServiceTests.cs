218e9c8dfede0556f6d8b9e7c8b47798e980f37f
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-16 14:04:59 +0300

    Create CuidCorrelationServiceTests.cs

---
+++ TIKSN.UnitTests.Shared/Integration/Correlation/CuidCorrelationServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TIKSN.DependencyInjection;
+
+namespace TIKSN.Integration.Correlation.Tests
+{
+    public class CuidCorrelationServiceTests
+    {
+        private readonly ICorrelationService _correlationService;
+        public CuidCorrelationServiceTests()
+        {
+            var services = new ServiceCollection();
+            services.AddFrameworkPlatform();
+            services.AddSingleton<ICorrelationService, CuidCorrelationService>();
+            var serviceProvider = services.BuildServiceProvider();
+            _correlationService = serviceProvider.GetRequiredService<ICorrelationService>();
+        }
+    }
+}
