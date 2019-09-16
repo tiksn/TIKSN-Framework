194c39914e04d106e8e35af376988d5b54ecbcf9
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-16 14:09:55 +0300

    Update CuidCorrelationServiceTests.cs

--- TIKSN.UnitTests.Shared/Integration/Correlation/CuidCorrelationServiceTests.cs
+++ TIKSN.UnitTests.Shared/Integration/Correlation/CuidCorrelationServiceTests.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 using TIKSN.DependencyInjection;
+using Xunit;
 
 namespace TIKSN.Integration.Correlation.Tests
 {
     public class CuidCorrelationServiceTests
     {
         private readonly ICorrelationService _correlationService;
+
         public CuidCorrelationServiceTests()
         {
             var services = new ServiceCollection();
@@ -13,6 +16,18 @@ namespace TIKSN.Integration.Correlation.Tests
             services.AddSingleton<ICorrelationService, CuidCorrelationService>();
             var serviceProvider = services.BuildServiceProvider();
             _correlationService = serviceProvider.GetRequiredService<ICorrelationService>();
+        }
+
+        [Fact]
+        public void GenerateAndParse()
+        {
+            var correlationID = _correlationService.Generate();
+            var correlationIDFromString = _correlationService.Create(correlationID.ToString());
+            var correlationIDFromBytes = _correlationService.Create(correlationID.ToByteArray());
+
+            correlationIDFromString.Should().Be(correlationID);
+            correlationIDFromBytes.Should().Be(correlationID);
+            correlationIDFromString.Should().Be(correlationIDFromBytes);
         }
     }
 }
