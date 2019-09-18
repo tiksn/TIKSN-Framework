f82a083e90f17bfcb1a09b1d4a584a792d044318
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-18 16:21:59 +0300

    Create Base62CorrelationService.cs

---
+++ TIKSN.Core/Integration/Correlation/Base62CorrelationService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+
+namespace TIKSN.Integration.Correlation
+{
+    public class Base62CorrelationService
+    {
+        private readonly Random _random;
+        private readonly IOptions<Base62CorrelationServiceOptions> _base62CorrelationServiceOptions;
+
+        public Base62CorrelationService(Random random, IOptions<Base62CorrelationServiceOptions> base62CorrelationServiceOptions)
+        {
+            _random = random ?? throw new ArgumentNullException(nameof(random));
+            _base62CorrelationServiceOptions = base62CorrelationServiceOptions ?? throw new ArgumentNullException(nameof(base62CorrelationServiceOptions));
+        }
+    }
+}
