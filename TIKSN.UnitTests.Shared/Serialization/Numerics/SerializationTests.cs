397c0f8a4bcda10dce9d17a7c3b1601679a0169c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-18 17:16:31 +0300

    Create SerializationTests.cs

---
+++ TIKSN.UnitTests.Shared/Serialization/Numerics/SerializationTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using System;
+using System.Numerics;
+using Xunit;
+
+namespace TIKSN.Serialization.Numerics.Tests
+{
+    public class SerializationTests
+    {
+        [Fact]
+        public void DeserializeSerializeUnsignedBigInteger()
+        {
+            var rng = new Random();
+            UnsignedBigIntegerBinarySerializer serializer = new UnsignedBigIntegerBinarySerializer();
+            UnsignedBigIntegerBinaryDeserializer deserializer = new UnsignedBigIntegerBinaryDeserializer();
+
+            var number = BigInteger.One;
+            number *= rng.Next();
+            number *= rng.Next();
+            number *= rng.Next();
+            number *= rng.Next();
+
+            var bytes = serializer.Serialize(number);
+            var recovered = deserializer.Deserialize(bytes);
+
+            recovered.Should().Be(number);
+        }
+    }
+}
