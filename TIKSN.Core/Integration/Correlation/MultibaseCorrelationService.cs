e91ec5c68cf4c46d6b97e1e46da172610e1fa124
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-18 18:32:29 +0300

    Create MultibaseCorrelationService.cs

---
+++ TIKSN.Core/Integration/Correlation/MultibaseCorrelationService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using Multiformats.Base;
+using System;
+
+namespace TIKSN.Integration.Correlation
+{
+    public class MultibaseCorrelationService : ICorrelationService
+    {
+        private readonly IOptions<MultibaseCorrelationServiceOptions> _multibaseCorrelationServiceOptions;
+        private readonly Random _random;
+
+        public MultibaseCorrelationService(
+            Random random,
+            IOptions<MultibaseCorrelationServiceOptions> multibaseCorrelationServiceOptions)
+        {
+            _random = random ?? throw new ArgumentNullException(nameof(random));
+            _multibaseCorrelationServiceOptions = multibaseCorrelationServiceOptions ?? throw new ArgumentNullException(nameof(multibaseCorrelationServiceOptions));
+        }
+
+        public CorrelationID Create(string stringRepresentation)
+        {
+            byte[] byteArrayRepresentation = Multibase.Decode(stringRepresentation, out MultibaseEncoding encoding);
+            return new CorrelationID(stringRepresentation, byteArrayRepresentation);
+        }
+
+        public CorrelationID Create(byte[] byteArrayRepresentation)
+        {
+            string stringRepresentation = Multibase.Encode(_multibaseCorrelationServiceOptions.Value.Encoding, byteArrayRepresentation);
+            return new CorrelationID(stringRepresentation, byteArrayRepresentation);
+        }
+
+        public CorrelationID Generate()
+        {
+            var byteArrayRepresentation = new byte[_multibaseCorrelationServiceOptions.Value.ByteLength];
+            _random.NextBytes(byteArrayRepresentation);
+            return Create(byteArrayRepresentation);
+        }
+    }
+}
