cd6365e9745046c856be2b4edeac1f254392a76a
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-18 16:47:31 +0300

    Create Base62CorrelationServiceTests.cs

---
+++ TIKSN.UnitTests.Shared/Integration/Correlation/Base62CorrelationServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using TIKSN.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TIKSN.Integration.Correlation.Tests
+{
+    public class Base62CorrelationServiceTests
+    {
+        private readonly ICorrelationService _correlationService;
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Base62CorrelationServiceTests(ITestOutputHelper testOutputHelper)
+        {
+            var services = new ServiceCollection();
+            services.AddFrameworkPlatform();
+            services.AddSingleton<ICorrelationService, Base62CorrelationService>();
+            var serviceProvider = services.BuildServiceProvider();
+            _correlationService = serviceProvider.GetRequiredService<ICorrelationService>();
+            _testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+        }
+
+        [Fact]
+        public void GenerateAndParse()
+        {
+            var correlationID = _correlationService.Generate();
+            LogOutput(correlationID, nameof(correlationID));
+            var correlationIDFromString = _correlationService.Create(correlationID.ToString());
+            LogOutput(correlationIDFromString, nameof(correlationIDFromString));
+            var correlationIDFromBytes = _correlationService.Create(correlationID.ToByteArray());
+            LogOutput(correlationIDFromBytes, nameof(correlationIDFromBytes));
+
+            correlationIDFromString.Should().Be(correlationID);
+            correlationIDFromBytes.Should().Be(correlationID);
+            correlationIDFromString.Should().Be(correlationIDFromBytes);
+        }
+
+        private void LogOutput(CorrelationID correlationID, string name)
+        {
+            _testOutputHelper.WriteLine("-------------------------");
+            _testOutputHelper.WriteLine(name);
+            _testOutputHelper.WriteLine(correlationID.ToString());
+            _testOutputHelper.WriteLine(BitConverter.ToString(correlationID.ToByteArray()));
+            _testOutputHelper.WriteLine("");
+        }
+    }
+}
