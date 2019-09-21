69ea7c7ea4d82c6edc624534cd748bcbf547f841
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 13:43:59 +0300

    Create SerializerException.cs

---
+++ TIKSN.Core/Serialization/SerializerException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TIKSN.Serialization
+{
+    [Serializable]
+    public class SerializerException : Exception
+    {
+        public SerializerException()
+        {
+        }
+
+        public SerializerException(string message) : base(message)
+        {
+        }
+
+        public SerializerException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected SerializerException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
