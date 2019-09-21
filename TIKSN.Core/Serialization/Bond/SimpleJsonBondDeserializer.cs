4fbda9e790afc3774d0328c41ed5dbe3614c8e2b
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 14:36:23 +0300

    Update SimpleJsonBondDeserializer.cs

--- TIKSN.Core/Serialization/Bond/SimpleJsonBondDeserializer.cs
+++ TIKSN.Core/Serialization/Bond/SimpleJsonBondDeserializer.cs
@@ -1,15 +1,10 @@
 ﻿using Bond.Protocols;
 using System.IO;
-using TIKSN.Analytics.Telemetry;
 
 namespace TIKSN.Serialization.Bond
 {
     public class SimpleJsonBondDeserializer : DeserializerBase<string>
     {
-        public SimpleJsonBondDeserializer(IExceptionTelemeter exceptionTelemeter) : base(exceptionTelemeter)
-        {
-        }
-
         protected override T DeserializeInternal<T>(string serial)
         {
             var reader = new SimpleJsonReader(new StringReader(serial));
