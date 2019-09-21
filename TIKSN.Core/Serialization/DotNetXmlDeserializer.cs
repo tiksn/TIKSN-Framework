d64956799a756e358e73e135095a3195fcbb8dc5
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 14:36:10 +0300

    Update DotNetXmlDeserializer.cs

--- TIKSN.Core/Serialization/DotNetXmlDeserializer.cs
+++ TIKSN.Core/Serialization/DotNetXmlDeserializer.cs
@@ -1,16 +1,11 @@
 using System.IO;
 using System.Text;
 using System.Xml.Serialization;
-using TIKSN.Analytics.Telemetry;
 
 namespace TIKSN.Serialization
 {
     public class DotNetXmlDeserializer : DeserializerBase<string>
     {
-        public DotNetXmlDeserializer(IExceptionTelemeter exceptionTelemeter) : base(exceptionTelemeter)
-        {
-        }
-
         protected override T DeserializeInternal<T>(string serial)
         {
             if (string.IsNullOrEmpty(serial))
