87c3b9e7fef807bab5a7f6f487a011db1e9d2d6d
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-21 14:36:26 +0300

    Update SimpleBinaryBondSerializer.cs

--- TIKSN.Core/Serialization/Bond/SimpleBinaryBondSerializer.cs
+++ TIKSN.Core/Serialization/Bond/SimpleBinaryBondSerializer.cs
@@ -1,15 +1,10 @@
 ﻿using Bond.IO.Safe;
 using Bond.Protocols;
-using TIKSN.Analytics.Telemetry;
 
 namespace TIKSN.Serialization.Bond
 {
     public class SimpleBinaryBondSerializer : SerializerBase<byte[]>
     {
-        public SimpleBinaryBondSerializer(IExceptionTelemeter exceptionTelemeter) : base(exceptionTelemeter)
-        {
-        }
-
         protected override byte[] SerializeInternal(object obj)
         {
             var output = new OutputBuffer();
