9fad7a14bedd421f1f739204cc7e0183572f8e73
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-22 18:03:40 +0300

    Update JsonSerializer.cs

--- TIKSN.Core/Serialization/JsonSerializer.cs
+++ TIKSN.Core/Serialization/JsonSerializer.cs
@@ -4,7 +4,7 @@ namespace TIKSN.Serialization
 {
     public class JsonSerializer : SerializerBase<string>
     {
-        protected override string SerializeInternal(object obj)
+        protected override string SerializeInternal<T>(T obj)
         {
             return JsonConvert.SerializeObject(obj);
         }
