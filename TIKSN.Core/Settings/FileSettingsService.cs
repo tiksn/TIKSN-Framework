56037e32a0b50529bd8c9da36e4a822a4932b848
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-02-13 21:08:23 +0400

    Update FileSettingsService.cs

--- TIKSN.Core/Settings/FileSettingsService.cs
+++ TIKSN.Core/Settings/FileSettingsService.cs
@@ -83,10 +83,11 @@ namespace TIKSN.Settings
             var db = new LiteDatabase(connectionString);
 
             settingsCollection = db.GetCollection("Settings");
-            bsonDocument = settingsCollection.FindOne(x => true);
+            bsonDocument = settingsCollection.FindById(Guid.Empty);
             if (bsonDocument == null)
             {
                 bsonDocument = new BsonDocument();
+                bsonDocument.Add("_id", Guid.Empty);
                 settingsCollection.Insert(bsonDocument);
             }
 
@@ -100,7 +101,8 @@ namespace TIKSN.Settings
                 if (bsonValue.IsNull)
                     return defaultValue;
 
-                var typeCode = Type.GetTypeCode(typeof(T));
+                var type = typeof(T);
+                var typeCode = Type.GetTypeCode(type);
 
                 void CheckType(BsonType bsonType)
                 {
@@ -115,30 +117,50 @@ namespace TIKSN.Settings
                     case TypeCode.Boolean:
                         CheckType(BsonType.Boolean);
                         return (T)(object)bsonValue.AsBoolean;
+
                     case TypeCode.Byte:
                     case TypeCode.Char:
                     case TypeCode.Int16:
                     case TypeCode.Int32:
                     case TypeCode.SByte:
                     case TypeCode.UInt16:
+                        CheckType(BsonType.Int32);
                         return (T)(object)bsonValue.AsInt32;
+
                     case TypeCode.DateTime:
+                        CheckType(BsonType.DateTime);
                         return (T)(object)bsonValue.AsDateTime;
+
                     case TypeCode.Decimal:
                     case TypeCode.UInt64:
+                        CheckType(BsonType.Decimal);
                         return (T)(object)bsonValue.AsDecimal;
+
                     case TypeCode.Double:
                     case TypeCode.Single:
+                        CheckType(BsonType.Double);
                         return (T)(object)bsonValue.AsDouble;
-                    case TypeCode.Empty:
-                        return defaultValue;
+
                     case TypeCode.Int64:
                     case TypeCode.UInt32:
+                        CheckType(BsonType.Int64);
                         return (T)(object)bsonValue.AsInt64;
+
                     case TypeCode.String:
+                        CheckType(BsonType.String);
                         return (T)(object)bsonValue.AsString;
+
                     default:
-                        throw new NotSupportedException("Type is not supported.");
+                        if (type == typeof(Guid))
+                        {
+                            CheckType(BsonType.Guid);
+                            return (T)(object)bsonValue.AsGuid;
+                            break;
+                        }
+                        else
+                        {
+                            throw new NotSupportedException("Type is not supported.");
+                        }
                 }
             }
 
@@ -164,58 +186,83 @@ namespace TIKSN.Settings
         {
             object valueObject = value;
 
-            switch (Type.GetTypeCode(typeof(T)))
+            var type = typeof(T);
+            switch (Type.GetTypeCode(type))
             {
                 case TypeCode.Boolean:
                     document[name] = new BsonValue((bool)valueObject);
                     break;
+
                 case TypeCode.Byte:
                     document[name] = new BsonValue((byte)valueObject);
                     break;
+
                 case TypeCode.Char:
                     document[name] = new BsonValue((char)valueObject);
                     break;
+
                 case TypeCode.DateTime:
                     document[name] = new BsonValue((DateTime)valueObject);
                     break;
+
                 case TypeCode.Decimal:
                     document[name] = new BsonValue((decimal)valueObject);
                     break;
+
                 case TypeCode.Double:
                     document[name] = new BsonValue((double)valueObject);
                     break;
+
                 case TypeCode.Empty:
                     document[name] = BsonValue.Null;
                     break;
+
                 case TypeCode.Int16:
                     document[name] = new BsonValue((short)valueObject);
                     break;
+
                 case TypeCode.Int32:
                     document[name] = new BsonValue((int)valueObject);
                     break;
+
                 case TypeCode.Int64:
                     document[name] = new BsonValue((long)valueObject);
                     break;
+
                 case TypeCode.SByte:
                     document[name] = new BsonValue((sbyte)valueObject);
                     break;
+
                 case TypeCode.Single:
                     document[name] = new BsonValue((float)valueObject);
                     break;
+
                 case TypeCode.String:
                     document[name] = new BsonValue((string)valueObject);
                     break;
+
                 case TypeCode.UInt16:
                     document[name] = new BsonValue((ushort)valueObject);
                     break;
+
                 case TypeCode.UInt32:
                     document[name] = new BsonValue((uint)valueObject);
                     break;
+
                 case TypeCode.UInt64:
                     document[name] = new BsonValue((decimal)valueObject);
                     break;
+
                 default:
-                    throw new NotSupportedException("Type is not supported.");
+                    if (type == typeof(Guid))
+                    {
+                        document[name] = new BsonValue((Guid)valueObject);
+                        break;
+                    }
+                    else
+                    {
+                        throw new NotSupportedException("Type is not supported.");
+                    }
             }
             return value;
         }
