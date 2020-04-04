8d8d7da0019a8e0145f155d3be21d62c69cae5b5
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 23:26:28 +0400

    Create WindowsRegistrySettingsServiceOptions.cs

---
+++ TIKSN.Framework.Core/Settings/WindowsRegistrySettingsServiceOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Win32;
+
+namespace TIKSN.Settings
+{
+    public class WindowsRegistrySettingsServiceOptions
+    {
+        public WindowsRegistrySettingsServiceOptions()
+        {
+            RegistryView = RegistryView.Default;
+        }
+
+        public RegistryView RegistryView { get; set; }
+
+        public string SubKey { get; set; }
+    }
+}
