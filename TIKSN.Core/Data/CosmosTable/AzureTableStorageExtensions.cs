d512000c18bed54ba15b4913132d6b2610953f3d
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-07-28 17:25:22 +0400

    Update AzureTableStorageExtensions.cs

--- TIKSN.Core/Data/CosmosTable/AzureTableStorageExtensions.cs
+++ TIKSN.Core/Data/CosmosTable/AzureTableStorageExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace TIKSN.Data.AzureStorage
+namespace TIKSN.Data.CosmosTable
 {
     public static class AzureTableStorageExtensions
     {
