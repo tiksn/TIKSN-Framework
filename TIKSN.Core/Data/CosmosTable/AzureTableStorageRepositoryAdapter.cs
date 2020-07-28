d20add0e7a7e7a11a00123520c0b6f16723063ff
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-07-28 17:25:41 +0400

    Update AzureTableStorageRepositoryAdapter.cs

--- TIKSN.Core/Data/CosmosTable/AzureTableStorageRepositoryAdapter.cs
+++ TIKSN.Core/Data/CosmosTable/AzureTableStorageRepositoryAdapter.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace TIKSN.Data.AzureStorage
+namespace TIKSN.Data.CosmosTable
 {
     public class AzureTableStorageRepositoryAdapter<T> : IRepository<T> where T : ITableEntity
     {
