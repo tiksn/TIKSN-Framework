09893a64a889eddcd3d7767d7772559c522a068a
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-01-26 22:50:22 +0200

    Update ILiteDbRepository.cs

--- TIKSN.Core/Data/LiteDB/ILiteDbRepository.cs
+++ TIKSN.Core/Data/LiteDB/ILiteDbRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace TIKSN.Data.LiteDB
 {
-    public interface ILiteDbRepository<TDocument, TIdentity> : IRepository<TDocument>, IQueryRepository<TDocument, TIdentity> where TDocument : IEntity<TIdentity> where TIdentity : IEquatable<TIdentity>
+    public interface ILiteDbRepository<TDocument, TIdentity> : IRepository<TDocument>, IQueryRepository<TDocument, TIdentity>, IStreamRepository<TDocument> where TDocument : IEntity<TIdentity> where TIdentity : IEquatable<TIdentity>
     {
         Task AddOrUpdateAsync(TDocument entity, CancellationToken cancellationToken);
 
