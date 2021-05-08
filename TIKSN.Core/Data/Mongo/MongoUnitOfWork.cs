a19daaff9a21fd1c61c0bbc97efc37768e0d96cf
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 02:43:48 +0300

    Create MongoUnitOfWork.cs

---
+++ TIKSN.Core/Data/Mongo/MongoUnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace TIKSN.Data.Mongo
+{
+    public class MongoUnitOfWork : UnitOfWorkBase, IMongoUnitOfWork
+    {
+        protected readonly IClientSessionHandle _clientSessionHandle;
+        private readonly IServiceScope _serviceScope;
+
+        public MongoUnitOfWork(IClientSessionHandle clientSessionHandle, IServiceScope serviceScope)
+        {
+            _clientSessionHandle = clientSessionHandle ?? throw new ArgumentNullException(nameof(clientSessionHandle));
+            _serviceScope = serviceScope ?? throw new ArgumentNullException(nameof(serviceScope));
+        }
+
+        public override Task CompleteAsync(CancellationToken cancellationToken)
+        {
+            return _clientSessionHandle.CommitTransactionAsync(cancellationToken);
+        }
+
+        public override Task DiscardAsync(CancellationToken cancellationToken)
+        {
+            return _clientSessionHandle.AbortTransactionAsync(cancellationToken);
+        }
+
+        protected override bool IsDirty()
+        {
+            return _clientSessionHandle.WrappedCoreSession.IsDirty;
+        }
+
+        public override void Dispose()
+        {
+            _serviceScope?.Dispose();
+            
+            base.Dispose();
+        }
+
+        public IServiceProvider Services => _serviceScope.ServiceProvider;
+    }
+}
