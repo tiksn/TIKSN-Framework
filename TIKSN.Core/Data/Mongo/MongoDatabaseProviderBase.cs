64c03aec0b7cb8cd4afd43a8db9936f5b48dda63
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 02:43:36 +0300

    Create MongoDatabaseProviderBase.cs

---
+++ TIKSN.Core/Data/Mongo/MongoDatabaseProviderBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace TIKSN.Data.Mongo
+{
+    public abstract class MongoDatabaseProviderBase : IMongoDatabaseProvider
+    {
+        private readonly IMongoClientProvider _mongoClientProvider;
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionStringKey;
+
+        public MongoDatabaseProviderBase(IMongoClientProvider mongoClientProvider, IConfiguration configuration, string connectionStringKey)
+        {
+            _mongoClientProvider = mongoClientProvider ?? throw new ArgumentNullException(nameof(mongoClientProvider));
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _connectionStringKey = connectionStringKey ?? throw new ArgumentNullException(nameof(connectionStringKey));
+        }
+
+        public IMongoDatabase GetDatabase()
+        {
+            var mongoClient = _mongoClientProvider.GetMongoClient();
+            var connectionString = _configuration.GetConnectionString(_connectionStringKey);
+            var databaseName = MongoUrl.Create(connectionString).DatabaseName;
+            return mongoClient.GetDatabase(databaseName);
+        }
+    }
+}
