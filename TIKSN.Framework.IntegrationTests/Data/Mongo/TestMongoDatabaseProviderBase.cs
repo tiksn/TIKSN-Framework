7d17d41f733c3826abf1ba2faf5b1cfcea018ae6
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 02:46:00 +0300

    Create TestMongoDatabaseProviderBase.cs

---
+++ TIKSN.Framework.IntegrationTests/Data/Mongo/TestMongoDatabaseProviderBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using TIKSN.Data.Mongo;
+
+namespace TIKSN.Framework.IntegrationTests.Data.Mongo
+{
+    public class TestMongoDatabaseProviderBase : MongoDatabaseProviderBase
+    {
+        public TestMongoDatabaseProviderBase(
+            IMongoClientProvider mongoClientProvider,
+            IConfiguration configuration) : base(
+            mongoClientProvider,
+            configuration,
+            "Mongo")
+        {
+        }
+    }
+}
