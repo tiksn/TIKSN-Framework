fa6331ea12ffaab9de072fdc397a8b61c3502921
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-08 21:55:30 +0300

    Create TestMongoEntity.cs

---
+++ TIKSN.Framework.IntegrationTests/Data/Mongo/TestMongoEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+using TIKSN.Data;
+
+namespace TIKSN.Framework.IntegrationTests.Data.Mongo
+{
+    public class TestMongoEntity : IEntity<Guid>
+    {
+        public Guid Value { get; set; }
+
+        [BsonId]
+        public Guid ID { get; set; }
+    }
+}
