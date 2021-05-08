8662f38fec4b561a1ac76ceece8a408197ad8499
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-08 21:55:53 +0300

    Create TestMongoRepository.cs

---
+++ TIKSN.Framework.IntegrationTests/Data/Mongo/TestMongoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TIKSN.Data.Mongo;
+
+namespace TIKSN.Framework.IntegrationTests.Data.Mongo
+{
+    public class TestMongoRepository : MongoRepository<TestMongoEntity, Guid>, ITestMongoRepository
+    {
+        public TestMongoRepository(IMongoDatabaseProvider mongoDatabaseProvider) : base(mongoDatabaseProvider, "Tests")
+        {
+        }
+    }
+}
