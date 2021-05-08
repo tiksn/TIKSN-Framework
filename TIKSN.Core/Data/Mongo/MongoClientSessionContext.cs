4c5b23fc5d5dadb29d37d0cdae97c65ade413f10
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 02:41:48 +0300

    Create MongoClientSessionContext.cs

---
+++ TIKSN.Core/Data/Mongo/MongoClientSessionContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LanguageExt;
+using MongoDB.Driver;
+
+namespace TIKSN.Data.Mongo
+{
+    public class MongoClientSessionContext : IMongoClientSessionStore, IMongoClientSessionProvider
+    {
+        private Option<IClientSessionHandle> _clientSessionHandle = Option<IClientSessionHandle>.None;
+
+        public Option<IClientSessionHandle> GetClientSessionHandle()
+        {
+            return _clientSessionHandle;
+        }
+
+        public void SetClientSessionHandle(IClientSessionHandle clientSessionHandle)
+        {
+            if (clientSessionHandle == null) throw new ArgumentNullException(nameof(clientSessionHandle));
+
+            _clientSessionHandle = Option<IClientSessionHandle>.Some(clientSessionHandle);
+        }
+
+        public void ClearClientSessionHandle()
+        {
+            _clientSessionHandle = Option<IClientSessionHandle>.None;
+        }
+    }
+}
