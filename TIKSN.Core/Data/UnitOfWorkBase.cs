b7fe0481f8442dea6528655d9141b0e9623a01d2
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 02:45:12 +0300

    Update UnitOfWorkBase.cs

--- TIKSN.Core/Data/UnitOfWorkBase.cs
+++ TIKSN.Core/Data/UnitOfWorkBase.cs
@@ -7,11 +7,22 @@ namespace TIKSN.Data
     public abstract class UnitOfWorkBase : IUnitOfWork
     {
         public abstract Task CompleteAsync(CancellationToken cancellationToken);
+        public abstract Task DiscardAsync(CancellationToken cancellationToken);
 
         public virtual void Dispose()
         {
             if (IsDirty())
+            {
                 throw new InvalidOperationException("Unit of work disposed without completion.");
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (IsDirty())
+            {
+                await DiscardAsync(default);
+            }
         }
 
         protected abstract bool IsDirty();
