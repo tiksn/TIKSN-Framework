0e50338b475c3aa90394647bdfa1bc0215d2f29a
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 02:41:41 +0300

    Create MongoClientProviderBase.cs

---
+++ TIKSN.Core/Data/Mongo/MongoClientProviderBase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace TIKSN.Data.Mongo
+{
+    public abstract class MongoClientProviderBase : IMongoClientProvider
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionStringKey;
+        private readonly object _locker;
+        private MongoClient _mongoClient;
+
+        protected MongoClientProviderBase(IConfiguration configuration, string connectionStringKey)
+        {
+            _locker = new object();
+            _configuration = configuration;
+            _connectionStringKey = connectionStringKey;
+        }
+
+        public IMongoClient GetMongoClient()
+        {
+            if (_mongoClient == null)
+            {
+                lock (_locker)
+                {
+                    if (_mongoClient == null)
+                    {
+                        var connectionString = _configuration.GetConnectionString(_connectionStringKey);
+                        var mongoUrl = MongoUrl.Create(connectionString);
+                        _mongoClient = new MongoClient(mongoUrl);
+                    }
+                }
+            }
+
+            return _mongoClient;
+        }
+    }
+}
