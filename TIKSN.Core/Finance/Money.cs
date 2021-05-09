ba62626209210ac860bd0de0aff155cdfff603e8
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 16:55:16 +0300

    Update Money.cs

--- TIKSN.Core/Finance/Money.cs
+++ TIKSN.Core/Finance/Money.cs
@@ -1,277 +1,57 @@
-﻿namespace TIKSN.Finance
-{
-    public class Money : System.IEquatable<Money>, System.IComparable<Money>, System.IFormattable
-    {
-        private decimal amount;
-        private CurrencyInfo currency;
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
 
+namespace TIKSN.Finance
+{
+    public class Money : IEquatable<Money>, IComparable<Money>, IFormattable
+    {
         public Money(CurrencyInfo Currency, decimal Amount = decimal.Zero)
         {
-            this.currency = Currency;
-            this.amount = Amount;
+            this.Currency = Currency;
+            this.Amount = Amount;
         }
 
-        public decimal Amount
-        {
-            get
-            {
-                return this.amount;
-            }
-        }
+        public decimal Amount { get; }
 
-        public CurrencyInfo Currency
-        {
-            get
-            {
-                return this.currency;
-            }
-        }
-
-        public static Money operator -(Money money)
-        {
-            return new Money(money.currency, -money.amount);
-        }
-
-        public static Money operator -(Money first, Money second)
-        {
-            AssertCurrencyIdentity(first, second);
-
-            return new Money(first.currency, first.amount - second.amount);
-        }
-
-        public static bool operator !=(Money first, Money second)
-        {
-            return !first.Equals(second);
-        }
-
-        public static Money operator %(Money dividend, sbyte divisor)
-        {
-            return new Money(dividend.currency, dividend.amount % divisor);
-        }
-
-        public static Money operator %(Money dividend, byte divisor)
-        {
-            return new Money(dividend.currency, dividend.amount % divisor);
-        }
-
-        public static Money operator %(Money dividend, short divisor)
-        {
-            return new Money(dividend.currency, dividend.amount % divisor);
-        }
-
-        public static Money operator %(Money dividend, ushort divisor)
-        {
-            return new Money(dividend.currency, dividend.amount % divisor);
-        }
-
-        public static Money operator %(Money dividend, int divisor)
-        {
-            return new Money(dividend.currency, dividend.amount % divisor);
-        }
-
-        public static Money operator %(Money dividend, uint divisor)
-        {
-            return new Money(dividend.currency, dividend.amount % divisor);
-        }
-
-        public static Money operator %(Money dividend, long divisor)
-        {
-            return new Money(dividend.currency, dividend.amount % divisor);
-        }
-
-        public static Money operator %(Money dividend, ulong divisor)
-        {
-            return new Money(dividend.currency, dividend.amount % divisor);
-        }
-
-        public static Money operator %(Money dividend, decimal divisor)
-        {
-            return new Money(dividend.currency, dividend.amount % divisor);
-        }
-
-        public static Money operator *(Money first, sbyte second)
-        {
-            return new Money(first.currency, first.amount * second);
-        }
-
-        public static Money operator *(Money first, byte second)
-        {
-            return new Money(first.currency, first.amount * second);
-        }
-
-        public static Money operator *(Money first, short second)
-        {
-            return new Money(first.currency, first.amount * second);
-        }
-
-        public static Money operator *(Money first, ushort second)
-        {
-            return new Money(first.currency, first.amount * second);
-        }
-
-        public static Money operator *(Money first, int second)
-        {
-            return new Money(first.currency, first.amount * second);
-        }
-
-        public static Money operator *(Money first, uint second)
-        {
-            return new Money(first.currency, first.amount * second);
-        }
-
-        public static Money operator *(Money first, long second)
-        {
-            return new Money(first.currency, first.amount * second);
-        }
-
-        public static Money operator *(Money first, ulong second)
-        {
-            return new Money(first.currency, first.amount * second);
-        }
-
-        public static Money operator *(Money first, decimal second)
-        {
-            return new Money(first.currency, first.amount * second);
-        }
-
-        public static Money operator /(Money dividend, sbyte divisor)
-        {
-            return new Money(dividend.currency, dividend.amount / divisor);
-        }
-
-        public static Money operator /(Money dividend, byte divisor)
-        {
-            return new Money(dividend.currency, dividend.amount / divisor);
-        }
-
-        public static Money operator /(Money dividend, short divisor)
-        {
-            return new Money(dividend.currency, dividend.amount / divisor);
-        }
-
-        public static Money operator /(Money dividend, ushort divisor)
-        {
-            return new Money(dividend.currency, dividend.amount / divisor);
-        }
-
-        public static Money operator /(Money dividend, int divisor)
-        {
-            return new Money(dividend.currency, dividend.amount / divisor);
-        }
-
-        public static Money operator /(Money dividend, uint divisor)
-        {
-            return new Money(dividend.currency, dividend.amount / divisor);
-        }
-
-        public static Money operator /(Money dividend, long divisor)
-        {
-            return new Money(dividend.currency, dividend.amount / divisor);
-        }
-
-        public static Money operator /(Money dividend, ulong divisor)
-        {
-            return new Money(dividend.currency, dividend.amount / divisor);
-        }
-
-        public static Money operator /(Money dividend, decimal divisor)
-        {
-            return new Money(dividend.currency, dividend.amount / divisor);
-        }
-
-        public static Money operator +(Money money)
-        {
-            return new Money(money.currency, money.amount);
-        }
-
-        public static Money operator +(Money first, Money second)
-        {
-            AssertCurrencyIdentity(first, second);
-
-            return new Money(first.currency, first.amount + second.amount);
-        }
-
-        public static bool operator <(Money first, Money second)
-        {
-            return first.CompareTo(second) < 0;
-        }
-
-        public static bool operator <=(Money first, Money second)
-        {
-            return first.CompareTo(second) <= 0;
-        }
-
-        public static bool operator ==(Money first, Money second)
-        {
-            return first.Equals(second);
-        }
-
-        public static bool operator >(Money first, Money second)
-        {
-            return first.CompareTo(second) > 0;
-        }
-
-        public static bool operator >=(Money first, Money second)
-        {
-            return first.CompareTo(second) >= 0;
-        }
+        public CurrencyInfo Currency { get; }
 
         public int CompareTo(Money that)
         {
-            if (object.ReferenceEquals(this, that))
+            if (ReferenceEquals(this, that))
             {
                 return 0;
             }
-            else
-            {
-                AssertCurrencyIdentity(this, that);
 
-                return this.amount.CompareTo(that.amount);
-            }
+            AssertCurrencyIdentity(this, that);
+
+            return this.Amount.CompareTo(that.Amount);
         }
 
         public bool Equals(Money that)
         {
-            if (object.ReferenceEquals(that, null))
+            if (ReferenceEquals(that, null))
             {
                 return false;
             }
-            else if (object.ReferenceEquals(this, that))
-            {
-                return true;
-            }
-            else
-            {
-                AssertCurrencyIdentity(this, that);
 
-                return this.amount.Equals(that.amount);
-            }
-        }
-
-        public override bool Equals(object that)
-        {
-            if (object.ReferenceEquals(this, that))
+            if (ReferenceEquals(this, that))
             {
                 return true;
             }
 
-            var thatMoney = that as Money;
+            AssertCurrencyIdentity(this, that);
 
-            if (!object.ReferenceEquals(thatMoney, null))
-            {
-                return this.Equals(thatMoney);
-            }
-
-            return false;
+            return this.Amount.Equals(that.Amount);
         }
 
-        public string ToString(string format, System.IFormatProvider formatProvider)
+        public string ToString(string format, IFormatProvider formatProvider)
         {
-            System.Globalization.NumberFormatInfo NFI = (System.Globalization.NumberFormatInfo)System.Globalization.NumberFormatInfo.GetInstance(formatProvider).Clone();
+            var NFI = (NumberFormatInfo)NumberFormatInfo.GetInstance(formatProvider).Clone();
 
-            System.Diagnostics.Debug.Assert(!NFI.IsReadOnly);
-            string currencyFormat = "S";
-            string amountFormat = "C";
+            Debug.Assert(!NFI.IsReadOnly);
+            var currencyFormat = "S";
+            var amountFormat = "C";
 
             if (!string.IsNullOrEmpty(format))
             {
@@ -283,35 +63,148 @@
             switch (currencyFormat)
             {
                 case "S":
-                    NFI.CurrencySymbol = this.currency.CurrencySymbol;
+                    NFI.CurrencySymbol = this.Currency.CurrencySymbol;
                     break;
 
                 case "I":
-                    NFI.CurrencySymbol = this.currency.ISOCurrencySymbol;
+                    NFI.CurrencySymbol = this.Currency.ISOCurrencySymbol;
                     break;
 
                 default:
-                    throw new System.FormatException();
+                    throw new FormatException();
             }
 
-            return amount.ToString(amountFormat, NFI);
+            return this.Amount.ToString(amountFormat, NFI);
         }
 
-        public override string ToString()
+        public static Money operator -(Money money) => new(money.Currency, -money.Amount);
+
+        public static Money operator -(Money first, Money second)
         {
-            return this.ToString(string.Empty);
+            AssertCurrencyIdentity(first, second);
+
+            return new Money(first.Currency, first.Amount - second.Amount);
         }
 
-        public string ToString(string format)
+        public static bool operator !=(Money first, Money second) => !first.Equals(second);
+
+        public static Money operator %(Money dividend, sbyte divisor) =>
+            new(dividend.Currency, dividend.Amount % divisor);
+
+        public static Money operator %(Money dividend, byte divisor) =>
+            new(dividend.Currency, dividend.Amount % divisor);
+
+        public static Money operator %(Money dividend, short divisor) =>
+            new(dividend.Currency, dividend.Amount % divisor);
+
+        public static Money operator %(Money dividend, ushort divisor) =>
+            new(dividend.Currency, dividend.Amount % divisor);
+
+        public static Money operator %(Money dividend, int divisor) =>
+            new(dividend.Currency, dividend.Amount % divisor);
+
+        public static Money operator %(Money dividend, uint divisor) =>
+            new(dividend.Currency, dividend.Amount % divisor);
+
+        public static Money operator %(Money dividend, long divisor) =>
+            new(dividend.Currency, dividend.Amount % divisor);
+
+        public static Money operator %(Money dividend, ulong divisor) =>
+            new(dividend.Currency, dividend.Amount % divisor);
+
+        public static Money operator %(Money dividend, decimal divisor) =>
+            new(dividend.Currency, dividend.Amount % divisor);
+
+        public static Money operator *(Money first, sbyte second) => new(first.Currency, first.Amount * second);
+
+        public static Money operator *(Money first, byte second) => new(first.Currency, first.Amount * second);
+
+        public static Money operator *(Money first, short second) => new(first.Currency, first.Amount * second);
+
+        public static Money operator *(Money first, ushort second) => new(first.Currency, first.Amount * second);
+
+        public static Money operator *(Money first, int second) => new(first.Currency, first.Amount * second);
+
+        public static Money operator *(Money first, uint second) => new(first.Currency, first.Amount * second);
+
+        public static Money operator *(Money first, long second) => new(first.Currency, first.Amount * second);
+
+        public static Money operator *(Money first, ulong second) => new(first.Currency, first.Amount * second);
+
+        public static Money operator *(Money first, decimal second) => new(first.Currency, first.Amount * second);
+
+        public static Money operator /(Money dividend, sbyte divisor) =>
+            new(dividend.Currency, dividend.Amount / divisor);
+
+        public static Money operator /(Money dividend, byte divisor) =>
+            new(dividend.Currency, dividend.Amount / divisor);
+
+        public static Money operator /(Money dividend, short divisor) =>
+            new(dividend.Currency, dividend.Amount / divisor);
+
+        public static Money operator /(Money dividend, ushort divisor) =>
+            new(dividend.Currency, dividend.Amount / divisor);
+
+        public static Money operator /(Money dividend, int divisor) =>
+            new(dividend.Currency, dividend.Amount / divisor);
+
+        public static Money operator /(Money dividend, uint divisor) =>
+            new(dividend.Currency, dividend.Amount / divisor);
+
+        public static Money operator /(Money dividend, long divisor) =>
+            new(dividend.Currency, dividend.Amount / divisor);
+
+        public static Money operator /(Money dividend, ulong divisor) =>
+            new(dividend.Currency, dividend.Amount / divisor);
+
+        public static Money operator /(Money dividend, decimal divisor) =>
+            new(dividend.Currency, dividend.Amount / divisor);
+
+        public static Money operator +(Money money) => new(money.Currency, money.Amount);
+
+        public static Money operator +(Money first, Money second)
         {
-            return this.ToString(format, System.Globalization.CultureInfo.CurrentCulture);
+            AssertCurrencyIdentity(first, second);
+
+            return new Money(first.Currency, first.Amount + second.Amount);
         }
+
+        public static bool operator <(Money first, Money second) => first.CompareTo(second) < 0;
+
+        public static bool operator <=(Money first, Money second) => first.CompareTo(second) <= 0;
+
+        public static bool operator ==(Money first, Money second) => first.Equals(second);
+
+        public static bool operator >(Money first, Money second) => first.CompareTo(second) > 0;
+
+        public static bool operator >=(Money first, Money second) => first.CompareTo(second) >= 0;
+
+        public override bool Equals(object that)
+        {
+            if (ReferenceEquals(this, that))
+            {
+                return true;
+            }
+
+            var thatMoney = that as Money;
+
+            if (!ReferenceEquals(thatMoney, null))
+            {
+                return this.Equals(thatMoney);
+            }
+
+            return false;
+        }
+
+        public override string ToString() => this.ToString(string.Empty);
+
+        public string ToString(string format) => this.ToString(format, CultureInfo.CurrentCulture);
 
         private static void AssertCurrencyIdentity(Money first, Money second)
         {
-            if (!first.currency.Equals(second.currency))
+            if (!first.Currency.Equals(second.Currency))
             {
-                throw new System.InvalidOperationException("Money values are in different currencies.");
+                throw new InvalidOperationException("Money values are in different currencies.");
             }
         }
     }
