3fd18d41d4d2d9601347309760f4e3a87258e530
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 17:17:16 +0300

    Update CommandBase.cs

--- TIKSN.Core/PowerShell/CommandBase.cs
+++ TIKSN.Core/PowerShell/CommandBase.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Nito.AsyncEx;
-using System;
+﻿using System;
 using System.Management.Automation;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nito.AsyncEx;
 
 namespace TIKSN.PowerShell
 {
@@ -13,44 +13,37 @@ namespace TIKSN.PowerShell
         protected CancellationTokenSource cancellationTokenSource;
         private IServiceScope serviceScope;
 
-        protected CommandBase()
-        {
-            cancellationTokenSource = new CancellationTokenSource();
-        }
+        protected CommandBase() => this.cancellationTokenSource = new CancellationTokenSource();
 
-        protected IServiceProvider ServiceProvider => serviceScope.ServiceProvider;
+        protected IServiceProvider ServiceProvider => this.serviceScope.ServiceProvider;
 
         protected override void BeginProcessing()
         {
-            cancellationTokenSource = new CancellationTokenSource();
+            this.cancellationTokenSource = new CancellationTokenSource();
 
             base.BeginProcessing();
 
-            var topServiceProvider = GetServiceProvider();
-            serviceScope = topServiceProvider.CreateScope();
-            ServiceProvider.GetRequiredService<ICurrentCommandStore>().SetCurrentCommand(this);
-            ConfigureLogger(ServiceProvider.GetRequiredService<ILoggerFactory>());
+            var topServiceProvider = this.GetServiceProvider();
+            this.serviceScope = topServiceProvider.CreateScope();
+            this.ServiceProvider.GetRequiredService<ICurrentCommandStore>().SetCurrentCommand(this);
+            this.ConfigureLogger(this.ServiceProvider.GetRequiredService<ILoggerFactory>());
         }
 
-        protected virtual void ConfigureLogger(ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddPowerShell(ServiceProvider);
-        }
+        protected virtual void ConfigureLogger(ILoggerFactory loggerFactory) =>
+            loggerFactory.AddPowerShell(this.ServiceProvider);
 
         protected abstract IServiceProvider GetServiceProvider();
 
-        protected sealed override void ProcessRecord()
-        {
-            AsyncContext.Run(async () => await ProcessRecordAsync(cancellationTokenSource.Token));
-        }
+        protected sealed override void ProcessRecord() => AsyncContext.Run(async () =>
+            await this.ProcessRecordAsync(this.cancellationTokenSource.Token));
 
         protected abstract Task ProcessRecordAsync(CancellationToken cancellationToken);
 
         protected override void StopProcessing()
         {
-            cancellationTokenSource.Cancel();
+            this.cancellationTokenSource.Cancel();
             base.StopProcessing();
-            serviceScope.Dispose();
+            this.serviceScope.Dispose();
         }
     }
 }
