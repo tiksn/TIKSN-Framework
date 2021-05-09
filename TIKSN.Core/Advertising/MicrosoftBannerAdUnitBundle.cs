72b7cc26d8b5fbace8665f995ea61f753b112b03
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 16:19:58 +0300

    Update MicrosoftBannerAdUnitBundle.cs

--- TIKSN.Core/Advertising/MicrosoftBannerAdUnitBundle.cs
+++ TIKSN.Core/Advertising/MicrosoftBannerAdUnitBundle.cs
@@ -2,7 +2,7 @@
 {
     public class MicrosoftBannerAdUnitBundle : MicrosoftAdUnitBundle
     {
-        public static readonly AdUnit MicrosoftTestModeBannerAdUnit = new AdUnit(AdProviders.Microsoft,
+        public static readonly AdUnit MicrosoftTestModeBannerAdUnit = new(AdProviders.Microsoft,
             "3f83fe91-d6be-434d-a0ae-7351c5a997f1",
             "10865270", true);
 
