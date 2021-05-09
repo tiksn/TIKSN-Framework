b000a4f87aa506f8857cf6263807a701b5c97972
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 16:21:09 +0300

    Update RemoteNLogViewerOptionsPartialConfigurationValidator.cs

--- TIKSN.Core/Analytics/Logging/NLog/RemoteNLogViewerOptionsPartialConfigurationValidator.cs
+++ TIKSN.Core/Analytics/Logging/NLog/RemoteNLogViewerOptionsPartialConfigurationValidator.cs
@@ -3,14 +3,14 @@ using TIKSN.Configuration.Validator;
 
 namespace TIKSN.Analytics.Logging.NLog
 {
-    public class RemoteNLogViewerOptionsPartialConfigurationValidator : PartialConfigurationFluentValidatorBase<RemoteNLogViewerOptions>
+    public class
+        RemoteNLogViewerOptionsPartialConfigurationValidator : PartialConfigurationFluentValidatorBase<
+            RemoteNLogViewerOptions>
     {
-        public RemoteNLogViewerOptionsPartialConfigurationValidator()
-        {
-            RuleFor(instance => instance.Url.Scheme)
+        public RemoteNLogViewerOptionsPartialConfigurationValidator() =>
+            this.RuleFor(instance => instance.Url.Scheme)
                 .Must(IsProperScheme)
                 .When(instance => instance.Url != null);
-        }
 
         private static bool IsProperScheme(string scheme)
         {
