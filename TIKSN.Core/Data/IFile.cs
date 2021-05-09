1c7642f4d9051d0d633b38d4285caf565e39824c
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 16:40:13 +0300

    Update IFile.cs

--- TIKSN.Core/Data/IFile.cs
+++ TIKSN.Core/Data/IFile.cs
@@ -11,7 +11,8 @@ namespace TIKSN.Data
     {
     }
 
-    public interface IFile<TIdentity, TMetadata> : IFile, IFileInfo<TIdentity, TMetadata> where TIdentity : IEquatable<TIdentity>
+    public interface IFile<TIdentity, TMetadata> : IFile, IFileInfo<TIdentity, TMetadata>
+        where TIdentity : IEquatable<TIdentity>
     {
     }
 }
