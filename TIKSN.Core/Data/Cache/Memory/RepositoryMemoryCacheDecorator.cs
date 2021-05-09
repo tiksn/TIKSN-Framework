6bdfda1c11d20efb5582b8051b21cbbb274d3128
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 16:38:15 +0300

    Update RepositoryMemoryCacheDecorator.cs

--- TIKSN.Core/Data/Cache/Memory/RepositoryMemoryCacheDecorator.cs
+++ TIKSN.Core/Data/Cache/Memory/RepositoryMemoryCacheDecorator.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Options;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 
 namespace TIKSN.Data.Cache.Memory
 {
@@ -21,87 +21,92 @@ namespace TIKSN.Data.Cache.Memory
             IOptions<MemoryCacheDecoratorOptions<TEntity>> specificOptions)
             : base(memoryCache, genericOptions, specificOptions)
         {
-            _repository = repository;
-            _cacheKeychain = new HashSet<object>();
+            this._repository = repository;
+            this._cacheKeychain = new HashSet<object>();
         }
 
         public async Task AddAsync(TEntity entity, CancellationToken cancellationToken)
         {
-            await _repository.AddAsync(entity, cancellationToken);
+            await this._repository.AddAsync(entity, cancellationToken);
 
-            InvalidateCacheItem(entity);
+            this.InvalidateCacheItem(entity);
         }
 
         public async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
         {
-            await _repository.AddRangeAsync(entities, cancellationToken);
+            await this._repository.AddRangeAsync(entities, cancellationToken);
 
-            InvalidateCacheItems(entities);
+            this.InvalidateCacheItems(entities);
         }
 
         public async Task RemoveAsync(TEntity entity, CancellationToken cancellationToken)
         {
-            await _repository.RemoveAsync(entity, cancellationToken);
+            await this._repository.RemoveAsync(entity, cancellationToken);
 
-            InvalidateCacheItem(entity);
+            this.InvalidateCacheItem(entity);
         }
 
         public async Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
         {
-            await _repository.RemoveRangeAsync(entities, cancellationToken);
+            await this._repository.RemoveRangeAsync(entities, cancellationToken);
 
-            InvalidateCacheItems(entities);
+            this.InvalidateCacheItems(entities);
         }
 
         public async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken)
         {
-            await _repository.RemoveAsync(entity, cancellationToken);
+            await this._repository.RemoveAsync(entity, cancellationToken);
 
-            InvalidateCacheItem(entity);
+            this.InvalidateCacheItem(entity);
         }
 
         public async Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
         {
-            await _repository.UpdateRangeAsync(entities, cancellationToken);
+            await this._repository.UpdateRangeAsync(entities, cancellationToken);
 
-            InvalidateCacheItems(entities);
+            this.InvalidateCacheItems(entities);
         }
 
-        protected Task<TResult> GetQueryFromMemoryCacheAsync<TResult>(object cacheKey, Func<Task<TResult>> getFromSource)
+        protected Task<TResult> GetQueryFromMemoryCacheAsync<TResult>(object cacheKey,
+            Func<Task<TResult>> getFromSource)
         {
-            _cacheKeychain.Add(cacheKey);
+            this._cacheKeychain.Add(cacheKey);
 
-            return GetFromMemoryCacheAsync(cacheKey, getFromSource);
+            return this.GetFromMemoryCacheAsync(cacheKey, getFromSource);
         }
 
         protected virtual void InvalidateCacheEntity(TEntity entity)
         {
             var cacheKey = Tuple.Create(entityType, CacheKeyKind.Entity, entity.ID);
 
-            _memoryCache.Remove(cacheKey);
+            this._memoryCache.Remove(cacheKey);
         }
 
         protected virtual void InvalidateCacheItem(TEntity entity)
         {
-            InvalidateCacheEntity(entity);
+            this.InvalidateCacheEntity(entity);
 
-            InvalidateCacheQueryItems();
+            this.InvalidateCacheQueryItems();
         }
 
         protected virtual void InvalidateCacheItems(IEnumerable<TEntity> entities)
         {
             foreach (var entity in entities)
-                InvalidateCacheEntity(entity);
+            {
+                this.InvalidateCacheEntity(entity);
+            }
 
-            InvalidateCacheQueryItems();
+            this.InvalidateCacheQueryItems();
         }
 
         protected virtual void InvalidateCacheQueryItems()
         {
-            foreach (var cacheKey in _cacheKeychain)
-                _memoryCache.Remove(cacheKey);
+            foreach (var cacheKey in this._cacheKeychain)
+            {
+                this._memoryCache.Remove(cacheKey);
+            }
 
-            _cacheKeychain.Clear();
+            this._cacheKeychain.Clear();
         }
     }
 }
