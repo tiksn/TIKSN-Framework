464c2aec38f1ff833a94e507df09c26ebf815a9b
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 16:40:51 +0300

    Update IUnitOfWork.cs

--- TIKSN.Core/Data/IUnitOfWork.cs
+++ TIKSN.Core/Data/IUnitOfWork.cs
@@ -7,7 +7,7 @@ namespace TIKSN.Data
     public interface IUnitOfWork : IDisposable, IAsyncDisposable
     {
         Task CompleteAsync(CancellationToken cancellationToken);
-        
+
         Task DiscardAsync(CancellationToken cancellationToken);
     }
 }
