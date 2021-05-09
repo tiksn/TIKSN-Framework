d8e8ee9e2a5ddc833a5145c37fa8eba973ed1b81
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 16:39:35 +0300

    Update ICosmosTableRepository.cs

--- TIKSN.Core/Data/CosmosTable/ICosmosTableRepository.cs
+++ TIKSN.Core/Data/CosmosTable/ICosmosTableRepository.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Azure.Cosmos.Table;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
 
 namespace TIKSN.Data.CosmosTable
 {
