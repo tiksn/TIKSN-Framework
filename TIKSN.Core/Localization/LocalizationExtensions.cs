a7e92e9ee8a3213f9837d0737bf8eb0ebc9e72ed
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 17:15:27 +0300

    Update LocalizationExtensions.cs

--- TIKSN.Core/Localization/LocalizationExtensions.cs
+++ TIKSN.Core/Localization/LocalizationExtensions.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Extensions.Localization;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.Localization;
 
 namespace TIKSN.Localization
 {
@@ -11,32 +11,27 @@ namespace TIKSN.Localization
             var localizedString = stringLocalizer.GetString(name);
 
             if (localizedString.ResourceNotFound)
+            {
                 throw new KeyNotFoundException($"Resource for key '{name}' is not found.");
+            }
 
             return localizedString.Value;
         }
 
-        public static string GetRequiredString(this IStringLocalizer stringLocalizer, Guid key)
-        {
-            return stringLocalizer.GetRequiredString(key.ToString());
-        }
+        public static string GetRequiredString(this IStringLocalizer stringLocalizer, Guid key) =>
+            stringLocalizer.GetRequiredString(key.ToString());
 
-        public static string GetRequiredString(this IStringLocalizer stringLocalizer, int key)
-        {
-            return stringLocalizer.GetRequiredString(key.ToString());
-        }
+        public static string GetRequiredString(this IStringLocalizer stringLocalizer, int key) =>
+            stringLocalizer.GetRequiredString(key.ToString());
 
-        public static string GetRequiredString(this IStringLocalizer stringLocalizer, Guid key, params object[] arguments)
-        {
-            return stringLocalizer.GetRequiredString(key.ToString(), arguments);
-        }
+        public static string GetRequiredString(this IStringLocalizer stringLocalizer, Guid key,
+            params object[] arguments) => stringLocalizer.GetRequiredString(key.ToString(), arguments);
 
-        public static string GetRequiredString(this IStringLocalizer stringLocalizer, int key, params object[] arguments)
-        {
-            return stringLocalizer.GetRequiredString(key.ToString(), arguments);
-        }
+        public static string GetRequiredString(this IStringLocalizer stringLocalizer, int key,
+            params object[] arguments) => stringLocalizer.GetRequiredString(key.ToString(), arguments);
 
-        public static string GetRequiredString(this IStringLocalizer stringLocalizer, string name, params object[] arguments)
+        public static string GetRequiredString(this IStringLocalizer stringLocalizer, string name,
+            params object[] arguments)
         {
             var format = stringLocalizer.GetRequiredString(name);
 
