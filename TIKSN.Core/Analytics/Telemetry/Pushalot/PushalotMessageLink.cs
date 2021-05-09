05e0cf1d6a6b3207757ae5fda6c16e58a141d81e
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 16:27:47 +0300

    Update PushalotMessageLink.cs

--- TIKSN.Core/Analytics/Telemetry/Pushalot/PushalotMessageLink.cs
+++ TIKSN.Core/Analytics/Telemetry/Pushalot/PushalotMessageLink.cs
@@ -1,35 +1,40 @@
-﻿namespace TIKSN.Analytics.Telemetry.Pushalot
+﻿using System;
+
+namespace TIKSN.Analytics.Telemetry.Pushalot
 {
     public class PushalotMessageLink
     {
         private const int LINK_MAXIMUM_LENGTH = 1000;
         private const int TITLE_MAXIMUM_LENGTH = 100;
 
-        public PushalotMessageLink(string title, System.Uri link)
+        public PushalotMessageLink(string title, Uri link)
         {
             if (!string.IsNullOrEmpty(title))
             {
                 if (title.Length > TITLE_MAXIMUM_LENGTH)
                 {
-                    throw new System.ArgumentException(string.Format("Message link title must be up to {0} characters long.", TITLE_MAXIMUM_LENGTH), "title");
+                    throw new ArgumentException(
+                        string.Format("Message link title must be up to {0} characters long.", TITLE_MAXIMUM_LENGTH),
+                        "title");
                 }
             }
 
             if (link == null)
             {
-                throw new System.ArgumentNullException("link");
+                throw new ArgumentNullException("link");
             }
 
             if (link.AbsoluteUri.Length > LINK_MAXIMUM_LENGTH)
             {
-                throw new System.ArgumentException(string.Format("Message link  must be up to {0} characters long.", LINK_MAXIMUM_LENGTH), "link");
+                throw new ArgumentException(
+                    string.Format("Message link  must be up to {0} characters long.", LINK_MAXIMUM_LENGTH), "link");
             }
 
             this.Title = title;
             this.Link = link;
         }
 
-        public System.Uri Link { get; private set; }
-        public string Title { get; private set; }
+        public Uri Link { get; }
+        public string Title { get; }
     }
 }
