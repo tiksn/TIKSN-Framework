c64b1978d707585d39e07cc0d9e7238aa519bece
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 17:36:44 +0300

    Update PowerShellMetricTelemeter.cs

--- TIKSN.Framework.Core/Analytics/Telemetry/PowerShellMetricTelemeter.cs
+++ TIKSN.Framework.Core/Analytics/Telemetry/PowerShellMetricTelemeter.cs
@@ -7,14 +7,11 @@ namespace TIKSN.Analytics.Telemetry
     {
         private readonly Cmdlet cmdlet;
 
-        public PowerShellMetricTelemeter(Cmdlet cmdlet)
-        {
-            this.cmdlet = cmdlet;
-        }
+        public PowerShellMetricTelemeter(Cmdlet cmdlet) => this.cmdlet = cmdlet;
 
         public Task TrackMetric(string metricName, decimal metricValue)
         {
-            cmdlet.WriteVerbose($"METRIC: {metricName} - {metricValue}");
+            this.cmdlet.WriteVerbose($"METRIC: {metricName} - {metricValue}");
 
             return Task.FromResult<object>(null);
         }
