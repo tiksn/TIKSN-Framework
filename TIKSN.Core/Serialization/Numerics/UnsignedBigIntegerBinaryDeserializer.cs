dda815a5fddebb0c03aa94ca8a1d11f4b48bc787
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 17:29:41 +0300

    Update UnsignedBigIntegerBinaryDeserializer.cs

--- TIKSN.Core/Serialization/Numerics/UnsignedBigIntegerBinaryDeserializer.cs
+++ TIKSN.Core/Serialization/Numerics/UnsignedBigIntegerBinaryDeserializer.cs
@@ -4,12 +4,12 @@ using System.Numerics;
 namespace TIKSN.Serialization.Numerics
 {
     /// <summary>
-    /// Custom (specialized or typed) deserializer for unsigned <see cref="BigInteger"/>
+    ///     Custom (specialized or typed) deserializer for unsigned <see cref="BigInteger" />
     /// </summary>
     public class UnsignedBigIntegerBinaryDeserializer : ICustomDeserializer<byte[], BigInteger>
     {
         /// <summary>
-        /// Deserializes byte array to unsigned <see cref="BigInteger"/>
+        ///     Deserializes byte array to unsigned <see cref="BigInteger" />
         /// </summary>
         /// <param name="serial"></param>
         /// <returns></returns>
@@ -18,9 +18,11 @@ namespace TIKSN.Serialization.Numerics
             var last = serial[serial.Length - 1];
 
             if (last < 0b_1000_0000)
+            {
                 return new BigInteger(serial);
+            }
 
-            return new BigInteger(serial.Concat(new byte[] { 0b_0000_0000 }).ToArray());
+            return new BigInteger(serial.Concat(new byte[] {0b_0000_0000}).ToArray());
         }
     }
 }
