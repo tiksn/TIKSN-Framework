2ceb47dab0cf01c46d61f150e83a00c34434f194
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 16:32:20 +0300

    Update AzureBlobStorageRepository.cs

--- TIKSN.Core/Data/AzureStorage/AzureBlobStorageRepository.cs
+++ TIKSN.Core/Data/AzureStorage/AzureBlobStorageRepository.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.Azure.Storage;
-using Microsoft.Azure.Storage.Blob;
-using Microsoft.Extensions.Configuration;
-using System.IO;
+﻿using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
 
 namespace TIKSN.Data.AzureStorage
 {
-    public class AzureBlobStorageRepository : AzureStorageBase, IAzureBlobStorageRepository, IAzureBlobStorageRepositoryInitializer
+    public class AzureBlobStorageRepository : AzureStorageBase, IAzureBlobStorageRepository,
+        IAzureBlobStorageRepositoryInitializer
     {
         private readonly BlobContainerPublicAccessType _accessType;
         private readonly string _containerName;
@@ -22,26 +23,28 @@ namespace TIKSN.Data.AzureStorage
             BlobRequestOptions options,
             OperationContext operationContext) : base(configuration, connectionStringKey)
         {
-            _containerName = containerName;
-            _accessType = accessType;
-            _options = options;
-            _operationContext = operationContext;
+            this._containerName = containerName;
+            this._accessType = accessType;
+            this._options = options;
+            this._operationContext = operationContext;
         }
 
         public Task DeleteAsync(string path, CancellationToken cancellationToken)
         {
-            var blob = GetCloudBlobContainer().GetBlobReference(path);
+            var blob = this.GetCloudBlobContainer().GetBlobReference(path);
 
-            return blob.DeleteAsync(DeleteSnapshotsOption.IncludeSnapshots, AccessCondition.GenerateEmptyCondition(), _options, _operationContext, cancellationToken);
+            return blob.DeleteAsync(DeleteSnapshotsOption.IncludeSnapshots, AccessCondition.GenerateEmptyCondition(),
+                this._options, this._operationContext, cancellationToken);
         }
 
         public async Task<IFile> DownloadAsync(string path, CancellationToken cancellationToken)
         {
-            var blob = GetCloudBlobContainer().GetBlobReference(path);
+            var blob = this.GetCloudBlobContainer().GetBlobReference(path);
 
             using (var stream = new MemoryStream())
             {
-                await blob.DownloadToStreamAsync(stream, AccessCondition.GenerateEmptyCondition(), _options, _operationContext, cancellationToken);
+                await blob.DownloadToStreamAsync(stream, AccessCondition.GenerateEmptyCondition(), this._options,
+                    this._operationContext, cancellationToken);
 
                 return new File(blob.Name, stream.ToArray());
             }
@@ -49,30 +52,29 @@ namespace TIKSN.Data.AzureStorage
 
         public Task<bool> ExistsAsync(string path, CancellationToken cancellationToken)
         {
-            var blob = GetCloudBlobContainer().GetBlobReference(path);
+            var blob = this.GetCloudBlobContainer().GetBlobReference(path);
 
-            return blob.ExistsAsync(_options, _operationContext, cancellationToken);
-        }
-
-        public Task InitializeAsync(CancellationToken cancellationToken)
-        {
-            return GetCloudBlobContainer().CreateIfNotExistsAsync(_accessType, _options, _operationContext, cancellationToken);
+            return blob.ExistsAsync(this._options, this._operationContext, cancellationToken);
         }
 
         public Task UploadAsync(string path, byte[] content, CancellationToken cancellationToken)
         {
-            var blob = GetCloudBlobContainer().GetBlockBlobReference(path);
+            var blob = this.GetCloudBlobContainer().GetBlockBlobReference(path);
 
-            return blob.UploadFromByteArrayAsync(content, 0, content.Length, AccessCondition.GenerateEmptyCondition(), _options, _operationContext, cancellationToken);
+            return blob.UploadFromByteArrayAsync(content, 0, content.Length, AccessCondition.GenerateEmptyCondition(),
+                this._options, this._operationContext, cancellationToken);
         }
+
+        public Task InitializeAsync(CancellationToken cancellationToken) => this.GetCloudBlobContainer()
+            .CreateIfNotExistsAsync(this._accessType, this._options, this._operationContext, cancellationToken);
 
         protected CloudBlobContainer GetCloudBlobContainer()
         {
-            var storageAccount = GetCloudStorageAccount();
+            var storageAccount = this.GetCloudStorageAccount();
 
             var blobClient = storageAccount.CreateCloudBlobClient();
 
-            return blobClient.GetContainerReference(_containerName);
+            return blobClient.GetContainerReference(this._containerName);
         }
     }
 }
