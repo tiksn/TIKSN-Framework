020d29f7bb2476bd93ed61fee6650c75a072434c
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 17:17:52 +0300

    Update PowerShellLoggerProvider.cs

--- TIKSN.Core/PowerShell/PowerShellLoggerProvider.cs
+++ TIKSN.Core/PowerShell/PowerShellLoggerProvider.cs
@@ -1,34 +1,32 @@
-﻿using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
+﻿using System;
 using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace TIKSN.PowerShell
 {
     public class PowerShellLoggerProvider : ILoggerProvider
     {
         private readonly ICurrentCommandProvider _currentCommandProvider;
-        private readonly ConcurrentDictionary<string, PowerShellLogger> _loggers = new ConcurrentDictionary<string, PowerShellLogger>();
+        private readonly ConcurrentDictionary<string, PowerShellLogger> _loggers = new();
         private readonly IOptions<PowerShellLoggerOptions> options;
 
-        public PowerShellLoggerProvider(ICurrentCommandProvider currentCommandProvider, IOptions<PowerShellLoggerOptions> options)
+        public PowerShellLoggerProvider(ICurrentCommandProvider currentCommandProvider,
+            IOptions<PowerShellLoggerOptions> options)
         {
             this.options = options;
-            _currentCommandProvider = currentCommandProvider ?? throw new ArgumentNullException(nameof(currentCommandProvider));
+            this._currentCommandProvider = currentCommandProvider ??
+                                           throw new ArgumentNullException(nameof(currentCommandProvider));
         }
 
-        public ILogger CreateLogger(string categoryName)
-        {
-            return _loggers.GetOrAdd(categoryName, CreateLoggerImplementation);
-        }
+        public ILogger CreateLogger(string categoryName) =>
+            this._loggers.GetOrAdd(categoryName, this.CreateLoggerImplementation);
 
         public void Dispose()
         {
         }
 
-        private PowerShellLogger CreateLoggerImplementation(string name)
-        {
-            return new PowerShellLogger(_currentCommandProvider, options, name);
-        }
+        private PowerShellLogger CreateLoggerImplementation(string name) =>
+            new(this._currentCommandProvider, this.options, name);
     }
 }
