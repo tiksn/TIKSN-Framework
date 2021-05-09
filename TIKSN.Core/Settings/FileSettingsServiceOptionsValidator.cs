34d6e311e9771a50491d967d70ff4335aa204243
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 17:30:11 +0300

    Update FileSettingsServiceOptionsValidator.cs

--- TIKSN.Core/Settings/FileSettingsServiceOptionsValidator.cs
+++ TIKSN.Core/Settings/FileSettingsServiceOptionsValidator.cs
@@ -3,11 +3,9 @@ using TIKSN.Configuration.Validator;
 
 namespace TIKSN.Settings
 {
-    public class FileSettingsServiceOptionsValidator : PartialConfigurationFluentValidatorBase<FileSettingsServiceOptions>
+    public class
+        FileSettingsServiceOptionsValidator : PartialConfigurationFluentValidatorBase<FileSettingsServiceOptions>
     {
-        public FileSettingsServiceOptionsValidator()
-        {
-            RuleFor(x => x.RelativePath).NotNull().NotEmpty();
-        }
+        public FileSettingsServiceOptionsValidator() => this.RuleFor(x => x.RelativePath).NotNull().NotEmpty();
     }
 }
