8e04657a0d01458a85774a5cd70eb6c6da1098b7
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 17:36:22 +0300

    Update UriTemplate.cs

--- TIKSN.Core/Web/UriTemplate.cs
+++ TIKSN.Core/Web/UriTemplate.cs
@@ -10,44 +10,46 @@ namespace TIKSN.Web
 
         public UriTemplate(string template)
         {
-            _template = template;
-            _values = new Dictionary<string, string>();
+            this._template = template;
+            this._values = new Dictionary<string, string>();
         }
 
         public Uri Compose()
         {
-            var resourceLocation = _template;
+            var resourceLocation = this._template;
 
-            foreach (var parameter in _values)
+            foreach (var parameter in this._values)
             {
                 var parameterName = $"{{{parameter.Key}}}";
                 var escapedParameterValue = Uri.EscapeUriString(parameter.Value) ?? string.Empty;
 
                 if (resourceLocation.Contains(parameterName))
+                {
                     resourceLocation = resourceLocation.Replace(parameterName, escapedParameterValue);
+                }
                 else
                 {
                     var queryToAppend = $"{parameterName}={escapedParameterValue}";
                     if (resourceLocation.EndsWith("?"))
+                    {
                         resourceLocation = $"{resourceLocation}{queryToAppend}";
+                    }
                     else if (resourceLocation.Contains("?"))
+                    {
                         resourceLocation = $"{resourceLocation}&{queryToAppend}";
+                    }
                     else
+                    {
                         resourceLocation = $"{resourceLocation}?{queryToAppend}";
+                    }
                 }
             }
 
             return new Uri(resourceLocation, UriKind.Relative);
         }
 
-        public void Fill(string name, string value)
-        {
-            _values.Add(name, value);
-        }
+        public void Fill(string name, string value) => this._values.Add(name, value);
 
-        public void Unfill(string name)
-        {
-            _values.Remove(name);
-        }
+        public void Unfill(string name) => this._values.Remove(name);
     }
 }
