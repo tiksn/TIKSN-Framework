d5064cf13ab77d36d5fe8a71ea21e5a053d0ef68
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 17:35:39 +0300

    Update RestEndpointAttribute.cs

--- TIKSN.Core/Web/Rest/RestEndpointAttribute.cs
+++ TIKSN.Core/Web/Rest/RestEndpointAttribute.cs
@@ -2,26 +2,27 @@
 
 namespace TIKSN.Web.Rest
 {
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class)]
     public class RestEndpointAttribute : Attribute
     {
-        public RestEndpointAttribute(string apiKey, RestVerb verb, string resourceTemplate, RestAuthenticationType authentication = RestAuthenticationType.None, string mediaType = "application/json")
+        public RestEndpointAttribute(string apiKey, RestVerb verb, string resourceTemplate,
+            RestAuthenticationType authentication = RestAuthenticationType.None, string mediaType = "application/json")
         {
-            ApiKey = new Guid(apiKey);
-            Verb = verb;
-            ResourceTemplate = resourceTemplate;
-            MediaType = mediaType;
-            Authentication = authentication;
+            this.ApiKey = new Guid(apiKey);
+            this.Verb = verb;
+            this.ResourceTemplate = resourceTemplate;
+            this.MediaType = mediaType;
+            this.Authentication = authentication;
         }
 
-        public Guid ApiKey { get; private set; }
+        public Guid ApiKey { get; }
 
-        public RestAuthenticationType Authentication { get; private set; }
+        public RestAuthenticationType Authentication { get; }
 
-        public string MediaType { get; private set; }
+        public string MediaType { get; }
 
-        public string ResourceTemplate { get; private set; }
+        public string ResourceTemplate { get; }
 
-        public RestVerb Verb { get; private set; }
+        public RestVerb Verb { get; }
     }
 }
