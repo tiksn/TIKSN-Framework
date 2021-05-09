ad8ef3e2c77dd8a74b61496d6ea9ed55d0ef0ce7
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-05-09 17:38:32 +0300

    Update WindowsRegistrySettingsService.cs

--- TIKSN.Framework.Core/Settings/WindowsRegistrySettingsService.cs
+++ TIKSN.Framework.Core/Settings/WindowsRegistrySettingsService.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Extensions.Options;
-using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+using Microsoft.Win32;
 
 namespace TIKSN.Settings
 {
@@ -11,70 +11,52 @@ namespace TIKSN.Settings
 
         public WindowsRegistrySettingsService(IOptions<WindowsRegistrySettingsServiceOptions> options)
         {
-            _options = options;
-            ValidateOptions();
+            this._options = options;
+            this.ValidateOptions();
         }
 
-        public T GetLocalSetting<T>(string name, T defaultValue)
-        {
-            return Process(RegistryHive.CurrentUser, name, defaultValue, GetSetting);
-        }
+        public T GetLocalSetting<T>(string name, T defaultValue) =>
+            this.Process(RegistryHive.CurrentUser, name, defaultValue, this.GetSetting);
 
-        public T GetRoamingSetting<T>(string name, T defaultValue)
-        {
-            return Process(RegistryHive.LocalMachine, name, defaultValue, GetSetting);
-        }
+        public T GetRoamingSetting<T>(string name, T defaultValue) =>
+            this.Process(RegistryHive.LocalMachine, name, defaultValue, this.GetSetting);
 
-        public IReadOnlyCollection<string> ListLocalSetting()
-        {
-            return Process<IReadOnlyCollection<string>>(RegistryHive.CurrentUser, null, null, ListNames);
-        }
+        public IReadOnlyCollection<string> ListLocalSetting() =>
+            this.Process<IReadOnlyCollection<string>>(RegistryHive.CurrentUser, null, null, this.ListNames);
 
-        public IReadOnlyCollection<string> ListRoamingSetting()
-        {
-            return Process<IReadOnlyCollection<string>>(RegistryHive.LocalMachine, null, null, ListNames);
-        }
+        public IReadOnlyCollection<string> ListRoamingSetting() =>
+            this.Process<IReadOnlyCollection<string>>(RegistryHive.LocalMachine, null, null, this.ListNames);
 
-        public void RemoveLocalSetting(string name)
-        {
-            Process<object>(RegistryHive.CurrentUser, name, null, RemoveSetting);
-        }
+        public void RemoveLocalSetting(string name) =>
+            this.Process<object>(RegistryHive.CurrentUser, name, null, this.RemoveSetting);
 
-        public void RemoveRoamingSetting(string name)
-        {
-            Process<object>(RegistryHive.LocalMachine, name, null, RemoveSetting);
-        }
+        public void RemoveRoamingSetting(string name) =>
+            this.Process<object>(RegistryHive.LocalMachine, name, null, this.RemoveSetting);
 
-        public void SetLocalSetting<T>(string name, T value)
-        {
-            Process(RegistryHive.CurrentUser, name, value, SetSetting);
-        }
+        public void SetLocalSetting<T>(string name, T value) =>
+            this.Process(RegistryHive.CurrentUser, name, value, this.SetSetting);
 
-        public void SetRoamingSetting<T>(string name, T value)
-        {
-            Process(RegistryHive.LocalMachine, name, value, SetSetting);
-        }
+        public void SetRoamingSetting<T>(string name, T value) =>
+            this.Process(RegistryHive.LocalMachine, name, value, this.SetSetting);
 
-        private T GetSetting<T>(RegistryKey subKey, string name, T defaultValue)
-        {
-            return (T)subKey.GetValue(name, defaultValue);
-        }
+        private T GetSetting<T>(RegistryKey subKey, string name, T defaultValue) =>
+            (T)subKey.GetValue(name, defaultValue);
 
-        private IReadOnlyCollection<string> ListNames(RegistryKey subKey, string name, IReadOnlyCollection<string> value)
-        {
-            return subKey.GetValueNames();
-        }
+        private IReadOnlyCollection<string> ListNames(RegistryKey subKey, string name,
+            IReadOnlyCollection<string> value) => subKey.GetValueNames();
 
         private T Process<T>(RegistryHive hiveKey, string name, T value, Func<RegistryKey, string, T, T> processor)
         {
             if (processor == null)
-                throw new ArgumentNullException(nameof(processor));
-
-            ValidateOptions();
-
-            using (var rootKey = RegistryKey.OpenBaseKey(hiveKey, _options.Value.RegistryView))
             {
-                using (var registrySubKey = rootKey.CreateSubKey(_options.Value.SubKey, writable: true))
+                throw new ArgumentNullException(nameof(processor));
+            }
+
+            this.ValidateOptions();
+
+            using (var rootKey = RegistryKey.OpenBaseKey(hiveKey, this._options.Value.RegistryView))
+            {
+                using (var registrySubKey = rootKey.CreateSubKey(this._options.Value.SubKey, true))
                 {
                     return processor(registrySubKey, name, value);
                 }
@@ -97,8 +79,10 @@ namespace TIKSN.Settings
 
         private void ValidateOptions()
         {
-            if (string.IsNullOrWhiteSpace(_options.Value.SubKey))
-                throw new ArgumentException("Parameter is null or white space.", nameof(_options.Value.SubKey));
+            if (string.IsNullOrWhiteSpace(this._options.Value.SubKey))
+            {
+                throw new ArgumentException("Parameter is null or white space.", nameof(this._options.Value.SubKey));
+            }
         }
     }
 }
