3afa8b53060b5c1566ae064f32a26e8d8fead136
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-07-01 16:49:40 +0300

    Update gig.ps1

--- gig.ps1
+++ gig.ps1
@@ -2,11 +2,12 @@
 Function gig {
     param(
         [Parameter(Mandatory = $true)]
-        [string[]]$list 
+        [string[]]$list
     )
     $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ','
-    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params" 
+    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params"
     | Select-Object -ExpandProperty content | Out-File -FilePath $(Join-Path -Path $pwd -ChildPath '.gitignore') -Encoding ascii
 }
 
 gig -list visualstudio, visualstudiocode, fsharp, powershell, macos, intellij+all
+Write-Output '.trash' | Out-File -Append -FilePath .\.gitignore
