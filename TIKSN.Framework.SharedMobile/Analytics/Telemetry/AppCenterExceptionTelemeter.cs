e6fea23a34395c4e449cd9e1cf61ec77540e4f9a
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-08-10 23:51:49 +0300

    Update AppCenterExceptionTelemeter.cs

--- TIKSN.Framework.SharedMobile/Analytics/Telemetry/AppCenterExceptionTelemeter.cs
+++ TIKSN.Framework.SharedMobile/Analytics/Telemetry/AppCenterExceptionTelemeter.cs
@@ -6,12 +6,12 @@ namespace TIKSN.Analytics.Telemetry
 {
     public class AppCenterExceptionTelemeter : IExceptionTelemeter
     {
-        public Task TrackException(Exception exception)
+        public Task TrackExceptionAsync(Exception exception)
         {
-            return TrackException(exception, TelemetrySeverityLevel.Information);
+            return TrackExceptionAsync(exception, TelemetrySeverityLevel.Information);
         }
 
-        public Task TrackException(Exception exception, TelemetrySeverityLevel severityLevel)
+        public Task TrackExceptionAsync(Exception exception, TelemetrySeverityLevel severityLevel)
         {
             switch (severityLevel)
             {
