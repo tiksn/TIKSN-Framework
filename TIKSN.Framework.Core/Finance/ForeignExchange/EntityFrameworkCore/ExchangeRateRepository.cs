da1a1094730d3664840cc21773c660364c09656a
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-08-10 23:07:28 +0300

    Update ExchangeRateRepository.cs

--- TIKSN.Framework.Core/Finance/ForeignExchange/EntityFrameworkCore/ExchangeRateRepository.cs
+++ TIKSN.Framework.Core/Finance/ForeignExchange/EntityFrameworkCore/ExchangeRateRepository.cs
@@ -33,7 +33,7 @@ namespace TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore
         {
             var entity = await this.Entities
                 .OrderByDescending(item => item.ID)
-                .FirstOrDefaultAsync(cancellationToken);
+                .FirstOrDefaultAsync(cancellationToken).ConfigureAwait(false);
 
             if (entity == null)
             {
@@ -57,6 +57,6 @@ namespace TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore
                     item.ForeignExchangeID == foreignExchangeID &&
                     item.AsOn >= dateFrom && item.AsOn <= dateTo)
                 .Include(item => item.ForeignExchange)
-                .ToArrayAsync(cancellationToken);
+                .ToArrayAsync(cancellationToken).ConfigureAwait(false);
     }
 }
