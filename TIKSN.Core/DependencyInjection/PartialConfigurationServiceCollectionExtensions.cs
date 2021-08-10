433dae9d3999137fedd4d6bfb54e453e8f108ae1
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-08-10 22:57:15 +0300

    Update PartialConfigurationServiceCollectionExtensions.cs

--- TIKSN.Core/DependencyInjection/PartialConfigurationServiceCollectionExtensions.cs
+++ TIKSN.Core/DependencyInjection/PartialConfigurationServiceCollectionExtensions.cs
@@ -13,18 +13,18 @@ namespace TIKSN.DependencyInjection
         {
             if (mandatoryValidation)
             {
-                services
+                _ = services
                     .AddSingleton<IConfigurationValidationStrategy<TOptions>,
                         MandatoryConfigurationValidationStrategy<TOptions>>();
             }
             else
             {
-                services
+                _ = services
                     .AddSingleton<IConfigurationValidationStrategy<TOptions>,
                         OptionalConfigurationValidationStrategy<TOptions>>();
             }
 
-            services.AddSingleton<IPartialConfiguration<TOptions>, PartialConfiguration<TOptions>>();
+            _ = services.AddSingleton<IPartialConfiguration<TOptions>, PartialConfiguration<TOptions>>();
 
             return services.Configure<TOptions>(config);
         }
@@ -34,7 +34,7 @@ namespace TIKSN.DependencyInjection
             where TOptions : class, new()
             where TValidator : class, IPartialConfigurationValidator<TOptions>
         {
-            services.AddSingleton<IPartialConfigurationValidator<TOptions>, TValidator>();
+            _ = services.AddSingleton<IPartialConfigurationValidator<TOptions>, TValidator>();
 
             return services.ConfigurePartial<TOptions>(config, true);
         }
