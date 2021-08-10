7756e6a7591f2644ed91eca805d4dbb4efbcf31b
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-08-10 22:49:36 +0300

    Update IEventTelemeter.cs

--- TIKSN.Core/Analytics/Telemetry/IEventTelemeter.cs
+++ TIKSN.Core/Analytics/Telemetry/IEventTelemeter.cs
@@ -5,8 +5,8 @@ namespace TIKSN.Analytics.Telemetry
 {
     public interface IEventTelemeter
     {
-        Task TrackEvent(string name);
+        Task TrackEventAsync(string name);
 
-        Task TrackEvent(string name, IDictionary<string, string> properties);
+        Task TrackEventAsync(string name, IDictionary<string, string> properties);
     }
 }
