1f66629bf21290bb27d07f6ef031e5a93e183a60
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-08-10 23:38:28 +0300

    Update MongoFileRepository.cs

--- TIKSN.Core/Data/Mongo/MongoFileRepository.cs
+++ TIKSN.Core/Data/Mongo/MongoFileRepository.cs
@@ -35,26 +35,26 @@ namespace TIKSN.Data.Mongo
 
         public async Task DeleteAsync(string path, CancellationToken cancellationToken)
         {
-            var fileInfo = await this._bucket
-                .Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Filename, path))
-                .SingleAsync(cancellationToken);
+            var fileInfo = await (await this._bucket
+                .FindAsync(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Filename, path), cancellationToken: cancellationToken).ConfigureAwait(false))
+                .SingleAsync(cancellationToken).ConfigureAwait(true);
 
-            await this._bucket.DeleteAsync(fileInfo.Id, cancellationToken);
+            await this._bucket.DeleteAsync(fileInfo.Id, cancellationToken).ConfigureAwait(true);
         }
 
         public async Task<IFile> DownloadAsync(string path, CancellationToken cancellationToken)
         {
-            var fileInfo = await this._bucket
-                .Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Filename, path))
-                .SingleAsync(cancellationToken);
+            var fileInfo = await (await this._bucket
+                .FindAsync(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Filename, path), cancellationToken: cancellationToken).ConfigureAwait(false))
+                .SingleAsync(cancellationToken).ConfigureAwait(true);
 
-            var content = await this._bucket.DownloadAsBytesAsync(fileInfo.Id, null, cancellationToken);
+            var content = await this._bucket.DownloadAsBytesAsync(fileInfo.Id, null, cancellationToken).ConfigureAwait(true);
 
             return new File(fileInfo.Filename, content);
         }
 
         public Task<bool> ExistsAsync(string path, CancellationToken cancellationToken) => this._bucket
-            .Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Filename, path))
+            .Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Filename, path), cancellationToken: cancellationToken)
             .AnyAsync(cancellationToken);
 
         public Task UploadAsync(string path, byte[] content, CancellationToken cancellationToken) =>
@@ -63,8 +63,8 @@ namespace TIKSN.Data.Mongo
         public async Task<IFileInfo<TIdentity, TMetadata>> DownloadOnlyMetadataAsync(TIdentity id,
             CancellationToken cancellationToken)
         {
-            var fileInfo = await this._bucket.Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Id, id))
-                .SingleAsync(cancellationToken);
+            var fileInfo = await this._bucket.Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Id, id), cancellationToken: cancellationToken)
+                .SingleAsync(cancellationToken).ConfigureAwait(true);
 
             return new FileInfo<TIdentity, TMetadata>(id, fileInfo.Filename,
                 BsonSerializer.Deserialize<TMetadata>(fileInfo.Metadata));
@@ -73,10 +73,10 @@ namespace TIKSN.Data.Mongo
         public async Task<IFile<TIdentity, TMetadata>> DownloadWithMetadataAsync(TIdentity id,
             CancellationToken cancellationToken)
         {
-            var content = await this._bucket.DownloadAsBytesAsync(id, null, cancellationToken);
+            var content = await this._bucket.DownloadAsBytesAsync(id, null, cancellationToken).ConfigureAwait(true);
 
-            var fileInfo = await this._bucket.Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Id, id))
-                .SingleAsync(cancellationToken);
+            var fileInfo = await this._bucket.Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Id, id), cancellationToken: cancellationToken)
+                .SingleAsync(cancellationToken).ConfigureAwait(true);
 
             return new File<TIdentity, TMetadata>(id, fileInfo.Filename,
                 BsonSerializer.Deserialize<TMetadata>(fileInfo.Metadata), content);
@@ -92,16 +92,16 @@ namespace TIKSN.Data.Mongo
 
         public async Task<IFile<TIdentity>> DownloadByIdAsync(TIdentity id, CancellationToken cancellationToken)
         {
-            var content = await this._bucket.DownloadAsBytesAsync(id, null, cancellationToken);
+            var content = await this._bucket.DownloadAsBytesAsync(id, null, cancellationToken).ConfigureAwait(true);
 
-            var fileInfo = await this._bucket.Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Id, id))
-                .SingleAsync(cancellationToken);
+            var fileInfo = await this._bucket.Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Id, id), cancellationToken: cancellationToken)
+                .SingleAsync(cancellationToken).ConfigureAwait(true);
 
             return new File<TIdentity>(id, fileInfo.Filename, content);
         }
 
-        public Task<bool> ExistsByIdAsync(TIdentity id, CancellationToken cancellationToken) => this._bucket
-            .Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Id, id)).AnyAsync(cancellationToken);
+        public Task<bool> ExistsByIdAsync(TIdentity id, CancellationToken cancellationToken) =>
+            this._bucket.Find(Builders<GridFSFileInfo<TIdentity>>.Filter.Eq(item => item.Id, id), cancellationToken: cancellationToken).AnyAsync(cancellationToken);
 
         public Task UploadByIdAsync(TIdentity id, string path, byte[] content, CancellationToken cancellationToken) =>
             this._bucket.UploadFromBytesAsync(id, path, content, null, cancellationToken);
