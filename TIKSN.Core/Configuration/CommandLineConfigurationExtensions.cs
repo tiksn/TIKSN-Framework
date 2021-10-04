48d4c797b34f69e9ee1be450eea3e7056cacbc01
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-10-04 13:45:05 +0300

    Create CommandLineConfigurationExtensions.cs

---
+++ TIKSN.Core/Configuration/CommandLineConfigurationExtensions.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using TIKSN.Analytics.Logging.NLog;
+
+namespace TIKSN.Configuration
+{
+    public static class CommandLineConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddFrameworkCommandLine(
+            this IConfigurationBuilder configurationBuilder,
+            string[] args,
+            IDictionary<string, string> switchMappings)
+        {
+            var frameworkSwitchMappings = MergeFrameworkSwitchMappings(switchMappings);
+            return configurationBuilder.AddCommandLine(args, frameworkSwitchMappings);
+        }
+
+        private static IDictionary<string, string> GetFrameworkSwitchMappings() => new Dictionary<string, string>
+            {
+                {
+                    "--nlog-viewer-address",
+                    ConfigurationPath.Combine(RemoteNLogViewerOptions.RemoteNLogViewerConfigurationSection,
+                    nameof(RemoteNLogViewerOptions.Address))
+                },
+                {
+                    "--nlog-viewer-include-call-site",
+                    ConfigurationPath.Combine(RemoteNLogViewerOptions.RemoteNLogViewerConfigurationSection,
+                    nameof(RemoteNLogViewerOptions.IncludeCallSite))
+                },
+                {
+                    "--nlog-viewer-include-source-info",
+                    ConfigurationPath.Combine(RemoteNLogViewerOptions.RemoteNLogViewerConfigurationSection,
+                    nameof(RemoteNLogViewerOptions.IncludeSourceInfo))
+                }
+            };
+
+        private static IDictionary<string, string> MergeFrameworkSwitchMappings(IDictionary<string, string> switchMappings)
+        {
+            var mappings = GetFrameworkSwitchMappings();
+
+            if (switchMappings is not null)
+            {
+                foreach (var item in switchMappings)
+                {
+                    mappings[item.Key] = item.Value;
+                }
+            }
+
+            return mappings;
+        }
+
+        public static IConfigurationBuilder AddFrameworkCommandLine(
+            this IConfigurationBuilder configurationBuilder,
+            string[] args) => configurationBuilder.AddFrameworkCommandLine(args, switchMappings: null);
+    }
+}
