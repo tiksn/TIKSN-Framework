89405672d8b10fb27d0c4ceea0902e58a1d633af
Tigran TIKSN Torosyan <personal@tiksn.com>
2022-10-26 12:52:38 +0200

    Create IdentityMapper.cs

---
+++ TIKSN.Core/Data/IdentityMapper.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace TIKSN.Data
+{
+    public class IdentityMapper<T> : IMapper<T, T>
+    {
+        private static readonly Lazy<IdentityMapper<T>> LazyInstance = new(() => new IdentityMapper<T>());
+
+        private IdentityMapper()
+        { }
+
+        public static IdentityMapper<T> Instance => LazyInstance.Value;
+
+        public T Map(T source) => source;
+    }
+}
