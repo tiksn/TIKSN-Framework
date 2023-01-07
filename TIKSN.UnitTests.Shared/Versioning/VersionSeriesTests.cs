384e87affae6c7f62a442d38d9b8d5c8e9098d6e
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-01-07 17:26:37 +0100

    Update VersionSeriesTests.cs

--- TIKSN.UnitTests.Shared/Versioning/VersionSeriesTests.cs
+++ TIKSN.UnitTests.Shared/Versioning/VersionSeriesTests.cs
@@ -1,4 +1,3 @@
-using System;
 using FluentAssertions;
 using Xunit;
 
@@ -22,6 +21,25 @@ namespace TIKSN.Versioning.Tests
             var versionSeries = VersionSeries.Parse(series);
 
             versionSeries.ToString().Should().Be(series);
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("1.2")]
+        [InlineData("1.2.3")]
+        [InlineData("1.2.3.4")]
+        [InlineData("1.2-rc")]
+        [InlineData("1.2.3-rc")]
+        [InlineData("1.2.3.4-rc")]
+        [InlineData("1.2-rc.5")]
+        [InlineData("1.2.3-rc.5")]
+        [InlineData("1.2.3.4-rc.5")]
+        public void GivenSeries_WhenTwoIdenticalVersionSeriesCreated_ThenTheyShouldBeEqual(string series)
+        {
+            var versionSeries1 = VersionSeries.Parse(series);
+            var versionSeries2 = VersionSeries.Parse(series);
+
+            versionSeries1.Equals(versionSeries2).Should().BeTrue();
         }
     }
 }
