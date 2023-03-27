59127d96fcefa60248754ea280ec9adab0b846e8
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-03-27 23:34:22 +0200

    Update NationalBankOfUkraineTests.cs

--- TIKSN.Framework.IntegrationTests/Finance/ForeignExchange/NationalBankOfUkraineTests.cs
+++ TIKSN.Framework.IntegrationTests/Finance/ForeignExchange/NationalBankOfUkraineTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using TIKSN.Finance.ForeignExchange.Bank;
@@ -10,24 +11,27 @@ namespace TIKSN.Finance.ForeignExchange.IntegrationTests
 {
     public class NationalBankOfUkraineTests
     {
-        private readonly ICurrencyFactory _currencyFactory;
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly ICurrencyFactory currencyFactory;
 
         public NationalBankOfUkraineTests()
         {
             var services = new ServiceCollection();
             _ = services.AddMemoryCache();
+            _ = services.AddHttpClient();
             _ = services.AddSingleton<ICurrencyFactory, CurrencyFactory>();
             _ = services.AddSingleton<IRegionFactory, RegionFactory>();
 
             var serviceProvider = services.BuildServiceProvider();
-            this._currencyFactory = serviceProvider.GetRequiredService<ICurrencyFactory>();
+            this.currencyFactory = serviceProvider.GetRequiredService<ICurrencyFactory>();
+            this.httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
         }
 
         [Fact]
         public async Task ConvertCurrencyAsync001Async()
         {
             var date = new DateTimeOffset(2016, 05, 06, 0, 0, 0, TimeSpan.Zero);
-            var nbu = new NationalBankOfUkraine(this._currencyFactory);
+            var nbu = new NationalBankOfUkraine(this.httpClientFactory, this.currencyFactory);
             var pairs = await nbu.GetCurrencyPairsAsync(date, default).ConfigureAwait(true);
 
             foreach (var pair in pairs)
@@ -43,7 +47,7 @@ namespace TIKSN.Finance.ForeignExchange.IntegrationTests
         [Fact]
         public async Task GetCurrencyPairsAsync001Async()
         {
-            var nbu = new NationalBankOfUkraine(this._currencyFactory);
+            var nbu = new NationalBankOfUkraine(this.httpClientFactory, this.currencyFactory);
 
             var pairs = await nbu.GetCurrencyPairsAsync(new DateTimeOffset(2016, 05, 06, 0, 0, 0, TimeSpan.Zero), default).ConfigureAwait(true);
 
@@ -54,7 +58,7 @@ namespace TIKSN.Finance.ForeignExchange.IntegrationTests
         public async Task GetExchangeRateAsync001Async()
         {
             var date = new DateTimeOffset(2016, 05, 06, 0, 0, 0, TimeSpan.Zero);
-            var nbu = new NationalBankOfUkraine(this._currencyFactory);
+            var nbu = new NationalBankOfUkraine(this.httpClientFactory, this.currencyFactory);
             var pairs = await nbu.GetCurrencyPairsAsync(date, default).ConfigureAwait(true);
 
             foreach (var pair in pairs)
