935ed6bc064590c19cbf76f398bd7b423df7d3c8
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-03-28 17:43:55 +0200

    Update ISerial.cs

--- TIKSN.Framework.Core/Numbering/ISerial.cs
+++ TIKSN.Framework.Core/Numbering/ISerial.cs
@@ -6,7 +6,6 @@ namespace TIKSN.Numbering;
 public interface ISerial<TSelf> : ISpanParsable<TSelf>, ISpanFormattable, IEquatable<TSelf>
     where TSelf : ISerial<TSelf>
 {
-
     static abstract Option<TSelf> Parse(string s, bool asciiOnly, IFormatProvider provider);
 
     static abstract Option<TSelf> Parse(ReadOnlySpan<char> s, bool asciiOnly, IFormatProvider provider);
