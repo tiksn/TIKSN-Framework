46424467ca4e29c06cc2f2920a5b285460cc1dcd
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-04-05 16:38:01 +0200

    Update TelemetryLogger.cs

--- TIKSN.Core/Analytics/Telemetry/TelemetryLogger.cs
+++ TIKSN.Core/Analytics/Telemetry/TelemetryLogger.cs
@@ -1,69 +1,82 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
 namespace TIKSN.Analytics.Telemetry
 {
     public class TelemetryLogger : IEventTelemeter, IExceptionTelemeter, IMetricTelemeter, ITraceTelemeter
     {
-        private readonly ILogger<TelemetryLogger> _logger;
+        private readonly ILogger<TelemetryLogger> logger;
 
-        public TelemetryLogger(ILogger<TelemetryLogger> logger) => this._logger = logger;
+        public TelemetryLogger(ILogger<TelemetryLogger> logger) => this.logger = logger;
 
         public Task TrackEventAsync(string name)
         {
-            this._logger.LogInformation("Telemetry Event '{0}' tracked.", name);
+            this.logger.LogInformation(
+                1064151770,
+                "Telemetry Event '{EventName}' tracked.",
+                name);
 
-            return Task.FromResult<object>(null);
+            return Task.CompletedTask;
         }
 
-        public Task TrackEventAsync(string name, IDictionary<string, string> properties)
+        public Task TrackEventAsync(string name, IReadOnlyDictionary<string, string> properties)
         {
-            this._logger.LogInformation("Telemetry Event '{0}' tracked. with properties {1}", name,
+            this.logger.LogInformation(
+                1781631658,
+                "Telemetry Event '{EventName}' tracked. with properties {EventProperties}",
+                name,
                 string.Join(" ", properties.Select(item => string.Format("{0} is {1}", item.Key, item.Value))));
 
-            return Task.FromResult<object>(null);
+            return Task.CompletedTask;
         }
 
         public Task TrackExceptionAsync(Exception exception)
         {
-            this._logger.LogError(1206969841, exception, "Telemetry Exception of type '{0}' is tracked.",
+            this.logger.LogError(
+                1206969841,
+                exception,
+                "Telemetry Exception of type '{ExceptionType}' is tracked.",
                 exception.GetType().FullName);
 
-            return Task.FromResult<object>(null);
+            return Task.CompletedTask;
         }
 
         public Task TrackExceptionAsync(Exception exception, TelemetrySeverityLevel severityLevel)
         {
-            this._logger.LogError(1215703672, exception,
+            this.logger.LogError(1215703672, exception,
                 "Telemetry Exception of type '{0}' is tracked with severity level of '{1}'.",
                 exception.GetType().FullName, severityLevel);
 
-            return Task.FromResult<object>(null);
+            return Task.CompletedTask;
         }
 
         public Task TrackMetricAsync(string metricName, decimal metricValue)
         {
-            this._logger.LogInformation("Telemetry Metric '{0}' tracked with value {1}.", metricName, metricValue);
+            this.logger.LogInformation(
+                635392194,
+                "Telemetry Metric '{MetricName}' tracked with value {MetricValue}.",
+                metricName, metricValue);
 
-            return Task.FromResult<object>(null);
+            return Task.CompletedTask;
         }
 
         public Task TrackTraceAsync(string message)
         {
-            this._logger.LogTrace("Telemetry Trace is tracked with message: {0}.", message);
+            this.logger.LogTrace(
+                1728345664,
+                "Telemetry Trace is tracked with message: {TraceMessage}.",
+                message);
 
-            return Task.FromResult<object>(null);
+            return Task.CompletedTask;
         }
 
         public Task TrackTraceAsync(string message, TelemetrySeverityLevel severityLevel)
         {
-            this._logger.LogTrace("Telemetry Trace is tracked with severity level of '{1}' and message: {0}.", message,
-                severityLevel);
+            this.logger.LogTrace(
+                93981428,
+                "Telemetry Trace is tracked with severity level of '{TraceLevel}' and message: {TraceMessage}.",
+                severityLevel, message);
 
-            return Task.FromResult<object>(null);
+            return Task.CompletedTask;
         }
     }
 }
