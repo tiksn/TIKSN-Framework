32c4b530543cc318a067d192811ad521ed599736
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-04-05 16:37:47 +0200

    Update AudibleTelemeter.cs

--- TIKSN.Core/Analytics/Telemetry/AudibleTelemeter.cs
+++ TIKSN.Core/Analytics/Telemetry/AudibleTelemeter.cs
@@ -1,33 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using TIKSN.Speech;
 
 namespace TIKSN.Analytics.Telemetry
 {
     public class AudibleTelemeter : IEventTelemeter, IExceptionTelemeter, IMetricTelemeter, ITraceTelemeter
     {
-        private readonly ITextToSpeechService _textToSpeechService;
+        private readonly ITextToSpeechService textToSpeechService;
 
-        public AudibleTelemeter(ITextToSpeechService textToSpeechService) => this._textToSpeechService =
+        public AudibleTelemeter(ITextToSpeechService textToSpeechService) => this.textToSpeechService =
             textToSpeechService ?? throw new ArgumentNullException(nameof(textToSpeechService));
 
-        public Task TrackEventAsync(string name) => this._textToSpeechService.SpeakAsync($"Event {name} occurred.");
+        public Task TrackEventAsync(string name) => this.textToSpeechService.SpeakAsync($"Event {name} occurred.");
 
-        public Task TrackEventAsync(string name, IDictionary<string, string> properties) =>
-            this._textToSpeechService.SpeakAsync($"Event {name} occurred.");
+        public Task TrackEventAsync(string name, IReadOnlyDictionary<string, string> properties) =>
+            this.textToSpeechService.SpeakAsync($"Event {name} occurred.");
 
-        public Task TrackExceptionAsync(Exception exception) => this._textToSpeechService.SpeakAsync(exception.Message);
+        public Task TrackExceptionAsync(Exception exception) => this.textToSpeechService.SpeakAsync(exception.Message);
 
         public Task TrackExceptionAsync(Exception exception, TelemetrySeverityLevel severityLevel) =>
-            this._textToSpeechService.SpeakAsync($"{severityLevel}. {exception.Message}");
+            this.textToSpeechService.SpeakAsync($"{severityLevel}. {exception.Message}");
 
         public Task TrackMetricAsync(string metricName, decimal metricValue) =>
-            this._textToSpeechService.SpeakAsync($"Metric {metricName} is {metricValue}.");
+            this.textToSpeechService.SpeakAsync($"Metric {metricName} is {metricValue}.");
 
-        public Task TrackTraceAsync(string message) => this._textToSpeechService.SpeakAsync(message);
+        public Task TrackTraceAsync(string message) => this.textToSpeechService.SpeakAsync(message);
 
         public Task TrackTraceAsync(string message, TelemetrySeverityLevel severityLevel) =>
-            this._textToSpeechService.SpeakAsync($"{severityLevel} {message}");
+            this.textToSpeechService.SpeakAsync($"{severityLevel} {message}");
     }
 }
