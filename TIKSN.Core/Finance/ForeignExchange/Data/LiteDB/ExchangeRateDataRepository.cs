2e14a79000f7c472bc6961d44d9a1e24d26bb790
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-04-08 23:42:49 +0200

    Update ExchangeRateDataRepository.cs

--- TIKSN.Core/Finance/ForeignExchange/Data/LiteDB/ExchangeRateDataRepository.cs
+++ TIKSN.Core/Finance/ForeignExchange/Data/LiteDB/ExchangeRateDataRepository.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using LiteDB;
 using TIKSN.Data.LiteDB;
 
@@ -13,6 +8,20 @@ namespace TIKSN.Finance.ForeignExchange.Data.LiteDB
         public ExchangeRateDataRepository(ILiteDbDatabaseProvider databaseProvider) : base(databaseProvider,
             "ExchangeRates", x => new BsonValue(x))
         {
+        }
+
+        public Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
+            string baseCurrencyCode,
+            string counterCurrencyCode,
+            DateTime dateFrom,
+            DateTime dateTo,
+            CancellationToken cancellationToken)
+        {
+            var results = this.collection.Find(x =>
+                x.BaseCurrencyCode == baseCurrencyCode &&
+                x.CounterCurrencyCode == counterCurrencyCode && x.AsOn >= dateFrom && x.AsOn <= dateTo);
+
+            return Task.FromResult<IReadOnlyList<ExchangeRateDataEntity>>(results.ToArray());
         }
 
         public Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
