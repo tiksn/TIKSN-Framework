eb9677293c8b29784abcfdf7e3bf065ccf2e9735
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-04-08 23:43:04 +0200

    Update IExchangeRateDataRepository.cs

--- TIKSN.Core/Finance/ForeignExchange/Data/Mongo/IExchangeRateDataRepository.cs
+++ TIKSN.Core/Finance/ForeignExchange/Data/Mongo/IExchangeRateDataRepository.cs
@@ -1,13 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using TIKSN.Data.Mongo;
 
 namespace TIKSN.Finance.ForeignExchange.Data.Mongo
 {
     public interface IExchangeRateDataRepository : IMongoRepository<ExchangeRateDataEntity, Guid>
     {
+        Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
+            string baseCurrencyCode,
+            string counterCurrencyCode,
+            DateTime dateFrom,
+            DateTime dateTo,
+            CancellationToken cancellationToken);
+
         Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
             Guid foreignExchangeID,
             string baseCurrencyCode,
