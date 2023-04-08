c32ad3d50721cff6576ffc51ee0d3a799e3e9b65
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-04-08 20:33:19 +0200

    Update ExchangeRateEntity.cs

--- TIKSN.Core/Finance/ForeignExchange/Data/ExchangeRateEntity.cs
+++ TIKSN.Core/Finance/ForeignExchange/Data/ExchangeRateEntity.cs
@@ -1,20 +1,35 @@
-using System;
 using TIKSN.Data;
 
 namespace TIKSN.Finance.ForeignExchange.Data
 {
     public class ExchangeRateEntity : IEntity<Guid>
     {
-        public DateTime AsOn { get; set; }
+        public ExchangeRateEntity(
+            Guid id,
+            string baseCurrencyCode,
+            string counterCurrencyCode,
+            Guid? foreignExchangeId,
+            DateTime asOn,
+            decimal rate)
+        {
+            this.ID = id;
+            this.BaseCurrencyCode = baseCurrencyCode;
+            this.CounterCurrencyCode = counterCurrencyCode;
+            this.ForeignExchangeID = foreignExchangeId;
+            this.AsOn = asOn;
+            this.Rate = rate;
+        }
 
-        public string BaseCurrencyCode { get; set; }
+        public DateTime AsOn { get; }
 
-        public string CounterCurrencyCode { get; set; }
+        public string BaseCurrencyCode { get; }
 
-        public Guid? ForeignExchangeID { get; set; }
+        public string CounterCurrencyCode { get; }
 
-        public Guid ID { get; set; }
+        public Guid? ForeignExchangeID { get; }
 
-        public decimal Rate { get; set; }
+        public Guid ID { get; }
+
+        public decimal Rate { get; }
     }
 }
