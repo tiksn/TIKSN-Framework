d0a46f33490d422f0447679a153854e00e869343
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-06-21 20:44:10 +0300

    Update IdentityMapper.cs

--- TIKSN.Core/Mapping/IdentityMapper.cs
+++ TIKSN.Core/Mapping/IdentityMapper.cs
@@ -1,6 +1,6 @@
 namespace TIKSN.Mapping
 {
-    public class IdentityMapper<T> : IMapper<T, T>
+    public class IdentityMapper<T> : IMapper<T, T>, IAsyncMapper<T, T>
     {
         private static readonly Lazy<IdentityMapper<T>> LazyInstance = new(() => new IdentityMapper<T>());
 
@@ -10,5 +10,8 @@ namespace TIKSN.Mapping
         public static IdentityMapper<T> Instance => LazyInstance.Value;
 
         public T Map(T source) => source;
+
+        public Task<T> MapAsync(T source, CancellationToken cancellationToken)
+            => Task.FromResult(source);
     }
 }
