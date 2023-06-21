cca36978c6fc1aeaf50cf9a921cbad9c9dd240cb
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-06-22 02:23:51 +0300

    Update MultibaseCorrelationServiceTests.cs

--- TIKSN.UnitTests.Shared/Integration/Correlation/MultibaseCorrelationServiceTests.cs
+++ TIKSN.UnitTests.Shared/Integration/Correlation/MultibaseCorrelationServiceTests.cs
@@ -5,45 +5,44 @@ using TIKSN.DependencyInjection;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace TIKSN.Integration.Correlation.Tests
+namespace TIKSN.Integration.Correlation.Tests;
+
+public class MultibaseCorrelationServiceTests
 {
-    public class MultibaseCorrelationServiceTests
+    private readonly ICorrelationService correlationService;
+    private readonly ITestOutputHelper testOutputHelper;
+
+    public MultibaseCorrelationServiceTests(ITestOutputHelper testOutputHelper)
     {
-        private readonly ICorrelationService _correlationService;
-        private readonly ITestOutputHelper _testOutputHelper;
+        var services = new ServiceCollection();
+        _ = services.AddFrameworkPlatform();
+        _ = services.AddSingleton<ICorrelationService, MultibaseCorrelationService>();
+        var serviceProvider = services.BuildServiceProvider();
+        this.correlationService = serviceProvider.GetRequiredService<ICorrelationService>();
+        this.testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+    }
 
-        public MultibaseCorrelationServiceTests(ITestOutputHelper testOutputHelper)
-        {
-            var services = new ServiceCollection();
-            _ = services.AddFrameworkPlatform();
-            _ = services.AddSingleton<ICorrelationService, MultibaseCorrelationService>();
-            var serviceProvider = services.BuildServiceProvider();
-            this._correlationService = serviceProvider.GetRequiredService<ICorrelationService>();
-            this._testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
-        }
+    [Fact]
+    public void GenerateAndParse()
+    {
+        var correlationID = this.correlationService.Generate();
+        this.LogOutput(correlationID, nameof(correlationID));
+        var correlationIDFromString = this.correlationService.Create(correlationID.ToString());
+        this.LogOutput(correlationIDFromString, nameof(correlationIDFromString));
+        var correlationIDFromBytes = this.correlationService.Create(correlationID.ToBinary());
+        this.LogOutput(correlationIDFromBytes, nameof(correlationIDFromBytes));
 
-        [Fact]
-        public void GenerateAndParse()
-        {
-            var correlationID = this._correlationService.Generate();
-            this.LogOutput(correlationID, nameof(correlationID));
-            var correlationIDFromString = this._correlationService.Create(correlationID.ToString());
-            this.LogOutput(correlationIDFromString, nameof(correlationIDFromString));
-            var correlationIDFromBytes = this._correlationService.Create(correlationID.ToByteArray());
-            this.LogOutput(correlationIDFromBytes, nameof(correlationIDFromBytes));
+        _ = correlationIDFromString.Should().Be(correlationID);
+        _ = correlationIDFromBytes.Should().Be(correlationID);
+        _ = correlationIDFromString.Should().Be(correlationIDFromBytes);
+    }
 
-            _ = correlationIDFromString.Should().Be(correlationID);
-            _ = correlationIDFromBytes.Should().Be(correlationID);
-            _ = correlationIDFromString.Should().Be(correlationIDFromBytes);
-        }
-
-        private void LogOutput(CorrelationID correlationID, string name)
-        {
-            this._testOutputHelper.WriteLine("-------------------------");
-            this._testOutputHelper.WriteLine(name);
-            this._testOutputHelper.WriteLine(correlationID.ToString());
-            this._testOutputHelper.WriteLine(BitConverter.ToString(correlationID.ToByteArray()));
-            this._testOutputHelper.WriteLine("");
-        }
+    private void LogOutput(CorrelationId correlationID, string name)
+    {
+        this.testOutputHelper.WriteLine("-------------------------");
+        this.testOutputHelper.WriteLine(name);
+        this.testOutputHelper.WriteLine(correlationID.ToString());
+        this.testOutputHelper.WriteLine(BitConverter.ToString(correlationID.ToBinary().ToArray()));
+        this.testOutputHelper.WriteLine("");
     }
 }
