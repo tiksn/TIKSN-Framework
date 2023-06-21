94b153524dd0b2c2d5423839eeee57e1ace037e0
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-06-22 02:22:52 +0300

    Update CorrelationId.cs

--- TIKSN.Core/Integration/Correlation/CorrelationId.cs
+++ TIKSN.Core/Integration/Correlation/CorrelationId.cs
@@ -1,98 +1,95 @@
-using System;
-using System.Linq;
+using LanguageExt;
 
-namespace TIKSN.Integration.Correlation
+namespace TIKSN.Integration.Correlation;
+
+/// <summary>
+///     Correlation ID
+/// </summary>
+public readonly struct CorrelationId : IEquatable<CorrelationId>
 {
     /// <summary>
-    ///     Correlation ID
+    ///     Empty Correlation ID.
     /// </summary>
-    public readonly struct CorrelationId : IEquatable<CorrelationId>
+    public static readonly CorrelationId Empty;
+
+    private readonly Seq<byte> binaryRepresentation;
+    private readonly string stringRepresentation;
+
+    internal CorrelationId(string stringRepresentation, Seq<byte> binaryRepresentation)
     {
-        /// <summary>
-        ///     Empty Correlation ID.
-        /// </summary>
-        public static readonly CorrelationId Empty;
-
-        private readonly byte[] _byteArrayRepresentation;
-        private readonly string _stringRepresentation;
-
-        internal CorrelationId(string stringRepresentation, byte[] byteArrayRepresentation)
-        {
-            this._stringRepresentation =
-                stringRepresentation ?? throw new ArgumentNullException(nameof(stringRepresentation));
-            this._byteArrayRepresentation = byteArrayRepresentation ??
-                                            throw new ArgumentNullException(nameof(byteArrayRepresentation));
-        }
-
-        /// <summary>
-        ///     Implicitly convert <see cref="CorrelationId" /> to byte array.
-        /// </summary>
-        /// <param name="correlationId"></param>
-        public static implicit operator byte[](CorrelationId correlationId) => correlationId._byteArrayRepresentation;
-
-        /// <summary>
-        ///     Implicitly convert <see cref="CorrelationId" /> to string.
-        /// </summary>
-        /// <param name="correlationId"></param>
-        public static implicit operator string(CorrelationId correlationId) => correlationId._stringRepresentation;
-
-        /// <summary>
-        ///     Indicates whether this instance and a <paramref name="other" /> are equal by their binary representation.
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        public bool Equals(CorrelationId other) =>
-            this._byteArrayRepresentation.SequenceEqual(other._byteArrayRepresentation);
-
-        /// <summary>
-        ///     Indicates whether this instance and a specified object are equal.
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public override bool Equals(object obj)
-        {
-            if (obj is null or not CorrelationId)
-            {
-                return false;
-            }
-
-            var other = (CorrelationId)obj;
-
-            return this.Equals(other);
-        }
-
-        /// <summary>
-        ///     Returns the hash code for this instance.
-        /// </summary>
-        /// <returns>A 32-bit signed integer that is the hash code for this instance.</returns>
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                var hash = 27;
-                for (var i = 0; i < this._byteArrayRepresentation.Length && i < 4; i++)
-                {
-                    hash = (13 * hash) + this._byteArrayRepresentation[i].GetHashCode();
-                }
-
-                return hash;
-            }
-        }
-
-        /// <summary>
-        ///     Returns binary representation.
-        /// </summary>
-        /// <returns></returns>
-        public byte[] ToByteArray() => this._byteArrayRepresentation;
-
-        /// <summary>
-        ///     Returns string representation.
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString() => this._stringRepresentation;
-
-        public static bool operator ==(CorrelationId left, CorrelationId right) => left.Equals(right);
-
-        public static bool operator !=(CorrelationId left, CorrelationId right) => !(left == right);
+        this.stringRepresentation =
+            stringRepresentation ?? throw new ArgumentNullException(nameof(stringRepresentation));
+        this.binaryRepresentation = binaryRepresentation;
     }
+
+    /// <summary>
+    ///     Implicitly convert <see cref="CorrelationId" /> to byte array.
+    /// </summary>
+    /// <param name="correlationId"></param>
+    public static implicit operator Seq<byte>(CorrelationId correlationId) => correlationId.binaryRepresentation;
+
+    /// <summary>
+    ///     Implicitly convert <see cref="CorrelationId" /> to string.
+    /// </summary>
+    /// <param name="correlationId"></param>
+    public static implicit operator string(CorrelationId correlationId) => correlationId.stringRepresentation;
+
+    /// <summary>
+    ///     Indicates whether this instance and a <paramref name="other" /> are equal by their binary representation.
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public bool Equals(CorrelationId other) =>
+        this.binaryRepresentation.SequenceEqual(other.binaryRepresentation);
+
+    /// <summary>
+    ///     Indicates whether this instance and a specified object are equal.
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object obj)
+    {
+        if (obj is null or not CorrelationId)
+        {
+            return false;
+        }
+
+        var other = (CorrelationId)obj;
+
+        return this.Equals(other);
+    }
+
+    /// <summary>
+    ///     Returns the hash code for this instance.
+    /// </summary>
+    /// <returns>A 32-bit signed integer that is the hash code for this instance.</returns>
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            var hash = 27;
+            for (var i = 0; i < this.binaryRepresentation.Length && i < 4; i++)
+            {
+                hash = (13 * hash) + this.binaryRepresentation[i].GetHashCode();
+            }
+
+            return hash;
+        }
+    }
+
+    /// <summary>
+    ///     Returns binary representation.
+    /// </summary>
+    /// <returns></returns>
+    public Seq<byte> ToBinary() => this.binaryRepresentation;
+
+    /// <summary>
+    ///     Returns string representation.
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => this.stringRepresentation;
+
+    public static bool operator ==(CorrelationId left, CorrelationId right) => left.Equals(right);
+
+    public static bool operator !=(CorrelationId left, CorrelationId right) => !(left == right);
 }
