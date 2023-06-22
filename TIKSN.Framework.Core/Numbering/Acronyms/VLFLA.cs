c18ed3f772f9b86a08c6fc5eee39689206334b7d
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-06-22 15:48:06 +0300

    Update VLFLA.cs

--- TIKSN.Framework.Core/Numbering/Acronyms/VLFLA.cs
+++ TIKSN.Framework.Core/Numbering/Acronyms/VLFLA.cs
@@ -4,7 +4,7 @@ public sealed class VLFLA : Acronym<VLFLA>
 {
     private static readonly int LetterCount = 5;
 
-    protected VLFLA(string value) : base(value)
+    private VLFLA(string value) : base(value)
     {
     }
 }
