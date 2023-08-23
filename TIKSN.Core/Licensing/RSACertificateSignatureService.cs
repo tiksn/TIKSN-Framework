8af2349c84dfe0c7b891277b33f0ea0441b4dddf
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-24 00:53:11 +0200

    Create RSACertificateSignatureService.cs

---
+++ TIKSN.Core/Licensing/RSACertificateSignatureService.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TIKSN.Licensing;
+
+public class RSACertificateSignatureService : ICertificateSignatureService
+{
+    public byte[] Sign(
+        byte[] data,
+        X509Certificate2 privateCertificate)
+    {
+        using var privateKey = privateCertificate.GetRSAPrivateKey();
+        return privateKey.SignData(
+            data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+    }
+
+    public bool Verify(
+        byte[] data,
+        byte[] signature,
+        X509Certificate2 publicCertificate)
+    {
+        using var publicKey = publicCertificate.GetRSAPublicKey();
+        return publicKey.VerifyData(data, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+    }
+}
