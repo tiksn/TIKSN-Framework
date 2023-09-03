866fa73ce26e648da93b5b6d32074492a2ea73d0
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-03 17:16:25 +0200

    Update LicenseTests.cs

--- TIKSN.Framework.Core.Tests/Licensing/LicenseTests.cs
+++ TIKSN.Framework.Core.Tests/Licensing/LicenseTests.cs
@@ -5,8 +5,10 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 using TIKSN.DependencyInjection;
 using TIKSN.Licensing;
+using TIKSN.Time;
 using Xunit;
 using static LanguageExt.Prelude;
 
@@ -34,6 +36,11 @@ public class LicenseTests
         _ = services.AddSingleton<IEntitlementsConverter<TestEntitlements, TestLicenseEntitlements>, TestEntitlementsConverter>();
         _ = services.AddFrameworkPlatform();
 
+        var fakeTimeProvider = Substitute.For<ITimeProvider>();
+        _ = fakeTimeProvider.GetCurrentTime()
+            .Returns(new DateTimeOffset(2022, 9, 24, 0, 0, 0, TimeSpan.Zero));
+
+        _ = services.AddSingleton(fakeTimeProvider);
         ContainerBuilder containerBuilder = new();
         _ = containerBuilder.RegisterModule<CoreModule>();
         _ = containerBuilder.RegisterModule<PlatformModule>();
