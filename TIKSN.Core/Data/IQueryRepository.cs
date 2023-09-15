31a50cddcede5134f0e2ba2b6bc67d0e75b5d7cb
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-15 14:19:26 +0200

    Update IQueryRepository.cs

--- TIKSN.Core/Data/IQueryRepository.cs
+++ TIKSN.Core/Data/IQueryRepository.cs
@@ -1,20 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+namespace TIKSN.Data;
 
-namespace TIKSN.Data
+public interface IQueryRepository<TEntity, TIdentity>
+    where TEntity : IEntity<TIdentity>
+    where TIdentity : IEquatable<TIdentity>
 {
-    public interface IQueryRepository<TEntity, TIdentity>
-        where TEntity : IEntity<TIdentity>
-        where TIdentity : IEquatable<TIdentity>
-    {
-        Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken);
+    Task<bool> ExistsAsync(TIdentity id, CancellationToken cancellationToken);
 
-        Task<TEntity> GetOrDefaultAsync(TIdentity id, CancellationToken cancellationToken);
+    Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken);
 
-        Task<bool> ExistsAsync(TIdentity id, CancellationToken cancellationToken);
+    Task<TEntity> GetOrDefaultAsync(TIdentity id, CancellationToken cancellationToken);
 
-        Task<IEnumerable<TEntity>> ListAsync(IEnumerable<TIdentity> ids, CancellationToken cancellationToken);
-    }
+    Task<IEnumerable<TEntity>> ListAsync(IEnumerable<TIdentity> ids, CancellationToken cancellationToken);
+
+    Task<PageResult<TEntity>> PageAsync(PageQuery pageQuery, CancellationToken cancellationToken);
 }
