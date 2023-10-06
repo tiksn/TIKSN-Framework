a6c15d3f1ebd444b2396e5936ac03d5b67da6dbf
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-10-06 12:47:05 +0200

    Create PlatformModuleBase.cs

---
+++ TIKSN.Framework.Maui/DependencyInjection/PlatformModuleBase.cs
@@ -0,0 +1,10 @@
+using Autofac;
+
+namespace TIKSN.DependencyInjection;
+
+public abstract class PlatformModuleBase : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+    }
+}
