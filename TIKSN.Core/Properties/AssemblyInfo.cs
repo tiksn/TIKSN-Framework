863a54ee1d2edad3269d3122330f0d1c6fa94b79
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-10-08 00:20:31 +0200

    Remove Standard Project

--- TIKSN.Core/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("TIKSN.Core")]
-[assembly: NeutralResourcesLanguage("en")]
-[assembly: InternalsVisibleTo("TIKSN.Framework.Tests")]
-[assembly: InternalsVisibleTo("TIKSN.Core.Explorables")]
