1c2d8f9fc08f46bb6f8914ae214dac8c547d58aa
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-10-07 23:52:34 +0200

    Update ServiceCollectionExtensions.cs

--- TIKSN.Framework.Core/DependencyInjection/ServiceCollectionExtensions.cs
+++ TIKSN.Framework.Core/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,21 +1,70 @@
+using System.Numerics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Localization;
+using MsgPack.Serialization;
+using ReactiveUI;
 using TIKSN.FileSystem;
+using TIKSN.Globalization;
+using TIKSN.Identity;
+using TIKSN.Serialization;
+using TIKSN.Serialization.Bond;
+using TIKSN.Serialization.MessagePack;
+using TIKSN.Serialization.Numerics;
 using TIKSN.Shell;
+using TIKSN.Time;
+using TIKSN.Web.Rest;
+using MessagePackSerializer = TIKSN.Serialization.MessagePack.MessagePackSerializer;
 
-namespace TIKSN.DependencyInjection
+namespace TIKSN.DependencyInjection;
+
+public static class ServiceCollectionExtensions
 {
-    public static class ServiceCollectionExtensions
+    public static IServiceCollection AddFrameworkCore(this IServiceCollection services)
     {
-        public static IServiceCollection AddFrameworkPlatform(
-            this IServiceCollection services)
-        {
-            _ = services.AddFrameworkCore();
+        _ = services.AddLocalization();
+        _ = services.AddMemoryCache();
+        _ = services.AddHttpClient();
+        _ = services.AddOptions();
 
-            services.TryAddSingleton<IConsoleService, ConsoleService>();
-            services.TryAddSingleton<IKnownFolders, KnownFolders>();
+        services.TryAddSingleton<ICultureFactory, CultureFactory>();
+        services.TryAddSingleton<ICurrencyFactory, CurrencyFactory>();
+        services.TryAddSingleton<IRegionFactory, RegionFactory>();
+        services.TryAddSingleton<IResourceNamesCache, ResourceNamesCache>();
+        services.TryAddSingleton<IShellCommandEngine, ShellCommandEngine>();
+        services.TryAddSingleton<ITimeProvider, TimeProvider>();
+        services.TryAddSingleton<Random>();
+        services.TryAddSingleton(SerializationContext.Default);
+        services.TryAddSingleton<IKnownFolders, KnownFolders>();
 
-            return services;
-        }
+        services.TryAddSingleton<CompactBinaryBondDeserializer>();
+        services.TryAddSingleton<CompactBinaryBondSerializer>();
+        services.TryAddSingleton<DotNetXmlDeserializer>();
+        services.TryAddSingleton<DotNetXmlSerializer>();
+        services.TryAddSingleton<FastBinaryBondDeserializer>();
+        services.TryAddSingleton<FastBinaryBondSerializer>();
+        services.TryAddSingleton<ICustomDeserializer<byte[], BigInteger>, UnsignedBigIntegerBinaryDeserializer>();
+        services.TryAddSingleton<ICustomSerializer<byte[], BigInteger>, UnsignedBigIntegerBinarySerializer>();
+        services.TryAddSingleton<IRestRequester, RestRequester>();
+        services.TryAddSingleton<JsonDeserializer>();
+        services.TryAddSingleton<JsonSerializer>();
+        services.TryAddSingleton<MessagePackDeserializer>();
+        services.TryAddSingleton<MessagePackSerializer>();
+        services.TryAddSingleton<SimpleBinaryBondDeserializer>();
+        services.TryAddSingleton<SimpleBinaryBondSerializer>();
+        services.TryAddSingleton<SimpleJsonBondDeserializer>();
+        services.TryAddSingleton<SimpleJsonBondSerializer>();
+        services.TryAddSingleton<SimpleXmlBondDeserializer>();
+        services.TryAddSingleton<SimpleXmlBondSerializer>();
+        services.TryAddSingleton<IIdentityGenerator<long>, IdGenIdentityGenerator>();
+
+        services.TryAddScoped<IShellCommandContext, ShellCommandContext>();
+
+        _ = services.AddSingleton(MessageBus.Current);
+
+        services.TryAddSingleton<IConsoleService, ConsoleService>();
+        services.TryAddSingleton<IKnownFolders, KnownFolders>();
+
+        return services;
     }
 }
