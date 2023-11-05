cd00b327b773e4af488092458f35b44b2732340d
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-05 21:11:43 +0100

    Create UserBareEntityResolver.cs

---
+++ TIKSN.Framework.Core/Data/BareEntityResolvers/UserBareEntityResolver.cs
@@ -0,0 +1,24 @@
+namespace TIKSN.Data.BareEntityResolvers;
+
+public class UserBareEntityResolver<TEntity, TEntityIdentity, TUserIdentity>
+  : IBareEntityResolver<TEntity, UserEntity<TEntityIdentity, TUserIdentity>, TEntityIdentity>
+  where TEntity : IUserEntity<TEntityIdentity, TUserIdentity>
+  where TEntityIdentity : IEquatable<TEntityIdentity>
+  where TUserIdentity : IEquatable<TUserIdentity>
+{
+    private readonly IQueryRepository<TEntity, TEntityIdentity> queryRepository;
+
+    public UserBareEntityResolver(
+      IQueryRepository<TEntity, TEntityIdentity> queryRepository)
+        => this.queryRepository = queryRepository ?? throw new ArgumentNullException(nameof(queryRepository));
+
+    public async Task<UserEntity<TEntityIdentity, TUserIdentity>> ResolveAsync(
+      TEntityIdentity id,
+      CancellationToken cancellationToken)
+    {
+        var entity = await this.queryRepository.GetAsync(id, cancellationToken).ConfigureAwait(false);
+
+        return new UserEntity<TEntityIdentity, TUserIdentity>(
+          entity.ID, entity.UserID);
+    }
+}
