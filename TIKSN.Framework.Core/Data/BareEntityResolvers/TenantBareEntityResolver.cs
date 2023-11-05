3a5ce1f9ca22af4a5a4fce358152bb288c2de81e
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-05 21:10:09 +0100

    Create TenantBareEntityResolver.cs

---
+++ TIKSN.Framework.Core/Data/BareEntityResolvers/TenantBareEntityResolver.cs
@@ -0,0 +1,24 @@
+namespace TIKSN.Data.BareEntityResolvers;
+
+public class TenantBareEntityResolver<TEntity, TEntityIdentity, TTenantIdentity>
+  : IBareEntityResolver<TEntity, TenantEntity<TEntityIdentity, TTenantIdentity>, TEntityIdentity>
+  where TEntity : ITenantEntity<TEntityIdentity, TTenantIdentity>
+  where TEntityIdentity : IEquatable<TEntityIdentity>
+  where TTenantIdentity : IEquatable<TTenantIdentity>
+{
+    private readonly IQueryRepository<TEntity, TEntityIdentity> queryRepository;
+
+    public TenantBareEntityResolver(
+      IQueryRepository<TEntity, TEntityIdentity> queryRepository)
+        => this.queryRepository = queryRepository ?? throw new ArgumentNullException(nameof(queryRepository));
+
+    public async Task<TenantEntity<TEntityIdentity, TTenantIdentity>> ResolveAsync(
+      TEntityIdentity id,
+      CancellationToken cancellationToken)
+    {
+        var entity = await this.queryRepository.GetAsync(id, cancellationToken).ConfigureAwait(false);
+
+        return new TenantEntity<TEntityIdentity, TTenantIdentity>(
+          entity.ID, entity.TenantID);
+    }
+}
