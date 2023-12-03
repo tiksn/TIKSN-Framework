de214258a6652b3e812b957f127e3f02de28de59
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 00:11:44 +0100

    Create INationalBankOfUkraine.cs

---
+++ TIKSN.Framework.Core/Finance/ForeignExchange/Bank/INationalBankOfUkraine.cs
@@ -0,0 +1,9 @@
+namespace TIKSN.Finance.ForeignExchange.Bank;
+
+/// <summary>
+///     Exchange rate converter of National Bank of Ukraine
+/// </summary>
+/// <seealso cref="ICurrencyConverter" />
+public interface INationalBankOfUkraine : ICurrencyConverter, IExchangeRatesProvider
+{
+}
