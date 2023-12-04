7c168f2bd315a4e7842059191cfa0d7189629bfc
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:32:39 +0100

    Update ILiteDbDatabaseProvider.cs

--- TIKSN.Framework.Core/Data/LiteDB/ILiteDbDatabaseProvider.cs
+++ TIKSN.Framework.Core/Data/LiteDB/ILiteDbDatabaseProvider.cs
@@ -1,23 +1,22 @@
 using LiteDB;
 
-namespace TIKSN.Data.LiteDB
+namespace TIKSN.Data.LiteDB;
+
+/// <summary>
+///     Create LiteDB database
+/// </summary>
+public interface ILiteDbDatabaseProvider
 {
     /// <summary>
-    ///     Create LiteDB database
+    ///     Creates LiteDB database
     /// </summary>
-    public interface ILiteDbDatabaseProvider
-    {
-        /// <summary>
-        ///     Creates LiteDB database
-        /// </summary>
-        /// <returns></returns>
-        LiteDatabase GetDatabase();
+    /// <returns></returns>
+    LiteDatabase GetDatabase();
 
-        /// <summary>
-        ///     Creates LiteDB database with mapper
-        /// </summary>
-        /// <param name="mapper">Mapper</param>
-        /// <returns></returns>
-        LiteDatabase GetDatabase(BsonMapper mapper);
-    }
+    /// <summary>
+    ///     Creates LiteDB database with mapper
+    /// </summary>
+    /// <param name="mapper">Mapper</param>
+    /// <returns></returns>
+    LiteDatabase GetDatabase(BsonMapper mapper);
 }
