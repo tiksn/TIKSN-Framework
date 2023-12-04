ed82bd6f6022de3053abc41240e2194228f43ef3
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:23:17 +0100

    Update AdUnitBundle.cs

--- TIKSN.Framework.Core/Advertising/AdUnitBundle.cs
+++ TIKSN.Framework.Core/Advertising/AdUnitBundle.cs
@@ -1,29 +1,26 @@
-using System;
+namespace TIKSN.Advertising;
 
-namespace TIKSN.Advertising
+public class AdUnitBundle
 {
-    public class AdUnitBundle
+    public AdUnitBundle(AdUnit designTime, AdUnit production)
     {
-        public AdUnitBundle(AdUnit designTime, AdUnit production)
+        this.DesignTime = designTime ?? throw new ArgumentNullException(nameof(designTime));
+        this.Production = production ?? throw new ArgumentNullException(nameof(production));
+
+        if (!designTime.IsTest)
         {
-            this.DesignTime = designTime ?? throw new ArgumentNullException(nameof(designTime));
-            this.Production = production ?? throw new ArgumentNullException(nameof(production));
-
-            if (!designTime.IsTest)
-            {
-                throw new ArgumentException($"Value of {nameof(designTime)}.{nameof(designTime.IsTest)} must be true.",
-                    nameof(designTime));
-            }
-
-            if (production.IsTest)
-            {
-                throw new ArgumentException($"Value of {nameof(production)}.{nameof(production.IsTest)} must be false.",
-                    nameof(production));
-            }
+            throw new ArgumentException($"Value of {nameof(designTime)}.{nameof(designTime.IsTest)} must be true.",
+                nameof(designTime));
         }
 
-        public AdUnit DesignTime { get; }
-
-        public AdUnit Production { get; }
+        if (production.IsTest)
+        {
+            throw new ArgumentException($"Value of {nameof(production)}.{nameof(production.IsTest)} must be false.",
+                nameof(production));
+        }
     }
+
+    public AdUnit DesignTime { get; }
+
+    public AdUnit Production { get; }
 }
