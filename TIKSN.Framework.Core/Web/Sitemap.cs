f4eeb6902e5f76fca60d7305ac35f079fcffdb5a
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:42:51 +0100

    Update Sitemap.cs

--- TIKSN.Framework.Core/Web/Sitemap.cs
+++ TIKSN.Framework.Core/Web/Sitemap.cs
@@ -1,158 +1,155 @@
-using System;
-using System.Collections.Generic;
 using System.Xml;
 
-namespace TIKSN.Web
+namespace TIKSN.Web;
+
+public class Sitemap
 {
-    public class Sitemap
+    public Sitemap() => this.Pages = new HashSet<Page>();
+
+    public HashSet<Page> Pages { get; }
+
+    public void Write(XmlWriter XWriter)
     {
-        public Sitemap() => this.Pages = new HashSet<Page>();
+        XWriter.WriteStartDocument();
 
-        public HashSet<Page> Pages { get; }
+        XWriter.WriteStartElement("urlset", "http://www.sitemaps.org/schemas/sitemap/0.9");
 
-        public void Write(XmlWriter XWriter)
+        foreach (var P in this.Pages)
         {
-            XWriter.WriteStartDocument();
+            XWriter.WriteStartElement("url");
 
-            XWriter.WriteStartElement("urlset", "http://www.sitemaps.org/schemas/sitemap/0.9");
+            XWriter.WriteStartElement("loc");
+            XWriter.WriteValue(P.Address.AbsoluteUri);
+            XWriter.WriteEndElement();
 
-            foreach (var P in this.Pages)
+            if (P.LastModified.HasValue)
             {
-                XWriter.WriteStartElement("url");
-
-                XWriter.WriteStartElement("loc");
-                XWriter.WriteValue(P.Address.AbsoluteUri);
+                XWriter.WriteStartElement("lastmod");
+                XWriter.WriteValue(P.LastModified.Value.ToString("yyyy-MM-dd"));
                 XWriter.WriteEndElement();
+            }
 
-                if (P.LastModified.HasValue)
-                {
-                    XWriter.WriteStartElement("lastmod");
-                    XWriter.WriteValue(P.LastModified.Value.ToString("yyyy-MM-dd"));
-                    XWriter.WriteEndElement();
-                }
+            if (P.ChangeFrequency.HasValue)
+            {
+                XWriter.WriteStartElement("changefreq");
+                XWriter.WriteValue(P.ChangeFrequency.Value.ToString().ToLower(System.Globalization.CultureInfo.CurrentCulture));
+                XWriter.WriteEndElement();
+            }
 
-                if (P.ChangeFrequency.HasValue)
-                {
-                    XWriter.WriteStartElement("changefreq");
-                    XWriter.WriteValue(P.ChangeFrequency.Value.ToString().ToLower(System.Globalization.CultureInfo.CurrentCulture));
-                    XWriter.WriteEndElement();
-                }
-
-                if (P.Priority.HasValue)
-                {
-                    XWriter.WriteStartElement("priority");
-                    XWriter.WriteValue(P.Priority.Value);
-                    XWriter.WriteEndElement();
-                }
-
+            if (P.Priority.HasValue)
+            {
+                XWriter.WriteStartElement("priority");
+                XWriter.WriteValue(P.Priority.Value);
                 XWriter.WriteEndElement();
             }
 
             XWriter.WriteEndElement();
-
-            XWriter.Flush();
         }
 
-        public class Page : IEquatable<Page>
+        XWriter.WriteEndElement();
+
+        XWriter.Flush();
+    }
+
+    public class Page : IEquatable<Page>
+    {
+        public enum Frequency { Always = 0, Hourly = 1, Daily = 2, Weekly = 3, Monthly = 4, Yearly = 5, Never = 6 }
+
+        private Uri address;
+
+        private double? priority;
+
+        public Page(Uri Address, DateTime? LastModified, Frequency? ChangeFrequency, double? Priority)
         {
-            public enum Frequency { Always = 0, Hourly = 1, Daily = 2, Weekly = 3, Monthly = 4, Yearly = 5, Never = 6 }
+            this.Address = Address;
+            this.LastModified = LastModified;
+            this.ChangeFrequency = ChangeFrequency;
+            this.Priority = Priority;
+        }
 
-            private Uri address;
-
-            private double? priority;
-
-            public Page(Uri Address, DateTime? LastModified, Frequency? ChangeFrequency, double? Priority)
+        public Uri Address
+        {
+            get => this.address;
+            private set
             {
-                this.Address = Address;
-                this.LastModified = LastModified;
-                this.ChangeFrequency = ChangeFrequency;
-                this.Priority = Priority;
-            }
-
-            public Uri Address
-            {
-                get => this.address;
-                private set
+                if (value is null)
                 {
-                    if (value is null)
-                    {
-                        throw new ArgumentNullException("Address");
-                    }
-
-                    this.address = value;
+                    throw new ArgumentNullException("Address");
                 }
+
+                this.address = value;
             }
+        }
 
-            public Frequency? ChangeFrequency { get; }
+        public Frequency? ChangeFrequency { get; }
 
-            public DateTime? LastModified { get; }
+        public DateTime? LastModified { get; }
 
-            public double? Priority
+        public double? Priority
+        {
+            get => this.priority;
+            private set
             {
-                get => this.priority;
-                private set
+                if (!value.HasValue || (value.Value >= 0.0d && value.Value <= 1.0d))
                 {
-                    if (!value.HasValue || (value.Value >= 0.0d && value.Value <= 1.0d))
-                    {
-                        this.priority = value;
-                    }
-                    else
-                    {
-                        throw new ArgumentOutOfRangeException("Valid values range from 0.0 to 1.0.");
-                    }
+                    this.priority = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Valid values range from 0.0 to 1.0.");
                 }
             }
+        }
 
-            public bool Equals(Page other)
+        public bool Equals(Page other)
+        {
+            if (other is null)
             {
-                if (other is null)
-                {
-                    return false;
-                }
-
-                if (ReferenceEquals(this, other))
-                {
-                    return true;
-                }
-
-                return this.Address == other.Address;
+                return false;
             }
 
-            public static bool operator !=(Page page1, Page page2) => !Equals(page1, page2);
-
-            public static bool operator ==(Page page1, Page page2) => Equals(page1, page2);
-
-            public override bool Equals(object obj)
+            if (ReferenceEquals(this, other))
             {
-                if (obj is null)
-                {
-                    return false;
-                }
-
-                if (ReferenceEquals(this, obj))
-                {
-                    return true;
-                }
-
-                if (obj is not Page p)
-                {
-                    return false;
-                }
-
-                return this.Equals(p);
+                return true;
             }
 
-            public override int GetHashCode() => this.Address.GetHashCode();
+            return this.Address == other.Address;
+        }
 
-            private static bool Equals(Page page1, Page page2)
+        public static bool operator !=(Page page1, Page page2) => !Equals(page1, page2);
+
+        public static bool operator ==(Page page1, Page page2) => Equals(page1, page2);
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
             {
-                if (page1 is null)
-                {
-                    return page2 is null;
-                }
-
-                return page1.Equals(page2);
+                return false;
             }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj is not Page p)
+            {
+                return false;
+            }
+
+            return this.Equals(p);
+        }
+
+        public override int GetHashCode() => this.Address.GetHashCode();
+
+        private static bool Equals(Page page1, Page page2)
+        {
+            if (page1 is null)
+            {
+                return page2 is null;
+            }
+
+            return page1.Equals(page2);
         }
     }
 }
