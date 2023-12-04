1f040965ce00bef9f0c10d55274cd1ec775719b4
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:23:14 +0100

    Update AdUnit.cs

--- TIKSN.Framework.Core/Advertising/AdUnit.cs
+++ TIKSN.Framework.Core/Advertising/AdUnit.cs
@@ -1,21 +1,20 @@
-namespace TIKSN.Advertising
+namespace TIKSN.Advertising;
+
+public class AdUnit
 {
-    public class AdUnit
+    public AdUnit(string provider, string applicationId, string adUnitId, bool isTest = false)
     {
-        public AdUnit(string provider, string applicationId, string adUnitId, bool isTest = false)
-        {
-            this.Provider = provider;
-            this.ApplicationId = applicationId;
-            this.AdUnitId = adUnitId;
-            this.IsTest = isTest;
-        }
-
-        public string AdUnitId { get; }
-
-        public string ApplicationId { get; }
-
-        public bool IsTest { get; }
-
-        public string Provider { get; }
+        this.Provider = provider;
+        this.ApplicationId = applicationId;
+        this.AdUnitId = adUnitId;
+        this.IsTest = isTest;
     }
+
+    public string AdUnitId { get; }
+
+    public string ApplicationId { get; }
+
+    public bool IsTest { get; }
+
+    public string Provider { get; }
 }
