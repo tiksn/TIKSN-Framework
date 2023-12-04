84c75d5e3679ef5279ce6e0fad7549c19f4fb7e6
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:29:01 +0100

    Update PowerShellEventTelemeter.cs

--- TIKSN.Framework.Core/Analytics/Telemetry/PowerShellEventTelemeter.cs
+++ TIKSN.Framework.Core/Analytics/Telemetry/PowerShellEventTelemeter.cs
@@ -1,26 +1,25 @@
 using System.Management.Automation;
 
-namespace TIKSN.Analytics.Telemetry
+namespace TIKSN.Analytics.Telemetry;
+
+public class PowerShellEventTelemeter : IEventTelemeter
 {
-    public class PowerShellEventTelemeter : IEventTelemeter
+    private readonly Cmdlet cmdlet;
+
+    public PowerShellEventTelemeter(Cmdlet cmdlet) => this.cmdlet = cmdlet;
+
+    public Task TrackEventAsync(string name)
     {
-        private readonly Cmdlet cmdlet;
+        this.cmdlet.WriteVerbose($"EVENT: {name}");
 
-        public PowerShellEventTelemeter(Cmdlet cmdlet) => this.cmdlet = cmdlet;
+        return Task.FromResult<object>(null);
+    }
 
-        public Task TrackEventAsync(string name)
-        {
-            this.cmdlet.WriteVerbose($"EVENT: {name}");
+    public Task TrackEventAsync(string name, IReadOnlyDictionary<string, string> properties)
+    {
+        this.cmdlet.WriteVerbose(
+            $"EVENT: {name} with {string.Join(", ", properties.Select(item => string.Format("{0} is {1}", item.Key, item.Value)))}");
 
-            return Task.FromResult<object>(null);
-        }
-
-        public Task TrackEventAsync(string name, IReadOnlyDictionary<string, string> properties)
-        {
-            this.cmdlet.WriteVerbose(
-                $"EVENT: {name} with {string.Join(", ", properties.Select(item => string.Format("{0} is {1}", item.Key, item.Value)))}");
-
-            return Task.FromResult<object>(null);
-        }
+        return Task.FromResult<object>(null);
     }
 }
