97d25c243262febe9ca16dc89ec01064f14cf615
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:37:49 +0100

    Update ICorrelationService.cs

--- TIKSN.Framework.Core/Integration/Correlation/ICorrelationService.cs
+++ TIKSN.Framework.Core/Integration/Correlation/ICorrelationService.cs
@@ -1,30 +1,29 @@
 using LanguageExt;
 
-namespace TIKSN.Integration.Correlation
+namespace TIKSN.Integration.Correlation;
+
+/// <summary>
+///     Service for generating and parsing <see cref="CorrelationId" />.
+/// </summary>
+public interface ICorrelationService
 {
     /// <summary>
-    ///     Service for generating and parsing <see cref="CorrelationId" />.
+    ///     Creates <see cref="CorrelationId" /> from string representation.
     /// </summary>
-    public interface ICorrelationService
-    {
-        /// <summary>
-        ///     Creates <see cref="CorrelationId" /> from string representation.
-        /// </summary>
-        /// <param name="stringRepresentation"></param>
-        /// <returns>Created <see cref="CorrelationId"/></returns>
-        CorrelationId Create(string stringRepresentation);
+    /// <param name="stringRepresentation"></param>
+    /// <returns>Created <see cref="CorrelationId"/></returns>
+    CorrelationId Create(string stringRepresentation);
 
-        /// <summary>
-        ///     Creates <see cref="CorrelationId" /> from binary representation.
-        /// </summary>
-        /// <param name="binaryRepresentation"></param>
-        /// <returns>Created <see cref="CorrelationId"/></returns>
-        CorrelationId Create(Seq<byte> binaryRepresentation);
+    /// <summary>
+    ///     Creates <see cref="CorrelationId" /> from binary representation.
+    /// </summary>
+    /// <param name="binaryRepresentation"></param>
+    /// <returns>Created <see cref="CorrelationId"/></returns>
+    CorrelationId Create(Seq<byte> binaryRepresentation);
 
-        /// <summary>
-        ///     Generates new <see cref="CorrelationId" />
-        /// </summary>
-        /// <returns>Generated <see cref="CorrelationId"/></returns>
-        CorrelationId Generate();
-    }
+    /// <summary>
+    ///     Generates new <see cref="CorrelationId" />
+    /// </summary>
+    /// <returns>Generated <see cref="CorrelationId"/></returns>
+    CorrelationId Generate();
 }
