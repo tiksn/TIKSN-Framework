9281f226fc12ff464396f548da0459b53c9d99e7
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:40:02 +0100

    Update SimpleXmlBondDeserializer.cs

--- TIKSN.Framework.Core/Serialization/Bond/SimpleXmlBondDeserializer.cs
+++ TIKSN.Framework.Core/Serialization/Bond/SimpleXmlBondDeserializer.cs
@@ -1,16 +1,14 @@
-using System.IO;
 using System.Xml;
 using Bond.Protocols;
 
-namespace TIKSN.Serialization.Bond
-{
-    public class SimpleXmlBondDeserializer : DeserializerBase<string>
-    {
-        protected override T DeserializeInternal<T>(string serial)
-        {
-            var reader = new SimpleXmlReader(XmlReader.Create(new StringReader(serial)));
+namespace TIKSN.Serialization.Bond;
 
-            return global::Bond.Deserialize<T>.From(reader);
-        }
+public class SimpleXmlBondDeserializer : DeserializerBase<string>
+{
+    protected override T DeserializeInternal<T>(string serial)
+    {
+        var reader = new SimpleXmlReader(XmlReader.Create(new StringReader(serial)));
+
+        return global::Bond.Deserialize<T>.From(reader);
     }
 }
