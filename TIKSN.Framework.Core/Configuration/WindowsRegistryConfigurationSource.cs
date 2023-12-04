f1f9720366f3bf6e39427df5f219d733405472ff
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:30:41 +0100

    Update WindowsRegistryConfigurationSource.cs

--- TIKSN.Framework.Core/Configuration/WindowsRegistryConfigurationSource.cs
+++ TIKSN.Framework.Core/Configuration/WindowsRegistryConfigurationSource.cs
@@ -1,14 +1,13 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Win32;
 
-namespace TIKSN.Configuration
-{
-    public class WindowsRegistryConfigurationSource : IConfigurationSource
-    {
-        public RegistryView RegistryView { get; set; }
-        public string RootKey { get; set; }
+namespace TIKSN.Configuration;
 
-        public IConfigurationProvider Build(IConfigurationBuilder builder) =>
-            new WindowsRegistryConfigurationProvider(this.RootKey, this.RegistryView);
-    }
+public class WindowsRegistryConfigurationSource : IConfigurationSource
+{
+    public RegistryView RegistryView { get; set; }
+    public string RootKey { get; set; }
+
+    public IConfigurationProvider Build(IConfigurationBuilder builder) =>
+        new WindowsRegistryConfigurationProvider(this.RootKey, this.RegistryView);
 }
