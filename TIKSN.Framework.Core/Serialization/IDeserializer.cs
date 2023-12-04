0e7b4942a8b76217f81a6c32a471735cea9cf274
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:40:19 +0100

    Update IDeserializer.cs

--- TIKSN.Framework.Core/Serialization/IDeserializer.cs
+++ TIKSN.Framework.Core/Serialization/IDeserializer.cs
@@ -1,17 +1,16 @@
-namespace TIKSN.Serialization
+namespace TIKSN.Serialization;
+
+/// <summary>
+///     Deserializer interface
+/// </summary>
+/// <typeparam name="TSerial">Type to deserialize from, usually string or byte array</typeparam>
+public interface IDeserializer<in TSerial> where TSerial : class
 {
     /// <summary>
-    ///     Deserializer interface
+    ///     Deserialize from <typeparamref name="TSerial" /> type
     /// </summary>
-    /// <typeparam name="TSerial">Type to deserialize from, usually string or byte array</typeparam>
-    public interface IDeserializer<in TSerial> where TSerial : class
-    {
-        /// <summary>
-        ///     Deserialize from <typeparamref name="TSerial" /> type
-        /// </summary>
-        /// <typeparam name="T">Type to be deserialized to</typeparam>
-        /// <param name="serial">Object to be deserialized</param>
-        /// <returns></returns>
-        T Deserialize<T>(TSerial serial);
-    }
+    /// <typeparam name="T">Type to be deserialized to</typeparam>
+    /// <param name="serial">Object to be deserialized</param>
+    /// <returns></returns>
+    T Deserialize<T>(TSerial serial);
 }
