e2a183c948cb96ffbd427e7ad0a5e5b9acaece49
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:40:35 +0100

    Update UnsignedBigIntegerBinaryDeserializer.cs

--- TIKSN.Framework.Core/Serialization/Numerics/UnsignedBigIntegerBinaryDeserializer.cs
+++ TIKSN.Framework.Core/Serialization/Numerics/UnsignedBigIntegerBinaryDeserializer.cs
@@ -1,28 +1,26 @@
-using System.Linq;
 using System.Numerics;
 
-namespace TIKSN.Serialization.Numerics
+namespace TIKSN.Serialization.Numerics;
+
+/// <summary>
+///     Custom (specialized or typed) deserializer for unsigned <see cref="BigInteger" />
+/// </summary>
+public class UnsignedBigIntegerBinaryDeserializer : ICustomDeserializer<byte[], BigInteger>
 {
     /// <summary>
-    ///     Custom (specialized or typed) deserializer for unsigned <see cref="BigInteger" />
+    ///     Deserializes byte array to unsigned <see cref="BigInteger" />
     /// </summary>
-    public class UnsignedBigIntegerBinaryDeserializer : ICustomDeserializer<byte[], BigInteger>
+    /// <param name="serial"></param>
+    /// <returns></returns>
+    public BigInteger Deserialize(byte[] serial)
     {
-        /// <summary>
-        ///     Deserializes byte array to unsigned <see cref="BigInteger" />
-        /// </summary>
-        /// <param name="serial"></param>
-        /// <returns></returns>
-        public BigInteger Deserialize(byte[] serial)
+        var last = serial[serial.Length - 1];
+
+        if (last < 0b_1000_0000)
         {
-            var last = serial[serial.Length - 1];
-
-            if (last < 0b_1000_0000)
-            {
-                return new BigInteger(serial);
-            }
-
-            return new BigInteger(serial.Concat(new byte[] { 0b_0000_0000 }).ToArray());
+            return new BigInteger(serial);
         }
+
+        return new BigInteger(serial.Concat(new byte[] { 0b_0000_0000 }).ToArray());
     }
 }
