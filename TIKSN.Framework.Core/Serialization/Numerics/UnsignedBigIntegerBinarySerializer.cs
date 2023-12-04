78e21493451260cfbfe089d64d20a259d66e3ebd
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:40:37 +0100

    Update UnsignedBigIntegerBinarySerializer.cs

--- TIKSN.Framework.Core/Serialization/Numerics/UnsignedBigIntegerBinarySerializer.cs
+++ TIKSN.Framework.Core/Serialization/Numerics/UnsignedBigIntegerBinarySerializer.cs
@@ -1,34 +1,31 @@
-using System;
-using System.Linq;
 using System.Numerics;
 
-namespace TIKSN.Serialization.Numerics
+namespace TIKSN.Serialization.Numerics;
+
+/// <summary>
+///     Custom (specialized or typed) serializer for unsigned <see cref="BigInteger" />
+/// </summary>
+public class UnsignedBigIntegerBinarySerializer : ICustomSerializer<byte[], BigInteger>
 {
     /// <summary>
-    ///     Custom (specialized or typed) serializer for unsigned <see cref="BigInteger" />
+    ///     Serializes unsigned <see cref="BigInteger" /> to byte array
     /// </summary>
-    public class UnsignedBigIntegerBinarySerializer : ICustomSerializer<byte[], BigInteger>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public byte[] Serialize(BigInteger obj)
     {
-        /// <summary>
-        ///     Serializes unsigned <see cref="BigInteger" /> to byte array
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public byte[] Serialize(BigInteger obj)
+        if (obj.Sign < 0)
         {
-            if (obj.Sign < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(obj), obj, "Big integer must be non-negative number.");
-            }
-
-            var underlyingBytes = obj.ToByteArray();
-            var last = underlyingBytes[underlyingBytes.Length - 1];
-            if (last == 0b_0000_0000 && underlyingBytes.Length > 1)
-            {
-                return underlyingBytes.SkipLast(1).ToArray();
-            }
-
-            return underlyingBytes;
+            throw new ArgumentOutOfRangeException(nameof(obj), obj, "Big integer must be non-negative number.");
         }
+
+        var underlyingBytes = obj.ToByteArray();
+        var last = underlyingBytes[underlyingBytes.Length - 1];
+        if (last == 0b_0000_0000 && underlyingBytes.Length > 1)
+        {
+            return underlyingBytes.SkipLast(1).ToArray();
+        }
+
+        return underlyingBytes;
     }
 }
