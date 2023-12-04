31979c38b22186f8a85b6f0d62f4538a24ec4372
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:33:32 +0100

    Update RavenUnitOfWorkFactoryBase.cs

--- TIKSN.Framework.Core/Data/RavenDB/RavenUnitOfWorkFactoryBase.cs
+++ TIKSN.Framework.Core/Data/RavenDB/RavenUnitOfWorkFactoryBase.cs
@@ -1,28 +1,26 @@
-using System;
 using Microsoft.Extensions.Options;
 using Raven.Client.Documents;
 
-namespace TIKSN.Data.RavenDB
+namespace TIKSN.Data.RavenDB;
+
+public abstract class RavenUnitOfWorkFactoryBase<TUnitOfWork> : IRavenUnitOfWorkFactory<TUnitOfWork>, IDisposable
+    where TUnitOfWork : IUnitOfWork
 {
-    public abstract class RavenUnitOfWorkFactoryBase<TUnitOfWork> : IRavenUnitOfWorkFactory<TUnitOfWork>, IDisposable
-        where TUnitOfWork : IUnitOfWork
+    private readonly IDocumentStore _store;
+
+    protected RavenUnitOfWorkFactoryBase(IOptions<RavenUnitOfWorkFactoryOptions<TUnitOfWork>> options)
     {
-        private readonly IDocumentStore _store;
-
-        protected RavenUnitOfWorkFactoryBase(IOptions<RavenUnitOfWorkFactoryOptions<TUnitOfWork>> options)
+        if (options == null)
         {
-            if (options == null)
-            {
-                throw new ArgumentNullException(nameof(options));
-            }
-
-            this._store = new DocumentStore { Urls = options.Value.Urls, Database = options.Value.Database }.Initialize();
+            throw new ArgumentNullException(nameof(options));
         }
 
-        public void Dispose() => this._store.Dispose();
-
-        public TUnitOfWork Create() => this.Create(this._store);
-
-        protected abstract TUnitOfWork Create(IDocumentStore store);
+        this._store = new DocumentStore { Urls = options.Value.Urls, Database = options.Value.Database }.Initialize();
     }
+
+    public void Dispose() => this._store.Dispose();
+
+    public TUnitOfWork Create() => this.Create(this._store);
+
+    protected abstract TUnitOfWork Create(IDocumentStore store);
 }
