65ba43d9c6783d2b178c1d575203821a7bf0bfd2
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:43:14 +0100

    Update DisposableProgress.cs

--- TIKSN.Framework.Core/Progress/DisposableProgress.cs
+++ TIKSN.Framework.Core/Progress/DisposableProgress.cs
@@ -1,11 +1,8 @@
-using System;
+namespace TIKSN.Progress;
 
-namespace TIKSN.Progress
+public class DisposableProgress<T> : Progress<T>, IDisposable
 {
-    public class DisposableProgress<T> : Progress<T>, IDisposable
+    public virtual void Dispose()
     {
-        public virtual void Dispose()
-        {
-        }
     }
 }
