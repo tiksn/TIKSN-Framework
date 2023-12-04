3cba6dce368abb596836ce78f3aedecd9121a98e
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:29:38 +0100

    Update CommandLineConfigurationExtensions.cs

--- TIKSN.Framework.Core/Configuration/CommandLineConfigurationExtensions.cs
+++ TIKSN.Framework.Core/Configuration/CommandLineConfigurationExtensions.cs
@@ -1,57 +1,55 @@
-using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using TIKSN.Analytics.Logging.NLog;
 
-namespace TIKSN.Configuration
+namespace TIKSN.Configuration;
+
+public static class CommandLineConfigurationExtensions
 {
-    public static class CommandLineConfigurationExtensions
+    public static IConfigurationBuilder AddFrameworkCommandLine(
+        this IConfigurationBuilder configurationBuilder,
+        string[] args,
+        IDictionary<string, string> switchMappings)
     {
-        public static IConfigurationBuilder AddFrameworkCommandLine(
-            this IConfigurationBuilder configurationBuilder,
-            string[] args,
-            IDictionary<string, string> switchMappings)
-        {
-            var frameworkSwitchMappings = MergeFrameworkSwitchMappings(switchMappings);
-            return configurationBuilder.AddCommandLine(args, frameworkSwitchMappings);
-        }
-
-        private static IDictionary<string, string> GetFrameworkSwitchMappings() => new Dictionary<string, string>
-(StringComparer.OrdinalIgnoreCase)
-        {
-                {
-                    "--nlog-viewer-address",
-                    ConfigurationPath.Combine(RemoteNLogViewerOptions.RemoteNLogViewerConfigurationSection,
-                    nameof(RemoteNLogViewerOptions.Address))
-                },
-                {
-                    "--nlog-viewer-include-call-site",
-                    ConfigurationPath.Combine(RemoteNLogViewerOptions.RemoteNLogViewerConfigurationSection,
-                    nameof(RemoteNLogViewerOptions.IncludeCallSite))
-                },
-                {
-                    "--nlog-viewer-include-source-info",
-                    ConfigurationPath.Combine(RemoteNLogViewerOptions.RemoteNLogViewerConfigurationSection,
-                    nameof(RemoteNLogViewerOptions.IncludeSourceInfo))
-                },
-            };
-
-        private static IDictionary<string, string> MergeFrameworkSwitchMappings(IDictionary<string, string> switchMappings)
-        {
-            var mappings = GetFrameworkSwitchMappings();
-
-            if (switchMappings is not null)
-            {
-                foreach (var item in switchMappings)
-                {
-                    mappings[item.Key] = item.Value;
-                }
-            }
-
-            return mappings;
-        }
-
-        public static IConfigurationBuilder AddFrameworkCommandLine(
-            this IConfigurationBuilder configurationBuilder,
-            string[] args) => configurationBuilder.AddFrameworkCommandLine(args, switchMappings: null);
+        var frameworkSwitchMappings = MergeFrameworkSwitchMappings(switchMappings);
+        return configurationBuilder.AddCommandLine(args, frameworkSwitchMappings);
     }
+
+    private static IDictionary<string, string> GetFrameworkSwitchMappings() => new Dictionary<string, string>
+(StringComparer.OrdinalIgnoreCase)
+    {
+            {
+                "--nlog-viewer-address",
+                ConfigurationPath.Combine(RemoteNLogViewerOptions.RemoteNLogViewerConfigurationSection,
+                nameof(RemoteNLogViewerOptions.Address))
+            },
+            {
+                "--nlog-viewer-include-call-site",
+                ConfigurationPath.Combine(RemoteNLogViewerOptions.RemoteNLogViewerConfigurationSection,
+                nameof(RemoteNLogViewerOptions.IncludeCallSite))
+            },
+            {
+                "--nlog-viewer-include-source-info",
+                ConfigurationPath.Combine(RemoteNLogViewerOptions.RemoteNLogViewerConfigurationSection,
+                nameof(RemoteNLogViewerOptions.IncludeSourceInfo))
+            },
+        };
+
+    private static IDictionary<string, string> MergeFrameworkSwitchMappings(IDictionary<string, string> switchMappings)
+    {
+        var mappings = GetFrameworkSwitchMappings();
+
+        if (switchMappings is not null)
+        {
+            foreach (var item in switchMappings)
+            {
+                mappings[item.Key] = item.Value;
+            }
+        }
+
+        return mappings;
+    }
+
+    public static IConfigurationBuilder AddFrameworkCommandLine(
+        this IConfigurationBuilder configurationBuilder,
+        string[] args) => configurationBuilder.AddFrameworkCommandLine(args, switchMappings: null);
 }
