8e6774e01089ea4efd706740f7358e840a7f3281
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:33:21 +0100

    Update MongoUnitOfWork.cs

--- TIKSN.Framework.Core/Data/Mongo/MongoUnitOfWork.cs
+++ TIKSN.Framework.Core/Data/Mongo/MongoUnitOfWork.cs
@@ -1,38 +1,34 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Driver;
 
-namespace TIKSN.Data.Mongo
+namespace TIKSN.Data.Mongo;
+
+public class MongoUnitOfWork : UnitOfWorkBase
 {
-    public class MongoUnitOfWork : UnitOfWorkBase
+    protected readonly IClientSessionHandle _clientSessionHandle;
+    private readonly IServiceScope _serviceScope;
+
+    public MongoUnitOfWork(IClientSessionHandle clientSessionHandle, IServiceScope serviceScope)
     {
-        protected readonly IClientSessionHandle _clientSessionHandle;
-        private readonly IServiceScope _serviceScope;
-
-        public MongoUnitOfWork(IClientSessionHandle clientSessionHandle, IServiceScope serviceScope)
-        {
-            this._clientSessionHandle =
-                clientSessionHandle ?? throw new ArgumentNullException(nameof(clientSessionHandle));
-            this._serviceScope = serviceScope ?? throw new ArgumentNullException(nameof(serviceScope));
-        }
-
-        public override Task CompleteAsync(CancellationToken cancellationToken) =>
-            this._clientSessionHandle.CommitTransactionAsync(cancellationToken);
-
-        public override Task DiscardAsync(CancellationToken cancellationToken) =>
-            this._clientSessionHandle.AbortTransactionAsync(cancellationToken);
-
-        public override void Dispose()
-        {
-            this._serviceScope?.Dispose();
-
-            base.Dispose();
-        }
-
-        public override IServiceProvider Services => this._serviceScope.ServiceProvider;
-
-        protected override bool IsDirty() => this._clientSessionHandle.WrappedCoreSession.IsDirty;
+        this._clientSessionHandle =
+            clientSessionHandle ?? throw new ArgumentNullException(nameof(clientSessionHandle));
+        this._serviceScope = serviceScope ?? throw new ArgumentNullException(nameof(serviceScope));
     }
+
+    public override Task CompleteAsync(CancellationToken cancellationToken) =>
+        this._clientSessionHandle.CommitTransactionAsync(cancellationToken);
+
+    public override Task DiscardAsync(CancellationToken cancellationToken) =>
+        this._clientSessionHandle.AbortTransactionAsync(cancellationToken);
+
+    public override void Dispose()
+    {
+        this._serviceScope?.Dispose();
+
+        base.Dispose();
+    }
+
+    public override IServiceProvider Services => this._serviceScope.ServiceProvider;
+
+    protected override bool IsDirty() => this._clientSessionHandle.WrappedCoreSession.IsDirty;
 }
