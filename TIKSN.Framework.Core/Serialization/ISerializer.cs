a4ac108e209a69ccb8f3095641bb2ddfd7b3a142
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:40:21 +0100

    Update ISerializer.cs

--- TIKSN.Framework.Core/Serialization/ISerializer.cs
+++ TIKSN.Framework.Core/Serialization/ISerializer.cs
@@ -1,17 +1,16 @@
-namespace TIKSN.Serialization
+namespace TIKSN.Serialization;
+
+/// <summary>
+///     Serializer interface
+/// </summary>
+/// <typeparam name="TSerial">Type to serialize to, usually string or byte array</typeparam>
+public interface ISerializer<out TSerial> where TSerial : class
 {
     /// <summary>
-    ///     Serializer interface
+    ///     Serialize to <typeparamref name="TSerial" /> type
     /// </summary>
-    /// <typeparam name="TSerial">Type to serialize to, usually string or byte array</typeparam>
-    public interface ISerializer<out TSerial> where TSerial : class
-    {
-        /// <summary>
-        ///     Serialize to <typeparamref name="TSerial" /> type
-        /// </summary>
-        /// <typeparam name="T">Type to be serialized</typeparam>
-        /// <param name="obj">Object to be serialized</param>
-        /// <returns></returns>
-        TSerial Serialize<T>(T obj);
-    }
+    /// <typeparam name="T">Type to be serialized</typeparam>
+    /// <param name="obj">Object to be serialized</param>
+    /// <returns></returns>
+    TSerial Serialize<T>(T obj);
 }
