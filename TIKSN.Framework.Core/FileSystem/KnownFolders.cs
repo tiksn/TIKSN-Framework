0a7d9b5ccbaf0439d8a5476e8d88e73553670752
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:34:11 +0100

    Update KnownFolders.cs

--- TIKSN.Framework.Core/FileSystem/KnownFolders.cs
+++ TIKSN.Framework.Core/FileSystem/KnownFolders.cs
@@ -1,71 +1,68 @@
-using System;
 using System.Diagnostics;
-using System.IO;
 using System.Reflection;
 using Microsoft.Extensions.FileProviders;
 
-namespace TIKSN.FileSystem
+namespace TIKSN.FileSystem;
+
+public class KnownFolders : IKnownFolders
 {
-    public class KnownFolders : IKnownFolders
+    private readonly Assembly _mainAssembly;
+    private readonly KnownFolderVersionConsideration _versionConsideration;
+
+    public KnownFolders(KnownFoldersConfiguration knownFoldersConfiguration)
     {
-        private readonly Assembly _mainAssembly;
-        private readonly KnownFolderVersionConsideration _versionConsideration;
+        this._mainAssembly = knownFoldersConfiguration.MainAssembly;
+        this._versionConsideration = knownFoldersConfiguration.VersionConsideration;
 
-        public KnownFolders(KnownFoldersConfiguration knownFoldersConfiguration)
+        this.LocalAppData = this.GetFromSpecialFolder(Environment.SpecialFolder.LocalApplicationData);
+        this.RoamingAppData = this.GetFromSpecialFolder(Environment.SpecialFolder.ApplicationData);
+    }
+
+    public IFileProvider LocalAppData { get; }
+
+    public IFileProvider RoamingAppData { get; }
+
+    private static IFileProvider GetFromFolderPath(string folderPath)
+    {
+        _ = Directory.CreateDirectory(folderPath);
+
+        return new PhysicalFileProvider(folderPath);
+    }
+
+    private IFileProvider GetFromSpecialFolder(Environment.SpecialFolder specialFolder)
+    {
+        var folderPath = Environment.GetFolderPath(specialFolder);
+
+        var fileVersionInfo = FileVersionInfo.GetVersionInfo(this._mainAssembly.Location);
+
+        folderPath = Path.Combine(folderPath, fileVersionInfo.CompanyName);
+        folderPath = Path.Combine(folderPath, fileVersionInfo.ProductName);
+
+        switch (this._versionConsideration)
         {
-            this._mainAssembly = knownFoldersConfiguration.MainAssembly;
-            this._versionConsideration = knownFoldersConfiguration.VersionConsideration;
+            case KnownFolderVersionConsideration.None:
+                break;
 
-            this.LocalAppData = this.GetFromSpecialFolder(Environment.SpecialFolder.LocalApplicationData);
-            this.RoamingAppData = this.GetFromSpecialFolder(Environment.SpecialFolder.ApplicationData);
+            case KnownFolderVersionConsideration.Major:
+                folderPath = Path.Combine(folderPath, this._mainAssembly.GetName().Version.ToString(1));
+                break;
+
+            case KnownFolderVersionConsideration.MajorMinor:
+                folderPath = Path.Combine(folderPath, this._mainAssembly.GetName().Version.ToString(2));
+                break;
+
+            case KnownFolderVersionConsideration.MajorMinorBuild:
+                folderPath = Path.Combine(folderPath, this._mainAssembly.GetName().Version.ToString(3));
+                break;
+
+            case KnownFolderVersionConsideration.MajorMinorBuildRevision:
+                folderPath = Path.Combine(folderPath, this._mainAssembly.GetName().Version.ToString(4));
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException(nameof(this._versionConsideration));
         }
 
-        public IFileProvider LocalAppData { get; }
-
-        public IFileProvider RoamingAppData { get; }
-
-        private static IFileProvider GetFromFolderPath(string folderPath)
-        {
-            _ = Directory.CreateDirectory(folderPath);
-
-            return new PhysicalFileProvider(folderPath);
-        }
-
-        private IFileProvider GetFromSpecialFolder(Environment.SpecialFolder specialFolder)
-        {
-            var folderPath = Environment.GetFolderPath(specialFolder);
-
-            var fileVersionInfo = FileVersionInfo.GetVersionInfo(this._mainAssembly.Location);
-
-            folderPath = Path.Combine(folderPath, fileVersionInfo.CompanyName);
-            folderPath = Path.Combine(folderPath, fileVersionInfo.ProductName);
-
-            switch (this._versionConsideration)
-            {
-                case KnownFolderVersionConsideration.None:
-                    break;
-
-                case KnownFolderVersionConsideration.Major:
-                    folderPath = Path.Combine(folderPath, this._mainAssembly.GetName().Version.ToString(1));
-                    break;
-
-                case KnownFolderVersionConsideration.MajorMinor:
-                    folderPath = Path.Combine(folderPath, this._mainAssembly.GetName().Version.ToString(2));
-                    break;
-
-                case KnownFolderVersionConsideration.MajorMinorBuild:
-                    folderPath = Path.Combine(folderPath, this._mainAssembly.GetName().Version.ToString(3));
-                    break;
-
-                case KnownFolderVersionConsideration.MajorMinorBuildRevision:
-                    folderPath = Path.Combine(folderPath, this._mainAssembly.GetName().Version.ToString(4));
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(this._versionConsideration));
-            }
-
-            return GetFromFolderPath(folderPath);
-        }
+        return GetFromFolderPath(folderPath);
     }
 }
