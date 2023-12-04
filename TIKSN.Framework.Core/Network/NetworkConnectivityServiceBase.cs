aaff6375adebeb6769ab23b5ab013da4bd9e6966
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:38:32 +0100

    Update NetworkConnectivityServiceBase.cs

--- TIKSN.Framework.Core/Network/NetworkConnectivityServiceBase.cs
+++ TIKSN.Framework.Core/Network/NetworkConnectivityServiceBase.cs
@@ -1,37 +1,35 @@
-using System;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 
-namespace TIKSN.Network
+namespace TIKSN.Network;
+
+public abstract class NetworkConnectivityServiceBase : INetworkConnectivityService, IDisposable
 {
-    public abstract class NetworkConnectivityServiceBase : INetworkConnectivityService, IDisposable
+    private readonly Subject<InternetConnectivityState> manualChecks;
+    protected IObservable<InternetConnectivityState> internetConnectivityStateInternal;
+
+    protected NetworkConnectivityServiceBase() => this.manualChecks = new Subject<InternetConnectivityState>();
+
+    public IObservable<InternetConnectivityState> InternetConnectivityChanged =>
+        this.internetConnectivityStateInternal
+            .Merge(this.manualChecks)
+            .DistinctUntilChanged();
+
+    public InternetConnectivityState GetInternetConnectivityState() => this.GetInternetConnectivityState(true);
+
+    protected abstract InternetConnectivityState GetInternetConnectivityStateInternal();
+
+    private InternetConnectivityState GetInternetConnectivityState(bool broadcast)
     {
-        private readonly Subject<InternetConnectivityState> manualChecks;
-        protected IObservable<InternetConnectivityState> internetConnectivityStateInternal;
+        var result = this.GetInternetConnectivityStateInternal();
 
-        protected NetworkConnectivityServiceBase() => this.manualChecks = new Subject<InternetConnectivityState>();
-
-        public IObservable<InternetConnectivityState> InternetConnectivityChanged =>
-            this.internetConnectivityStateInternal
-                .Merge(this.manualChecks)
-                .DistinctUntilChanged();
-
-        public InternetConnectivityState GetInternetConnectivityState() => this.GetInternetConnectivityState(true);
-
-        protected abstract InternetConnectivityState GetInternetConnectivityStateInternal();
-
-        private InternetConnectivityState GetInternetConnectivityState(bool broadcast)
+        if (broadcast)
         {
-            var result = this.GetInternetConnectivityStateInternal();
-
-            if (broadcast)
-            {
-                this.manualChecks.OnNext(result);
-            }
-
-            return result;
+            this.manualChecks.OnNext(result);
         }
 
-        public void Dispose() => throw new NotImplementedException();
+        return result;
     }
+
+    public void Dispose() => throw new NotImplementedException();
 }
