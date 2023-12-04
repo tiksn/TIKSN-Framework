304aec8490a3d01a796f1f44c230c658501288d3
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:31:14 +0100

    Update CosmosTableExtensions.cs

--- TIKSN.Framework.Core/Data/CosmosTable/CosmosTableExtensions.cs
+++ TIKSN.Framework.Core/Data/CosmosTable/CosmosTableExtensions.cs
@@ -1,44 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos.Table;
 
-namespace TIKSN.Data.CosmosTable
+namespace TIKSN.Data.CosmosTable;
+
+public static class CosmosTableExtensions
 {
-    public static class CosmosTableExtensions
+    public static Task<IEnumerable<T>> RetrieveAllAsync<T>(this ICosmosTableQueryRepository<T> repository,
+        CancellationToken cancellationToken)
+        where T : ITableEntity
     {
-        public static Task<IEnumerable<T>> RetrieveAllAsync<T>(this ICosmosTableQueryRepository<T> repository,
-            CancellationToken cancellationToken)
-            where T : ITableEntity
+        if (repository == null)
         {
-            if (repository == null)
-            {
-                throw new ArgumentNullException(nameof(repository));
-            }
-
-            var filters = new Dictionary<string, object>();
-
-            return repository.SearchAsync(filters, cancellationToken);
+            throw new ArgumentNullException(nameof(repository));
         }
 
-        public static Task<IEnumerable<T>> SearchAsync<T>(this ICosmosTableQueryRepository<T> repository,
-            string fieldName,
-            object givenValue,
-            CancellationToken cancellationToken)
-            where T : ITableEntity
+        var filters = new Dictionary<string, object>();
+
+        return repository.SearchAsync(filters, cancellationToken);
+    }
+
+    public static Task<IEnumerable<T>> SearchAsync<T>(this ICosmosTableQueryRepository<T> repository,
+        string fieldName,
+        object givenValue,
+        CancellationToken cancellationToken)
+        where T : ITableEntity
+    {
+        if (repository == null)
         {
-            if (repository == null)
-            {
-                throw new ArgumentNullException(nameof(repository));
-            }
-
-            var filters = new Dictionary<string, object>
-            {
-                { fieldName, givenValue }
-            };
-
-            return repository.SearchAsync(filters, cancellationToken);
+            throw new ArgumentNullException(nameof(repository));
         }
+
+        var filters = new Dictionary<string, object>
+        {
+            { fieldName, givenValue }
+        };
+
+        return repository.SearchAsync(filters, cancellationToken);
     }
 }
