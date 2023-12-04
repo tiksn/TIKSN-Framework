b5cf1bed9d22bdc159bc95097d1ae2865c8edfdf
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:29:03 +0100

    Update PowerShellExceptionTelemeter.cs

--- TIKSN.Framework.Core/Analytics/Telemetry/PowerShellExceptionTelemeter.cs
+++ TIKSN.Framework.Core/Analytics/Telemetry/PowerShellExceptionTelemeter.cs
@@ -1,27 +1,24 @@
-using System;
 using System.Management.Automation;
-using System.Threading.Tasks;
 
-namespace TIKSN.Analytics.Telemetry
+namespace TIKSN.Analytics.Telemetry;
+
+public class PowerShellExceptionTelemeter : IExceptionTelemeter
 {
-    public class PowerShellExceptionTelemeter : IExceptionTelemeter
+    private readonly Cmdlet cmdlet;
+
+    public PowerShellExceptionTelemeter(Cmdlet cmdlet) => this.cmdlet = cmdlet;
+
+    public Task TrackExceptionAsync(Exception exception, TelemetrySeverityLevel severityLevel)
     {
-        private readonly Cmdlet cmdlet;
+        this.cmdlet.WriteError(new ErrorRecord(exception, errorId: null, ErrorCategory.InvalidOperation, targetObject: null));
 
-        public PowerShellExceptionTelemeter(Cmdlet cmdlet) => this.cmdlet = cmdlet;
+        return Task.FromResult<object>(null);
+    }
 
-        public Task TrackExceptionAsync(Exception exception, TelemetrySeverityLevel severityLevel)
-        {
-            this.cmdlet.WriteError(new ErrorRecord(exception, errorId: null, ErrorCategory.InvalidOperation, targetObject: null));
+    public Task TrackExceptionAsync(Exception exception)
+    {
+        this.cmdlet.WriteError(new ErrorRecord(exception, errorId: null, ErrorCategory.InvalidOperation, targetObject: null));
 
-            return Task.FromResult<object>(null);
-        }
-
-        public Task TrackExceptionAsync(Exception exception)
-        {
-            this.cmdlet.WriteError(new ErrorRecord(exception, errorId: null, ErrorCategory.InvalidOperation, targetObject: null));
-
-            return Task.FromResult<object>(null);
-        }
+        return Task.FromResult<object>(null);
     }
 }
