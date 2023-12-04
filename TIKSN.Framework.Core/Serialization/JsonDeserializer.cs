ef06b927f198a0c95fc20dc3b4a3f3090122709a
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:40:26 +0100

    Update JsonDeserializer.cs

--- TIKSN.Framework.Core/Serialization/JsonDeserializer.cs
+++ TIKSN.Framework.Core/Serialization/JsonDeserializer.cs
@@ -1,9 +1,8 @@
 using Newtonsoft.Json;
 
-namespace TIKSN.Serialization
+namespace TIKSN.Serialization;
+
+public class JsonDeserializer : DeserializerBase<string>
 {
-    public class JsonDeserializer : DeserializerBase<string>
-    {
-        protected override T DeserializeInternal<T>(string serial) => JsonConvert.DeserializeObject<T>(serial);
-    }
+    protected override T DeserializeInternal<T>(string serial) => JsonConvert.DeserializeObject<T>(serial);
 }
