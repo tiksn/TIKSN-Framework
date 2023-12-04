c2cd3a34a820e657873114bf558bb22ea9aa608a
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:42:25 +0100

    Update IRestRepository.cs

--- TIKSN.Framework.Core/Web/Rest/IRestRepository.cs
+++ TIKSN.Framework.Core/Web/Rest/IRestRepository.cs
@@ -1,19 +1,15 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using TIKSN.Data;
 
-namespace TIKSN.Web.Rest
+namespace TIKSN.Web.Rest;
+
+public interface IRestRepository<TEntity, TIdentity> where TEntity : IEntity<TIdentity>
+    where TIdentity : IEquatable<TIdentity>
 {
-    public interface IRestRepository<TEntity, TIdentity> where TEntity : IEntity<TIdentity>
-        where TIdentity : IEquatable<TIdentity>
-    {
-        Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken);
 
-        Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken);
+    Task<TEntity> GetAsync(TIdentity id, CancellationToken cancellationToken);
 
-        Task RemoveAsync(TEntity entity, CancellationToken cancellationToken);
+    Task RemoveAsync(TEntity entity, CancellationToken cancellationToken);
 
-        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
-    }
+    Task UpdateAsync(TEntity entity, CancellationToken cancellationToken);
 }
