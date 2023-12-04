47fb4dd973a5ccc7bc95345fa922be186bd86a21
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:41:15 +0100

    Update ShellAttributeBase.cs

--- TIKSN.Framework.Core/Shell/ShellAttributeBase.cs
+++ TIKSN.Framework.Core/Shell/ShellAttributeBase.cs
@@ -1,31 +1,29 @@
-using System;
 using Microsoft.Extensions.Localization;
 using TIKSN.Localization;
 
-namespace TIKSN.Shell
+namespace TIKSN.Shell;
+
+public abstract class ShellAttributeBase : Attribute
 {
-    public abstract class ShellAttributeBase : Attribute
+    private readonly int? _integerNameKey;
+    private readonly string _stringNameKey;
+
+    protected ShellAttributeBase(int nameKey) => this._integerNameKey = nameKey;
+
+    protected ShellAttributeBase(string nameKey) => this._stringNameKey = nameKey;
+
+    public string GetName(IStringLocalizer stringLocalizer)
     {
-        private readonly int? _integerNameKey;
-        private readonly string _stringNameKey;
-
-        protected ShellAttributeBase(int nameKey) => this._integerNameKey = nameKey;
-
-        protected ShellAttributeBase(string nameKey) => this._stringNameKey = nameKey;
-
-        public string GetName(IStringLocalizer stringLocalizer)
+        if (this._integerNameKey.HasValue)
         {
-            if (this._integerNameKey.HasValue)
-            {
-                return stringLocalizer.GetRequiredString(this._integerNameKey.Value);
-            }
-
-            if (Guid.TryParse(this._stringNameKey, out var key))
-            {
-                return stringLocalizer.GetRequiredString(key);
-            }
-
-            return stringLocalizer.GetRequiredString(this._stringNameKey);
+            return stringLocalizer.GetRequiredString(this._integerNameKey.Value);
         }
+
+        if (Guid.TryParse(this._stringNameKey, out var key))
+        {
+            return stringLocalizer.GetRequiredString(key);
+        }
+
+        return stringLocalizer.GetRequiredString(this._stringNameKey);
     }
 }
