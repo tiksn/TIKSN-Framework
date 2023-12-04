d017e4a7b4994f990589637a4b3704bb8efac8dc
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:35:19 +0100

    Update ForeignExchangeDataRepository.cs

--- TIKSN.Framework.Core/Finance/ForeignExchange/Data/LiteDB/ForeignExchangeDataRepository.cs
+++ TIKSN.Framework.Core/Finance/ForeignExchange/Data/LiteDB/ForeignExchangeDataRepository.cs
@@ -1,14 +1,12 @@
-using System;
 using LiteDB;
 using TIKSN.Data.LiteDB;
 
-namespace TIKSN.Finance.ForeignExchange.Data.LiteDB
+namespace TIKSN.Finance.ForeignExchange.Data.LiteDB;
+
+public class ForeignExchangeDataRepository : LiteDbRepository<ForeignExchangeDataEntity, Guid>, IForeignExchangeDataRepository
 {
-    public class ForeignExchangeDataRepository : LiteDbRepository<ForeignExchangeDataEntity, Guid>, IForeignExchangeDataRepository
+    public ForeignExchangeDataRepository(ILiteDbDatabaseProvider databaseProvider) : base(databaseProvider,
+        "ForeignExchanges", x => new BsonValue(x))
     {
-        public ForeignExchangeDataRepository(ILiteDbDatabaseProvider databaseProvider) : base(databaseProvider,
-            "ForeignExchanges", x => new BsonValue(x))
-        {
-        }
     }
 }
