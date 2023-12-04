631b45a4aa91cc58adc8f17012261346aed4dd62
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:36:32 +0100

    Update ExchangeRatesContext.cs

--- TIKSN.Framework.Core/Finance/ForeignExchange/EntityFrameworkCore/ExchangeRatesContext.cs
+++ TIKSN.Framework.Core/Finance/ForeignExchange/EntityFrameworkCore/ExchangeRatesContext.cs
@@ -1,56 +1,55 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore
+namespace TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore;
+
+public class ExchangeRatesContext : DbContext
 {
-    public class ExchangeRatesContext : DbContext
+    public ExchangeRatesContext(DbContextOptions<ExchangeRatesContext> dbContextOptions) : base(dbContextOptions)
     {
-        public ExchangeRatesContext(DbContextOptions<ExchangeRatesContext> dbContextOptions) : base(dbContextOptions)
-        {
-        }
+    }
 
-        public virtual DbSet<ExchangeRateDataEntity> ExchangeRates { get; set; }
-        public virtual DbSet<ForeignExchangeDataEntity> ForeignExchanges { get; set; }
+    public virtual DbSet<ExchangeRateDataEntity> ExchangeRates { get; set; }
+    public virtual DbSet<ForeignExchangeDataEntity> ForeignExchanges { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            _ = modelBuilder.Entity<ExchangeRateDataEntity>(entity =>
-              {
-                  _ = entity.ToTable("ExchangeRates");
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        _ = modelBuilder.Entity<ExchangeRateDataEntity>(entity =>
+          {
+              _ = entity.ToTable("ExchangeRates");
 
-                  _ = entity.Property(e => e.ID).HasColumnName("ID");
+              _ = entity.Property(e => e.ID).HasColumnName("ID");
 
-                  _ = entity.Property(e => e.AsOn)
-                      .IsRequired()
-                      .HasColumnType("DATETIME");
+              _ = entity.Property(e => e.AsOn)
+                  .IsRequired()
+                  .HasColumnType("DATETIME");
 
-                  _ = entity.Property(e => e.BaseCurrencyCode)
-                      .IsRequired()
-                      .HasColumnType("STRING (3, 3)");
+              _ = entity.Property(e => e.BaseCurrencyCode)
+                  .IsRequired()
+                  .HasColumnType("STRING (3, 3)");
 
-                  _ = entity.Property(e => e.CounterCurrencyCode)
-                      .IsRequired()
-                      .HasColumnType("STRING (3, 3)");
+              _ = entity.Property(e => e.CounterCurrencyCode)
+                  .IsRequired()
+                  .HasColumnType("STRING (3, 3)");
 
-                  _ = entity.Property(e => e.ForeignExchangeID).HasColumnName("ForeignExchangeID");
+              _ = entity.Property(e => e.ForeignExchangeID).HasColumnName("ForeignExchangeID");
 
-                  _ = entity.Property(e => e.Rate)
-                      .IsRequired()
-                      .HasColumnType("DECIMAL");
+              _ = entity.Property(e => e.Rate)
+                  .IsRequired()
+                  .HasColumnType("DECIMAL");
 
-                  _ = entity.HasOne(d => d.ForeignExchange)
-                      .WithMany(p => p.ExchangeRates)
-                      .HasForeignKey(d => d.ForeignExchangeID);
-              });
+              _ = entity.HasOne(d => d.ForeignExchange)
+                  .WithMany(p => p.ExchangeRates)
+                  .HasForeignKey(d => d.ForeignExchangeID);
+          });
 
-            _ = modelBuilder.Entity<ForeignExchangeDataEntity>(entity =>
-              {
-                  _ = entity.ToTable("ForeignExchanges");
-                  _ = entity.Property(e => e.ID).HasColumnName("ID");
+        _ = modelBuilder.Entity<ForeignExchangeDataEntity>(entity =>
+          {
+              _ = entity.ToTable("ForeignExchanges");
+              _ = entity.Property(e => e.ID).HasColumnName("ID");
 
-                  _ = entity.Property(e => e.CountryCode)
-                      .IsRequired()
-                      .HasColumnType("STRING (2, 3)");
-              });
-        }
+              _ = entity.Property(e => e.CountryCode)
+                  .IsRequired()
+                  .HasColumnType("STRING (2, 3)");
+          });
     }
 }
