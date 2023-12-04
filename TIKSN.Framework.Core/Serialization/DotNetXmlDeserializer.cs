cc158d034a5b8284ecbf0251d4e6235d22abc185
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:40:10 +0100

    Update DotNetXmlDeserializer.cs

--- TIKSN.Framework.Core/Serialization/DotNetXmlDeserializer.cs
+++ TIKSN.Framework.Core/Serialization/DotNetXmlDeserializer.cs
@@ -1,21 +1,19 @@
-using System.IO;
 using System.Text;
 using System.Xml.Serialization;
 
-namespace TIKSN.Serialization
-{
-    public class DotNetXmlDeserializer : DeserializerBase<string>
-    {
-        protected override T DeserializeInternal<T>(string serial)
-        {
-            if (string.IsNullOrEmpty(serial))
-            {
-                return default;
-            }
+namespace TIKSN.Serialization;
 
-            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(serial));
-            var serializer = new XmlSerializer(typeof(T));
-            return (T)serializer.Deserialize(stream);
+public class DotNetXmlDeserializer : DeserializerBase<string>
+{
+    protected override T DeserializeInternal<T>(string serial)
+    {
+        if (string.IsNullOrEmpty(serial))
+        {
+            return default;
         }
+
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(serial));
+        var serializer = new XmlSerializer(typeof(T));
+        return (T)serializer.Deserialize(stream);
     }
 }
