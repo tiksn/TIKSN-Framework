6b471640a97da1d1acc77671248376325715845f
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:24:33 +0100

    Update ApplicationInsightsExceptionTelemeter.cs

--- TIKSN.Framework.Core/Analytics/Telemetry/ApplicationInsightsExceptionTelemeter.cs
+++ TIKSN.Framework.Core/Analytics/Telemetry/ApplicationInsightsExceptionTelemeter.cs
@@ -1,34 +1,33 @@
 using System.Diagnostics;
 using Microsoft.ApplicationInsights.DataContracts;
 
-namespace TIKSN.Analytics.Telemetry
+namespace TIKSN.Analytics.Telemetry;
+
+public class ApplicationInsightsExceptionTelemeter : IExceptionTelemeter
 {
-    public class ApplicationInsightsExceptionTelemeter : IExceptionTelemeter
+    [Obsolete]
+    public async Task TrackExceptionAsync(Exception exception) => await TrackExceptionInternalAsync(exception, severityLevel: null).ConfigureAwait(false);
+
+    [Obsolete]
+    public Task TrackExceptionAsync(Exception exception, TelemetrySeverityLevel severityLevel) =>
+        TrackExceptionInternalAsync(exception, severityLevel);
+
+    [Obsolete]
+    private static Task TrackExceptionInternalAsync(Exception exception, TelemetrySeverityLevel? severityLevel)
     {
-        [Obsolete]
-        public async Task TrackExceptionAsync(Exception exception) => await TrackExceptionInternalAsync(exception, severityLevel: null).ConfigureAwait(false);
-
-        [Obsolete]
-        public Task TrackExceptionAsync(Exception exception, TelemetrySeverityLevel severityLevel) =>
-            TrackExceptionInternalAsync(exception, severityLevel);
-
-        [Obsolete]
-        private static Task TrackExceptionInternalAsync(Exception exception, TelemetrySeverityLevel? severityLevel)
+        try
         {
-            try
+            var telemetry = new ExceptionTelemetry(exception)
             {
-                var telemetry = new ExceptionTelemetry(exception)
-                {
-                    SeverityLevel = ApplicationInsightsHelper.ConvertSeverityLevel(severityLevel),
-                };
-                ApplicationInsightsHelper.TrackException(telemetry);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-
-            return Task.CompletedTask;
+                SeverityLevel = ApplicationInsightsHelper.ConvertSeverityLevel(severityLevel),
+            };
+            ApplicationInsightsHelper.TrackException(telemetry);
         }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+
+        return Task.CompletedTask;
     }
 }
