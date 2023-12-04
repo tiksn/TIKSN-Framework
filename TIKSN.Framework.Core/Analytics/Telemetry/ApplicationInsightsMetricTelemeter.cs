0337868cddaa740baa93929245d5bf608bee9459
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:24:41 +0100

    Update ApplicationInsightsMetricTelemeter.cs

--- TIKSN.Framework.Core/Analytics/Telemetry/ApplicationInsightsMetricTelemeter.cs
+++ TIKSN.Framework.Core/Analytics/Telemetry/ApplicationInsightsMetricTelemeter.cs
@@ -1,26 +1,23 @@
-using System;
 using System.Diagnostics;
-using System.Threading.Tasks;
 using Microsoft.ApplicationInsights.DataContracts;
 
-namespace TIKSN.Analytics.Telemetry
-{
-    public class ApplicationInsightsMetricTelemeter : IMetricTelemeter
-    {
-        [Obsolete]
-        public Task TrackMetricAsync(string metricName, decimal metricValue)
-        {
-            try
-            {
-                var telemetry = new MetricTelemetry(metricName, (double)metricValue);
-                ApplicationInsightsHelper.TrackMetric(telemetry);
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
+namespace TIKSN.Analytics.Telemetry;
 
-            return Task.FromResult<object>(null);
+public class ApplicationInsightsMetricTelemeter : IMetricTelemeter
+{
+    [Obsolete]
+    public Task TrackMetricAsync(string metricName, decimal metricValue)
+    {
+        try
+        {
+            var telemetry = new MetricTelemetry(metricName, (double)metricValue);
+            ApplicationInsightsHelper.TrackMetric(telemetry);
         }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+
+        return Task.FromResult<object>(null);
     }
 }
