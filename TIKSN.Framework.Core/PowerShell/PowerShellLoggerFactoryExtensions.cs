34148efdd59a40566a0ffe916c539d1ef3fb2d3e
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:39:10 +0100

    Update PowerShellLoggerFactoryExtensions.cs

--- TIKSN.Framework.Core/PowerShell/PowerShellLoggerFactoryExtensions.cs
+++ TIKSN.Framework.Core/PowerShell/PowerShellLoggerFactoryExtensions.cs
@@ -1,19 +1,17 @@
-using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace TIKSN.PowerShell
-{
-    public static class PowerShellLoggerFactoryExtensions
-    {
-        public static ILoggerFactory AddPowerShell(this ILoggerFactory factory, IServiceProvider serviceProvider)
-        {
-            factory.AddProvider(new PowerShellLoggerProvider(
-                serviceProvider.GetRequiredService<ICurrentCommandProvider>(),
-                serviceProvider.GetRequiredService<IOptions<PowerShellLoggerOptions>>()));
+namespace TIKSN.PowerShell;
 
-            return factory;
-        }
+public static class PowerShellLoggerFactoryExtensions
+{
+    public static ILoggerFactory AddPowerShell(this ILoggerFactory factory, IServiceProvider serviceProvider)
+    {
+        factory.AddProvider(new PowerShellLoggerProvider(
+            serviceProvider.GetRequiredService<ICurrentCommandProvider>(),
+            serviceProvider.GetRequiredService<IOptions<PowerShellLoggerOptions>>()));
+
+        return factory;
     }
 }
