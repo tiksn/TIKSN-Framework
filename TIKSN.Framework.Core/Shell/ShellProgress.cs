ea4574e598d205b3d31906abeb3c5afdd3adbfaf
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:41:36 +0100

    Update ShellProgress.cs

--- TIKSN.Framework.Core/Shell/ShellProgress.cs
+++ TIKSN.Framework.Core/Shell/ShellProgress.cs
@@ -1,36 +1,35 @@
 using ShellProgressBar;
 using TIKSN.Progress;
 
-namespace TIKSN.Shell
+namespace TIKSN.Shell;
+
+public class ShellProgress : DisposableProgress<OperationProgressReport>
 {
-    public class ShellProgress : DisposableProgress<OperationProgressReport>
+    private readonly int _accuracy;
+    private readonly ProgressBar _progressBar;
+
+    public ShellProgress(string message, int accuracy)
     {
-        private readonly int _accuracy;
-        private readonly ProgressBar _progressBar;
-
-        public ShellProgress(string message, int accuracy)
-        {
-            this._accuracy = accuracy;
-            var options = new ProgressBarOptions();
-            this._progressBar = new ProgressBar(this.EstimateTicks(100d), message, options);
-        }
-
-        private int EstimateTicks(double percentage) => (int)(percentage * this._accuracy);
-
-        protected override void OnReport(OperationProgressReport value)
-        {
-            base.OnReport(value);
-
-            this._progressBar.Message = $"{value.StatusDescription} {value.CurrentOperation}";
-
-            var updatedTicks = this.EstimateTicks(value.PercentComplete);
-
-            while (this._progressBar.CurrentTick < updatedTicks)
-            {
-                this._progressBar.Tick();
-            }
-        }
-
-        public override void Dispose() => this._progressBar.Dispose();
+        this._accuracy = accuracy;
+        var options = new ProgressBarOptions();
+        this._progressBar = new ProgressBar(this.EstimateTicks(100d), message, options);
     }
+
+    private int EstimateTicks(double percentage) => (int)(percentage * this._accuracy);
+
+    protected override void OnReport(OperationProgressReport value)
+    {
+        base.OnReport(value);
+
+        this._progressBar.Message = $"{value.StatusDescription} {value.CurrentOperation}";
+
+        var updatedTicks = this.EstimateTicks(value.PercentComplete);
+
+        while (this._progressBar.CurrentTick < updatedTicks)
+        {
+            this._progressBar.Tick();
+        }
+    }
+
+    public override void Dispose() => this._progressBar.Dispose();
 }
