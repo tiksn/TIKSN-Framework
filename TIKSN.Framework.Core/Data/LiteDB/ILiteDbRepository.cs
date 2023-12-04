5273fbfbd5e212a597fb5254b2f17bbf27b54240
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:32:42 +0100

    Update ILiteDbRepository.cs

--- TIKSN.Framework.Core/Data/LiteDB/ILiteDbRepository.cs
+++ TIKSN.Framework.Core/Data/LiteDB/ILiteDbRepository.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+namespace TIKSN.Data.LiteDB;
 
-namespace TIKSN.Data.LiteDB
+public interface ILiteDbRepository<TDocument, TIdentity> :
+    IRepository<TDocument>,
+    IQueryRepository<TDocument, TIdentity>,
+    IStreamRepository<TDocument> where TDocument : IEntity<TIdentity> where TIdentity : IEquatable<TIdentity>
 {
-    public interface ILiteDbRepository<TDocument, TIdentity> :
-        IRepository<TDocument>,
-        IQueryRepository<TDocument, TIdentity>,
-        IStreamRepository<TDocument> where TDocument : IEntity<TIdentity> where TIdentity : IEquatable<TIdentity>
-    {
-        Task AddOrUpdateAsync(TDocument entity, CancellationToken cancellationToken);
+    Task AddOrUpdateAsync(TDocument entity, CancellationToken cancellationToken);
 
-        Task AddOrUpdateRangeAsync(IEnumerable<TDocument> entities, CancellationToken cancellationToken);
-    }
+    Task AddOrUpdateRangeAsync(IEnumerable<TDocument> entities, CancellationToken cancellationToken);
 }
