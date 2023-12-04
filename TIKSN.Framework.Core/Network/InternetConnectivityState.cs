8d8715a7de956238561dd482047d56bac358e913
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:38:26 +0100

    Update InternetConnectivityState.cs

--- TIKSN.Framework.Core/Network/InternetConnectivityState.cs
+++ TIKSN.Framework.Core/Network/InternetConnectivityState.cs
@@ -1,45 +1,41 @@
-using System;
-using System.Collections.Generic;
+namespace TIKSN.Network;
 
-namespace TIKSN.Network
+public class InternetConnectivityState : IEqualityComparer<InternetConnectivityState>,
+    IEquatable<InternetConnectivityState>
 {
-    public class InternetConnectivityState : IEqualityComparer<InternetConnectivityState>,
-        IEquatable<InternetConnectivityState>
+    public InternetConnectivityState(bool isInternetAvailable, bool isWiFiAvailable,
+        bool isCellularNetworkAvailable)
     {
-        public InternetConnectivityState(bool isInternetAvailable, bool isWiFiAvailable,
-            bool isCellularNetworkAvailable)
-        {
-            this.IsInternetAvailable = isInternetAvailable;
-            this.IsWiFiAvailable = isWiFiAvailable;
-            this.IsCellularNetworkAvailable = isCellularNetworkAvailable;
-        }
-
-        public bool IsCellularNetworkAvailable { get; }
-        public bool IsInternetAvailable { get; }
-
-        public bool IsWiFiAvailable { get; }
-
-        public bool Equals(InternetConnectivityState x, InternetConnectivityState y) =>
-            x is null ? y is null : x.Equals(y);
-
-        public int GetHashCode(InternetConnectivityState obj) => this.IsInternetAvailable.GetHashCode() ^
-                                                                 this.IsWiFiAvailable.GetHashCode() ^
-                                                                 this.IsCellularNetworkAvailable.GetHashCode();
-
-        public bool Equals(InternetConnectivityState other)
-        {
-            if (other is null)
-            {
-                return false;
-            }
-
-            return this.IsInternetAvailable == other.IsInternetAvailable &&
-                   this.IsWiFiAvailable == other.IsWiFiAvailable &&
-                   this.IsCellularNetworkAvailable == other.IsCellularNetworkAvailable;
-        }
-
-        public override bool Equals(object obj) => this.Equals(obj as InternetConnectivityState);
-
-        public override int GetHashCode() => throw new NotImplementedException();
+        this.IsInternetAvailable = isInternetAvailable;
+        this.IsWiFiAvailable = isWiFiAvailable;
+        this.IsCellularNetworkAvailable = isCellularNetworkAvailable;
     }
+
+    public bool IsCellularNetworkAvailable { get; }
+    public bool IsInternetAvailable { get; }
+
+    public bool IsWiFiAvailable { get; }
+
+    public bool Equals(InternetConnectivityState x, InternetConnectivityState y) =>
+        x is null ? y is null : x.Equals(y);
+
+    public int GetHashCode(InternetConnectivityState obj) => this.IsInternetAvailable.GetHashCode() ^
+                                                             this.IsWiFiAvailable.GetHashCode() ^
+                                                             this.IsCellularNetworkAvailable.GetHashCode();
+
+    public bool Equals(InternetConnectivityState other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return this.IsInternetAvailable == other.IsInternetAvailable &&
+               this.IsWiFiAvailable == other.IsWiFiAvailable &&
+               this.IsCellularNetworkAvailable == other.IsCellularNetworkAvailable;
+    }
+
+    public override bool Equals(object obj) => this.Equals(obj as InternetConnectivityState);
+
+    public override int GetHashCode() => throw new NotImplementedException();
 }
