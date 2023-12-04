46a406bbf103206bbbc9886ee1787dea2e1e69cb
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:36:19 +0100

    Update ForeignExchangeRepositoryAdapter.cs

--- TIKSN.Framework.Core/Finance/ForeignExchange/Data/Mongo/ForeignExchangeRepositoryAdapter.cs
+++ TIKSN.Framework.Core/Finance/ForeignExchange/Data/Mongo/ForeignExchangeRepositoryAdapter.cs
@@ -1,22 +1,21 @@
 using TIKSN.Data.Mongo;
 using TIKSN.Mapping;
 
-namespace TIKSN.Finance.ForeignExchange.Data.Mongo
-{
-    public class ForeignExchangeRepositoryAdapter
-        : MongoRepositoryAdapter<ForeignExchangeEntity, Guid, ForeignExchangeDataEntity, Guid>
-        , IForeignExchangeRepository
-    {
-        protected readonly IForeignExchangeDataRepository dataRepository;
+namespace TIKSN.Finance.ForeignExchange.Data.Mongo;
 
-        public ForeignExchangeRepositoryAdapter(
-            IForeignExchangeDataRepository dataRepository,
-            IMapper<ForeignExchangeEntity, ForeignExchangeDataEntity> domainEntityToDataEntityMapper,
-            IMapper<ForeignExchangeDataEntity, ForeignExchangeEntity> dataEntityToDomainEntityMapper) : base(
-                domainEntityToDataEntityMapper,
-                dataEntityToDomainEntityMapper,
-                IdentityMapper<Guid>.Instance,
-                IdentityMapper<Guid>.Instance,
-                dataRepository) => this.dataRepository = dataRepository ?? throw new ArgumentNullException(nameof(dataRepository));
-    }
+public class ForeignExchangeRepositoryAdapter
+    : MongoRepositoryAdapter<ForeignExchangeEntity, Guid, ForeignExchangeDataEntity, Guid>
+    , IForeignExchangeRepository
+{
+    protected readonly IForeignExchangeDataRepository dataRepository;
+
+    public ForeignExchangeRepositoryAdapter(
+        IForeignExchangeDataRepository dataRepository,
+        IMapper<ForeignExchangeEntity, ForeignExchangeDataEntity> domainEntityToDataEntityMapper,
+        IMapper<ForeignExchangeDataEntity, ForeignExchangeEntity> dataEntityToDomainEntityMapper) : base(
+            domainEntityToDataEntityMapper,
+            dataEntityToDomainEntityMapper,
+            IdentityMapper<Guid>.Instance,
+            IdentityMapper<Guid>.Instance,
+            dataRepository) => this.dataRepository = dataRepository ?? throw new ArgumentNullException(nameof(dataRepository));
 }
