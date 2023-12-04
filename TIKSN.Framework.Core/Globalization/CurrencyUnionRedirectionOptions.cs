64abeee074694bc5ae4e0e7be264f5868aa1ec94
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:37:30 +0100

    Update CurrencyUnionRedirectionOptions.cs

--- TIKSN.Framework.Core/Globalization/CurrencyUnionRedirectionOptions.cs
+++ TIKSN.Framework.Core/Globalization/CurrencyUnionRedirectionOptions.cs
@@ -1,21 +1,18 @@
-using System.Collections.Generic;
+namespace TIKSN.Globalization;
 
-namespace TIKSN.Globalization
+public class CurrencyUnionRedirectionOptions
 {
-    public class CurrencyUnionRedirectionOptions
-    {
-        public CurrencyUnionRedirectionOptions() =>
-            this.CurrencyUnionRedirections = new Dictionary<string, string>
+    public CurrencyUnionRedirectionOptions() =>
+        this.CurrencyUnionRedirections = new Dictionary<string, string>
 (StringComparer.Ordinal)
-            {
-                {"GGP", "en-GB" /*"en-GG"*/},
-                {"JEP", "en-GB" /*"en-JE"*/},
-                {"IMP", "en-GB" /*"en-IM"*/},
-                {"FKP", "en-GB"},
-                {"GIP", "en-GB"},
-                {"SHP", "en-GB"},
-            };
+        {
+            {"GGP", "en-GB" /*"en-GG"*/},
+            {"JEP", "en-GB" /*"en-JE"*/},
+            {"IMP", "en-GB" /*"en-IM"*/},
+            {"FKP", "en-GB"},
+            {"GIP", "en-GB"},
+            {"SHP", "en-GB"},
+        };
 
-        public Dictionary<string, string> CurrencyUnionRedirections { get; set; }
-    }
+    public Dictionary<string, string> CurrencyUnionRedirections { get; set; }
 }
