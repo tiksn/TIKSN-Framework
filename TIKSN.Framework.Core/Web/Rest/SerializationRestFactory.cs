c393c3fb86d6a94735959e867a023e255138c4c2
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:42:53 +0100

    Update SerializationRestFactory.cs

--- TIKSN.Framework.Core/Web/Rest/SerializationRestFactory.cs
+++ TIKSN.Framework.Core/Web/Rest/SerializationRestFactory.cs
@@ -1,28 +1,25 @@
-using System;
-using System.Collections.Generic;
 using TIKSN.Serialization;
 
-namespace TIKSN.Web.Rest
+namespace TIKSN.Web.Rest;
+
+public class SerializationRestFactory : ISerializerRestFactory, IDeserializerRestFactory
 {
-    public class SerializationRestFactory : ISerializerRestFactory, IDeserializerRestFactory
-    {
-        private readonly IDictionary<string, Tuple<ISerializer<string>, IDeserializer<string>>> map;
+    private readonly IDictionary<string, Tuple<ISerializer<string>, IDeserializer<string>>> map;
 
-        public SerializationRestFactory(JsonSerializer jsonSerializer, JsonDeserializer jsonDeserializer,
-            DotNetXmlSerializer dotNetXmlSerializer, DotNetXmlDeserializer dotNetXmlDeserializer) => this.map = new Dictionary<string, Tuple<ISerializer<string>, IDeserializer<string>>>
+    public SerializationRestFactory(JsonSerializer jsonSerializer, JsonDeserializer jsonDeserializer,
+        DotNetXmlSerializer dotNetXmlSerializer, DotNetXmlDeserializer dotNetXmlDeserializer) => this.map = new Dictionary<string, Tuple<ISerializer<string>, IDeserializer<string>>>
+        {
             {
-                {
-                    "application/json",
-                    new Tuple<ISerializer<string>, IDeserializer<string>>(jsonSerializer, jsonDeserializer)
-                },
-                {
-                    "application/xml",
-                    new Tuple<ISerializer<string>, IDeserializer<string>>(dotNetXmlSerializer, dotNetXmlDeserializer)
-                }
-            };
+                "application/json",
+                new Tuple<ISerializer<string>, IDeserializer<string>>(jsonSerializer, jsonDeserializer)
+            },
+            {
+                "application/xml",
+                new Tuple<ISerializer<string>, IDeserializer<string>>(dotNetXmlSerializer, dotNetXmlDeserializer)
+            }
+        };
 
-        IDeserializer<string> IDeserializerRestFactory.Create(string mediaType) => this.map[mediaType].Item2;
+    IDeserializer<string> IDeserializerRestFactory.Create(string mediaType) => this.map[mediaType].Item2;
 
-        ISerializer<string> ISerializerRestFactory.Create(string mediaType) => this.map[mediaType].Item1;
-    }
+    ISerializer<string> ISerializerRestFactory.Create(string mediaType) => this.map[mediaType].Item1;
 }
