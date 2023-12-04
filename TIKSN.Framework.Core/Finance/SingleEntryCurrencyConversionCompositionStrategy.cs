a86a61464ee44a3d18c1d6ce2a8dfaab0b9d8ae3
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:37:22 +0100

    Update SingleEntryCurrencyConversionCompositionStrategy.cs

--- TIKSN.Framework.Core/Finance/SingleEntryCurrencyConversionCompositionStrategy.cs
+++ TIKSN.Framework.Core/Finance/SingleEntryCurrencyConversionCompositionStrategy.cs
@@ -1,33 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using TIKSN.Finance.Helpers;
 
-namespace TIKSN.Finance
+namespace TIKSN.Finance;
+
+public class SingleEntryCurrencyConversionCompositionStrategy : ICurrencyConversionCompositionStrategy
 {
-    public class SingleEntryCurrencyConversionCompositionStrategy : ICurrencyConversionCompositionStrategy
+    public async Task<Money> ConvertCurrencyAsync(Money baseMoney, IEnumerable<ICurrencyConverter> converters,
+        CurrencyInfo counterCurrency, DateTimeOffset asOn, CancellationToken cancellationToken)
     {
-        public async Task<Money> ConvertCurrencyAsync(Money baseMoney, IEnumerable<ICurrencyConverter> converters,
-            CurrencyInfo counterCurrency, DateTimeOffset asOn, CancellationToken cancellationToken)
-        {
-            var filteredConverters = await CurrencyHelper.FilterConvertersAsync(converters, baseMoney.Currency,
-                counterCurrency, asOn, cancellationToken).ConfigureAwait(false);
+        var filteredConverters = await CurrencyHelper.FilterConvertersAsync(converters, baseMoney.Currency,
+            counterCurrency, asOn, cancellationToken).ConfigureAwait(false);
 
-            var converter = filteredConverters.Single();
+        var converter = filteredConverters.Single();
 
-            return await converter.ConvertCurrencyAsync(baseMoney, counterCurrency, asOn, cancellationToken).ConfigureAwait(false);
-        }
+        return await converter.ConvertCurrencyAsync(baseMoney, counterCurrency, asOn, cancellationToken).ConfigureAwait(false);
+    }
 
-        public async Task<decimal> GetExchangeRateAsync(IEnumerable<ICurrencyConverter> converters, CurrencyPair pair,
-            DateTimeOffset asOn, CancellationToken cancellationToken)
-        {
-            var filteredConverters = await CurrencyHelper.FilterConvertersAsync(converters, pair, asOn, cancellationToken).ConfigureAwait(false);
+    public async Task<decimal> GetExchangeRateAsync(IEnumerable<ICurrencyConverter> converters, CurrencyPair pair,
+        DateTimeOffset asOn, CancellationToken cancellationToken)
+    {
+        var filteredConverters = await CurrencyHelper.FilterConvertersAsync(converters, pair, asOn, cancellationToken).ConfigureAwait(false);
 
-            var converter = filteredConverters.Single();
+        var converter = filteredConverters.Single();
 
-            return await converter.GetExchangeRateAsync(pair, asOn, cancellationToken).ConfigureAwait(false);
-        }
+        return await converter.GetExchangeRateAsync(pair, asOn, cancellationToken).ConfigureAwait(false);
     }
 }
