e28f03c5237fbceee03d4636cd487e2cabab44df
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:36:40 +0100

    Update IExchangeRateDataRepository.cs

--- TIKSN.Framework.Core/Finance/ForeignExchange/EntityFrameworkCore/IExchangeRateDataRepository.cs
+++ TIKSN.Framework.Core/Finance/ForeignExchange/EntityFrameworkCore/IExchangeRateDataRepository.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+namespace TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore;
 
-namespace TIKSN.Finance.ForeignExchange.Data.EntityFrameworkCore
+public interface IExchangeRateDataRepository
 {
-    public interface IExchangeRateDataRepository
-    {
-        Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
-            Guid foreignExchangeID,
-            string baseCurrencyCode,
-            string counterCurrencyCode,
-            DateTime dateFrom,
-            DateTime dateTo,
-            CancellationToken cancellationToken);
-    }
+    Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
+        Guid foreignExchangeID,
+        string baseCurrencyCode,
+        string counterCurrencyCode,
+        DateTime dateFrom,
+        DateTime dateTo,
+        CancellationToken cancellationToken);
 }
