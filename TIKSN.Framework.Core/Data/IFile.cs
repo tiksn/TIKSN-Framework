f4099cb95c48a6db9efd5f57c1028822f975b1e2
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:32:18 +0100

    Update IFile.cs

--- TIKSN.Framework.Core/Data/IFile.cs
+++ TIKSN.Framework.Core/Data/IFile.cs
@@ -1,18 +1,15 @@
-using System;
+namespace TIKSN.Data;
 
-namespace TIKSN.Data
+public interface IFile : IFileInfo
 {
-    public interface IFile : IFileInfo
-    {
-        byte[] Content { get; }
-    }
+    byte[] Content { get; }
+}
 
-    public interface IFile<TIdentity> : IFile, IFileInfo<TIdentity> where TIdentity : IEquatable<TIdentity>
-    {
-    }
+public interface IFile<TIdentity> : IFile, IFileInfo<TIdentity> where TIdentity : IEquatable<TIdentity>
+{
+}
 
-    public interface IFile<TIdentity, TMetadata> : IFile, IFileInfo<TIdentity, TMetadata>
-        where TIdentity : IEquatable<TIdentity>
-    {
-    }
+public interface IFile<TIdentity, TMetadata> : IFile, IFileInfo<TIdentity, TMetadata>
+    where TIdentity : IEquatable<TIdentity>
+{
 }
