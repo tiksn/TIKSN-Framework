cdffc10c567af9cfb3d00f0dc05543da28651c4a
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-04 21:35:53 +0100

    Update ExchangeRateDataRepository.cs

--- TIKSN.Framework.Core/Finance/ForeignExchange/Data/Mongo/ExchangeRateDataRepository.cs
+++ TIKSN.Framework.Core/Finance/ForeignExchange/Data/Mongo/ExchangeRateDataRepository.cs
@@ -1,51 +1,50 @@
 using MongoDB.Driver;
 using TIKSN.Data.Mongo;
 
-namespace TIKSN.Finance.ForeignExchange.Data.Mongo
+namespace TIKSN.Finance.ForeignExchange.Data.Mongo;
+
+public class ExchangeRateDataRepository : MongoRepository<ExchangeRateDataEntity, Guid>, IExchangeRateDataRepository
 {
-    public class ExchangeRateDataRepository : MongoRepository<ExchangeRateDataEntity, Guid>, IExchangeRateDataRepository
+    public ExchangeRateDataRepository(
+        IMongoClientSessionProvider mongoClientSessionProvider,
+        IMongoDatabaseProvider mongoDatabaseProvider) : base(
+            mongoClientSessionProvider,
+            mongoDatabaseProvider,
+            "ExchangeRates")
     {
-        public ExchangeRateDataRepository(
-            IMongoClientSessionProvider mongoClientSessionProvider,
-            IMongoDatabaseProvider mongoDatabaseProvider) : base(
-                mongoClientSessionProvider,
-                mongoDatabaseProvider,
-                "ExchangeRates")
-        {
-        }
+    }
 
-        public Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
-            string baseCurrencyCode,
-            string counterCurrencyCode,
-            DateTime dateFrom,
-            DateTime dateTo,
-            CancellationToken cancellationToken)
-        {
-            var filter = Builders<ExchangeRateDataEntity>.Filter.And(
-                Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.BaseCurrencyCode, baseCurrencyCode),
-                Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.CounterCurrencyCode, counterCurrencyCode),
-                Builders<ExchangeRateDataEntity>.Filter.Gte(item => item.AsOn, dateFrom),
-                Builders<ExchangeRateDataEntity>.Filter.Lte(item => item.AsOn, dateTo));
+    public Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
+        string baseCurrencyCode,
+        string counterCurrencyCode,
+        DateTime dateFrom,
+        DateTime dateTo,
+        CancellationToken cancellationToken)
+    {
+        var filter = Builders<ExchangeRateDataEntity>.Filter.And(
+            Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.BaseCurrencyCode, baseCurrencyCode),
+            Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.CounterCurrencyCode, counterCurrencyCode),
+            Builders<ExchangeRateDataEntity>.Filter.Gte(item => item.AsOn, dateFrom),
+            Builders<ExchangeRateDataEntity>.Filter.Lte(item => item.AsOn, dateTo));
 
-            return base.SearchAsync(filter, cancellationToken);
-        }
+        return base.SearchAsync(filter, cancellationToken);
+    }
 
-        public Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
-            Guid foreignExchangeID,
-            string baseCurrencyCode,
-            string counterCurrencyCode,
-            DateTime dateFrom,
-            DateTime dateTo,
-            CancellationToken cancellationToken)
-        {
-            var filter = Builders<ExchangeRateDataEntity>.Filter.And(
-                Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.ForeignExchangeID, foreignExchangeID),
-                Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.BaseCurrencyCode, baseCurrencyCode),
-                Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.CounterCurrencyCode, counterCurrencyCode),
-                Builders<ExchangeRateDataEntity>.Filter.Gte(item => item.AsOn, dateFrom),
-                Builders<ExchangeRateDataEntity>.Filter.Lte(item => item.AsOn, dateTo));
+    public Task<IReadOnlyList<ExchangeRateDataEntity>> SearchAsync(
+        Guid foreignExchangeID,
+        string baseCurrencyCode,
+        string counterCurrencyCode,
+        DateTime dateFrom,
+        DateTime dateTo,
+        CancellationToken cancellationToken)
+    {
+        var filter = Builders<ExchangeRateDataEntity>.Filter.And(
+            Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.ForeignExchangeID, foreignExchangeID),
+            Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.BaseCurrencyCode, baseCurrencyCode),
+            Builders<ExchangeRateDataEntity>.Filter.Eq(item => item.CounterCurrencyCode, counterCurrencyCode),
+            Builders<ExchangeRateDataEntity>.Filter.Gte(item => item.AsOn, dateFrom),
+            Builders<ExchangeRateDataEntity>.Filter.Lte(item => item.AsOn, dateTo));
 
-            return base.SearchAsync(filter, cancellationToken);
-        }
+        return base.SearchAsync(filter, cancellationToken);
     }
 }
