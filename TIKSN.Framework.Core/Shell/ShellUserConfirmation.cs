1d615b306c4b4e72c1ff4edded93ed383a32668e
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-08 18:33:45 +0100

    Update ShellUserConfirmation.cs

--- TIKSN.Framework.Core/Shell/ShellUserConfirmation.cs
+++ TIKSN.Framework.Core/Shell/ShellUserConfirmation.cs
@@ -4,22 +4,22 @@ namespace TIKSN.Shell;
 
 public class ShellUserConfirmation : IUserConfirmation
 {
-    private readonly IShellCommandContext _shellCommandContext;
+    private readonly IShellCommandContext shellCommandContext;
 
     public ShellUserConfirmation(IShellCommandContext shellCommandContext) =>
-        this._shellCommandContext = shellCommandContext;
+        this.shellCommandContext = shellCommandContext;
 
     public bool ShouldContinue(string query, string caption) =>
-        this._shellCommandContext.ShouldContinue(query, caption);
+        this.shellCommandContext.ShouldContinue(query, caption);
 
     public bool ShouldContinue(string query, string caption, ref bool yesToAll, ref bool noToAll) =>
-        this._shellCommandContext.ShouldContinue(query, caption, ref yesToAll, ref noToAll);
+        this.shellCommandContext.ShouldContinue(query, caption, ref yesToAll, ref noToAll);
 
-    public bool ShouldProcess(string target) => this._shellCommandContext.ShouldProcess(target);
+    public bool ShouldProcess(string target) => this.shellCommandContext.ShouldProcess(target);
 
     public bool ShouldProcess(string target, string action) =>
-        this._shellCommandContext.ShouldProcess(target, action);
+        this.shellCommandContext.ShouldProcess(target, action);
 
     public bool ShouldProcess(string verboseDescription, string verboseWarning, string caption) =>
-        this._shellCommandContext.ShouldProcess(verboseDescription, verboseWarning, caption);
+        this.shellCommandContext.ShouldProcess(verboseDescription, verboseWarning, caption);
 }
