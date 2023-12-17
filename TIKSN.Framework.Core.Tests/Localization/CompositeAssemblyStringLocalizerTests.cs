134bc580681a119ad382c46c07d300108654fec3
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-17 04:18:45 +0100

    Update CompositeAssemblyStringLocalizerTests.cs

--- TIKSN.Framework.Core.Tests/Localization/CompositeAssemblyStringLocalizerTests.cs
+++ TIKSN.Framework.Core.Tests/Localization/CompositeAssemblyStringLocalizerTests.cs
@@ -9,57 +9,56 @@ using TIKSN.DependencyInjection;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace TIKSN.Localization.Tests
+namespace TIKSN.Tests.Localization;
+
+public class CompositeAssemblyStringLocalizerTests
 {
-    public class CompositeAssemblyStringLocalizerTests
+    private readonly IServiceProvider serviceProvider;
+
+    public CompositeAssemblyStringLocalizerTests(ITestOutputHelper testOutputHelper)
     {
-        private readonly IServiceProvider _serviceProvider;
-
-        public CompositeAssemblyStringLocalizerTests(ITestOutputHelper testOutputHelper)
+        var services = new ServiceCollection();
+        _ = services.AddFrameworkCore();
+        _ = services.AddLogging(builder =>
         {
-            var services = new ServiceCollection();
-            _ = services.AddFrameworkCore();
-            _ = services.AddLogging(builder =>
-            {
-                _ = builder.AddDebug();
-                var loggger = new LoggerConfiguration()
-                    .MinimumLevel.Verbose()
-                    .WriteTo.TestOutput(testOutputHelper)
-                    .CreateLogger();
-                _ = builder.AddSerilog(loggger);
-            });
-            this._serviceProvider = services.BuildServiceProvider();
-        }
+            _ = builder.AddDebug();
+            var loggger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.TestOutput(testOutputHelper)
+                .CreateLogger();
+            _ = builder.AddSerilog(loggger);
+        });
+        this.serviceProvider = services.BuildServiceProvider();
+    }
 
-        [Fact]
-        public void KeyUniqueness()
+    [Fact]
+    public void KeyUniqueness()
+    {
+        var resourceNamesCache = new ResourceNamesCache();
+        var testStringLocalizer = new TestStringLocalizer(resourceNamesCache, this.serviceProvider.GetRequiredService<ILogger<TestStringLocalizer>>());
+        var allStrings = testStringLocalizer
+            .GetAllStrings()
+            .GroupBy(item => item.Name.ToLowerInvariant())
+            .ToArray();
+
+        var duplicates = allStrings
+            .Where(item => item.Count() > 1)
+            .ToArray();
+
+        var duplicatesCount = duplicates.Length;
+        var logger = this.serviceProvider.GetRequiredService<ILogger<CompositeAssemblyStringLocalizerTests>>();
+
+        foreach (var duplicate in duplicates)
         {
-            var resourceNamesCache = new ResourceNamesCache();
-            var testStringLocalizer = new TestStringLocalizer(resourceNamesCache, this._serviceProvider.GetRequiredService<ILogger<TestStringLocalizer>>());
-            var allStrings = testStringLocalizer
-                .GetAllStrings()
-                .GroupBy(item => item.Name.ToLowerInvariant())
-                .ToArray();
+            logger.LogInformation("Localization Key {LocalizationKey}", duplicate.Key);
 
-            var duplicates = allStrings
-                .Where(item => item.Count() > 1)
-                .ToArray();
-
-            var duplicatesCount = duplicates.Length;
-            var logger = this._serviceProvider.GetRequiredService<ILogger<CompositeAssemblyStringLocalizerTests>>();
-
-            foreach (var duplicate in duplicates)
+            foreach (var duplicateItem in duplicate)
             {
-                logger.LogInformation("Localization Key {LocalizationKey}", duplicate.Key);
-
-                foreach (var duplicateItem in duplicate)
-                {
-                    logger.LogInformation("Localization Name {LocalizationName}", duplicateItem.Name);
-                    logger.LogInformation("Localization Value {LocalizationValue}", duplicateItem.Value);
-                    logger.LogInformation("Localization SearchedLocation {LocalizationSearchedLocation}", duplicateItem.SearchedLocation);
-                }
+                logger.LogInformation("Localization Name {LocalizationName}", duplicateItem.Name);
+                logger.LogInformation("Localization Value {LocalizationValue}", duplicateItem.Value);
+                logger.LogInformation("Localization SearchedLocation {LocalizationSearchedLocation}", duplicateItem.SearchedLocation);
             }
-            _ = duplicatesCount.Should().Be(0);
         }
+        _ = duplicatesCount.Should().Be(0);
     }
 }
