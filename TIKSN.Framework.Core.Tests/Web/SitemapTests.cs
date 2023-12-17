b8d73d9ae6cf119c1d2a75f76e8848e71908bea2
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-17 03:21:07 +0100

    Update SitemapTests.cs

--- TIKSN.Framework.Core.Tests/Web/SitemapTests.cs
+++ TIKSN.Framework.Core.Tests/Web/SitemapTests.cs
@@ -1,105 +1,102 @@
+using FluentAssertions;
+using TIKSN.Web;
 using Xunit;
 
-namespace TIKSN.Web.Tests
+namespace TIKSN.Tests.Web;
+
+public class SitemapTests
 {
-    public class SitemapTests
+    [Fact]
+    public void Pages001()
     {
-        [Fact]
-        public void Pages001()
-        {
-            var p1 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now, Sitemap.Page.Frequency.Always, 0.5);
-            var p2 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now, Sitemap.Page.Frequency.Always, 0.5);
+        var p1 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now, Sitemap.Page.Frequency.Always, 0.5);
+        var p2 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now, Sitemap.Page.Frequency.Always, 0.5);
 
-            var map = new Sitemap();
+        var map = new Sitemap();
 
-            Assert.True(map.Pages.Add(p1));
-            Assert.False(map.Pages.Add(p2));
-        }
+        _ = map.Pages.Add(p1).Should().BeTrue();
+        _ = map.Pages.Add(p2).Should().BeFalse();
+    }
 
-        [Fact]
-        public void Pages002()
-        {
-            var p1 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now, Sitemap.Page.Frequency.Always, 0.5);
+    [Fact]
+    public void Pages002()
+    {
+        var p1 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now, Sitemap.Page.Frequency.Always, 0.5);
 
-            var map = new Sitemap();
+        var map = new Sitemap();
 
-            Assert.True(map.Pages.Add(p1));
-            Assert.False(map.Pages.Add(p1));
-        }
+        _ = map.Pages.Add(p1).Should().BeTrue();
+        _ = map.Pages.Add(p1).Should().BeFalse();
+    }
 
-        [Fact]
-        public void Pages003()
-        {
-            var p1 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now, Sitemap.Page.Frequency.Always, 0.5);
-            var p2 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now.AddDays(10d), Sitemap.Page.Frequency.Monthly, 0.2);
+    [Fact]
+    public void Pages003()
+    {
+        var p1 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now, Sitemap.Page.Frequency.Always, 0.5);
+        var p2 = new Sitemap.Page(new System.Uri("http://www.microsoft.com/"), System.DateTime.Now.AddDays(10d), Sitemap.Page.Frequency.Monthly, 0.2);
 
-            var map = new Sitemap();
+        var map = new Sitemap();
 
-            Assert.True(map.Pages.Add(p1));
-            Assert.False(map.Pages.Add(p2));
-        }
+        _ = map.Pages.Add(p1).Should().BeTrue();
+        _ = map.Pages.Add(p2).Should().BeFalse();
+    }
 
-        [Fact]
-        public void Write001()
-        {
-            var sbuilder = new System.Text.StringBuilder();
-            var xwriter = System.Xml.XmlWriter.Create(sbuilder);
+    [Fact]
+    public void Write001()
+    {
+        var sbuilder = new System.Text.StringBuilder();
+        var xwriter = System.Xml.XmlWriter.Create(sbuilder);
 
-            var map = new Sitemap();
+        var map = new Sitemap();
 
-            map.Write(xwriter);
+        map.Write(xwriter);
 
-            var XmlOutput = sbuilder.ToString();
+        var xmlOutput = sbuilder.ToString();
 
-            Assert.Equal(
-                "<?xml version=\"1.0\" encoding=\"utf-16\"?>" +
-                "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" />",
-                XmlOutput);
-        }
+        _ = xmlOutput.Should().Be("<?xml version=\"1.0\" encoding=\"utf-16\"?>" +
+            "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" />");
+    }
 
-        [Fact]
-        public void Write002()
-        {
-            var sbuilder = new System.Text.StringBuilder();
-            var xwriter = System.Xml.XmlWriter.Create(sbuilder);
+    [Fact]
+    public void Write002()
+    {
+        var sbuilder = new System.Text.StringBuilder();
+        var xwriter = System.Xml.XmlWriter.Create(sbuilder);
 
-            var map = new Sitemap();
+        var map = new Sitemap();
 
-            _ = map.Pages.Add(new Sitemap.Page(new System.Uri("http://microsoft.com/"), new System.DateTime(2012, 8, 3), Sitemap.Page.Frequency.Daily, 0.2));
-            _ = map.Pages.Add(new Sitemap.Page(new System.Uri("http://microsoft.com/sitemap.aspx"), new System.DateTime(2012, 8, 3), Sitemap.Page.Frequency.Daily, 0.2));
-            _ = map.Pages.Add(new Sitemap.Page(new System.Uri("http://microsoft.com/default.aspx"), new System.DateTime(2012, 4, 5), Sitemap.Page.Frequency.Monthly, 0.8));
+        _ = map.Pages.Add(new Sitemap.Page(new System.Uri("http://microsoft.com/"), new System.DateTime(2012, 8, 3), Sitemap.Page.Frequency.Daily, 0.2));
+        _ = map.Pages.Add(new Sitemap.Page(new System.Uri("http://microsoft.com/sitemap.aspx"), new System.DateTime(2012, 8, 3), Sitemap.Page.Frequency.Daily, 0.2));
+        _ = map.Pages.Add(new Sitemap.Page(new System.Uri("http://microsoft.com/default.aspx"), new System.DateTime(2012, 4, 5), Sitemap.Page.Frequency.Monthly, 0.8));
 
-            map.Write(xwriter);
+        map.Write(xwriter);
 
-            var XmlOutput = sbuilder.ToString();
+        var xmlOutput = sbuilder.ToString();
 
-            Assert.Equal(
-                "<?xml version=\"1.0\" encoding=\"utf-16\"?>" +
-                "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" +
+        _ = xmlOutput.Should().Be("<?xml version=\"1.0\" encoding=\"utf-16\"?>" +
+            "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" +
 
-                "<url>" +
-                "<loc>http://microsoft.com/</loc>" +
-                "<lastmod>2012-08-03</lastmod>" +
-                "<changefreq>daily</changefreq>" +
-                "<priority>0.2</priority>" +
-                "</url>" +
+            "<url>" +
+            "<loc>http://microsoft.com/</loc>" +
+            "<lastmod>2012-08-03</lastmod>" +
+            "<changefreq>daily</changefreq>" +
+            "<priority>0.2</priority>" +
+            "</url>" +
 
-                "<url>" +
-                "<loc>http://microsoft.com/sitemap.aspx</loc>" +
-                "<lastmod>2012-08-03</lastmod>" +
-                "<changefreq>daily</changefreq>" +
-                "<priority>0.2</priority>" +
-                "</url>" +
+            "<url>" +
+            "<loc>http://microsoft.com/sitemap.aspx</loc>" +
+            "<lastmod>2012-08-03</lastmod>" +
+            "<changefreq>daily</changefreq>" +
+            "<priority>0.2</priority>" +
+            "</url>" +
 
-                "<url>" +
-                "<loc>http://microsoft.com/default.aspx</loc>" +
-                "<lastmod>2012-04-05</lastmod>" +
-                "<changefreq>monthly</changefreq>" +
-                "<priority>0.8</priority>" +
-                "</url>" +
+            "<url>" +
+            "<loc>http://microsoft.com/default.aspx</loc>" +
+            "<lastmod>2012-04-05</lastmod>" +
+            "<changefreq>monthly</changefreq>" +
+            "<priority>0.8</priority>" +
+            "</url>" +
 
-                "</urlset>",
-                XmlOutput);
-        }
+            "</urlset>");
     }
 }
