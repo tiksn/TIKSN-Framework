4cd68d5c0911166c2f18057fdd0b3a7b1481c67c
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-19 00:14:51 +0100

    Update ICurrencyConversionCompositionStrategy.cs

--- TIKSN.Framework.Core/Finance/ICurrencyConversionCompositionStrategy.cs
+++ TIKSN.Framework.Core/Finance/ICurrencyConversionCompositionStrategy.cs
@@ -2,9 +2,16 @@ namespace TIKSN.Finance;
 
 public interface ICurrencyConversionCompositionStrategy
 {
-    Task<Money> ConvertCurrencyAsync(Money baseMoney, IEnumerable<ICurrencyConverter> converters,
-        CurrencyInfo counterCurrency, DateTimeOffset asOn, CancellationToken cancellationToken);
+    Task<Money> ConvertCurrencyAsync(
+        Money baseMoney,
+        IEnumerable<ICurrencyConverter> converters,
+        CurrencyInfo counterCurrency,
+        DateTimeOffset asOn,
+        CancellationToken cancellationToken);
 
-    Task<decimal> GetExchangeRateAsync(IEnumerable<ICurrencyConverter> converters, CurrencyPair pair,
-        DateTimeOffset asOn, CancellationToken cancellationToken);
+    Task<decimal> GetExchangeRateAsync(
+        IEnumerable<ICurrencyConverter> converters,
+        CurrencyPair pair,
+        DateTimeOffset asOn,
+        CancellationToken cancellationToken);
 }
