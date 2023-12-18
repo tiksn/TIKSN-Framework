dfb818540aa291fc1876bb4becadb125ea383b8c
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-12-19 00:21:01 +0100

    Update ICustomDeserializer.cs

--- TIKSN.Framework.Core/Serialization/ICustomDeserializer.cs
+++ TIKSN.Framework.Core/Serialization/ICustomDeserializer.cs
@@ -10,7 +10,7 @@ public interface ICustomDeserializer<in TSerial, out TModel>
     /// <summary>
     ///     Deserialize from <typeparamref name="TSerial" /> type to <typeparamref name="TModel" />
     /// </summary>
-    /// <param name="serial"></param>
-    /// <returns></returns>
+    /// <param name="serial">Serial to be deserialized into model</param>
+    /// <returns>Deserialized model</returns>
     TModel Deserialize(TSerial serial);
 }
