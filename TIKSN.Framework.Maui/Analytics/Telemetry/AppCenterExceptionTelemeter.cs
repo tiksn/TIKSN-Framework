548a2ce43afe3626802b133b7f2318fff2fca7a7
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-01-04 15:04:55 +0100

    Update AppCenterExceptionTelemeter.cs

---
+++ TIKSN.Framework.Maui/Analytics/Telemetry/AppCenterExceptionTelemeter.cs
@@ -0,0 +1,36 @@
+using Microsoft.AppCenter;
+
+namespace TIKSN.Analytics.Telemetry;
+
+public class AppCenterExceptionTelemeter : IExceptionTelemeter
+{
+    public Task TrackExceptionAsync(Exception exception) => this.TrackExceptionAsync(exception, TelemetrySeverityLevel.Information);
+
+    public Task TrackExceptionAsync(Exception exception, TelemetrySeverityLevel severityLevel)
+    {
+        switch (severityLevel)
+        {
+            case TelemetrySeverityLevel.Verbose:
+                AppCenterLog.Verbose(AppCenterLog.LogTag, exception.Message, exception);
+                break;
+
+            case TelemetrySeverityLevel.Information:
+                AppCenterLog.Info(AppCenterLog.LogTag, exception.Message, exception);
+                break;
+
+            case TelemetrySeverityLevel.Warning:
+                AppCenterLog.Warn(AppCenterLog.LogTag, exception.Message, exception);
+                break;
+
+            case TelemetrySeverityLevel.Error:
+            case TelemetrySeverityLevel.Critical:
+                AppCenterLog.Error(AppCenterLog.LogTag, exception.Message, exception);
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException(nameof(severityLevel));
+        }
+
+        return Task.CompletedTask;
+    }
+}
