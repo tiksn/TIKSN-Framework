aceef487ff00defa51fc1f5fe0fb03bba9be54a5
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-01-04 15:05:19 +0100

    Create AppCenterEventTelemeter.cs

---
+++ TIKSN.Framework.Maui/Analytics/Telemetry/AppCenterEventTelemeter.cs
@@ -0,0 +1,30 @@
+using AppCenterAnalytics = Microsoft.AppCenter.Analytics.Analytics;
+
+namespace TIKSN.Analytics.Telemetry;
+
+public class AppCenterEventTelemeter : IEventTelemeter
+{
+    public Task TrackEventAsync(string name)
+    {
+        AppCenterAnalytics.TrackEvent(name);
+        return Task.CompletedTask;
+    }
+
+    public Task TrackEventAsync(string name, IReadOnlyDictionary<string, string> properties)
+    {
+        if (properties is null)
+        {
+            AppCenterAnalytics.TrackEvent(name);
+        }
+        else if (properties is IDictionary<string, string> dictionary)
+        {
+            AppCenterAnalytics.TrackEvent(name, dictionary);
+        }
+        else
+        {
+            AppCenterAnalytics.TrackEvent(name, properties.ToDictionary());
+        }
+
+        return Task.CompletedTask;
+    }
+}
