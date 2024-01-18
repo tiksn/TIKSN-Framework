d7fc0b856b41586c2f33f24b03cb81d6aedf319e
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-01-18 16:20:42 +0100

    Update RestRepositoryOptions.cs

--- TIKSN.Framework.Core/Web/Rest/RestRepositoryOptions.cs
+++ TIKSN.Framework.Core/Web/Rest/RestRepositoryOptions.cs
@@ -2,21 +2,24 @@ using System.Text;
 
 namespace TIKSN.Web.Rest;
 
+#pragma warning disable S2326 // Unused type parameters should be removed
 public class RestRepositoryOptions<T>
+#pragma warning restore S2326 // Unused type parameters should be removed
 {
     public RestRepositoryOptions()
     {
         this.MediaType = "application/json";
         this.Encoding = Encoding.UTF8;
+        this.AcceptLanguages = new Dictionary<double, string>();
     }
 
-    public string ApiKey { get; set; }
+    public string EndpointKey { get; set; }
 
     public string MediaType { get; set; }
 
     public RestAuthenticationType Authentication { get; set; }
 
-    public Dictionary<double, string> AcceptLanguages { get; set; }
+    public IDictionary<double, string> AcceptLanguages { get; }
 
     public string ResourceTemplate { get; set; }
 
