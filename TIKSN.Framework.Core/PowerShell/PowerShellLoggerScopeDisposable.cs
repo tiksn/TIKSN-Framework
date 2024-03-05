7d1f20efddd28fc426499144b30c680249ac4a4c
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-03-05 12:43:02 +0100

    Update PowerShellLoggerScopeDisposable.cs

--- TIKSN.Framework.Core/PowerShell/PowerShellLoggerScopeDisposable.cs
+++ TIKSN.Framework.Core/PowerShell/PowerShellLoggerScopeDisposable.cs
@@ -5,8 +5,26 @@ namespace TIKSN.PowerShell;
 public class PowerShellLoggerScopeDisposable : IDisposable
 {
     private readonly ConcurrentStack<object> scopes;
+    private bool disposedValue;
 
     public PowerShellLoggerScopeDisposable(ConcurrentStack<object> scopes) => this.scopes = scopes;
 
-    public void Dispose() => _ = this.scopes.TryPop(out _);
+    public void Dispose()
+    {
+        this.Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!this.disposedValue)
+        {
+            if (disposing)
+            {
+                _ = this.scopes.TryPop(out _);
+            }
+
+            this.disposedValue = true;
+        }
+    }
 }
