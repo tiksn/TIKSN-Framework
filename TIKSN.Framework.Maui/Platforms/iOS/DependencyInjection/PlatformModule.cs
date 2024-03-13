1dfd88e9d372b59f9bb268dd3c3c7887a7c3a5e6
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-03-13 14:45:49 +0100

    Update PlatformModule.cs

--- TIKSN.Framework.Maui/Platforms/iOS/DependencyInjection/PlatformModule.cs
+++ TIKSN.Framework.Maui/Platforms/iOS/DependencyInjection/PlatformModule.cs
@@ -1,9 +1,3 @@
-using TIKSN.DependencyInjection;
+namespace TIKSN.DependencyInjection;
 
-#pragma warning disable IDE1006 // Naming Styles
-namespace TIKSN.Platforms.iOS.DependencyInjection;
-#pragma warning restore IDE1006 // Naming Styles
-
-public class PlatformModule : PlatformModuleBase
-{
-}
+public class PlatformModule : PlatformModuleBase;
