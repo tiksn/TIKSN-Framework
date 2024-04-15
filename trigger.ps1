549172e4f18ea5e15e25c888102f7c8ac0d808e9
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-04-15 02:12:54 +0200

    Create trigger.ps1

---
+++ trigger.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $Task,
+    [Parameter()]
+    [string]
+    $Instance,
+    [Parameter()]
+    [string]
+    $Version
+)
+
+if ([System.String]::IsNullOrWhiteSpace($Instance)) {
+    $Instance = Get-Date -Format 'yyyyMMddHHmmss'
+    Invoke-Build -Task $Task -Instance $Instance -Version $Version
+}
+else {
+    $trashFolder = Join-Path -Path . -ChildPath '.trash'
+    $trashFolder = Join-Path -Path $trashFolder -ChildPath $date
+    if (-not (Test-Path -Path $trashFolder)) {
+        New-Item -Path $trashFolder -ItemType Directory | Out-Null
+    }
+    $checkpointFile = Join-Path -Path $trashFolder -ChildPath "$Instance.clixml"
+    if (Test-Path -Path $checkpointFile) {
+        $checkpointData = Import-Clixml -Path $checkpointFile
+        $checkpointData.Task = @($Task)
+        $checkpointData | Export-Clixml -Path $checkpointFile
+    }
+    Build-Checkpoint -Checkpoint $checkpointFile -Build @{Task = $Task; Instance = $Instance; Version = $Version } -Preserve -Auto
+}
