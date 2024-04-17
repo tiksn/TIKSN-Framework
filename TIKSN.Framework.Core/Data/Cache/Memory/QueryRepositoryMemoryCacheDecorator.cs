22bf737db52a27ff39a755866eea74b2d507d49d
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-04-17 23:40:53 +0200

    Update QueryRepositoryMemoryCacheDecorator.cs

--- TIKSN.Framework.Core/Data/Cache/Memory/QueryRepositoryMemoryCacheDecorator.cs
+++ TIKSN.Framework.Core/Data/Cache/Memory/QueryRepositoryMemoryCacheDecorator.cs
@@ -48,9 +48,10 @@ public class QueryRepositoryMemoryCacheDecorator<TEntity, TIdentity>
             () => this.QueryRepository.GetOrDefaultAsync(id, cancellationToken));
     }
 
-    public async Task<IEnumerable<TEntity>>
-        ListAsync(IEnumerable<TIdentity> ids, CancellationToken cancellationToken) =>
-        await BatchOperationHelper.BatchOperationAsync(ids, this.GetAsync, cancellationToken).ConfigureAwait(false);
+    public async Task<IReadOnlyList<TEntity>> ListAsync(
+        IEnumerable<TIdentity> ids,
+        CancellationToken cancellationToken)
+        => await BatchOperationHelper.BatchOperationAsync(ids, this.GetAsync, cancellationToken).ConfigureAwait(false);
 
     public async Task<PageResult<TEntity>> PageAsync(
         PageQuery pageQuery,
