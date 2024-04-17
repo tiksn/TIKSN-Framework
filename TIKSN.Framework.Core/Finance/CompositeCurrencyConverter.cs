0b68c5880b7b5373853052d983406f588ed8db41
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-04-17 23:58:09 +0200

    Update CompositeCurrencyConverter.cs

--- TIKSN.Framework.Core/Finance/CompositeCurrencyConverter.cs
+++ TIKSN.Framework.Core/Finance/CompositeCurrencyConverter.cs
@@ -10,16 +10,23 @@ public abstract class CompositeCurrencyConverter : ICompositeCurrencyConverter
     }
 
     protected ICurrencyConversionCompositionStrategy CompositionStrategy { get; }
+
     protected IList<ICurrencyConverter> Converters { get; }
 
     public void Add(ICurrencyConverter converter) => this.Converters.Add(converter);
 
-    public abstract Task<Money> ConvertCurrencyAsync(Money baseMoney, CurrencyInfo counterCurrency,
-        DateTimeOffset asOn, CancellationToken cancellationToken);
-
-    public abstract Task<IEnumerable<CurrencyPair>> GetCurrencyPairsAsync(DateTimeOffset asOn,
+    public abstract Task<Money> ConvertCurrencyAsync(
+        Money baseMoney,
+        CurrencyInfo counterCurrency,
+        DateTimeOffset asOn,
         CancellationToken cancellationToken);
 
-    public abstract Task<decimal> GetExchangeRateAsync(CurrencyPair pair, DateTimeOffset asOn,
+    public abstract Task<IReadOnlyCollection<CurrencyPair>> GetCurrencyPairsAsync(
+        DateTimeOffset asOn,
+        CancellationToken cancellationToken);
+
+    public abstract Task<decimal> GetExchangeRateAsync(
+        CurrencyPair pair,
+        DateTimeOffset asOn,
         CancellationToken cancellationToken);
 }
