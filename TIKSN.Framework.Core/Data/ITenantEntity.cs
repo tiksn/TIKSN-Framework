d49be357f5c8bf2f6cc885c3ca429b8a6b2b3538
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-12 01:44:27 +0200

    Update ITenantEntity.cs

--- TIKSN.Framework.Core/Data/ITenantEntity.cs
+++ TIKSN.Framework.Core/Data/ITenantEntity.cs
@@ -1,9 +1,9 @@
 namespace TIKSN.Data;
 
 public interface ITenantEntity<TEntityIdentity, out TTenantIdentity>
-  : IEntity<TEntityIdentity>
-  where TEntityIdentity : IEquatable<TEntityIdentity>
-  where TTenantIdentity : IEquatable<TTenantIdentity>
+    : IEntity<TEntityIdentity>
+    where TEntityIdentity : IEquatable<TEntityIdentity>
+    where TTenantIdentity : IEquatable<TTenantIdentity>
 {
     TTenantIdentity TenantID { get; }
 }
