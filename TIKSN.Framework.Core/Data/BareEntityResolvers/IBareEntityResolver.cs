804990b9163ed51a8f6856f0707c6a9ebfd66787
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-12 01:44:12 +0200

    Update IBareEntityResolver.cs

--- TIKSN.Framework.Core/Data/BareEntityResolvers/IBareEntityResolver.cs
+++ TIKSN.Framework.Core/Data/BareEntityResolvers/IBareEntityResolver.cs
@@ -3,16 +3,16 @@ namespace TIKSN.Data.BareEntityResolvers;
 #pragma warning disable S2326 // Unused type parameters should be removed
 public interface IBareEntityResolver<TEntity, TBareEntity, in TIdentity>
 #pragma warning restore S2326 // Unused type parameters should be removed
-  : IBareEntityResolver<TBareEntity, TIdentity>
-  where TEntity : IEntity<TIdentity>
-  where TBareEntity : IEntity<TIdentity>
-  where TIdentity : IEquatable<TIdentity>;
+    : IBareEntityResolver<TBareEntity, TIdentity>
+    where TEntity : IEntity<TIdentity>
+    where TBareEntity : IEntity<TIdentity>
+    where TIdentity : IEquatable<TIdentity>;
 
 public interface IBareEntityResolver<TBareEntity, in TIdentity>
-  where TBareEntity : IEntity<TIdentity>
-  where TIdentity : IEquatable<TIdentity>
+    where TBareEntity : IEntity<TIdentity>
+    where TIdentity : IEquatable<TIdentity>
 {
     Task<TBareEntity> ResolveAsync(
-      TIdentity id,
-      CancellationToken cancellationToken);
+        TIdentity id,
+        CancellationToken cancellationToken);
 }
