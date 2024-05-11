6c8d1fe0ac1c46145a492bec2bdc0e29fcf618f1
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-12 01:44:20 +0200

    Update UserBareEntityResolver.cs

--- TIKSN.Framework.Core/Data/BareEntityResolvers/UserBareEntityResolver.cs
+++ TIKSN.Framework.Core/Data/BareEntityResolvers/UserBareEntityResolver.cs
@@ -1,20 +1,20 @@
 namespace TIKSN.Data.BareEntityResolvers;
 
 public class UserBareEntityResolver<TEntity, TEntityIdentity, TUserIdentity>
-  : IBareEntityResolver<TEntity, UserEntity<TEntityIdentity, TUserIdentity>, TEntityIdentity>
-  where TEntity : IUserEntity<TEntityIdentity, TUserIdentity>
-  where TEntityIdentity : IEquatable<TEntityIdentity>
-  where TUserIdentity : IEquatable<TUserIdentity>
+    : IBareEntityResolver<TEntity, UserEntity<TEntityIdentity, TUserIdentity>, TEntityIdentity>
+    where TEntity : IUserEntity<TEntityIdentity, TUserIdentity>
+    where TEntityIdentity : IEquatable<TEntityIdentity>
+    where TUserIdentity : IEquatable<TUserIdentity>
 {
     private readonly IQueryRepository<TEntity, TEntityIdentity> queryRepository;
 
     public UserBareEntityResolver(
-      IQueryRepository<TEntity, TEntityIdentity> queryRepository)
-        => this.queryRepository = queryRepository ?? throw new ArgumentNullException(nameof(queryRepository));
+        IQueryRepository<TEntity, TEntityIdentity> queryRepository)
+            => this.queryRepository = queryRepository ?? throw new ArgumentNullException(nameof(queryRepository));
 
     public async Task<UserEntity<TEntityIdentity, TUserIdentity>> ResolveAsync(
-      TEntityIdentity id,
-      CancellationToken cancellationToken)
+        TEntityIdentity id,
+        CancellationToken cancellationToken)
     {
         var entity = await this.queryRepository.GetAsync(id, cancellationToken).ConfigureAwait(false);
 
