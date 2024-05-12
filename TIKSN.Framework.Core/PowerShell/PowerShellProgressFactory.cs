88054677c3605fcd41ca8a5e33e6957f651d0902
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-12 02:20:37 +0200

    Update PowerShellProgressFactory.cs

--- TIKSN.Framework.Core/PowerShell/PowerShellProgressFactory.cs
+++ TIKSN.Framework.Core/PowerShell/PowerShellProgressFactory.cs
@@ -7,8 +7,8 @@ public class PowerShellProgressFactory : IOperationProgressFactory
     private readonly ICurrentCommandProvider currentCommandProvider;
 
     public PowerShellProgressFactory(ICurrentCommandProvider currentCommandProvider) =>
-        this.currentCommandProvider = currentCommandProvider ??
-                                       throw new ArgumentNullException(nameof(currentCommandProvider));
+        this.currentCommandProvider = currentCommandProvider
+        ?? throw new ArgumentNullException(nameof(currentCommandProvider));
 
     public IDisposableProgress<OperationProgressReport> Create(string activity, string statusDescription) =>
         new PowerShellProgress(this.currentCommandProvider, activity, statusDescription);
