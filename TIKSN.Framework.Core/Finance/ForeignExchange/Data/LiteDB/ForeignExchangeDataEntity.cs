6863efb4dae253518b0750f8c9d41bfefa480b7d
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-17 00:25:36 +0200

    Update ForeignExchangeDataEntity.cs

--- TIKSN.Framework.Core/Finance/ForeignExchange/Data/LiteDB/ForeignExchangeDataEntity.cs
+++ TIKSN.Framework.Core/Finance/ForeignExchange/Data/LiteDB/ForeignExchangeDataEntity.cs
@@ -5,7 +5,7 @@ namespace TIKSN.Finance.ForeignExchange.Data.LiteDB;
 
 public class ForeignExchangeDataEntity : IEntity<Guid>
 {
-    public string CountryCode { get; set; }
+    public string? CountryCode { get; set; }
 
     [BsonId]
     public Guid ID { get; set; }
