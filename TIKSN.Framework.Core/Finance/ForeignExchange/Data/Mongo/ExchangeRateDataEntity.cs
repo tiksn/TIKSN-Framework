370daca2213552d41bdb851b87310ab6207eeec6
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-16 23:41:47 +0200

    Update ExchangeRateDataEntity.cs

--- TIKSN.Framework.Core/Finance/ForeignExchange/Data/Mongo/ExchangeRateDataEntity.cs
+++ TIKSN.Framework.Core/Finance/ForeignExchange/Data/Mongo/ExchangeRateDataEntity.cs
@@ -7,9 +7,9 @@ public class ExchangeRateDataEntity : IEntity<Guid>
 {
     public DateTime AsOn { get; set; }
 
-    public string BaseCurrencyCode { get; set; }
+    public string? BaseCurrencyCode { get; set; }
 
-    public string CounterCurrencyCode { get; set; }
+    public string? CounterCurrencyCode { get; set; }
 
     public Guid? ForeignExchangeID { get; set; }
 
