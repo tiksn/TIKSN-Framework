369ba7c5b6b0205ee1688c20795e97c1fe2a0bba
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-10 16:46:29 -0500

    Update lint.ps1

--- lint.ps1
+++ lint.ps1
@@ -4,6 +4,8 @@ docker run --rm `
     -e ENABLE_GITHUB_ACTIONS_GROUP_TITLE=true `
     -e FIX_MODE_ENABLED=true `
     -e FIX_CSHARP=true `
+    -e FIX_CSS_PRETTIER=true `
+    -e FIX_CSS=true `
     -e FIX_DOTNET_SLN_FORMAT_ANALYZERS=true `
     -e FIX_DOTNET_SLN_FORMAT_STYLE=true `
     -e FIX_DOTNET_SLN_FORMAT_WHITESPACE=true `
@@ -14,10 +16,16 @@ docker run --rm `
     -e FIX_JSON_PRETTIER=true `
     -e FIX_MARKDOWN=true `
     -e FIX_MARKDOWN_PRETTIER=true `
+    -e FIX_POWERSHELL=true `
     -e FIX_NATURAL_LANGUAGE=true `
     -e FIX_SHELL_SHFMT=true `
+    -e FIX_TSX=true `
+    -e FIX_TYPESCRIPT_ES=true `
+    -e FIX_TYPESCRIPT_PRETTIER=true `
+    -e FIX_TYPESCRIPT_STANDARD=true `
     -e FIX_YAML_PRETTIER=true `
     -e LOG_LEVEL=DEBUG `
+    -e ACTIONS_RUNNER_DEBUG=true `
     -e RUN_LOCAL=true `
     -e VALIDATE_JSCPD=false `
     -e VALIDATE_ALL_CODEBASE=true `
