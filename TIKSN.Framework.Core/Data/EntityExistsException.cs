be1dd99ffca1d9c8c95f417556dccc368e64878c
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-11-19 00:17:26 -0600

    Create EntityExistsException.cs

---
+++ TIKSN.Framework.Core/Data/EntityExistsException.cs
@@ -0,0 +1,8 @@
+﻿namespace TIKSN.Data;
+
+public class EntityExistsException : Exception
+{
+    public EntityExistsException() { }
+    public EntityExistsException(string message) : base(message) { }
+    public EntityExistsException(string message, Exception inner) : base(message, inner) { }
+}
