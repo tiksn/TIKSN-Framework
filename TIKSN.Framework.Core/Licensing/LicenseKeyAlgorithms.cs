2412c30548473efd11ce19e3ff0eb64586c17bf9
Tigran TIKSN Torosyan <personal@tiksn.com>
2026-08-05 12:11:42 -0500

    Fix SonarAnalyzer S1313 false positive on Ed25519 OID

--- TIKSN.Framework.Core/Licensing/LicenseKeyAlgorithms.cs
+++ TIKSN.Framework.Core/Licensing/LicenseKeyAlgorithms.cs
@@ -3,6 +3,8 @@ namespace TIKSN.Licensing;
 internal static class LicenseKeyAlgorithms
 {
     internal const string Dsa = "1.2.840.10040.4.1";
+#pragma warning disable S1313 // Using hardcoded IP addresses is security-sensitive
     internal const string Ed25519 = "1.3.101.112";
+#pragma warning restore S1313 // Using hardcoded IP addresses is security-sensitive
     internal const string Rsa = "1.2.840.113549.1.1.1";
 }
